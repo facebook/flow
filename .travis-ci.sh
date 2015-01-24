9d68be0eac29f57bff8e906266e98afcdde4f1e7
Gabe Levi <glevi@fb.com>
2015-01-24 02:01:46 -0800

    Minimum ocaml version is 4.01.0

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -6,9 +6,6 @@ case "$OCAML_VERSION,$OPAM_VERSION" in
 4.02.1,1.1.0) ppa=avsm/ocaml42+opam11 ;;
 4.01.0,1.2.0) ppa=avsm/ocaml41+opam12 ;;
 4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
-4.00.1,1.2.0) ppa=avsm/ocaml40+opam12 ;;
-4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
-4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 
