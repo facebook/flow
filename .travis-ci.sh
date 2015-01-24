c13baf51699cc6cb441325f5a6b71162ae51e64a
Gabe Levi <glevi@fb.com>
2015-01-24 01:53:54 -0800

    Some small fixes

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,8 +1,9 @@
 OPAM_DEPENDS=
          
+# TODO Test mac build
 case "$OCAML_VERSION,$OPAM_VERSION" in
-4.02.0,1.2.0) ppa=avsm/ocaml42+opam12 ;;
-4.02.0,1.1.0) ppa=avsm/ocaml42+opam11 ;;
+4.02.1,1.2.0) ppa=avsm/ocaml42+opam12 ;;
+4.02.1,1.1.0) ppa=avsm/ocaml42+opam11 ;;
 4.01.0,1.2.0) ppa=avsm/ocaml41+opam12 ;;
 4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
 4.00.1,1.2.0) ppa=avsm/ocaml40+opam12 ;;
@@ -16,8 +17,8 @@ sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
 opam init 
-opam install ${OPAM_DEPENDS}
+# TODO: Install js_of_ocaml and test the parser
+# opam install ${OPAM_DEPENDS}
 eval `opam config env`
-cd src
 make
 make test
