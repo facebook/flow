49716f7208734c3cf0b2eb6667e65913d1f03aad
Sam Goldman <samwgoldman@gmail.com>
2015-02-27 16:51:59 -1000

    Add unchecked oneOf prop type to react test

--- tests/react/jsx_spread.js
+++ tests/react/jsx_spread.js
@@ -3,9 +3,10 @@
 var React = require('react');
 var Foo = React.createClass({
   propTypes: {
+    foo: React.PropTypes.oneOf(["foo"]),
     bar: React.PropTypes.string.isRequired,
   },
 });
 
-var props = {bar: 42};
+var props = {foo: "bar", bar: 42};
 var blah = <Foo {...props} />; // error bar, number given string expected
