049a77ff15a2d85cc7fa4899eb0c68d2f9065837
Sam Goldman <samwgoldman@gmail.com>
2015-03-05 22:41:04 -1000

    Add type checking to React.PropTypes.object
    This proptype works exactly like the "Object" type annotation, accepting
    any object.

--- src/typing/type_inference_js.ml
+++ src/typing/type_inference_js.ml
@@ -3042,7 +3042,7 @@ and mk_proptype cx = Ast.Expression.(function
         (_, {Ast.Identifier.name = "object"; _ });
       _
     } ->
-      AnyT.at vloc (* TODO *)
+      AnyObjT (mk_reason "object" vloc)
 
   | vloc, Member { Member.
       property = Member.PropertyIdentifier
