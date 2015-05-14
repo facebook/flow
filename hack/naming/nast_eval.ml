6060dd70afa52787b490bedfdc1b049632f72b3c
Jez Ng <jezng@fb.com>
2015-05-14 15:41:46 -0700

    Have Nast_eval.static_string return a Result type
    Summary: Throwing exceptions is error-prone because OCaml can't
    exhaustively verify that you've handled all of them. Indeed, I did miss
    a spot, and this resulted in lint crashing on some files. This diff
    fixes it and makes the code more type-safe.
    
    Reviewed By: @elgenie
    
    Differential Revision: D2070540

--- hack/naming/nast_eval.ml
+++ hack/naming/nast_eval.ml
@@ -10,10 +10,15 @@
 
 open Nast
 
-exception Not_static of Pos.t
-(* type errors are already handled by naming / typing code, so the caller can
- * usually ignore them *)
-exception Type_error
+type eval_error =
+  | Not_static of Pos.t
+  | Type_error (* type errors are already handled by naming / typing code,
+                * so the caller can usually ignore them *)
+
+(* these should never be exposed / thrown outside of this module; translate
+ * them into eval_error types first *)
+exception Not_static_exn of Pos.t
+exception Type_error_exn
 
 let unescape_string unescaper s =
   let buf = Buffer.create (String.length s) in
@@ -57,17 +62,17 @@ let double_quote_unescaper s i buf =
       Buffer.add_char buf '\\';
       Buffer.add_char buf c
 
-let rec static_string ~allow_consts cclass = function
+let rec static_string_exn ~allow_consts cclass = function
   | _, Binop (Ast.Dot, s1, s2) ->
-      let p1, s1 = static_string ~allow_consts cclass s1 in
-      let _p2, s2 = static_string ~allow_consts cclass s2 in
+      let p1, s1 = static_string_exn ~allow_consts cclass s1 in
+      let _p2, s2 = static_string_exn ~allow_consts cclass s2 in
       p1, s1 ^ s2
   | _, String (p, s) -> p, unescape_string single_quote_unescaper s
   (* This matches double-quoted strings w/o interpolated variables *)
   | p, String2 ([], s) -> p, unescape_string double_quote_unescaper s
   | p, Class_const (CIself, (_, s)) when allow_consts -> begin
       match cclass with
-      | None -> raise Type_error
+      | None -> raise Type_error_exn
       | Some class_ ->
           (* TODO: return a list of positions so we can pinpoint lint errors
            * to the exact constant in which they occurred *)
@@ -75,20 +80,25 @@ let rec static_string ~allow_consts cclass = function
     end
   | p, Class_const (CI (_, id), (_, s)) when allow_consts -> begin
       match Naming_heap.ClassHeap.get id with
-      | None -> raise Type_error
+      | None -> raise Type_error_exn
       | Some class_ -> p, snd (get_const s cclass class_)
     end
-  | p, _ -> raise (Not_static p)
+  | p, _ -> raise (Not_static_exn p)
 
 and get_const s cclass class_ =
     let (_, (p, _), cst_expr_opt) =
       try
         List.find (fun (_, (_, id), _) -> id = s) class_.c_consts
       with Not_found ->
-        raise Type_error in
+        raise Type_error_exn in
     match cst_expr_opt with
-    | Some cst_expr -> static_string ~allow_consts:true cclass cst_expr
-    | None -> raise (Not_static p)
+    | Some cst_expr -> static_string_exn ~allow_consts:true cclass cst_expr
+    | None -> raise (Not_static_exn p)
+
+let static_string ~allow_consts cclass expr =
+  try Result.Ok (static_string_exn ~allow_consts cclass expr) with
+  | Type_error_exn -> Result.Error Type_error
+  | Not_static_exn p -> Result.Error (Not_static p)
 
 (* e.g. user attributes do not support consts *)
 let static_string_no_consts = static_string ~allow_consts:false None
