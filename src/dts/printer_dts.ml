494346cee065fdf5e558e6745990ef236a7c767c
Kunal Singhal <soapmkr@fb.com>
2015-06-01 10:57:10 -0700

    Convert class and get scope
    Summary: Now converting classes in dts files and computing a list of all the modules in global scope. The list of modules would be used for siblings modules to be able to access each other's exported properties.
    
    Reviewed By: @avikchaudhuri
    
    Differential Revision: D2113707

--- src/dts/printer_dts.ml
+++ src/dts/printer_dts.ml
@@ -45,12 +45,59 @@ let opt f fmt = function
   | Some x -> f fmt x
 
 (* eventually make this assert false *)
+
+
+let fail_on_todo = true
+
+
 let todo fmt =
-  fprintf fmt "@[%s@]"
-    "#######"
+    assert (not fail_on_todo);
+    fprintf fmt "@[%s@]"
+      "#######"
+
+
+(* get_modules_in_scope computes the list of modules which are
+   declared int he cuurent scope. In particular we shall use it to
+   find a list of modules in global scope.
+
+   declare module M {
+       export class A extends B implements C { }
+   }
+
+   declare module N {
+       var x : M.A
+   }
+
+   For the above d.ts file, the function will return the following
+   list -> "M" :: "N" :: []
+*)
+
+let rec get_modules_in_scope = function
+  | [] -> []
+  | x :: xs ->
+    extract_module (get_modules_in_scope xs) x
+
+and extract_module tail = Statement.(function
+  | _, ModuleDeclaration { Module.id; _; } ->
+    get_names tail id
+  | _ -> tail
+)
+
+and get_names tail = function
+  | _ , { IdPath.ids; _} ->
+    append_names tail ids
+
+and append_names tail = function
+  | [] -> tail
+  | x :: xs -> (id_name x) :: tail
+
+and id_name = function
+  | _, { Identifier.name; _ } -> name
 
 let rec program fmt (_, stmts, _) =
   Format.set_margin 80;
+  let scope = get_modules_in_scope stmts in
+  let () = printf "%d\n" (List.length scope) in
   fprintf fmt "@[<v>@,%a@]@."
     (list ~sep:";" statement) stmts
 
@@ -62,7 +109,7 @@ and statement fmt = Statement.(function
       | [(_, { Declarator.id; _ })] ->
           fprintf fmt "@[<hv>declare var %a@]"
             pattern id
-      | _ -> assert false
+      | _ -> todo fmt
     )
 
   | _, InterfaceDeclaration { Interface.
@@ -71,7 +118,7 @@ and statement fmt = Statement.(function
       fprintf fmt "@[<v>declare class %a%a%a %a@]"
         id_ id
         (opt (non_empty "<@[<h>%a@]>" (list ~sep:"," type_param))) typeParameters
-        extends_ extends
+        extends_interface extends
         object_type (snd body)
 
   | _, ModuleDeclaration { Module.id; body; } ->
@@ -87,6 +134,16 @@ and statement fmt = Statement.(function
   | _, ExportAssignment id ->
       fprintf fmt "@[<h>declare var exports: typeof %a@]"
         id_ id
+
+  | _, AmbientClassDeclaration { AmbientClass.
+      id; body; typeParameters; extends; implements;
+    } ->
+      fprintf fmt "@[<v>declare class %a%a%a%a %a@]"
+        id_ id
+        (opt (non_empty "<@[<h>%a@]>" (list ~sep:"," type_param))) typeParameters
+        extends_class extends
+        implements_class implements
+        object_type (snd body)
 
   | _ ->
       todo fmt
@@ -131,7 +188,7 @@ and type_ fmt = Type.(function
   | _, Object t -> object_type fmt t
   | _, Array t -> array_type fmt t
   | _, Generic t -> generic_type fmt t
-  | _ -> assert false
+  | _ -> todo fmt
 )
 
 and type_param fmt = Type.(function
@@ -139,12 +196,56 @@ and type_param fmt = Type.(function
       id_ fmt id
 )
 
-and extends_ fmt = function
+(* A class can only extend one class unlike interfaces which can
+   extend multiple interfaces. Thus, the same function does not work
+   for classes.
+
+   declare module M {
+       export class A extends B { }
+       export class D { }
+   }
+
+   In the above .d.ts program we see that class A have "extends"
+   property where as class D does not. Thus "extends" is
+   an optional property. This is handled in the extends_class_ function.
+
+*)
+and extends_class fmt = function
+  | None -> ()
+  | Some (_, t) ->
+      fprintf fmt "@[ extends %a@]"
+        generic_type t
+
+(* This helper function is for handling extend statement in an
+   interface declration. Since an interface can extend more than one
+   interfaces, we have a list of interfaces which are extended by the
+   current interface.
+*)
+and extends_interface fmt = function
   | [] -> ()
   | [_, t] ->
       fprintf fmt "@[ extends %a@]"
         generic_type t
-  | _ -> assert false
+  | _ -> todo fmt
+
+(* Implements_class is a helper function to print "implements"
+   property in a class. A class can have more than one or zero
+   interface so we have a list of interfaces which are implemented by
+   he current class.
+
+   Eg. :
+
+   declare module M {
+       export class A extends B implements C { }
+   }
+
+*)
+and implements_class fmt = function
+  | [] -> ()
+  | [_, t] ->
+      fprintf fmt "@[ implements %a@]"
+        generic_type t
+  | _ -> todo fmt
 
 and object_type fmt = Type.(function
   | { Object.properties; indexers; } ->
@@ -164,7 +265,7 @@ and property fmt = Type.Object.(function
           fprintf fmt "@[<hv>%a: %a@]"
             id_ id
             type_ value
-      | _ -> assert false
+      | _ -> todo fmt
     )
 )
 
