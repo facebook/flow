c98ab1140f4b1dc5962e2adb7849a31dbc3faeed
Jez Ng <jezng@fb.com>
2015-06-04 16:52:38 -0700

    [hack] Fix up hh_json
    Summary: * Make sure strings get quoted
    * Less creative formatting
    
    When people don't address your comments...
    
    tableflip
    
    Reviewed By: @jwatzman
    
    Differential Revision: D2128483

--- hack/utils/hh_json.ml
+++ hack/utils/hh_json.ml
@@ -11,11 +11,12 @@
 
 (**
  * Json pretty printing library.
- *
  *)
 
+open Core
+
 type json =
-    JList of json list
+  | JList of json list
   | JBool of bool
   | JString of string
   | JAssoc of (string * json) list
@@ -24,61 +25,42 @@ type json =
   | JFloat of float
 
 (* buf_concat :
-   Designed as a substitute for String.concat that passes a buffer
-   into which intermediate strings are added, and also includes left
-   and right bracket (lb and rb) in addition to sep. They are strings,
-   despite common case of (), [],{}, or even <>, to handle missing brackets,
-   brackets with spacing and multichar brackets like OCaml's arrays ([| and |]).
-   The A conc_elt function parameter performs the operation of transforming
-   the list element to a string and adding it to the buffer, the simplest
-   example would be fun x -> Buffer.add_string (to_string x)
+ * Designed as a substitute for String.concat that passes a buffer
+ * into which intermediate strings are added, and also includes left
+ * and right bracket (lb and rb) in addition to sep. They are strings,
+ * despite common case of (), [],{}, or even <>, to handle missing brackets,
+ * brackets with spacing and multichar brackets like OCaml's arrays ([| and |]).
+ * The A conc_elt function parameter performs the operation of transforming
+ * the list element to a string and adding it to the buffer, the simplest
+ * example would be fun x -> Buffer.add_string (to_string x)
  *)
-
 let buf_concat ~buf ~lb ~rb ~sep ~concat_elt l =
   Buffer.add_string buf lb;
   (match l with
    | [] -> ()
-   | elt::elts ->
-      concat_elt buf elt;
-      List.iter
-        (fun e -> Buffer.add_string buf sep; concat_elt buf e)
-        elts);
+   | elt :: elts ->
+       concat_elt buf elt;
+       List.iter elts
+         (fun e -> Buffer.add_string buf sep; concat_elt buf e));
   Buffer.add_string buf rb
-
-(* quote_string s:
-   A replacement for fun s -> Printf.sprintf "%S" s
-*)
-let quote_string s =
-  let len = String.length s in
-  let r = String.create (len + 2) in
-  String.set r 0 '\"';
-  String.set r (len+1) '\"';
-  String.unsafe_blit s 0 r 1 len;
-  r
 
 let add_char buf c = Buffer.add_char buf c
 let add_string buf s = Buffer.add_string buf s
 
 let rec add_json_to_buffer (buf:Buffer.t) (json:json): unit =
   match json with
-  | JList l -> buf_concat
-                 ~buf
-                 ~lb:"[" ~rb:"]" ~sep:","
-                 ~concat_elt:add_json_to_buffer
-       l
-  | JAssoc l -> buf_concat
-                  ~buf
-                  ~lb:"{" ~rb:"}" ~sep:","
-                  ~concat_elt:add_assoc_to_buffer
-                  l
+  | JList l ->
+      buf_concat ~buf ~lb:"[" ~rb:"]" ~sep:"," ~concat_elt:add_json_to_buffer l
+  | JAssoc l ->
+      buf_concat ~buf ~lb:"{" ~rb:"}" ~sep:"," ~concat_elt:add_assoc_to_buffer l
   | JBool b -> if b then add_string buf "true" else add_string buf "false"
-  | JString s -> add_string buf (quote_string s)
+  | JString s -> add_string buf (Printf.sprintf "%S" s)
   | JNull -> add_string buf "null"
   | JInt i -> add_string buf (string_of_int i)
   | JFloat f -> add_string buf (string_of_float f)
 
 and add_assoc_to_buffer (buf:Buffer.t) (k,v) =
-  add_string buf (quote_string k);
+  add_string buf (Printf.sprintf "%S" k);
   add_char buf ':';
   add_json_to_buffer buf v
 
@@ -93,14 +75,14 @@ let json_to_multiline json =
     if String.length single < 80 then single else
     match json with
     | JList l ->
-        let nl = List.map (loop (indent ^ "  ")) l in
+        let nl = List.map l (loop (indent ^ "  ")) in
         "[\n" ^ indent ^ "  " ^ (String.concat (",\n" ^ indent ^ "  ") nl) ^
           "\n" ^ indent ^ "]"
     | JAssoc l ->
-        let nl = List.map (fun (k, v) ->
+        let nl = List.map l begin fun (k, v) ->
           indent ^ "  " ^ (json_to_string (JString k)) ^ ":" ^
             (loop (indent ^ "  ") v)
-        ) l in
+        end in
         "{\n" ^ (String.concat ",\n" nl) ^ "\n" ^ indent ^ "}"
     | _ -> single
   in
