362d4488b7884819f52835444d8c282a8933402a
Gabe Levi <gabe@fb.com>
2015-07-13 14:28:28 -0400

    Update travis config to use containers
    This is pretty easy, just need to do apt-install-y things using the config
    rather than manually
    
    Fixes #626

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -9,12 +9,9 @@ setup_linux () {
   *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
   esac
 
-  echo "yes" | sudo add-apt-repository ppa:$ppa
-  sudo apt-get update -qq
-  sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam libelf-dev
   export OPAMYES=1
   # We could in theory tell opam to init with a particular version of ocaml,
-  # but this is much slower than using the ppa
+  # but this is much slower than using the ppa & apt
   opam init -a -y
   # TODO: Install js_of_ocaml and test the parser
   # opam install ${OPAM_DEPENDS}
