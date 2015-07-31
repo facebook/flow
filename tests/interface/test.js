3c7ec98f0873a2a78d897afa42a77f49bd9a0467
Tim Popham <popham@uw.edu>
2015-07-31 16:27:17 -0700

    Include broken error message commentary

--- tests/interface/test.js
+++ tests/interface/test.js
@@ -60,12 +60,12 @@ class D extends C {
   }
 }
 
-type EI<T> = class {
+type EI<T> = class EI {
   eMethod1(): T;
   static esMethod1(): Class<T>;
 }
 
-class E {
+class E { // implements EI<B>
   eMethod1() {
     return new B();
   }
@@ -81,7 +81,7 @@ class E {
 }
 
 class F {
-  eMethod1() {
+  eMethod1() { // implements EI<D>, EI<C>, EI<B>
     return new D();
   }
   eMethod2() {
@@ -115,7 +115,7 @@ function ab2(M: Class<AI>&Class<BI>): string {
   return M.asMethod1() + M.bsMethod1();
 }
 
-function e1(N: Class<EI<B>>): string { // Shifted TypeApp
+function e1(N: Class<EI<B>>): string {
   var Bp = N.esMethod1();
   return Bp.bsMethod1() + Bp.bsMethod2();
 }
@@ -145,7 +145,140 @@ var Z3 = e.constructor.esMethod1();
 var Z4 = e.constructor.esNonmethod(b,d); // NG: Type checks despite bad name.
 var Z5 = E.esMethod1();
 var Z6 = F.esMethod1();
+
 var z7 = e1(E);
+/*
+ * If I put this assignment prior to `var f...`, then the `var f` error makes no
+ * mention of `z7`'s site:
+test.js:29:7,7: B
+This type is incompatible with
+test.js:54:7,7: D
+
+test.js:118:25,25: B
+This type is incompatible with
+test.js:54:7,7: D
+Error path:
+:                   MixedT [Object]
+test.js:54:7,7:     ~> ExtendsT [extends D] comes from
+test.js:118:25,25:  . InstanceT [B]
+test.js:54:7,7:     . ~> ExtendsT [extends D] comes from
+test.js:118:25,25:  . . InstanceT [B]
+test.js:54:7,7:     . . ~> InstanceT [D] comes from
+test.js:118:25,25:  . . . OpenT [B]
+test.js:54:7,7:     . . . ~> InstanceT [D] comes from
+test.js:118:25,25:  . . . . AnnotT [B]
+test.js:54:7,7:     . . . . ~> InstanceT [D] comes from
+test.js:118:25,25:  . . . . . AnnotT [B]
+test.js:143:11,11:  . . . . . ~> OpenT [D] comes from
+test.js:118:25,25:  . . . . . . AnnotT [B]
+test.js:143:11,11:  . . . . . . ~> AnnotT [D] comes from
+test.js:64:3,15:    . . . . . . . FunT [function type]
+test.js:69:3,71:3:  . . . . . . . ~> FunT [method eMethod1] comes from
+test.js:142:13,13:  . . . . . . . . InstanceT [E]
+test.js:64:3,15:    . . . . . . . . ~> LookupT [property eMethod1] comes from
+test.js:142:13,13:  . . . . . . . . . InstanceT [E]
+test.js:63:14,66:1: . . . . . . . . . ~> ClsT [EI] comes from
+test.js:142:13,13:  . . . . . . . . . . InstanceT [E]
+test.js:142:13,13:  . . . . . . . . . . ~> OpenT [E] comes from
+test.js:142:13,13:  . . . . . . . . . . . InstanceT [E]
+test.js:142:13,13:  . . . . . . . . . . . ~> AnnotT [E] comes from
+test.js:142:13,13:  . . . . . . . . . . . . ShiftT [class type: E]
+test.js:118:22,26:  . . . . . . . . . . . . ~> ShiftT [shifted type: type application of polymorphic type: type EI] comes from
+test.js:142:10,11:  . . . . . . . . . . . . . FunT [function]
+test.js:142:10,14:  . . . . . . . . . . . . . ~> CallT [function call]
+test.js:69:3,71:3:  . . . . . . . FunT [method eMethod1]
+test.js:64:3,15:    . . . . . . . ~> FunT [function type] comes from
+test.js:68:7,7:     . . . . . . . . InstanceT [E]
+test.js:64:3,15:    . . . . . . . . ~> LookupT [property eMethod1] comes from
+test.js:68:7,7:     . . . . . . . . . InstanceT [E]
+test.js:63:14,66:1: . . . . . . . . . ~> ClsT [EI] comes from
+test.js:68:7,7:     . . . . . . . . . . InstanceT [E]
+test.js:68:7,7:     . . . . . . . . . . ~> OpenT [E] comes from
+test.js:68:7,7:     . . . . . . . . . . . InstanceT [E]
+test.js:68:7,7:     . . . . . . . . . . . ~> AnnotT [E] comes from
+test.js:68:7,7:     . . . . . . . . . . . . InstanceT [E]
+test.js:143:8,12:   . . . . . . . . . . . . ~> TypeAppT [type application of type EI] comes from
+test.js:143:16,22:  . . . . . . . . . . . . . VoidT [undefined]
+test.js:68:7,7:     . . . . . . . . . . . . . ~> ObjTestT [constructor return] comes from
+test.js:68:7,7:     . . . . . . . . . . . . . . FunT [default constructor]
+test.js:143:16,22:  . . . . . . . . . . . . . . ~> CallT [constructor call] comes from
+test.js:68:7,7:     . . . . . . . . . . . . . . . InstanceT [E]
+test.js:143:16,22:  . . . . . . . . . . . . . . . ~> MethodT [constructor call] comes from
+test.js:68:7,7:     . . . . . . . . . . . . . . . . ShiftT [class type: E]
+test.js:143:16,22:  . . . . . . . . . . . . . . . . ~> ConstructorT [constructor call]
+test.js:68:7,7:     . . . . . . . . . . . . . . ShiftT [class type: E]
+test.js:143:16,22:  . . . . . . . . . . . . . . ~> ConstructorT [constructor call]
+ *
+ * However, if I leave this assignment here, then the `var f` error attaches the
+ * second message to the `z7` function call site:
+ *
+test.js:29:7,7: B
+This type is incompatible with
+test.js:54:7,7: D
+
+test.js:150:10,14: function call
+Error:
+test.js:118:25,25: B
+This type is incompatible with
+test.js:54:7,7: D
+Error path:
+:                   MixedT [Object]
+test.js:54:7,7:     ~> ExtendsT [extends D] comes from
+test.js:118:25,25:  . InstanceT [B]
+test.js:54:7,7:     . ~> ExtendsT [extends D] comes from
+test.js:118:25,25:  . . InstanceT [B]
+test.js:54:7,7:     . . ~> InstanceT [D] comes from
+test.js:118:25,25:  . . . OpenT [B]
+test.js:54:7,7:     . . . ~> InstanceT [D] comes from
+test.js:118:25,25:  . . . . AnnotT [B]
+test.js:54:7,7:     . . . . ~> InstanceT [D] comes from
+test.js:118:25,25:  . . . . . AnnotT [B]
+test.js:142:11,11:  . . . . . ~> OpenT [D] comes from
+test.js:118:25,25:  . . . . . . AnnotT [B]
+test.js:142:11,11:  . . . . . . ~> AnnotT [D] comes from
+test.js:64:3,15:    . . . . . . . FunT [function type]
+test.js:69:3,71:3:  . . . . . . . ~> FunT [method eMethod1] comes from
+test.js:150:13,13:  . . . . . . . . InstanceT [E]
+test.js:64:3,15:    . . . . . . . . ~> LookupT [property eMethod1] comes from
+test.js:150:13,13:  . . . . . . . . . InstanceT [E]
+test.js:63:14,66:1: . . . . . . . . . ~> ClsT [EI] comes from
+test.js:150:13,13:  . . . . . . . . . . InstanceT [E]
+test.js:150:13,13:  . . . . . . . . . . ~> OpenT [E] comes from
+test.js:150:13,13:  . . . . . . . . . . . InstanceT [E]
+test.js:150:13,13:  . . . . . . . . . . . ~> AnnotT [E] comes from
+test.js:150:13,13:  . . . . . . . . . . . . ShiftT [class type: E]
+test.js:118:22,26:  . . . . . . . . . . . . ~> ShiftT [shifted type: type application of polymorphic type: type EI] comes from
+test.js:150:10,11:  . . . . . . . . . . . . . FunT [function]
+test.js:150:10,14:  . . . . . . . . . . . . . ~> CallT [function call]
+test.js:69:3,71:3:  . . . . . . . FunT [method eMethod1]
+test.js:64:3,15:    . . . . . . . ~> FunT [function type] comes from
+test.js:142:20,20:  . . . . . . . . InstanceT [E]
+test.js:64:3,15:    . . . . . . . . ~> LookupT [property eMethod1] comes from
+test.js:142:20,20:  . . . . . . . . . InstanceT [E]
+test.js:63:14,66:1: . . . . . . . . . ~> ClsT [EI] comes from
+test.js:142:20,20:  . . . . . . . . . . InstanceT [E]
+test.js:142:20,20:  . . . . . . . . . . ~> OpenT [E] comes from
+test.js:142:20,20:  . . . . . . . . . . . InstanceT [E]
+test.js:142:20,20:  . . . . . . . . . . . ~> AnnotT [E] comes from
+test.js:142:20,20:  . . . . . . . . . . . . InstanceT [E]
+test.js:142:8,12:   . . . . . . . . . . . . ~> TypeAppT [type application of type EI] comes from
+test.js:142:16,22:  . . . . . . . . . . . . . VoidT [undefined]
+test.js:142:20,20:  . . . . . . . . . . . . . ~> ObjTestT [constructor return] comes from
+test.js:68:7,7:     . . . . . . . . . . . . . . FunT [default constructor]
+test.js:142:16,22:  . . . . . . . . . . . . . . ~> CallT [constructor call] comes from
+test.js:142:20,20:  . . . . . . . . . . . . . . . InstanceT [E]
+test.js:142:16,22:  . . . . . . . . . . . . . . . ~> MethodT [constructor call] comes from
+test.js:142:20,20:  . . . . . . . . . . . . . . . . ShiftT [class type: E]
+test.js:142:16,22:  . . . . . . . . . . . . . . . . ~> ConstructorT [constructor call]
+test.js:142:20,20:  . . . . . . . . . . . . . . ShiftT [class type: E]
+test.js:142:16,22:  . . . . . . . . . . . . . . ~> ConstructorT [constructor call]
+ *
+ * These errors are verbatim identical except for the first two lines.  This
+ * looks like a bug in the error handling.  If the EI<E> was cached at the
+ * signature of `e1` and then reused, then this might arise?  Save it for later
+ * when prettying up the error messages.
+ */
+
 var z8 = e1(F);
 var z9 = e2(e);
 /*
