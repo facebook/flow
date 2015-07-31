cbd80c5a8729bc822cd67817ba49daa493259f08
Tim Popham <popham@uw.edu>
2015-07-31 11:14:17 -0700

    More tests

--- tests/interface/test.js
+++ tests/interface/test.js
@@ -60,6 +60,41 @@ class D extends C {
   }
 }
 
+type EI<T> = class {
+  eMethod1(): T;
+  static esMethod1(): Class<T>;
+}
+
+class E {
+  eMethod1() {
+    return new B();
+  }
+  eMethod2() {
+    return 5;
+  }
+  static esMethod1() {
+    return B;
+  }
+  static esMethod2() {
+    return "e static";
+  }
+}
+
+class F {
+  eMethod1() {
+    return new D();
+  }
+  eMethod2() {
+    return "f method";
+  }
+  static esMethod1() {
+    return D;
+  }
+  static esMethod2() {
+    return 1;
+  }
+}
+
 function b1(ell: BI): number {
   return ell.bMethod1();
 }
@@ -77,17 +112,43 @@ function ab1(m: AI&BI): number {
 }
 
 function ab2(M: Class<AI>&Class<BI>): string {
-  /*
-   * Operator `Interface<.>` would be handy for stripping an interface from a
-   * class and for shifting interfaces (Class<SomeInterface> errors).
-   */
   return M.asMethod1() + M.bsMethod1();
 }
 
-var b = new B();
+function e1(N: Class<EI<B>>): string { // Shifted TypeApp
+  var Bp = N.esMethod1();
+  return Bp.bsMethod1() + Bp.bsMethod2();
+}
+
+function e2(n: EI<B>): string {
+  var b = n.eMethod1();
+  return b.bMethod1() + b.bMethod2();
+}
+
+type GI<T> = class Id<U> { // class<U> currently rejected by parser, right?
+  gMethod1(): U;
+  static gsMethod1(): U;
+};
+
+var b: BI = new B();
 var x1 = b1(b);
 var x2 = b2(C);
 var x3 = b3(C);
 var d = new D();
 var y1 = ab1(d);
-//var y2 = ab2(D);
+var y2 = ab2(D);
+var e: EI<B> = new F();
+//var f: EI<D> = new E(); // OK error: E returns B's, which may not have all of the methods of D's
+var z1 = e.eMethod1();
+var z2 = e.eMethod1(b,d); // Too many arguments => okay by Flow.
+var Z3 = e.constructor.esMethod1();
+var Z4 = e.constructor.esNonmethod(b,d); // NG: Type checks despite bad name.
+var Z5 = E.esMethod1();
+var Z6 = F.esMethod1();
+var z7 = e1(E);
+var z8 = e1(F);
+var z9 = e2(e);
+/*
+ * Operator `Interface<.>` would be handy for stripping an interface from a
+ * class and for shifting interfaces (Class<SomeInterface> errors).
+ */
