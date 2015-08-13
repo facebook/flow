ded38652341cf9ee42fc79ab83e18682ad09a9ec
Michael Sullivan <msullivan@fb.com>
2015-08-12 18:18:50 -0700

    [hack] Fix cmake build breakage caused by using Bytes module
    Summary: Apparently the version of ocaml we use for our build machines for the cmake build doesn't have the Bytes module. I landed a use of Bytes in a patch earlier to fix a different build break resulting from a lack of the Str regexp module in js_of_ocaml. Fix this by going back to the regexp version with different implementations for ocaml and js land.
    
    Reviewed By: @int3, @JoelMarcey
    
    Differential Revision: D2340274

--- hack/parsing/parser_hack.ml
+++ hack/parsing/parser_hack.ml
@@ -3606,7 +3606,7 @@ and xhp_text env start abs_start =
 
     let content = String.sub env.lb.Lexing.lex_buffer abs_start len in
     (* need to squash whitespace down to a single space *)
-    let squished = Utils.squash_whitespace content in
+    let squished = Regexp_utils.squash_whitespace content in
     (* if it is empty or all whitespace just ignore it *)
     if squished = "" || squished = " " then [] else
       [pos, String (pos, squished)]
---
+++ hack/utils/regexp_utils.ml
@@ -0,0 +1,19 @@
+(**
+ * Copyright (c) 2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the "hack" directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ *)
+
+(* Utility functions implemented with regular expressions.
+ * Needs to be duplicated between regular ocaml and JS since the
+ * JS regexp library differs. *)
+
+let nonempty_ws_regexp = Str.regexp "[ \n\t\r\012]+"
+(* Squash the whitespace in a string down the way that xhp expects it.
+ * In particular, replace all whitespace with spaces and replace all
+ * strings of multiple spaces with a single space. *)
+let squash_whitespace s = Str.global_replace nonempty_ws_regexp " " s
