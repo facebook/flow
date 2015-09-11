a53fbf8f98db6c6a852a66652144aee7960c5781
Avik Chaudhuri <avik@fb.com>
2015-09-11 13:55:41 -0700

    [hack] dynamic worklist
    Summary: Some multi-worker applications might involve a work queue that dynamically grows
    and shrinks. Simple bucketing (by cutting off chunks from a static queue)
    doesn't work in this case. This diff adds support for a more general bucketing
    protocol that signals workers to wait in addition to providing work.
    
    Reviewed By: @bhosmer
    
    Differential Revision: D2433376

--- hack/procs/multiWorker.ml
+++ hack/procs/multiWorker.ml
@@ -10,7 +10,7 @@
 
 open Core
 
-type 'a nextlist = 
+type 'a nextlist =
   unit -> 'a list
 
 let single_threaded_call job merge neutral next =
@@ -40,7 +40,7 @@ let call workers ~job ~merge ~neutral ~next =
             let bucket = next () in
             if bucket = [] then raise Exit;
             incr busy;
-            ignore (Worker.call proc 
+            ignore (Worker.call proc
                       begin fun xl ->
                         job neutral xl
                       end
@@ -54,6 +54,92 @@ let call workers ~job ~merge ~neutral ~next =
             acc := merge res !acc;
           end;
           procs := ready_procs;
+        done;
+        assert false
+      with Exit ->
+        while !busy > 0 do
+          List.iter (Worker.select workers) begin fun proc ->
+            let res = Worker.get_result proc (Obj.magic 0) in
+            decr busy;
+            acc := merge res !acc;
+          end;
+        done;
+        !acc
+
+type 'a nextlist_dynamic =
+  unit -> 'a list option
+
+(* Mostly a duplicate of the function `single_threaded_call` above, except that
+   this function runs job and then merge separately, whereas the other function
+   only runs job assuming that merge satisfies the equation:
+
+   job acc list = merge acc (job neutral list)
+
+   Running merge is especially important for dynamically changing workloads
+   because it provides perhaps the only hook to trigger such changes!
+*)
+let single_threaded_call_dynamic job merge neutral next =
+  let x = ref (next()) in
+  let acc = ref neutral in
+  (* This is a just a sanity check that the job is serializable and so
+   * that the same code will work both in single threaded and parallel
+   * mode.
+   *)
+  let _ = Marshal.to_string job [Marshal.Closures] in
+  while !x != Some [] do
+    match !x with
+    | None ->
+        (* this state should never be reached in single threaded mode, since
+           there is no hope for ever getting out of this state *)
+        failwith "stuck!"
+    | Some l ->
+      let res = job neutral l in
+        acc := merge !acc res;
+        x := next()
+  done;
+  !acc
+
+(* Adaptation of the function `call` above to handle dynamic workloads. *)
+let call_dynamic workers ~job ~merge ~neutral ~next =
+  match workers with
+  | None -> single_threaded_call_dynamic job merge neutral next
+  | Some workers ->
+    let acc = ref neutral in
+    let procs = ref workers in
+    let busy = ref 0 in
+    let waiting_procs = ref [] in
+      try
+        while true do
+          List.iter !procs begin fun proc ->
+            let bucket_opt = next () in
+            match bucket_opt with
+            | None -> waiting_procs := proc::!waiting_procs (* wait *)
+            | Some bucket -> (
+              if bucket = [] then raise Exit;
+              incr busy;
+              ignore (Worker.call proc
+                        begin fun xl ->
+                          job neutral xl
+                        end
+                        bucket
+              );
+            )
+          end;
+          let ready_procs = Worker.select workers in
+          List.iter ready_procs begin fun proc ->
+            let res = Worker.get_result proc (Obj.magic 0) in
+            decr busy;
+            acc := merge res !acc
+          end;
+          if ready_procs = [] then
+            (* nothing changed, so no use calling next with the waiting procs,
+               since they would end up waiting again *)
+            procs := []
+          else (
+            (* call next with read_procs and waiting_procs *)
+            procs := List.rev_append !waiting_procs ready_procs;
+            waiting_procs := [];
+          )
         done;
         assert false
       with Exit ->
--- hack/procs/multiWorker.mli
+++ hack/procs/multiWorker.mli
@@ -11,7 +11,7 @@
 (* The protocol for a next function is to return a list of elements.
  * It will be called repeatedly until it returns an empty list.
  *)
-type 'a nextlist = 
+type 'a nextlist =
   unit -> 'a list
 
 val call :
@@ -19,4 +19,18 @@ val call :
   job:('b -> 'a list -> 'b) ->
   merge:('b -> 'b -> 'b) -> neutral:'b ->
   next:'a nextlist ->
+  'b
+
+(* Variant of the above for dynamic workloads. The protocol for a next function
+   is to return either None (indicating that workers should wait until more
+   elements are added to the workload) or Some list of elements. It will be
+   called repeatedly until it returns Some empty list.  *)
+type 'a nextlist_dynamic =
+  unit -> 'a list option
+
+val call_dynamic :
+  Worker.t list option ->
+  job:('b -> 'a list -> 'b) ->
+  merge:('b -> 'b -> 'b) -> neutral:'b ->
+  next:'a nextlist_dynamic ->
   'b
