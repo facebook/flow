5b87eddd24d5d5acdd1c9292194e73a639b9bb31
Avik Chaudhuri <avik@fb.com>
2015-09-15 18:00:18 -0700

    [hack] remove code duplication in multiworker
    Summary: make MultiWorker.call a special case of MultiWorker.call_dynamic
    
    Reviewed By: @int3
    
    Differential Revision: D2445785

--- hack/procs/multiWorker.ml
+++ hack/procs/multiWorker.ml
@@ -10,62 +10,6 @@
 
 open Core
 
-type 'a nextlist =
-  unit -> 'a list
-
-let single_threaded_call job merge neutral next =
-  let x = ref (next()) in
-  let acc = ref neutral in
-  (* This is a just a sanity check that the job is serializable and so
-   * that the same code will work both in single threaded and parallel
-   * mode.
-   *)
-  let _ = Marshal.to_string job [Marshal.Closures] in
-  while !x != [] do
-    acc := job !acc !x;
-    x := next()
-  done;
-  !acc
-
-let call workers ~job ~merge ~neutral ~next =
-  match workers with
-  | None -> single_threaded_call job merge neutral next
-  | Some workers ->
-      let acc = ref neutral in
-      let procs = ref workers in
-      let busy = ref 0 in
-      try
-        while true do
-          List.iter !procs begin fun proc ->
-            let bucket = next () in
-            if bucket = [] then raise Exit;
-            incr busy;
-            ignore (Worker.call proc
-                      begin fun xl ->
-                        job neutral xl
-                      end
-                      bucket
-                   );
-          end;
-          let ready_procs = Worker.select workers in
-          List.iter ready_procs begin fun proc ->
-            let res = Worker.get_result proc (Obj.magic 0) in
-            decr busy;
-            acc := merge res !acc;
-          end;
-          procs := ready_procs;
-        done;
-        assert false
-      with Exit ->
-        while !busy > 0 do
-          List.iter (Worker.select workers) begin fun proc ->
-            let res = Worker.get_result proc (Obj.magic 0) in
-            decr busy;
-            acc := merge res !acc;
-          end;
-        done;
-        !acc
-
 type 'a bucket =
 | Job of 'a list
 | Wait
@@ -73,15 +17,6 @@ type 'a bucket =
 type 'a nextlist_dynamic =
   unit -> 'a bucket
 
-(* Mostly a duplicate of the function `single_threaded_call` above, except that
-   this function runs job and then merge separately, whereas the other function
-   only runs job assuming that merge satisfies the equation:
-
-   job acc list = merge acc (job neutral list)
-
-   Running merge is especially important for dynamically changing workloads
-   because it provides perhaps the only hook to trigger such changes!
-*)
 let single_threaded_call_dynamic job merge neutral next =
   let x = ref (next()) in
   let acc = ref neutral in
@@ -103,7 +38,6 @@ let single_threaded_call_dynamic job merge neutral next =
   done;
   !acc
 
-(* Adaptation of the function `call` above to handle dynamic workloads. *)
 let call_dynamic workers ~job ~merge ~neutral ~next =
   match workers with
   | None -> single_threaded_call_dynamic job merge neutral next
@@ -155,3 +89,11 @@ let call_dynamic workers ~job ~merge ~neutral ~next =
           end;
         done;
         !acc
+
+(* special case of call_dynamic with no waiting, useful for static worklists *)
+type 'a nextlist =
+  unit -> 'a list
+
+let call workers ~job ~merge ~neutral ~next =
+  let next = fun () -> Job (next ()) in
+  call_dynamic workers ~job ~merge ~neutral ~next
