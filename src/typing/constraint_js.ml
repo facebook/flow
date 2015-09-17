0ba82612daf3a06fcddc09518fae6ccaea5556a0
Marshall Roch <mroch@fb.com>
2015-09-17 11:38:44 -0700

    [easy] add printer for ShapeT
    Summary: a `$Shape<T>` annotation no longer makes `flow dump-types` throw.
    
    Reviewed By: @avikchaudhuri
    
    Differential Revision: D2453361

--- src/typing/constraint_js.ml
+++ src/typing/constraint_js.ml
@@ -1650,6 +1650,7 @@ let rec type_printer override fallback enclosure cx t =
 
     | AnnotT (_, t) -> pp EnclosureNone cx t
     | KeysT (_, t) -> spf "$Keys<%s>" (pp EnclosureNone cx t)
+    | ShapeT t -> spf "$Shape<%s>" (pp EnclosureNone cx t)
 
     (* The following types are not syntax-supported *)
     | ClassT t ->
