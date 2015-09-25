56af90bc1674690a09b82f387e7a5fa94a59ff8c
Tim Popham <popham@uw.edu>
2015-09-24 19:57:11 -0700

    Static methods can't be ctors

--- src/typing/type_inference_js.ml
+++ src/typing/type_inference_js.ml
@@ -5361,6 +5361,7 @@ and mk_class_elements cx instance_info static_info body = Ast.Class.(
         let ret = Flow_js.subst cx map_ ret in
         (* determine if we are in a derived constructor *)
         let derived_ctor = match super with
+          | ClassT (MixedT _) -> false
           | MixedT _ -> false
           | _ -> name = "constructor"
         in
