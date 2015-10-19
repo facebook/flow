7758123e33c6277d82a5609a2e201207483e462a
Alexander Chow <achow@fb.com>
2015-10-19 15:22:29 -0700

    [hack] Fix error in libancillary gnu-variable-sized-type-not-at-end
    Summary: clang doesn't support variable-sized structs in the non-terminal position
    
    Reviewed By: int3
    
    Differential Revision: D2556710
    
    fb-gh-sync-id: 6a8cb1ff9bdfb97923544eb321081ab18dffa3cd

--- hack/third-party/libancillary/ancillary.h
+++ hack/third-party/libancillary/ancillary.h
@@ -7,7 +7,7 @@
 /*
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *  1. Redistributions of source code must retain the above copyright notice,
  *     this list of conditions and the following disclaimer.
  *  2. Redistributions in binary form must reproduce the above copyright
@@ -15,7 +15,7 @@
  *     documentation and/or other materials provided with the distribution.
  *  3. The name of the author may not be used to endorse or promote products
  *     derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
@@ -68,8 +68,8 @@ ancil_recv_fds_with_buffer(int, int *, unsigned, void *);
 
 #define ANCIL_FD_BUFFER(n) \
     struct { \
-	struct cmsghdr h; \
-	int fd[n]; \
+  int fd[n]; \
+  struct cmsghdr h; \
     }
 /* ANCIL_FD_BUFFER(n)
  *
