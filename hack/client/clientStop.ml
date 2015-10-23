d3f6f17e07cff4afef9b48eb0b21f3557ff3cdce
Jez Ng <jezng@fb.com>
2015-10-23 15:47:09 -0700

    [hack] Make hh_client restart more reliable / helpful
    Summary: I imagine it's really annoying to be stuck with a broken hh_server (and we've
    been breaking it a lot lately), being told to restart it, then having the
    suggested command fail with no explanation. Improvements:
    
    * Wait a little longer after sending the kill signal before concluding that
      server won't die
    * Suggest pkill when that happens
    
    Incidentally, I believe that until recently we had a bug with restart:
    hh_server would write to the PidLog before it tried to acquire the file lock,
    so calling `hh_client start` when a server was already running would cause the
    PidLog to be overwritten and subsequent kills to fail. However, given that the
    lock has been moved to an earlier point in startup (see D2543288), this may no
    longer be an issue.
    
    TODO: Log to Scuba when a restart fails
    
    Reviewed By: dabek
    
    Differential Revision: D2569862
    
    fb-gh-sync-id: f119dd7029caf1ba4ce6c5d6079a44a5f409d427

--- hack/client/clientStop.ml
+++ hack/client/clientStop.ml
@@ -17,51 +17,65 @@ type env = {
   root: Path.t;
 }
 
-let nice_kill (ic, oc) env =
-  let root = env.root in
-  Printf.eprintf "Attempting to nicely kill server for %s\n%!"
-    (Path.to_string root);
-  let _response = ServerCommand.rpc (ic, oc) ServerRpc.KILL in
+let wait_for_death root secs =
   let i = ref 0 in
-  while CCS.server_exists root do
-    incr i;
-    if !i < 5 then ignore @@ Unix.sleep 1
-    else raise FailedToKill
-  done;
-  Printf.eprintf "Successfully killed server for %s\n%!"
-    (Path.to_string root)
+  try
+    while CCS.server_exists root do
+      incr i;
+      if !i < secs then ignore @@ Unix.sleep 1
+      else raise Exit
+    done;
+    true
+  with Exit -> false
+
+let nice_kill (ic, oc) env =
+  let root_s = Path.to_string env.root in
+  Printf.eprintf "Attempting to nicely kill server for %s\n%!" root_s;
+  let _response = ServerCommand.rpc (ic, oc) ServerRpc.KILL in
+  let success = wait_for_death env.root 3 in
+  if not success then begin
+    Printf.eprintf "Failed to kill server nicely for %s\n%!" root_s;
+    raise FailedToKill;
+  end else
+    Printf.eprintf "Successfully killed server for %s\n%!" root_s
 
 let mean_kill env =
-  Printf.fprintf stderr "Attempting to meanly kill server for %s\n%!"
-    (Path.to_string env.root);
+  let root_s = Path.to_string env.root in
+  Printf.eprintf "Attempting to meanly kill server for %s\n%!" root_s;
   let pids =
     try PidLog.get_pids (ServerFiles.pids_file env.root)
     with PidLog.FailedToGetPids ->
       Printf.eprintf "Unable to figure out pids of running Hack server. \
-        Try manually killing it with 'pkill hh-server'\n%!";
+        Try manually killing it with `pkill hh_server`\n%!";
       raise FailedToKill
   in
-  List.iter pids ~f:begin fun (pid, reason) ->
-    try Unix.kill pid 9
-    with Unix.Unix_error (Unix.ESRCH, "kill", _) ->
-      (* no such process *)
-      ()
-  end;
-  ignore(Unix.sleep 1);
-  if CCS.server_exists env.root
-  then raise FailedToKill
-  else Printf.fprintf stderr "Successfully killed server for %s\n%!"
-    (Path.to_string env.root)
+  let success =
+    try
+      List.iter pids ~f:begin fun (pid, reason) ->
+        try Unix.kill pid 9
+        with Unix.Unix_error (Unix.ESRCH, "kill", _) ->
+          (* no such process *)
+          ()
+      end;
+      wait_for_death env.root 3
+    with e ->
+      print_endline (Printexc.to_string e);
+      false
+  in
+  if not success then begin
+    Printf.eprintf "Failed to kill server meanly for %s. \
+      Try manually killing it with `pkill hh_server`\n%!" root_s;
+    raise FailedToKill
+  end else Printf.eprintf "Successfully killed server for %s\n%!" root_s
 
 let do_kill env =
   let root_s = Path.to_string env.root in
   match CCS.connect_once env.root with
   | Result.Ok conn ->
       begin
-        try nice_kill conn env
-        with FailedToKill ->
-          Printf.eprintf "Failed to kill server nicely for %s\n%!" root_s;
-          exit 1
+        try nice_kill conn env with FailedToKill ->
+          try mean_kill env with FailedToKill ->
+            raise Exit_status.(Exit_with Kill_error)
       end
   | Result.Error CCS.Server_missing ->
       Printf.eprintf "Error: no server to kill for %s\n%!" root_s
@@ -70,8 +84,7 @@ let do_kill env =
   | Result.Error (CCS.Server_busy | CCS.Server_initializing) ->
       try mean_kill env
       with FailedToKill ->
-        Printf.eprintf "Failed to kill server meanly for %s\n%!" root_s;
-        exit 1
+        raise Exit_status.(Exit_with Kill_error)
 
 let main env =
   do_kill env;
