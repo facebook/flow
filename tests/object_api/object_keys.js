c2faf59c147aa62cd3de1c297fa72f65fb5ae294
Sam Goldman <samwgoldman@gmail.com>
2015-10-25 23:51:41 -0700

    Add string constraint to object keys for unsealed objtypes
    Summary: Before this change, `Object.keys(o)`, where `o` is not a sealed object,
    would return effectively `Array<any>`, which is definitely unsound.
    
    I ran into this when I tried to use `Object.keys` on an object-as-map.
    Closes https://github.com/facebook/flow/pull/984
    
    Reviewed By: svcscm
    
    Differential Revision: D2580340
    
    Pulled By: mroch
    
    fb-gh-sync-id: 91d85e9b35e390aaaa9002330b96ca8f52e62954

--- tests/object_api/object_keys.js
+++ tests/object_api/object_keys.js
@@ -1,4 +1,14 @@
 /* @flow */
 
-var obj = {one: 'one', two: 'two'};
-var keys : Array<string> = Object.keys(obj);
+var sealed = {one: 'one', two: 'two'};
+var keys : Array<'one'|'two'> = Object.keys(sealed);
+
+var unsealed = {};
+Object.keys(unsealed).forEach(k => {
+  (k : number) // error: number ~> string
+});
+
+var dict: { [k: number]: string } = {};
+Object.keys(dict).forEach(k => {
+  (k : number) // error: number ~> string
+});
