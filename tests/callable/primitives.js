0682369e76509824f81ef31d5ad95408513c6f31
Sam Goldman <samwgoldman@gmail.com>
2015-11-05 17:55:24 -0800

    callable interfaces
    Summary: Before this commit, interfaces with callable signatures couldn't be
    called.
    
    For the included test, the pre-change failure was:
    
    ```
    primitives.js:12:1,8: function call
    Function cannot be called on
    primitives.js:12:1,5: ICall
    ```
    
    After this change, the expected failure correctly complains about the
    parameter type.
    Closes https://github.com/facebook/flow/pull/1036
    
    Reviewed By: svcscm
    
    Differential Revision: D2624636
    
    Pulled By: gabelevi
    
    fb-gh-sync-id: d62e78f8260072bfb83ef0e32f52e222e2d9a7f3

--- tests/callable/primitives.js
+++ tests/callable/primitives.js
@@ -4,3 +4,9 @@ foo(Boolean);
 
 var dict: { [k: string]: any } = {};
 dict(); // error, callable signature not found
+
+interface ICall {
+  (x: string): void;
+}
+declare var icall: ICall;
+icall(0); // error, number ~> string
