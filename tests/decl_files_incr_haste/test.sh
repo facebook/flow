2fdacb270955ad6ef4ae81b8d475ee0791496aa4
Gabe Levi <gabe@fb.com>
2015-11-09 22:09:28 -0500

    Fix mistake in test.sh for one test

--- tests/decl_files_incr_haste/test.sh
+++ tests/decl_files_incr_haste/test.sh
@@ -22,33 +22,29 @@ use_declaration_files() {
 }
 
 ignore_implementation_files() {
-  mv A.js tests/declaration_files_incremental_haste/A.js.ignored
-  mv ExplicitProvidesModuleDifferentName.js tests/declaration_files_incremental_haste/ExplicitProvidesModuleDifferentName.js.ignored
-  mv ExplicitProvidesModuleSameName.js tests/declaration_files_incremental_haste/ExplicitProvidesModuleSameName.js.ignored
-  mv external/_d3/min.js tests/declaration_files_incremental_haste/external/_d3/min.js.ignored
-  mv foo/bar/nested_test.js tests/declaration_files_incremental_haste/foo/bar/nested_test.js.ignored
-  mv ImplicitProvidesModule.js tests/declaration_files_incremental_haste/ImplicitProvidesModule.js.ignored
-  mv md5.js tests/declaration_files_incremental_haste/md5.js.ignored
-  mv node_modules/qux/corge/lib/index.js tests/declaration_files_incremental_haste/node_modules/qux/corge/lib/index.js.ignored
-  mv node_modules/qux/docblock.js tests/declaration_files_incremental_haste/node_modules/qux/docblock.js.ignored
-  mv test.js tests/declaration_files_incremental_haste/test.js.ignored
-  mv ws/index.js tests/declaration_files_incremental_haste/ws/index.js.ignored
-  mv ws/test/client.js tests/declaration_files_incremental_haste/ws/test/client.js.ignored
+  mv A.js A.js.ignored
+  mv ExplicitProvidesModuleDifferentName.js ExplicitProvidesModuleDifferentName.js.ignored
+  mv ExplicitProvidesModuleSameName.js ExplicitProvidesModuleSameName.js.ignored
+  mv external/_d3/min.js external/_d3/min.js.ignored
+  mv ImplicitProvidesModule.js ImplicitProvidesModule.js.ignored
+  mv md5.js md5.js.ignored
+  mv node_modules/qux/corge/lib/index.js node_modules/qux/corge/lib/index.js.ignored
+  mv node_modules/qux/docblock.js node_modules/qux/docblock.js.ignored
+  mv ws/index.js ws/index.js.ignored
+  mv ws/test/client.js ws/test/client.js.ignored
 }
 
 use_implementation_files() {
-  mv A.js.ignored tests/declaration_files_incremental_haste/A.js
-  mv ExplicitProvidesModuleDifferentName.js.ignored tests/declaration_files_incremental_haste/ExplicitProvidesModuleDifferentName.js
-  mv ExplicitProvidesModuleSameName.js.ignored tests/declaration_files_incremental_haste/ExplicitProvidesModuleSameName.js
-  mv external/_d3/min.js.ignored tests/declaration_files_incremental_haste/external/_d3/min.js
-  mv foo/bar/nested_test.js.ignored tests/declaration_files_incremental_haste/foo/bar/nested_test.js
-  mv ImplicitProvidesModule.js.ignored tests/declaration_files_incremental_haste/ImplicitProvidesModule.js
-  mv md5.js.ignored tests/declaration_files_incremental_haste/md5.js
-  mv node_modules/qux/corge/lib/index.js.ignored tests/declaration_files_incremental_haste/node_modules/qux/corge/lib/index.js
-  mv node_modules/qux/docblock.js.ignored tests/declaration_files_incremental_haste/node_modules/qux/docblock.js
-  mv test.js.ignored tests/declaration_files_incremental_haste/test.js
-  mv ws/index.js.ignored tests/declaration_files_incremental_haste/ws/index.js
-  mv ws/test/client.js.ignored tests/declaration_files_incremental_haste/ws/test/client.js
+  mv A.js.ignored A.js
+  mv ExplicitProvidesModuleDifferentName.js.ignored ExplicitProvidesModuleDifferentName.js
+  mv ExplicitProvidesModuleSameName.js.ignored ExplicitProvidesModuleSameName.js
+  mv external/_d3/min.js.ignored external/_d3/min.js
+  mv ImplicitProvidesModule.js.ignored ImplicitProvidesModule.js
+  mv md5.js.ignored md5.js
+  mv node_modules/qux/corge/lib/index.js.ignored node_modules/qux/corge/lib/index.js
+  mv node_modules/qux/docblock.js.ignored node_modules/qux/docblock.js
+  mv ws/index.js.ignored ws/index.js
+  mv ws/test/client.js.ignored ws/test/client.js
 }
 
 printf "======Start off with the .js files but without the .flow file======\n"
