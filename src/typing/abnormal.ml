8ffd6238f5250b865d7b36de33855ccddbe44688
Marshall Roch <mroch@fb.com>
2015-11-10 12:28:45 -0800

    [cleanup] move Abnormal and Refinement to their own files
    Summary: I know this exposes things that were technically private in `Type_inference_js` before, but that file is too big so splitting things up is going to be necessary anyway, I think.
    
    We can possibly use build dependencies (like, put this stuff in a folder that we only add to the search path while building `type_inference_js.ml`) to make this stuff private again, but I'm not worried enough about it to do that.
    
    The number of files in `src/typing/` is also getting out of hand, but i think it'll get worse before it gets better (once we can separate dependencies well, I can move them into subfolders).
    
    Reviewed By: bhosmer
    
    Differential Revision: D2635673
    
    fb-gh-sync-id: 6a183196b7726f93b0c2676552ca0f46c685f9c7

---
+++ src/typing/abnormal.ml
@@ -0,0 +1,98 @@
+(**
+ * Copyright (c) 2014, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the "flow" directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ *)
+
+(* we model abnormal control flows using exceptions during traversal *)
+
+(* control directives encountered during traversal *)
+type t =
+  | Return
+  | Throw
+  | Break of string option
+  | Continue of string option
+
+exception Exn of t
+
+open Utils
+open Utils_js
+
+(* called from traversal. value indicates control flow directive encountered *)
+let throw_control_flow_exception abnormal =
+  raise (Exn abnormal)
+
+(* if argument is Some abnormal, throw it *)
+let check_control_flow_exception = function
+  | None -> ()
+  | Some abnormal -> throw_control_flow_exception abnormal
+
+(* helper *)
+let check_env_depth depth =
+  let new_depth = Env_js.env_depth () in
+  if new_depth = depth then ()
+  else assert_false (spf
+    "env depth %d != %d after no control flow catch"
+    new_depth depth)
+
+(* run a function, return first control-flow exception or none *)
+let catch_control_flow_exception f =
+  let depth = Env_js.env_depth () in
+  try (
+    f ();
+    check_env_depth depth;
+    None
+  ) with
+  | Exn abnormal ->
+    Env_js.trunc_env depth;
+    Some abnormal
+  | exn ->
+    raise exn
+
+(* like check_control_flow_exception, except break statements
+   specifying the given label (or None) are ignored *)
+let ignore_break_to_label label f =
+  match catch_control_flow_exception f with
+    | Some (Break break_label) when break_label = label -> None
+    | result -> result
+
+(* like ignore_break_to_label, except continue statements
+   on the same label (or None) are also ignored *)
+let ignore_break_or_continue_to_label label f =
+  match ignore_break_to_label label f with
+    | Some (Continue cont_label) when cont_label = label -> None
+    | result -> result
+
+(* thread-local state used to detect abnormal control flows *)
+module AbSet = Set.Make(struct
+  type abnormal = t
+  type t = abnormal
+  let compare = Pervasives.compare
+end)
+
+let abnormals = ref AbSet.empty
+
+(* register a control flow directive *)
+let set abnormal =
+  abnormals := AbSet.add abnormal !abnormals;
+  throw_control_flow_exception abnormal
+
+(* swap in a new presence value for a given control directive,
+   and return the current value *)
+let swap abnormal newv =
+  let oldv = AbSet.mem abnormal !abnormals in
+  if oldv = newv then ()
+  else abnormals := AbSet.(if newv then add else remove) abnormal !abnormals;
+  oldv
+
+let string = function
+  | Return -> "return"
+  | Throw -> "throw"
+  | Break (Some lbl) -> spf "break `%s`" lbl
+  | Break None -> "break"
+  | Continue (Some lbl) -> spf "continue `%s`" lbl
+  | Continue None -> "continue"
---
+++ src/typing/refinement.ml
@@ -0,0 +1,68 @@
+(**
+ * Copyright (c) 2014, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the "flow" directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ *)
+
+module Ast = Spider_monkey_ast
+
+(*
+ * type refinements on expressions - wraps Env_js API
+ *)
+
+(* if expression is syntactically eligible for type refinement,
+   return Some (access key), otherwise None.
+   Eligible expressions are simple ids and chains of property|index
+   lookups from an id base
+ *)
+let rec key = Ast.Expression.(function
+
+| _, This ->
+  (* treat this as a property chain, in terms of refinement lifetime *)
+  Some (Reason_js.internal_name "this", [])
+
+| _, Identifier (_, { Ast.Identifier.name; _ }) when name != "undefined" -> (
+  (* ditto super *)
+  match name with
+  | "super" -> Some (Reason_js.internal_name "super", [])
+  | _ -> Some (name, [])
+  )
+
+| _, Member { Member._object;
+  (* foo.bar.baz -> Chain [Id baz; Id bar; Id foo] *)
+   property = Member.PropertyIdentifier (_ , { Ast.Identifier.name; _ });
+   _ } -> (
+  match key _object with
+  | Some (base, chain) ->
+    Some (base, Key.Prop name :: chain)
+  | None -> None
+  )
+
+| _, Member {
+    Member._object; property = Member.PropertyExpression index; _
+  } -> (
+  (* foo.bar[baz] -> Chain [Index baz; Id bar; Id foo] *)
+  match key _object with
+  | Some (base, chain) -> (
+    match key index with
+    | Some key ->
+      Some (base, Key.Elem key :: chain)
+    | None -> None
+    )
+  | None -> None
+  )
+
+| _ ->
+  (* other LHSes unsupported currently/here *)
+  None
+)
+
+(* get type refinement for expression, if it exists *)
+let get cx expr reason =
+  match key expr with
+  | Some k -> Env_js.get_refinement cx k reason
+  | None -> None
