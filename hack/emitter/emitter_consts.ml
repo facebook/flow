4da1f433eecb2eb4a95d09f9a7d549a75037d0d9
Sara Golemon <sgolemon@fb.com>
2015-11-12 13:32:59 -0800

    [hack] Finish converting asio to HNI
    Summary: Cheat the standard HNI-NativeData model by leaving the extra data
    for asio wait handles at the end of the ObjectData struct.
    Normally this would be verboten, but Asio objects have the
    unique property of being effectively final (private ctor)
    and can only be constructed by internal APIs.
    
    This conversion takes advantage of this and allows the
    object structure to remain as-is, greatly simplifying
    this conversion from what it would otherwise have been.
    
    Reviewed By: jano
    
    Differential Revision: D2476884
    
    fb-gh-sync-id: 861b41b1978b32b1cafc329250546fc24b735292

--- hack/emitter/emitter_consts.ml
+++ hack/emitter/emitter_consts.ml
@@ -18,7 +18,7 @@ open Utils
  * keep this synced eventually *)
 let header_kinds = [
   "Packed"; "Struct"; "Mixed"; "Empty"; "Apc"; "Globals"; "Proxy"; "String";
-  "Resource"; "Ref"; "Object"; "ResumableObj"; "AwaitAllWH";
+  "Resource"; "Ref"; "Object"; "WaitHandle"; "ResumableObj"; "AwaitAllWH";
   "Vector"; "Map"; "Set"; "Pair"; "ImmVector"; "ImmMap"; "ImmSet";
   "ResumableFrame"; "NativeData";
   "SmallMalloc"; "BigMalloc"; "BigObj"; "Free"; "Hole"
