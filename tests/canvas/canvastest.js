f56cb6ac156549c077b247933d778c87214d9604
Marshall Roch <mroch@fb.com>
2015-11-30 13:16:27 -0800

    Combine canvas tests into DOM tests
    Summary: `tests/dom/` will contain tests for all of the classes in `lib/dom.js`.
    Closes https://github.com/facebook/flow/pull/1135
    
    Reviewed By: svcscm
    
    Differential Revision: D2704451
    
    Pulled By: mroch
    
    fb-gh-sync-id: 1d7994f86dce16ea7f1f15cfededd73f3097c88f

--- tests/canvas/canvastest.js
+++
@@ -1,9 +0,0 @@
-var el = document.createElement('canvas');
-var ctx: ?CanvasRenderingContext2D = el.getContext('2d');
-if (ctx == null) {
-  // error handling
-} else {
-  // ctx here is specifically a CanvasRenderingContext2D (not a WebGL context)
-  ctx.fillRect(0, 0, 200, 100);
-  ctx.moveTo('0', '1');  // error: should be numbers
-}
---
+++ tests/dom/Document.js
@@ -0,0 +1,8 @@
+// @flow
+
+let tests = [
+  // createElement
+  function(document: Document) {
+    (document.createElement('canvas'): HTMLCanvasElement);
+  }
+];
