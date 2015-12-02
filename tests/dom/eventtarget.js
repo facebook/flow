00efcd7d55c390a23ab92eb093aa3b21f7cac5c7
Jesse Beach <jessebeach@fb.com>
2015-12-01 16:35:29 -0800

    Add attachEvent and detachEvent to dom.js in Flow
    Summary: Adding `attachEvent` and `detachEvent` to the `EventTarget`, which gets mixed into `Node`.
    
    Reviewed By: mroch
    
    Differential Revision: D2681479
    
    fb-gh-sync-id: 333bb4f9970d2be03f1d99c8d57c1e2bf34172ef

---
+++ tests/dom/eventtarget.js
@@ -0,0 +1,19 @@
+// @flow
+
+let listener: EventListener = function (event: Event) :void {};
+
+let tests = [
+  // attachEvent
+  function() {
+    let target = new EventTarget();
+    (target.attachEvent('foo', listener): void); // invalid, may be undefined
+    (target.attachEvent && target.attachEvent('foo', listener): void); // valid
+  },
+
+  // detachEvent
+  function() {
+    let target = new EventTarget();
+    (target.detachEvent('foo', listener): void); // invalid, may be undefined
+    (target.detachEvent && target.detachEvent('foo', listener): void); // valid
+  },
+];
