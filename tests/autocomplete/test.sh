1a56884f319a4032e2dbefb7f2697388221482f7
Marshall Roch <mroch@fb.com>
2015-12-09 13:12:51 -0800

    unbreak autocomplete tests in sh
    Summary:
    `echo` is a shell builtin and a command, and the builtin doesn't always support `-n`. so, just use `printf` instead.
    
    alternatively, could do this if `printf` ever causes problems:
    
    ```
    ECHO=`which echo`
    
    $ECHO -n "whatever.js = "
    ```
    
    Reviewed By: jeffmo
    
    Differential Revision: D2740643
    
    fb-gh-sync-id: 7b9eaf11b1ce2b9b621347f892d638e4deb6173d

--- tests/autocomplete/test.sh
+++ tests/autocomplete/test.sh
@@ -1,34 +1,34 @@
 FLOW=$1
 
-echo -n "foo_parse_fail.js = "
+printf "foo_parse_fail.js = "
 $FLOW autocomplete --strip-root foo_parse_fail.js 10 17 < foo_parse_fail.js
 
-echo -n "foo.js = "
+printf "foo.js = "
 $FLOW autocomplete --strip-root --json foo.js 10 5 < foo.js
 
-echo -n "bar.js = "
+printf "bar.js = "
 $FLOW autocomplete --strip-root --json bar.js 4 5 < bar.js
 
-echo -n "qux.js = "
+printf "qux.js = "
 $FLOW autocomplete --strip-root --json qux.js 6 3 < qux.js
 
-echo -n "str.js = "
+printf "str.js = "
 $FLOW autocomplete --strip-root --json str.js 3 9 < str.js
 
-echo -n "num.js = "
+printf "num.js = "
 $FLOW autocomplete --strip-root --json num.js 4 5 < num.js
 
-echo -n "bool.js = "
+printf "bool.js = "
 $FLOW autocomplete --strip-root --json bool.js 3 6 < bool.js
 
-echo -n "union.js = "
+printf "union.js = "
 $FLOW autocomplete --strip-root --json union.js 10 5 < union.js
 
-echo -n "object_builtins.js = "
+printf "object_builtins.js = "
 $FLOW autocomplete --strip-root --json object_builtins.js 4 5 < object_builtins.js
 
-echo -n "function_builtins.js = "
+printf "function_builtins.js = "
 $FLOW autocomplete --strip-root --json function_builtins.js 4 5 < function_builtins.js
 
-echo -n "fun.js = "
+printf "fun.js = "
 $FLOW autocomplete --strip-root --json fun.js 4 5 < fun.js
