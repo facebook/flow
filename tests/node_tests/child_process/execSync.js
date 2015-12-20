bb21a87512edf6f8d12f53316fc693e7f576d22f
Marshall Roch <mroch@fb.com>
2015-12-20 13:44:24 -0800

    improve Buffer encodings
    Summary: adds the list of valid encodings to the `Buffer` libs, and uses them in `child_process.execSync` to return a string when an encoding is set, and a `Buffer` when one is not.
    
    Reviewed By: gabelevi
    
    Differential Revision: D2684403
    
    fb-gh-sync-id: 26e50d3fcec6121c47645c3453c992c17838eceb

---
+++ tests/node_tests/child_process/execSync.js
@@ -0,0 +1,8 @@
+/* @flow */
+
+var execSync = require('child_process').execSync;
+
+(execSync('ls'): Buffer); // returns Buffer
+(execSync('ls', {encoding: 'buffer'}): Buffer); // returns Buffer
+(execSync('ls', {encoding: 'utf8'}): string); // returns string
+(execSync('ls', {timeout: '250'})); // error, no signatures match
