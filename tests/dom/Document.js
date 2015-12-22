dabb517c5d0f06497dfedbb287a9f119a705b081
Marshall Roch <mroch@fb.com>
2015-12-21 17:06:49 -0800

    Slightly more accurate type for HTML*Element.form
    Summary: the `form` attribute on `HTML*Element` is either an element or `null`, never `undefined`, so narrowed the type slightly so `elem.form === null` works.
    
    Reviewed By: bhosmer
    
    Differential Revision: D2782056
    
    fb-gh-sync-id: ffa81d53868680f06d210282e0f2dd658db2c928

--- tests/dom/Document.js
+++ tests/dom/Document.js
@@ -4,5 +4,7 @@ let tests = [
   // createElement
   function(document: Document) {
     (document.createElement('canvas'): HTMLCanvasElement);
+    (document.createElement('option'): HTMLOptionElement);
+    (document.createElement('select'): HTMLSelectElement);
   }
 ];
