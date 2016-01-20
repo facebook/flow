87a9262c5d1958fcbdb78b4dfa681b6c1421f312
Marshall Roch <mroch@fb.com>
2016-01-20 13:16:43 -0800

    Use a custom opam repo to download from github instead of caml.inria.fr
    Summary:
    OPAM points at caml.inria.fr to download compilers. This is an additional point of failure that can break our Travis builds, so we have here a minimal OPAM repository that instead points at GitHub tarballs for the versions of ocaml that we build for. As a plus, it also uses SSL.
    
    This also works on Macs, even though we currently use homebrew for them.
    
    Tested (on a mac!) with:
    
    ```
    rm -rf ~/.opam && TRAVIS_OS_NAME=linux OCAML_VERSION=4.01.0 OPAM_VERSION=1.1.1 bash -ex .travis-ci.sh
    rm -rf ~/.opam && TRAVIS_OS_NAME=linux OCAML_VERSION=4.02.1 OPAM_VERSION=1.1.1 bash -ex .travis-ci.sh
    rm -rf ~/.opam && TRAVIS_OS_NAME=linux OCAML_VERSION=4.01.0 OPAM_VERSION=1.2.0 bash -ex .travis-ci.sh
    rm -rf ~/.opam && TRAVIS_OS_NAME=linux OCAML_VERSION=4.02.1 OPAM_VERSION=1.2.0 bash -ex .travis-ci.sh
    ```
    Closes https://github.com/facebook/flow/pull/1291
    
    Reviewed By: gabelevi, svcscm
    
    Differential Revision: D2844447
    
    Pulled By: mroch
    
    fb-gh-sync-id: a714f77614e9272f61f1f37591d0087943e5c3f4

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,25 +1,50 @@
 OPAM_DEPENDS=
-         
+
+TMP=${TMPDIR:-/tmp}
+
+dlerror () {
+    echo "Couldn't download $url"
+    exit 1
+}
+
+getopam() {
+    opamfile=$2
+    url=$1/$opamfile
+
+    if which wget >/dev/null; then
+        wget -q -O "$TMP/$opamfile" "$url" || dlerror
+    else
+        curl -s -L -o "$TMP/$opamfile" "$url" || dlerror
+    fi
+}
+
 setup_linux () {
   printf "travis_fold:start:opam_installer\nInstalling ocaml %s and opam %s\n" $OCAML_VERSION $OPAM_VERSION
   export PREFIX="./usr"
   export BINDIR="$PREFIX/bin"
   export PATH="$BINDIR:$PATH"
 
-  wget -q -O opam_installer.sh "https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh"
-  if [ -n "${OPAM_VERSION:-}" ]; then
-    sed -i "s/^VERSION=.*$/VERSION='$OPAM_VERSION'/" opam_installer.sh
-  fi
+  file="opam-$OPAM_VERSION-$(uname -m || echo unknown)-$(uname -s || echo unknown)"
 
-  echo y | sh opam_installer.sh $BINDIR $OCAML_VERSION
+  echo Downloading OPAM...
+  getopam "https://github.com/ocaml/opam/releases/download/$OPAM_VERSION" $file
+
+  mkdir -p "$BINDIR" 2>/dev/null || true
+  install -m 755 $TMP/$file $BINDIR/opam
+  rm -f $TMP/$file
+
+  OPAM="$BINDIR/opam"
 
   export OPAMYES=1
   export OPAMVERBOSE=1
-  opam init -a -y
+  "$OPAM" init -a -y -k local flow resources/opam --comp "$OCAML_VERSION"
+  "$OPAM" repository add default https://opam.ocaml.org --priority=-1 >/dev/null
   # TODO: Install js_of_ocaml and test the parser
   # opam install ${OPAM_DEPENDS}
-  eval `opam config env`
+  eval `"$OPAM" config env`
   printf "travis_fold:end:opam_installer\n"
+
+  printf "Using ocaml %s and opam %s\n" $(ocaml -vnum) $("$OPAM" --version)
 
   # For some reason the Linux containers start killing the tests if too many
   # tests are run in parallel. Luckily we can easily configure that here
@@ -31,7 +56,7 @@ setup_osx () {
   4.02.1)
     brew update
     brew install ocaml ;;
-  4.01.0) 
+  4.01.0)
     brew install ocaml ;;
   *) echo Unknown $OCAML_VERSION; exit 1 ;;
   esac
@@ -42,14 +67,14 @@ setup_osx () {
   #  'opam init -a -y' failed.
   #  Fatal error:
   #  Sys_error("/Users/travis/.opam/repo/default/packages/ott/ott.0.21.2/opam: Too many open files")
+
+  printf "Using ocaml %s\n" $(ocaml -vnum)
 }
 
 case $TRAVIS_OS_NAME in
   osx) setup_osx ;;
 linux) setup_linux ;;
 esac
-
-printf "Using ocaml %s and opam %s\n" $(ocaml -vnum) $(opam --version)
 
 printf "travis_fold:start:make\nBuilding flow\n"
 make
