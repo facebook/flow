f7fc3b94c593d15db979be45bb3a976152c2d743
Kasper Kopec <kasper@fb.com>
2016-01-25 12:03:39 -0800

    [hack] Micro optimize server side auto-complete
    Reviewed By: int3, dlreeves
    
    Differential Revision: D2835619
    
    fb-gh-sync-id: a247c06e4b039a407d12de59d48a6bde5987be93

--- hack/server/serverAutoComplete.ml
+++ hack/server/serverAutoComplete.ml
@@ -6,7 +6,7 @@
  * LICENSE file in the "hack" directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  *
- *)
+*)
 
 
 (*****************************************************************************)
@@ -17,21 +17,23 @@ open Utils
 
 let auto_complete files_info content =
   AutocompleteService.attach_hooks();
-  let funs, classes =
+  let content_funs, content_classes =
     ServerIdeUtils.declare Relative_path.default content in
-  ServerIdeUtils.fix_file_and_def funs classes;
+  ServerIdeUtils.fix_file_and_def content_funs content_classes;
   let fun_names, class_names =
     files_info
     |> Relative_path.Map.values
     |> List.fold_left ~f:begin fun (f, c) { FileInfo.funs; classes; _ } ->
-      let add_all ids init =
-        List.fold_left ids ~init ~f:(fun s (_, x) -> SSet.add x s) in
-      add_all funs f, add_all classes c
-    end ~init:(funs, classes)
+      let add_all ids content_ids init =
+        List.fold_left ids ~init ~f: begin fun acc (_, x) ->
+          (* Duplicate class/function name is an error so should be rare, we
+           * only need to avoid adding the names declared in content twice *)
+          if SSet.mem x content_ids then acc else x::acc
+        end in
+      add_all funs content_funs f, add_all classes content_classes c
+    end ~init:(SSet.elements content_funs, SSet.elements content_classes)
   in
-  let fun_names = SSet.elements fun_names in
-  let class_names = SSet.elements class_names in
   let result = AutocompleteService.get_results fun_names class_names in
-  ServerIdeUtils.revive funs classes;
+  ServerIdeUtils.revive content_funs content_classes;
   AutocompleteService.detach_hooks();
   result
