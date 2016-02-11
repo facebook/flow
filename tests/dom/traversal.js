42a7a3f75bcc9f532b37e8ecec67eff6753d9a52
April Arcus <april.arcus@gmail.com>
2016-02-11 15:22:53 -0800

    NodeFilterInterface

--- tests/dom/traversal.js
+++ tests/dom/traversal.js
@@ -1,12 +1,32 @@
 // @flow
 
 let tests = [
+  // basic functionality
   function() {
     const i: NodeIterator = document.createNodeIterator(document.body);
     const filter: NodeFilter = i.filter;
+    const response: typeof NodeFilter.FILTER_ACCEPT | typeof NodeFilter.FILTER_REJECT | typeof NodeFilter.FILTER_SKIP = filter.acceptNode(document.body);
   },
   function() {
     const w: TreeWalker = document.createTreeWalker(document.body);
     const filter: NodeFilter = w.filter;
+    const response: typeof NodeFilter.FILTER_ACCEPT | typeof NodeFilter.FILTER_REJECT | typeof NodeFilter.FILTER_SKIP = filter.acceptNode(document.body);
+  },
+  // testing NodeFilterInterface
+  function() {
+    // valid
+    document.createNodeIterator(document.body, undefined, node => NodeFilter.FILTER_ACCEPT);
+    // valid
+    document.createNodeIterator(document.body, undefined, {accept: node => NodeFilter.FILTER_ACCEPT});
+    // invalid
+    document.createNodeIterator(document.body, undefined, {});
+  },
+  function() {
+    // valid
+    document.createTreeWalker(document.body, undefined, node => NodeFilter.FILTER_ACCEPT);
+    // valid
+    document.createTreeWalker(document.body, undefined, {accept: node => NodeFilter.FILTER_ACCEPT});
+    // invalid
+    document.createTreeWalker(document.body, undefined, {});
   },
 ];
