a9b32698590fa55ecf767e911770d21d5347d1ce
April Arcus <april.arcus@gmail.com>
2016-02-11 15:37:44 -0800

    test return types

--- tests/dom/traversal.js
+++ tests/dom/traversal.js
@@ -12,21 +12,37 @@ let tests = [
     const filter: NodeFilter = w.filter;
     const response: typeof NodeFilter.FILTER_ACCEPT | typeof NodeFilter.FILTER_REJECT | typeof NodeFilter.FILTER_SKIP = filter.acceptNode(document.body);
   },
+  // testing polymorphic return types
+  function() {
+    const i = document.createNodeIterator(document.body, -1, node => NodeFilter.FILTER_ACCEPT);
+    const previousNode: Node | null = i.previousNode();
+    const nextNode: Node | null = i.nextNode();
+  },
+  function() {
+    const w = document.createTreeWalker(document.body, -1, node => NodeFilter.FILTER_ACCEPT);
+    const parentNode: Node | null = w.parentNode();
+    const firstChild: Node | null = w.firstChild();
+    const lastChild: Node | null = w.lastChild();
+    const previousSibling: Node | null = w.previousSibling();
+    const nextSibling: Node | null = w.nextSibling();
+    const previousNode: Node | null = w.previousNode();
+    const nextNode: Node | null = w.nextNode();
+  },
   // testing NodeFilterInterface
   function() {
     // valid
-    document.createNodeIterator(document.body, undefined, node => NodeFilter.FILTER_ACCEPT);
+    document.createNodeIterator(document.body, -1, node => NodeFilter.FILTER_ACCEPT);
     // valid
-    document.createNodeIterator(document.body, undefined, {accept: node => NodeFilter.FILTER_ACCEPT});
+    document.createNodeIterator(document.body, -1, {accept: node => NodeFilter.FILTER_ACCEPT});
     // invalid
-    document.createNodeIterator(document.body, undefined, {});
+    document.createNodeIterator(document.body, -1, {});
   },
   function() {
     // valid
-    document.createTreeWalker(document.body, undefined, node => NodeFilter.FILTER_ACCEPT);
+    document.createTreeWalker(document.body, -1, node => NodeFilter.FILTER_ACCEPT);
     // valid
-    document.createTreeWalker(document.body, undefined, {accept: node => NodeFilter.FILTER_ACCEPT});
+    document.createTreeWalker(document.body, -1, {accept: node => NodeFilter.FILTER_ACCEPT});
     // invalid
-    document.createTreeWalker(document.body, undefined, {});
+    document.createTreeWalker(document.body, -1, {});
   },
 ];
