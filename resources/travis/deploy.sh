2720b6519e1dc4565ebd0226cad1ce5ed85fa9d7
Marshall Roch <mroch@fb.com>
2016-02-19 15:07:31 -0800

    [docs] remove files too
    Reviewed By: avikchaudhuri
    
    fb-gh-sync-id: 69baf90f6f453427bddf8377e78d5b40526deb17
    shipit-source-id: 69baf90f6f453427bddf8377e78d5b40526deb17

--- resources/travis/deploy.sh
+++ resources/travis/deploy.sh
@@ -30,7 +30,7 @@ PATH="${TRAVIS_BUILD_DIR}/bin:$PATH" jekyll build -s website/ -d "$PAGES_CHECKOU
 printf "travis_fold:end:jekyll_build\n"
 
 printf "travis_fold:start:push_gh_pages\nPushing gh-pages\n"
-git -C "$PAGES_CHECKOUT" add -f .
+git -C "$PAGES_CHECKOUT" add -f -A .
 git -C "$PAGES_CHECKOUT" commit -m "Updating to ${TRAVIS_REPO_SLUG}@${TRAVIS_COMMIT}" || :
 git -C "$PAGES_CHECKOUT" push -q "https://${GH_BOT_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" gh-pages 2>/dev/null
 printf "travis_fold:end:push_gh_pages\n"
