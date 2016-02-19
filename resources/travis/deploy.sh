edd54c8bd7b9afb25ae95625a5daae9fdaf06777
Marshall Roch <mroch@fb.com>
2016-02-19 00:45:34 -0800

    Use travis to deploy the website
    Summary: On each successful commit to `master`, travis will checkout the `gh-pages` branch, build the site into that directory, commit all the changes, and push to the `gh-pages` branch.
    
    Reviewed By: avikchaudhuri
    
    Differential Revision: D2953008
    
    fb-gh-sync-id: af88379e113688ba6d261e3c968250521de71cc9
    shipit-source-id: af88379e113688ba6d261e3c968250521de71cc9

---
+++ resources/travis/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -e
+
+REPO="https://${GH_BOT_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
+PAGES_CHECKOUT="$HOME/gh-pages"
+
+printf "travis_fold:start:clone_gh_pages\nCloning gh-pages\n"
+mkdir -p "$PAGES_CHECKOUT"
+git -C "$PAGES_CHECKOUT" init
+# pull the gh-pages branch from $REPO.
+# doesn't use `clone` to avoid writing the token to disk
+git -C "$PAGES_CHECKOUT" pull -q --depth 20 "$REPO" gh-pages
+printf "travis_fold:end:clone_gh_pages\n"
+
+printf "travis_fold:start:jekyll_build\nBuilding Jekyll site\n"
+PATH="${TRAVIS_BUILD_DIR}/bin:$PATH" jekyll build -s website/ -d "$PAGES_CHECKOUT"
+printf "travis_fold:end:jekyll_build\n"
+
+printf "travis_fold:start:push_gh_pages\nPushing gh-pages\n"
+git -C "$PAGES_CHECKOUT" add -f .
+git -C "$PAGES_CHECKOUT" commit -m "Updating to ${TRAVIS_REPO_SLUG}@${TRAVIS_COMMIT}" || :
+git -C "$PAGES_CHECKOUT" push -q "$REPO" gh-pages
+printf "travis_fold:end:push_gh_pages\n"
