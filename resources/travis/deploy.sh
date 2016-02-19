0c3ba9b1bee5c905e7e460a12e43932f94150919
Marshall Roch <mroch@fb.com>
2016-02-19 03:30:32 -0800

    move rvm install into deploy script
    Summary: This will hopefully make the ruby env correct inside the deploy script. $TRAVIS_RUBY_VERSION is not set since we're not using `language: ruby` AFAICT.
    
    Reviewed By: avikchaudhuri
    
    fb-gh-sync-id: e0aad02adee133cec0a55eef99c153133cf6603e
    shipit-source-id: e0aad02adee133cec0a55eef99c153133cf6603e

--- resources/travis/deploy.sh
+++ resources/travis/deploy.sh
@@ -1,24 +1,33 @@
 #!/bin/sh -e
 # do not set -x or you may expose the secure token in the logs!
 
-REPO="https://${GH_BOT_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
 PAGES_CHECKOUT="$HOME/gh-pages"
+
+printf "travis_fold:start:installing_ruby\nInstalling Ruby\n"
+rvm use 2.2 --install --binary
+printf "travis_fold:end:installing_ruby\n"
+
+printf "travis_fold:start:installing_jekyll\nInstalling Jekyll\n"
+gem install --no-rdoc --no-ri github-pages
+printf "travis_fold:end:installing_jekyll\n"
 
 printf "travis_fold:start:clone_gh_pages\nCloning gh-pages\n"
 mkdir -p "$PAGES_CHECKOUT"
-git -C "$PAGES_CHECKOUT" init
 # pull the gh-pages branch from $REPO.
 # doesn't use `clone` to avoid writing the token to disk
-git -C "$PAGES_CHECKOUT" pull -q --depth 20 "$REPO" gh-pages
+git -C "$PAGES_CHECKOUT" init
+git -C "$PAGES_CHECKOUT" checkout -b gh-pages
+git -C "$PAGES_CHECKOUT" config user.name "facebook-github-bot-0"
+git -C "$PAGES_CHECKOUT" config user.email "githubbot+0@fb.com"
+git -C "$PAGES_CHECKOUT" pull -q --depth 20 "https://github.com/${TRAVIS_REPO_SLUG}.git" gh-pages
 printf "travis_fold:end:clone_gh_pages\n"
 
 printf "travis_fold:start:jekyll_build\nBuilding Jekyll site\n"
-PATH="${TRAVIS_BUILD_DIR}/bin:$PATH" \
-  rvm "$TRAVIS_RUBY_VERSION" do jekyll build -s website/ -d "$PAGES_CHECKOUT"
+PATH="${TRAVIS_BUILD_DIR}/bin:$PATH" jekyll build -s website/ -d "$PAGES_CHECKOUT"
 printf "travis_fold:end:jekyll_build\n"
 
 printf "travis_fold:start:push_gh_pages\nPushing gh-pages\n"
 git -C "$PAGES_CHECKOUT" add -f .
 git -C "$PAGES_CHECKOUT" commit -m "Updating to ${TRAVIS_REPO_SLUG}@${TRAVIS_COMMIT}" || :
-git -C "$PAGES_CHECKOUT" push -q "$REPO" gh-pages
+git -C "$PAGES_CHECKOUT" push -q "https://${GH_BOT_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" gh-pages 2>/dev/null
 printf "travis_fold:end:push_gh_pages\n"
