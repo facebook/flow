20389da5271eaea8fa16538db78066ac9683438a
Marshall Roch <mroch@fb.com>
2016-02-19 18:16:36 -0800

    use bundler to install jekyll gems
    Summary: will prevent github-pages or any of our other deps from inadvertently updating to a version that breaks our plugin or CSS.
    
    Reviewed By: samwgoldman
    
    Differential Revision: D2957474
    
    fb-gh-sync-id: 6c5f68c9b9a92b919dff412b5f888f296baf83e1
    shipit-source-id: 6c5f68c9b9a92b919dff412b5f888f296baf83e1

--- resources/travis/deploy.sh
+++ resources/travis/deploy.sh
@@ -4,14 +4,16 @@
 set +x
 
 PAGES_CHECKOUT="$HOME/gh-pages"
+export BUNDLE_GEMFILE="${TRAVIS_BUILD_DIR}/website/Gemfile"
 
 printf "travis_fold:start:installing_ruby\nInstalling Ruby\n"
 source "$HOME/.rvm/scripts/rvm"
 rvm use 2.2 --install --binary
+gem install --no-rdoc --no-ri bundler
 printf "travis_fold:end:installing_ruby\n"
 
 printf "travis_fold:start:installing_jekyll\nInstalling Jekyll\n"
-gem install --no-rdoc --no-ri github-pages
+bundle install
 printf "travis_fold:end:installing_jekyll\n"
 
 printf "travis_fold:start:clone_gh_pages\nCloning gh-pages\n"
@@ -26,7 +28,9 @@ git -C "$PAGES_CHECKOUT" pull -q --depth 20 "https://github.com/${TRAVIS_REPO_SL
 printf "travis_fold:end:clone_gh_pages\n"
 
 printf "travis_fold:start:jekyll_build\nBuilding Jekyll site\n"
-PATH="${TRAVIS_BUILD_DIR}/bin:$PATH" jekyll build -s website/ -d "$PAGES_CHECKOUT"
+env \
+  PATH="${TRAVIS_BUILD_DIR}/bin:$PATH" \
+  bundle exec jekyll build -s website/ -d "$PAGES_CHECKOUT"
 printf "travis_fold:end:jekyll_build\n"
 
 printf "travis_fold:start:push_gh_pages\nPushing gh-pages\n"
