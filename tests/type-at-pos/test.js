019c2d5442b0e8465f867cdf57278453d420a32d
Jared Forsyth <jared@jaredforsyth.com>
2016-02-26 11:05:14 -0800

    [hack] Fix singleton print
    Summary:The type printer already has logic to print these types, but the
    `is_printed_type_parseable` fn didn't seem to know that. This fixes #1440, and
    now singleton strings, numbers, and bools don't foul up `type-at-pos`
    reporting.
    Closes https://github.com/facebook/flow/pull/1463
    
    Differential Revision: D2983110
    
    Pulled By: mroch
    
    fb-gh-sync-id: a15f8fc19cd548ef61d214a36c84056c94784a43
    shipit-source-id: a15f8fc19cd548ef61d214a36c84056c94784a43

--- tests/type-at-pos/test.js
+++ tests/type-at-pos/test.js
@@ -6,3 +6,9 @@ str
 
 type Point = [number, string];
 const x:Point = [1, "foo"];
+type MyStr = "cool";
+const y:MyStr = "cool";
+type MyBool = true;
+const z:MyBool = true;
+type MyNum = 42;
+const w:MyNum = 42;
--- tests/type-at-pos/test.sh
+++ tests/type-at-pos/test.sh
@@ -3,6 +3,9 @@ FLOW=$1
 $FLOW type-at-pos test.js 5 1 --strip-root --json
 $FLOW type-at-pos test.js 5 1 --strip-root --raw
 $FLOW type-at-pos test.js 8 7 --strip-root --json
+$FLOW type-at-pos test.js 10 7 --strip-root --json
+$FLOW type-at-pos test.js 12 7 --strip-root --json
+$FLOW type-at-pos test.js 14 7 --strip-root --json
 $FLOW type-at-pos generics.js 5 1 --strip-root --json
 $FLOW type-at-pos generics.js 10 1 --strip-root --json
 $FLOW type-at-pos generics.js 14 1 --strip-root --json
