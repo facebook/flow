3b102f507840a39fa5d29cfe8abc5bd19b6fa11b
Gabe Levi <glevi@fb.com>
2016-03-15 17:33:39 -0700

    Delete unused "auxiliary*" dirs
    Summary: Not sure what these were for, but nothing is using them
    
    Reviewed By: avikchaudhuri
    
    Differential Revision: D3056488
    
    fb-gh-sync-id: 9273a25a2a4b04e249c411f193d94ce87230f426
    shipit-source-id: 9273a25a2a4b04e249c411f193d94ce87230f426

--- auxiliary/lib/auxiliary.js
+++
@@ -1,7 +0,0 @@
-declare var XXX: string;
-declare module Foo {
-    declare var x: string;
-}
-declare module Bar {
-    declare var y: string;
-}
--- auxiliary_tests/module_test.js
+++
@@ -1,6 +0,0 @@
-/* @flow */
-var Foo = require('Foo');
-var Bar = require('Bar');
-
-var x:number = Foo.x;
-var y:number = Bar.y;
