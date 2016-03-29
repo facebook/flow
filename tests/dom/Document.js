9a2d135a50ecf1c9edad27b00584e9ad6206e195
Timm Preetz <timm@lunados.com>
2016-03-29 22:24:21 +0200

    DOM: Add new type HTMLLinkElement and return that when invoking document.createElement('link') Definition based on https://developer.mozilla.org/en/docs/Web/HTML/Element/link

--- tests/dom/Document.js
+++ tests/dom/Document.js
@@ -4,6 +4,7 @@ let tests = [
   // createElement
   function(document: Document) {
     (document.createElement('canvas'): HTMLCanvasElement);
+    (document.createElement('link'): HTMLLinkElement);
     (document.createElement('option'): HTMLOptionElement);
     (document.createElement('select'): HTMLSelectElement);
   }
