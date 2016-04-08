b9edcaeb9ad5769c48c619980154fbbcc97af2fb
Marshall Roch <mroch@fb.com>
2016-04-08 13:06:51 -0700

    stop pushing website to gh-pages
    Summary: we've moved!
    
    Reviewed By: gabelevi
    
    Differential Revision: D3156856
    
    fb-gh-sync-id: e8fbade53dadc9952343000bbeaa7017c7e1dec2
    fbshipit-source-id: e8fbade53dadc9952343000bbeaa7017c7e1dec2

--- resources/travis/deploy.sh
+++ resources/travis/deploy.sh
@@ -3,7 +3,7 @@
 # make sure -x (debugging) is off so we don't print the token in the logs
 set +x
 
-PAGES_CHECKOUT="$HOME/gh-pages"
+PAGES_CHECKOUT="$HOME/flowtype.org"
 export BUNDLE_GEMFILE="${TRAVIS_BUILD_DIR}/website/Gemfile"
 
 printf "travis_fold:start:installing_ruby\nInstalling Ruby\n"
@@ -16,18 +16,8 @@ printf "travis_fold:start:installing_jekyll\nInstalling Jekyll\n"
 bundle install
 printf "travis_fold:end:installing_jekyll\n"
 
-printf "travis_fold:start:clone_gh_pages\nCloning gh-pages\n"
-mkdir -p "$PAGES_CHECKOUT"
-# pull the gh-pages branch from $REPO.
-# doesn't use `clone` to avoid writing the token to disk
-git -C "$PAGES_CHECKOUT" init
-git -C "$PAGES_CHECKOUT" checkout -b gh-pages
-git -C "$PAGES_CHECKOUT" config user.name "flow-bot"
-git -C "$PAGES_CHECKOUT" config user.email "flow-bot+docs@fb.com"
-git -C "$PAGES_CHECKOUT" pull -q --depth 20 "https://github.com/${TRAVIS_REPO_SLUG}.git" gh-pages
-printf "travis_fold:end:clone_gh_pages\n"
-
 printf "travis_fold:start:jekyll_build\nBuilding Jekyll site\n"
+mkdir -p "$PAGES_CHECKOUT"
 mkdir -p "website/_assets/gen"
 cp "bin/flow.js" "website/_assets/gen/flow.js"
 cp -r "lib" "website/static/flowlib"
@@ -35,12 +25,6 @@ env \
   PATH="${TRAVIS_BUILD_DIR}/bin:$PATH" \
   bundle exec jekyll build -s website/ -d "$PAGES_CHECKOUT" --verbose
 printf "travis_fold:end:jekyll_build\n"
-
-printf "travis_fold:start:push_gh_pages\nPushing gh-pages\n"
-git -C "$PAGES_CHECKOUT" add -f -A .
-git -C "$PAGES_CHECKOUT" commit -m "Updating to ${TRAVIS_REPO_SLUG}@${TRAVIS_COMMIT}" || :
-git -C "$PAGES_CHECKOUT" push -q "https://${GH_BOT_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" gh-pages 2>/dev/null
-printf "travis_fold:end:push_gh_pages\n"
 
 printf "travis_fold:start:push_s3\nPushing to S3\n"
 bundle exec s3_website push --config-dir=website/ --site="$PAGES_CHECKOUT"
