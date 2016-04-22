5cf1d7d957ddf02416e5fe48cc1dee3b386545f6
Marshall Roch <mroch@fb.com>
2016-04-22 16:54:46 -0700

    temporarily unbreak flow.js
    Summary: since we can't realpath in JS, the real fix is to stop calling this function (via `Path.make`). but for now, we'll just stub it to fail, and spew in the console.
    
    Reviewed By: avikchaudhuri
    
    Differential Revision: D3215554
    
    fb-gh-sync-id: 93c94adf838681efe703b38a56059123d4188bbb
    fbshipit-source-id: 93c94adf838681efe703b38a56059123d4188bbb

---
+++ js/realpath.js
@@ -0,0 +1,6 @@
+//Provides: hh_realpath
+//Requires: js_print_stderr
+function hh_realpath(path) {
+  js_print_stderr("hh_realpath called, but does nothing: " + path);
+  return 0; // `None`
+}
--- website/_assets/js/prelude.js
+++ website/_assets/js/prelude.js
@@ -5,7 +5,8 @@ require.config({
   paths: {
     'docsearch': 'https://cdn.jsdelivr.net/docsearch.js/1/docsearch.min',
     'typekit': 'https://use.typekit.net/vqa1hcx'
-  }
+  },
+  waitSeconds: 30
 })
 
 require(['typekit'], function() {
