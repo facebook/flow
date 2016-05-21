810b7ca87ec8021c2109b47198aaeca042011aab
Tim Popham <popham@uw.edu>
2016-05-20 17:01:33 -0700

    Remove module aliases from interface

--- src/typing/classy.mli
+++ src/typing/classy.mli
@@ -1,39 +1,34 @@
-module Ast = Spider_monkey_ast
-module Sig = Classy_sig
-
-open Reason_js
-
 type self_t = Type.t
 type tparams_map_t = Type.t SMap.t
-type expr_fn_t = Context.t -> tparams_map_t -> Ast.Expression.t -> Type.t
-type stmt_fn_t = Context.t -> tparams_map_t -> Ast.Statement.t list -> unit
+type expr_fn_t = Context.t -> tparams_map_t -> Spider_monkey_ast.Expression.t -> Type.t
+type stmt_fn_t = Context.t -> tparams_map_t -> Spider_monkey_ast.Statement.t list -> unit
 
 module Class : sig
-  type classy_ast_t = Ast.Class.t
-  val mk_sig: Context.t -> tparams_map_t -> expr_fn_t -> Loc.t -> reason ->
-    self_t -> classy_ast_t -> Sig.t
-  val classtype: Context.t -> Sig.t -> Type.t
-  val generate_tests: Context.t -> (Sig.t -> unit) -> Sig.t -> unit
-  val check_super: Context.t -> Sig.t -> unit
+  type classy_ast_t = Spider_monkey_ast.Class.t
+  val mk_sig: Context.t -> tparams_map_t -> expr_fn_t -> Loc.t -> Reason_js.t ->
+    self_t -> classy_ast_t -> Classy_sig.t
+  val classtype: Context.t -> Classy_sig.t -> Type.t
+  val generate_tests: Context.t -> (Classy_sig.t -> unit) -> Classy_sig.t -> unit
+  val check_super: Context.t -> Classy_sig.t -> unit
 end
 
 module Interface : sig
-  type classy_ast_t = Ast.Statement.Interface.t
-  val mk_sig: Context.t -> tparams_map_t -> expr_fn_t -> Loc.t -> reason ->
-    self_t -> classy_ast_t -> Sig.t
-  val classtype: Context.t -> Sig.t -> Type.t
-  val generate_tests: Context.t -> (Sig.t -> unit) -> Sig.t -> unit
-  val check_super: Context.t -> Sig.t -> unit
+  type classy_ast_t = Spider_monkey_ast.Statement.Interface.t
+  val mk_sig: Context.t -> tparams_map_t -> expr_fn_t -> Loc.t -> Reason_js.t ->
+    self_t -> classy_ast_t -> Classy_sig.t
+  val classtype: Context.t -> Classy_sig.t -> Type.t
+  val generate_tests: Context.t -> (Classy_sig.t -> unit) -> Classy_sig.t -> unit
+  val check_super: Context.t -> Classy_sig.t -> unit
 end
 
 module DeclClass : sig
-  type classy_ast_t = Ast.Statement.Interface.t
-  val mk_sig: Context.t -> tparams_map_t -> expr_fn_t -> Loc.t -> reason ->
-    self_t -> classy_ast_t -> Sig.t
-  val classtype: Context.t -> Sig.t -> Type.t
-  val generate_tests: Context.t -> (Sig.t -> unit) -> Sig.t -> unit
-  val check_super: Context.t -> Sig.t -> unit
+  type classy_ast_t = Spider_monkey_ast.Statement.Interface.t
+  val mk_sig: Context.t -> tparams_map_t -> expr_fn_t -> Loc.t -> Reason_js.t ->
+    self_t -> classy_ast_t -> Classy_sig.t
+  val classtype: Context.t -> Classy_sig.t -> Type.t
+  val generate_tests: Context.t -> (Classy_sig.t -> unit) -> Classy_sig.t -> unit
+  val check_super: Context.t -> Classy_sig.t -> unit
 end
 
 val toplevels: Context.t -> decls:stmt_fn_t -> stmts:stmt_fn_t ->
-  expr:expr_fn_t -> Sig.t -> unit
+  expr:expr_fn_t -> Classy_sig.t -> unit
