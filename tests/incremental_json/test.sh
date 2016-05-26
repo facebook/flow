15109bc4acaed2664e618f2cff1a00b66a8d8663
Marshall Roch <marshall@mroch.com>
2016-05-26 02:25:35 -0700

    Fix incremental_json test on travis
    Summary:
    not sure why these seem to be escaped in whatever shell travis is running, but can work around it without knowing exactly why
    Closes https://github.com/facebook/flow/pull/1848
    
    Differential Revision: D3352272
    
    Pulled By: mroch
    
    fbshipit-source-id: 77efc219e4086108ea321ba2db5aa45814b49b87

--- tests/incremental_json/test.sh
+++ tests/incremental_json/test.sh
@@ -5,12 +5,12 @@ printf "\nInitial status:\n"
 $FLOW status --no-auto-start --old-output-format .
 
 printf "\nCreate data.json:\n"
-cp data.json{1,}
+cp data.json1 data.json
 $FLOW force-recheck --no-auto-start test.js
 $FLOW status --no-auto-start . --old-output-format
 
 printf "\nModify data.json:\n"
-cp data.json{2,}
+cp data.json2 data.json
 $FLOW force-recheck --no-auto-start test.js
 $FLOW status --no-auto-start . --old-output-format
 
