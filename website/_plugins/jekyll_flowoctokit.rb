763f27e346b28befeb27a5d6c39ec7acc9600886
Marshall Roch <mroch@fb.com>
2016-05-26 14:22:33 -0700

    [blog] add Recent Releases section to TOC
    Summary: pulls the list of releases from the GitHub API automatically
    
    Reviewed By: gabelevi
    
    Differential Revision: D3354818
    
    fbshipit-source-id: c79daeb0535d591dce5207eb8fa9ee3c07899812

--- website/_plugins/jekyll_flowoctokit.rb
+++ website/_plugins/jekyll_flowoctokit.rb
@@ -2,20 +2,18 @@ require 'octokit'
 
 module Jekyll
   class FlowGitHubGenerator < Jekyll::Generator
-    def self.latest_release(repo)
+    def self.releases(repo)
       @releases ||= Hash.new do |hash, key|
         access_token = ENV['GH_BOT_TOKEN']
         authed = access_token.nil? ? "" : " (with auth)"
-        Jekyll.logger.info "GitHub Query:", "latest release for #{key}#{authed}"
+        Jekyll.logger.info "GitHub Query:", "releases for #{key}#{authed}"
         client = Octokit::Client.new(:access_token => access_token)
-        hash[key] = client.latest_release(key)
+        hash[key] = client.releases(key)
       end
       @releases[repo]
     end
 
-    def generate(site)
-      repo = site.config['repository']
-      release = self.class.latest_release(repo)
+    def map_release(repo, release)
       tag = release.tag_name
 
       # we generate these URLs manually instead of using
@@ -26,16 +24,28 @@ module Jekyll
       linux_binary = "https://github.com/#{repo}/releases/download/#{tag}/flow-linux64-#{tag}.zip"
       mac_binary = "https://github.com/#{repo}/releases/download/#{tag}/flow-osx-#{tag}.zip"
 
-      site.config['latest_release'] = {
-        'name' => release.name,
+      {
+        'name' => release.name.empty? ? release.tag_name : release.name,
         'tag_name' => release.tag_name,
         'linux_binary' => {
           'url' => linux_binary
         },
         'mac_binary' => {
           'url' => mac_binary
-        }
+        },
+        'published_at' => release.published_at,
+        'html_url' => release.html_url
       }
+    end
+
+    def generate(site)
+      repo = site.config['repository']
+      site.config['releases'] = self.class.releases(repo).reject {|release|
+        release.draft
+      }.map {|release|
+        map_release(repo, release)
+      }
+      site.config['latest_release'] = site.config['releases'][0]
     end
   end
 end
