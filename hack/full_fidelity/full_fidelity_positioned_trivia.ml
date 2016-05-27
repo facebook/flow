49d06650b02f750df7f6a6ff870d5dbd30514506
Eric Lippert <ericlippert@fb.com>
2016-05-27 11:07:35 -0700

    [hack] Full fidelity "trivia" nodes; whitespace, newlines, comments
    Summary:
    This diff includes the simplest data structures used in the full-fidelity parse tree:
    
    * A module to represent the abstract notion of source text
    * A module that helps map line/column offsets to absolute offsets and vice-versa
    * Modules to represeint "trivia" -- whitespace, newlines, comments.
    
    Source text is for now just a thin wrapper around a string and an offset map. Eventually we'll have more information in here, like file name, and so on.
    
    The offset map is a linear search; this is not very good. Working up a better data structure here is possibly a good task for one of our summer interns.
    
    As we will see in later diffs, my aim here is to provide various different implementations of abstractions such as "trivia", "token" and "syntax tree node".
    
    Each will have different performance characteristics and capabilities. For this initial set of diffs, there are three main capabilities:
    
    * "minimal" nodes are designed to be as small in memory as possible, with low burden to the memory allocator, and capable of being serialized into a small format.
    * "editable" nodes are designed for scenarios in which a syntax tree is being manipulated programmatically to construct new trees.
    * "positioned" nodes are heavyweight, and know their exact position in a source text.
    
    Minimal trivia nodes consist of a kind and a width; that's it. They don't know their own text or their position. As we'll see in later diffs, we can compute both by descending the parse tree.
    
    Editable trivia nodes consist of a kind and their text. They don't know their positions; indeed, as the tree is being edited, the positions of every node might change.
    
    Positioned nodes know their absolute offset, their width, and what source text they came from.
    
    Reviewed By: dabek
    
    Differential Revision: D3325553
    
    fbshipit-source-id: ea26204e4f5435c5bf78cdeea2e310d805bf4a4a

---
+++ hack/full_fidelity/full_fidelity_positioned_trivia.ml
@@ -0,0 +1,82 @@
+(**
+ * Copyright (c) 2016, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the "hack" directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ *)
+
+(**
+ * Positioned trivia
+ *
+ * A positioned trivia stores the original source text, the offset of the
+ * start of the trivia, and its width.  From all this information we can
+ * rapidly compute the absolute position of the trivia, or obtain its text.
+ *
+ *)
+
+module TriviaKind = Full_fidelity_trivia_kind
+module SourceText = Full_fidelity_source_text
+module MinimalTrivia = Full_fidelity_minimal_trivia
+
+type t = {
+  kind: TriviaKind.t;
+  source_text : SourceText.t;
+  offset : int;
+  width : int
+}
+
+let make_whitespace source_text offset width =
+  { kind = TriviaKind.WhiteSpace; source_text; offset; width }
+
+let make_eol source_text offset width =
+  { kind = TriviaKind.EndOfLine; source_text; offset; width }
+
+let make_single_line_comment source_text offset width =
+  { kind = TriviaKind.SingleLineComment; source_text; offset; width }
+
+let make_delimited_comment source_text offset width =
+  { kind = TriviaKind.DelimitedComment; source_text; offset; width }
+
+let width trivia =
+  trivia.width
+
+let kind trivia =
+  trivia.kind
+
+let start_offset trivia =
+  trivia.offset
+
+let end_offset trivia =
+  trivia.offset + trivia.width - 1
+
+let source_text trivia =
+  trivia.source_text
+
+let text trivia =
+  SourceText.sub (source_text trivia) (start_offset trivia) (width trivia)
+
+let start_position trivia =
+  SourceText.offset_to_position (source_text trivia) (start_offset trivia)
+
+let end_position trivia =
+  SourceText.offset_to_position (source_text trivia) (end_offset trivia)
+
+let from_minimal source_text minimal_trivia offset =
+  let width = MinimalTrivia.width minimal_trivia in
+  match MinimalTrivia.kind minimal_trivia with
+  | TriviaKind.WhiteSpace -> make_whitespace source_text offset width
+  | TriviaKind.EndOfLine -> make_eol source_text offset width
+  | TriviaKind.SingleLineComment -> make_single_line_comment source_text offset width
+  | TriviaKind.DelimitedComment -> make_delimited_comment source_text offset width
+
+let from_minimal_list source_text ts offset =
+  let rec aux acc ts offset =
+    match ts with
+    | [] -> acc
+    | h :: t ->
+      let et = from_minimal source_text h offset in
+      aux (et :: acc) t (offset + (MinimalTrivia.width h)) in
+  List.rev (aux [] ts offset)
