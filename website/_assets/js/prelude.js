3e84cf4c9fecc44bdd66558e8aad2f2febaba92e
Sam Goldman <samgoldman@fb.com>
2016-05-26 17:58:47 -0700

    Tweak website fonts
    Summary:
    The default monospace font has a really low x-height which made inline <code>
    elements look pretty terrible, especially in headings. I tried out a few
    different alternatives and Roboto Mono looks the best to me.
    
    <code> blocks had padding/letter spacing that made them look even weirder
    inline. I just pulled these. I don't think the code blocks suffer.
    
    We weren't actually using TypeKit since it was never configured for our domain.
    I pulled it out and replaced it with "Helvetica Neue", Helvetica, Arial,
    sans-serif.
    
    Subpixel smoothing looks bad on body text, but looks pretty great on code IMO.
    
    Reviewed By: mroch
    
    Differential Revision: D3355949
    
    fbshipit-source-id: 0543de7272270e1602ecd3a0c99835b70a6e6f32

--- website/_assets/js/prelude.js
+++ website/_assets/js/prelude.js
@@ -4,13 +4,6 @@ require.config({
   baseUrl: '/assets',
   paths: {
     'docsearch': 'https://cdn.jsdelivr.net/docsearch.js/1/docsearch.min',
-    'typekit': 'https://use.typekit.net/vqa1hcx'
   },
   waitSeconds: 30
 })
-
-require(['typekit'], function() {
-  try{
-    Typekit.load({ async: true });
-  } catch (e) {}
-});
