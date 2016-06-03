4f286c63ecec4096fe1389e0df333171485aea72
Kasper Kopec <kasper@fb.com>
2016-06-02 17:12:24 -0700

    [hack] Refactor external API of FindRefsService, FindRefsService.action
    Summary:
    I'm going to add support for (other than method) class members, and instead of doing
    
      Method of string * string
      Property of string * string
      Typeconst of string * string
    
    where the first string is always the class name I'm changing it to a
    
      Member of string * member
    
    and factoring out member type to separate variant.
    
    Reviewed By: hermanventer
    
    Differential Revision: D3351791
    
    fbshipit-source-id: 7c77ef6770c4d61d1e9dade63a135d687ea438ce

--- hack/stubs/ai.ml
+++ hack/stubs/ai.ml
@@ -51,18 +51,24 @@ module ServerFindDepFiles = struct
 end
 
 module ServerFindRefs = struct
+  type member =
+    | Method of string
+
   type action =
     | Class of string
-    | Method of string * string
+    | Member of string * member
     | Function of string
 
   let go _  _ _ = []
 end
 
 module TraceService = struct
+  type member =
+    | Method of string
+
   type action =
     | Class of string
-    | Method of string * string
+    | Member of string * member
     | Function of string
 
   let go _ _ _ _ = ""
