de5044048bfd9e58555ab9f7234c4fb9f3afbfc1
Vladimir Kurchatkin <vladimir.kurchatkin@gmail.com>
2016-08-19 14:26:53 +0300

    Fix accessing number literal properties on objects

--- src/typing/flow_js.ml
+++ src/typing/flow_js.ml
@@ -3636,7 +3636,8 @@ let rec __flow cx ((l: Type.t), (u: Type.use_t)) trace =
       ->
       rec_flow cx trace (key, ElemT(reason_op, l, tout, Read))
 
-    | (StrT (reason_x, Literal x), ElemT(reason_op, (ObjT _ as o), t, rw)) ->
+    | (StrT (reason_x, Literal x), ElemT(reason_op, (ObjT _ as o), t, rw))
+    | (NumT (reason_x, Literal (_, x)), ElemT(reason_op, (ObjT _ as o), t, rw)) ->
       let reason_x = replace_reason (spf "property `%s`" x) reason_x in
       let u = match rw with
       | Read -> GetPropT (reason_op, (reason_x, x), t)
--- tests/objects/objects.js
+++ tests/objects/objects.js
@@ -3,12 +3,14 @@
 var x : {'123': string, bar: string} = {'123': 'val', bar: 'bar'};
 (x.foo : string);     // error, key doesn't exist
 (x['foo'] : string);  // error, key doesn't exist
-(x[123] : boolean);   // TODO: use the number's value to error here
+(x[345] : string);  // error, key doesn't exist
+(x[123] : boolean);   // error, string !~> boolean
 (x.bar: boolean);     // error, string !~> boolean
 (x['123'] : boolean); // error, string !~> boolean
 x['123'] = false;     // error, boolean !~> string
-x[123] = false;       // TODO: use the number's value to error here
+x[123] = false;       // error, boolean !~> string
 x['foo'+'bar'] = 'derp'; // ok since we can't tell
+x[123] = 'foo'; // ok
 (x[`foo`]: string);   // error, key doesn't exist
 
 var y : {foo: string} = {foo: 'bar'};
