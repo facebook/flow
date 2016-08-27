296dbd503be5ef72862f752af29e9987e232b766
Avik Chaudhuri <avik@fb.com>
2016-08-26 17:36:46 -0700

    autocomplete override test
    Summary:
    Confirmed that the recent fix for get-def on overrides (D3781312) also
    applies to autocomplete, so a regression test is warranted.
    
    Namely, an autocomplete on `this.` in a subclass that overrides a method would
    return the type of the method in the superclass, which is not necessarily the
    same as the type of the method in the subclass (return subtyping being only one
    way they could be different).
    
    Reviewed By: samwgoldman
    
    Differential Revision: D3781628
    
    fbshipit-source-id: 4513d930118fca23a6a7b8d933c4ed0fd8b11dab

--- tests/autocomplete/test.sh
+++ tests/autocomplete/test.sh
@@ -59,3 +59,6 @@ $FLOW autocomplete --strip-root --json issue-1368.js 20 10 < issue-1368.js
 
 printf "if.js = "
 $FLOW autocomplete --strip-root --json if.js 3 7 < if.js
+
+printf "override.js = "
+$FLOW autocomplete --strip-root --json override.js 10 16 < override.js
