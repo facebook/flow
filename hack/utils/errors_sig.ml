d2faa40b07b3161f6573f8686e5161dd7bed8696
Oguz Ulgen <oulgen@fb.com>
2016-08-27 13:00:15 -0700

    [hack] Prevent ::class from being used as a static variable in enums and classes
    Summary:
    This diff prevents users from being able to create enums and class constants named class.
    Foo::class should always refer to the class name of Foo not a class constant in Foo
    
    Reviewed By: dlreeves
    
    Differential Revision: D3755703
    
    fbshipit-source-id: cc0fdac67b2e16ad3581058a20bc250429b56b00

--- hack/utils/errors_sig.ml
+++ hack/utils/errors_sig.ml
@@ -77,6 +77,7 @@ module type S = sig
   val dynamic_method_call : Pos.t -> unit
   val nullsafe_property_write_context : Pos.t -> unit
   val illegal_fun : Pos.t -> unit
+  val illegal_member_variable_class : Pos.t -> unit
   val illegal_meth_fun : Pos.t -> unit
   val illegal_inst_meth : Pos.t -> unit
   val illegal_meth_caller : Pos.t -> unit
