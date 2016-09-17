f2f5f92bdd97a1543856641847b807bb74366335
Oguz Ulgen <oulgen@fb.com>
2016-09-16 19:55:40 -0700

    [hack] Add/Pass saved state load type to load script
    Summary: Adding load script options to the local server config
    
    Reviewed By: dabek
    
    Differential Revision: D3860072
    
    fbshipit-source-id: 6168cb33d1892a954dec7e5d41d8f6ad2792bff6

---
+++ hack/stubs/loadScriptConfig.ml
@@ -0,0 +1,16 @@
+(**
+ * Copyright (c) 2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the "hack" directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ *)
+
+type t = unit
+
+let createLoadScriptConfig _ = ()
+let default = ()
+let saved_state_load_type_to_string _ = ""
+let saved_state_load_type_ _ = ()
