e8417c5a01b7c6859d5df8a39642f5643ab25ad3
Sam Goldman <samgoldman@fb.com>
2016-10-19 16:39:14 -0700

    Fix generator return method to account for refused return
    Summary:
    As jesseschalken noted in the linked GitHub issue, a generator function can
    refuse a forced return by catching an exception and proceeding to generate
    values. We can't know exactly what happens, so we need to be more conservative.
    
    Generator.prototype.return, instead of returning a definitely done iterator
    result with the given type, should return a possibly-unfinished IteratorResult
    object with a return type widened by the argument.
    
    Fixes https://github.com/facebook/flow/issues/2589
    
    Reviewed By: gabelevi
    
    Differential Revision: D4019496
    
    fbshipit-source-id: 3cf7639ef1c64def345f77e9febe56ac9a279982

--- tests/generators/return.js
+++ tests/generators/return.js
@@ -1,7 +1,21 @@
 function test1(gen: Generator<void, string, void>) {
   // You can pass whatever you like to return, it doesn't need to be related to
   // the Generator's return type
-  var ret = gen.return(123);
-  (ret.done: false); // Error true ~> false
-  (ret.value: 456); // Error 123 ~> 456
+  var ret = gen.return(0);
+  (ret.value: void); // error: string | number ~> void
+}
+
+// However, a generator can "refuse" the return by catching an exception and
+// yielding or returning internally.
+function *refuse_return() {
+  try {
+    yield 1;
+  } finally {
+    return 0;
+  }
+}
+var refuse_return_gen = refuse_return();
+var refuse_return_result = refuse_return_gen.return("string");
+if (refuse_return_result.done) {
+  (refuse_return_result.value: string); // error: number | void ~> string
 }
