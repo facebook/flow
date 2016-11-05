de26fc4b6b54edeec128d10c8922872bd0b920ca
Gabe Levi <gabe@fb.com>
2016-11-05 10:34:40 -0700

    Add `--quiet` flag to gen_flow_files_command test
    Summary:
    This test was failing sporadically, like so:
    
      ✗ flow:newtests/gen_flow_files_command-status - main 87.939s (FAIL)
      Found 24 suites
      Tests will be built in /tmp/flow/tests/836e935536
      Running 1 suites
      [✗] FAIL:  gen_flow_files_command (20/20 tests. 40 steps passed. 1 steps failed)
    
      Flushing results to disk...
      FAILED: suite gen_flow_files_command, test default class exports (5 of 20), step 2 of 2
      Actual stderr (+) didn't match expected stderr (-)
      + The flow server is not responding (3 retries remaining): [processing] -
      Possible next steps:
        Rerun the tests you just ran
          ./tool test --rerun 836e935536
        Rerun the tests that just failed
          ./tool test --rerun-failed 836e935536
        Record the tests that just failed
          ./tool record --rerun-failed 836e935536
    
    The problem was that the test asserts that there is no stderr spew, but if the
    machine running the test is overloaded, you might get some stderr about trouble
    connecting.
    
    Rather than turn off the stderr assertion, I'm just adding the `--quiet` flag.
    
    I aslo fixed some 80char lint warnings due to this change, and then re-recorded
    the test
    
    Reviewed By: samwgoldman
    
    Differential Revision: D4135473
    
    fbshipit-source-id: f01aa56ce0e877a3c14b32374342ed4227ef8547

--- newtests/gen_flow_files_command/test.js
+++ newtests/gen_flow_files_command/test.js
@@ -6,7 +6,7 @@ import {suite, test} from '../../tsrc/test/Tester';
 export default suite(({addFile, addFiles, flowCmd}) => [
   test('named class exports', [
     addFile('named_class_exports.js'),
-    flowCmd(['gen-flow-files', 'named_class_exports.js']).stdout(`
+    flowCmd(['gen-flow-files', '--quiet', 'named_class_exports.js']).stdout(`
       // @flow
 
       declare class Class0 {
@@ -33,84 +33,133 @@ export default suite(({addFile, addFiles, flowCmd}) => [
 
   test('named variable exports', [
     addFile('named_variable_exports.js'),
-    flowCmd(['gen-flow-files', 'named_variable_exports.js']).stderr('').stdout(`
-      // @flow
+    flowCmd(['gen-flow-files', '--quiet', 'named_variable_exports.js'])
+      .stderr('')
+      .stdout(
+        `
+          // @flow
 
-      declare class Class0 {
-      }
-      declare export var constExport: number;
-      declare export var letExport: number;
-      export type typeExport = number;
-      declare export var unexportedAlias: string;
-      declare export var unexportedNominal: Class0;
-      declare export var varExport: number;
-    `)
+          declare class Class0 {
+
+          }
+          declare export var constExport: number;
+          declare export var letExport: number;
+          export type typeExport = number;
+          declare export var unexportedAlias: string;
+          declare export var unexportedNominal: Class0;
+          declare export var varExport: number;
+
+
+        `,
+      )
   ]),
 
   test('named function exports', [
     addFile('named_function_exports.js'),
-    flowCmd(['gen-flow-files', 'named_function_exports.js']).stderr('').stdout(`
-      // @flow
+    flowCmd(['gen-flow-files', '--quiet', 'named_function_exports.js'])
+      .stderr('')
+      .stdout(
+        `
+          // @flow
 
-      declare export function mono(a: number, b: {c: number}): number;
-      declare export function poly<T: number, U: T, V: U>(a: V): number;
-    `)
+          declare export function mono(a: number, b: {c: number}): number;
+          declare export function poly<T: number, U: T, V: U>(a: V): number;
+
+
+
+        `,
+      )
   ]),
 
   test('named type exports', [
     addFile('named_type_exports.js'),
-    flowCmd(['gen-flow-files', 'named_type_exports.js']).stderr('').stdout(`
-      // @flow
+    flowCmd(['gen-flow-files', '--quiet', 'named_type_exports.js'])
+      .stderr('')
+      .stdout(
+        `
+          // @flow
 
-      export type T1 = number;
-      export type T2<U, V> = Array<U>;
-      declare module.exports: {};
-    `),
+          export type T1 = number;
+          export type T2<U, V> = Array<U>;
+
+
+          declare module.exports: {};
+
+        `,
+      ),
   ]),
 
   test('default class exports', [
     addFile('default_class_export.js'),
-    flowCmd(['gen-flow-files', 'default_class_export.js']).stderr('').stdout(`
-      // @flow
+    flowCmd(['gen-flow-files', '--quiet', 'default_class_export.js'])
+      .stderr('')
+      .stdout(
+        `
+          // @flow
 
-      declare export class Base<A, B, C> {
-      }
-      declare export default class<A, B> extends Base<A, B, mixed> {
-        p: number;
-      }
-    `),
+          declare export class Base<A, B, C> {
+
+          }
+
+          declare export default class<A, B> extends Base<A, B, mixed> {
+
+            p: number;
+          }
+
+
+
+        `,
+      ),
   ]),
 
   test('default variable exports', [
     addFile('default_variable_exports.js'),
-    flowCmd(['gen-flow-files', 'default_variable_exports.js']).stderr('').stdout(`
-      // @flow
+    flowCmd(['gen-flow-files', '--quiet', 'default_variable_exports.js'])
+      .stderr('')
+      .stdout(
+        `
+          // @flow
 
-      declare export default number;
-      declare export var str: string;
-    `),
+          declare export default number;
+          declare export var str: string;
+
+
+        `,
+      ),
   ]),
 
   test('default function exports', [
     addFile('default_function_exports.js'),
-    flowCmd(['gen-flow-files', 'default_function_exports.js']).stderr('').stdout(`
-      // @flow
+    flowCmd(['gen-flow-files', '--quiet', 'default_function_exports.js'])
+      .stderr('')
+      .stdout(
+        `
+          // @flow
 
-      declare export default function<T: number, U: T, V: U>(a: V): number;
-      declare export function mono(a: number, b: {c: number}): number;
-    `),
+          declare export default function<T: number, U: T, V: U>(a: V): number;
+
+          declare export function mono(a: number, b: {c: number}): number;
+
+
+        `,
+      ),
   ]),
 
   test('non-@flow files', [
     addFile('non_flow_file.js'),
-    flowCmd(['gen-flow-files', '--strip-root', 'non_flow_file.js']).stderr('').stdout(`
-      // This file does not have an @flow at the top!
-    `),
+    flowCmd(['gen-flow-files', '--quiet', '--strip-root', 'non_flow_file.js'])
+      .stderr('')
+      .stdout(
+        `
+          // This file does not have an @flow at the top!
+
+        `,
+      ),
   ]),
 
   test('type errors halt and print to stderr', [
     addFile('type_error.js'),
-    flowCmd(['gen-flow-files', 'type_error.js']).stdout('').stderr(`
+    flowCmd(['gen-flow-files', '--quiet', 'type_error.js']).stdout('').stderr(`
       type_error.js:3
         3: export var a: string = 42;
                                   ^^ number. This type is incompatible with
@@ -124,71 +173,107 @@ export default suite(({addFile, addFiles, flowCmd}) => [
   test('imported class types arent redefined', [
     addFile('named_class_exports.js'),
     addFile('export_imported_type.js'),
-    flowCmd(['gen-flow-files', 'export_imported_type.js']).stderr('').stdout(`
-      // @flow
+    flowCmd(['gen-flow-files', '--quiet', 'export_imported_type.js'])
+      .stderr('')
+      .stdout(
+        `
+          // @flow
 
-      import {Child} from "./named_class_exports";
-      declare export var a: Child<number, string>;
-    `)
+          import {Child} from "./named_class_exports";
+          declare export var a: Child<number, string>;
+
+
+        `,
+      )
   ]),
 
   test('builtin class types arent redefined', [
     addFile('exports_builtins.js'),
-    flowCmd(['gen-flow-files', 'exports_builtins.js']).stderr('').stdout(`
-      // @flow
+    flowCmd(['gen-flow-files', '--quiet', 'exports_builtins.js'])
+      .stderr('')
+      .stdout(
+        `
+          // @flow
 
-      declare export function fn(a: Array<number>): void;
-    `)
+          declare export function fn(a: Array<number>): void;
+
+
+        `,
+      )
   ]),
 
   test('suppressed type errors get normalized', [
     addFile('suppressions.js'),
-    flowCmd(['gen-flow-files', 'suppressions.js']).stderr('').stdout(`
-      // @flow
+    flowCmd(['gen-flow-files', '--quiet', 'suppressions.js'])
+      .stderr('')
+      .stdout(
+        `
+          // @flow
 
-      declare export function fn(): number;
-    `)
+          declare export function fn(): number;
+
+
+        `,
+      )
   ]),
 
   test('literal types respect polarity', [
     addFile('literal_types.js'),
-    flowCmd(['gen-flow-files', 'literal_types.js']).stderr('').stdout(`
-      // @flow
+    flowCmd(['gen-flow-files', '--quiet', 'literal_types.js'])
+      .stderr('')
+      .stdout(
+        `
+          // @flow
 
-      declare export function f1(p: number): string;
-      declare export function f2(p: 42): "asdf";
-      declare export var varBool: boolean;
-      declare export var varBoolLiteral: true;
-      declare export var varNum: number;
-      declare export var varNumLiteral: 42;
-      declare export var varStr: string;
-      declare export var varStrLiteral: "asdf";
-    `)
+          declare export function f1(p: number): string;
+          declare export function f2(p: 42): "asdf";
+          declare export var varBool: boolean;
+          declare export var varBoolLiteral: true;
+          declare export var varNum: number;
+          declare export var varNumLiteral: 42;
+          declare export var varStr: string;
+          declare export var varStrLiteral: "asdf";
+
+
+        `,
+      )
   ]),
 
   test('optional types', [
     addFile('optional_types.js'),
-    flowCmd(['gen-flow-files', 'optional_types.js']).stderr('').stdout(`
-      // @flow
+    flowCmd(['gen-flow-files', '--quiet', 'optional_types.js'])
+      .stderr('')
+      .stdout(
+        `
+          // @flow
 
-      declare export var obj: {b?: number};
-      declare export function optFunc(p?: number): void | number;
-      declare export var optNum: void | number;
-    `)
+          declare export var obj: {b?: number};
+          declare export function optFunc(p?: number): void | number;
+          declare export var optNum: void | number;
+
+
+        `,
+      )
   ]),
 
   test('object types', [
     addFile('object_types.js'),
-    flowCmd(['gen-flow-files', 'object_types.js']).stderr('').stdout(`
-      // @flow
+    flowCmd(['gen-flow-files', '--quiet', 'object_types.js'])
+      .stderr('')
+      .stdout(
+        `
+          // @flow
 
-      declare export var dict: {[key: string]: string};
-      declare export var dictWithProps: {p1: string, [key: string]: number};
-      declare export var emptyObj: {};
-      declare export var multiProp: {p1: number, p2: number};
-      declare export var nestedObject: {p1: {p2: number}};
-      declare export var singleProp: {p1: number};
-    `)
+          declare export var dict: {[key: string]: string};
+          declare export var dictWithProps: {p1: string, [key: string]: number};
+          declare export var emptyObj: {};
+          declare export var multiProp: {p1: number, p2: number};
+          declare export var nestedObject: {p1: {p2: number}};
+          declare export var singleProp: {p1: number};
+
+
+        `,
+      )
   ]),
 
   /**
@@ -203,6 +288,7 @@ export default suite(({addFile, addFiles, flowCmd}) => [
     ),
     flowCmd([
       'gen-flow-files',
+      '--quiet',
       '--strip-root',
       './test_project/src/main.js',
       '--out-dir=./test_project/dist'
@@ -219,6 +305,7 @@ export default suite(({addFile, addFiles, flowCmd}) => [
     ),
     flowCmd([
       'gen-flow-files',
+      '--quiet',
       '--strip-root',
       './test_project/src/lib/utils.js',
       '--out-dir=./test_project/dist'
@@ -234,6 +321,7 @@ export default suite(({addFile, addFiles, flowCmd}) => [
     ),
     flowCmd([
       'gen-flow-files',
+      '--quiet',
       '--strip-root',
       './test_project/src',
     ]).stdout('').stderr(`
@@ -249,6 +337,7 @@ export default suite(({addFile, addFiles, flowCmd}) => [
     ),
     flowCmd([
       'gen-flow-files',
+      '--quiet',
       '--strip-root',
       './test_project/src',
       '--out-dir=./test_project/dist',
@@ -269,6 +358,7 @@ export default suite(({addFile, addFiles, flowCmd}) => [
     ),
     flowCmd([
       'gen-flow-files',
+      '--quiet',
       '--strip-root',
       './test_project/src',
       '--out-dir=./test_project/dist',
