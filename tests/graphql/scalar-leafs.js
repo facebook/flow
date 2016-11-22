9396fa79de82ae76afec81c7713f4a2253937259
Pavel Volokitin <pavelvolokitin@gmail.com>
2016-11-22 11:46:04 +0100

    [graphql] Port scalar-leafs tests from graphql-js.

---
+++ tests/graphql/scalar-leafs.js
@@ -0,0 +1,64 @@
+// @flow
+
+// valid scalar selection
+gql`
+  fragment scalarSelection on Dog {
+    barks
+  }
+`;
+
+// object type missing selection
+gql`
+  query directQueryOnObjectWithoutSubFields {
+    human # error
+  }
+`;
+
+// interface type missing selection
+gql`
+  {
+    human { pets } # error
+  }
+`;
+
+// valid scalar selection with args
+gql`
+  fragment scalarSelectionWithArgs on Dog {
+    doesKnowCommand(dogCommand: SIT)
+  }
+`;
+
+// scalar selection not allowed on Boolean
+gql`
+  fragment scalarSelectionsNotAllowedOnBoolean on Dog {
+    barks { sinceWhen } # error
+  }
+`;
+
+// scalar selection not allowed on Enum
+gql`
+  fragment scalarSelectionsNotAllowedOnEnum on Cat {
+    furColor { inHexdec } # error
+  }
+`;
+
+// scalar selection not allowed with args
+gql`
+  fragment scalarSelectionsNotAllowedWithArgs on Dog {
+    doesKnowCommand(dogCommand: SIT) { sinceWhen } # error
+  }
+`;
+
+// Scalar selection not allowed with directives
+gql`
+  fragment scalarSelectionsNotAllowedWithDirectives on Dog {
+    name @include(if: true) { isAlsoHumanName } # error
+  }
+`;
+
+// Scalar selection not allowed with directives and args
+gql`
+  fragment scalarSelectionsNotAllowedWithDirectivesAndArgs on Dog {
+    doesKnowCommand(dogCommand: SIT) @include(if: true) { sinceWhen } # error
+  }
+`;
