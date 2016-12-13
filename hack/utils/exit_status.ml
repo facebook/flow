ab146b30bebf93ee25ed95b822f9f7587dacafe5
Oguz Ulgen <oulgen@fb.com>
2016-12-13 09:24:08 -0800

    [hack] Factor out history from the local type in order to prepare for flow typing of locals with continuations
    Summary:
    Factor out history from the local type in order to prepare for flow typing of locals with continuations
    Eventually, we will remove the history since continuation based flow typing will deal with it. This is a preliminary step to change the current architecture
    
    Reviewed By: andrewjkennedy
    
    Differential Revision: D4314936
    
    fbshipit-source-id: 77293dbafe08b7e00c858041d3b0c0b1a7d4c2ec

--- hack/utils/exit_status.ml
+++ hack/utils/exit_status.ml
@@ -67,6 +67,7 @@ type t =
   | Parser_heap_build_error
   | Heap_full
   | Sql_assertion_failure
+  | Local_type_env_stale
 
 exception Exit_with of t
 
@@ -124,6 +125,7 @@ let exit_code = function
   | Parser_heap_build_error ->      210
   | File_heap_stale ->              211
   | Sql_assertion_failure ->        212
+  | Local_type_env_stale ->         213
 
 
 let exit t =
@@ -185,6 +187,7 @@ let to_string = function
   | Heap_full -> "Heap_full"
   | File_heap_stale -> "File_heap_stale"
   | Sql_assertion_failure -> "Sql_assertion_failure"
+  | Local_type_env_stale -> "Local_type_env_stale"
 
 
 let unpack = function
