3053cd0ba81129a470a866c3298301b1a10ebd78
Eric Shrewsberry <eshrews@fb.com>
2016-12-12 23:07:29 -0800

    [hack] v0.3.1a - Restructure the hackfmt binary to accept text from stdin
    Summary: TSIA
    
    Reviewed By: dabek
    
    Differential Revision: D4283484
    
    fbshipit-source-id: fbb2aebe99bbc9910f94a6a4139ed7e04a625d23

--- hack/hackfmt.ml
+++ hack/hackfmt.ml
@@ -12,21 +12,58 @@ module SyntaxError = Full_fidelity_syntax_error
 module SyntaxTree = Full_fidelity_syntax_tree
 module SourceText = Full_fidelity_source_text
 
-let usage = Printf.sprintf "Usage: %s filename\n" Sys.argv.(0)
+let usage = Printf.sprintf
+  "Usage: %s [--range s e] [filename or read from stdin]\n" Sys.argv.(0)
 
-let parse_and_print filename =
-  let file = Relative_path.create Relative_path.Dummy filename in
-  let source_text = SourceText.from_file file in
+let parse_and_print (source_text, debug) =
   let syntax_tree = SyntaxTree.make source_text in
-  let str = Debug.dump_full_fidelity syntax_tree in
 
-  Printf.printf "%s\n" (SourceText.get_text source_text);
-  Printf.printf "%s\n" str;
+  if debug then begin
+    let str = Debug.dump_full_fidelity syntax_tree in
+    Printf.printf "%s\n" (SourceText.get_text source_text);
+    Printf.printf "%s\n" str;
+  end;
   let editable = Full_fidelity_editable_syntax.from_tree syntax_tree in
-  let chunk_groups = Hack_format.run ~debug:true editable in
+  let chunk_groups = Hack_format.run ~debug editable in
   let formatted_string = Line_splitter.solve chunk_groups in
-  Printf.printf ("Formatting result:\n%s") formatted_string;
+  Printf.printf "%s" formatted_string;
   ()
 
-let () =
-  Arg.parse [] parse_and_print usage;
+let read_stdin () =
+  let buf = Buffer.create 256 in
+  try
+    while true do
+      Buffer.add_string buf (read_line());
+      Buffer.add_char buf '\n';
+    done;
+    assert false
+  with End_of_file ->
+    Buffer.contents buf
+
+let parse_options () =
+  let filename = ref None in
+  let start_char = ref None in
+  let end_char = ref None in
+  let debug = ref false in
+
+  let options = [
+    "--debug",
+      Arg.Unit (fun () -> debug := true),
+      " Print debug statements";
+    "--range",
+      Arg.Tuple ([
+        Arg.Int (fun x -> start_char := Some x);
+        Arg.Int (fun x -> end_char := Some x);
+      ]),
+      "<start end> Range of character positions to be formatted";
+  ] in
+  Arg.parse options (fun fn -> filename := Some fn) usage;
+  let source_text = match !filename with
+    | Some fn ->
+      SourceText.from_file @@ Relative_path.create Relative_path.Dummy fn
+    | None ->
+      SourceText.make @@ read_stdin ()
+  in
+  source_text, !debug
+
+let () = parse_and_print @@ parse_options ()
