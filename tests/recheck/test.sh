264cbac6294ab6483266e09fc768c973210d8006
Avik Chaudhuri <avik@fb.com>
2016-12-14 10:55:41 -0800

    hash literal types
    Summary: We were not hashing literal types in signatures, which caused missed rechecks.
    
    Reviewed By: mroch
    
    Differential Revision: D4325278
    
    fbshipit-source-id: fa19bcf33b3b1a319249a2674a817f0ca00bfe19

--- tests/recheck/test.sh
+++ tests/recheck/test.sh
@@ -1,6 +1,8 @@
 FLOW=$1
 mkdir tmp
 cp *.js tmp/
+
+printf "\nTest a:\n"
 $FLOW status .
 cp tmp1a/a1.js ./
 $FLOW force-recheck a1.js
@@ -8,20 +10,43 @@ $FLOW status .
 cp tmp2a/a1.js ./
 $FLOW force-recheck a1.js
 $FLOW status .
+rm a*.js
+
+printf "\nTest b:\n"
 cp tmp1b/b1.js ./
 $FLOW force-recheck b1.js
 $FLOW status .
 cp tmp2b/b0.js ./
 $FLOW force-recheck b0.js
 $FLOW status .
+rm b*.js
+
+printf "\nTest c:\n"
 cp tmp1c/c2.js ./
 $FLOW force-recheck c2.js
 $FLOW status .
 cp tmp2c/c1.js ./
 $FLOW force-recheck c1.js
 $FLOW status .
+rm c*.js
+
+printf "\nTest d:\n"
 cp tmp1d/d1.js ./
 $FLOW force-recheck d1.js
 $FLOW status .
+rm d*.js
+
+printf "\nTest e:\n"
+cp tmp1e/e2.js ./
+$FLOW force-recheck e2.js
+$FLOW status .
+cp tmp2e/e1.js ./
+$FLOW force-recheck e1.js
+$FLOW status .
+cp tmp3e/e1.js tmp3e/e2.js ./
+$FLOW force-recheck e1.js e2.js
+$FLOW status .
+rm e*.js
+
 mv tmp/*.js ./
 rmdir tmp
