a3435ec36a9009db10fba8c72e4dae9fe5074bf9
Seb Patane <seb@libero.com.au>
2016-12-14 17:39:38 +1000

    Mark defaultProps as covariant * This is so we don't have to include a dummy setter to satisfy Flow when using a getter to dynamically construct defaultProps

---
+++ tests/react_defaultProps/test.js
@@ -0,0 +1,35 @@
+/* @flow */
+
+import React from 'react';
+
+type DefaultProps = {
+    foo: number
+}
+
+class OnlyGetter extends React.Component {
+    static get defaultProps(): DefaultProps {
+        return {
+            foo: 3
+        }
+    }
+}
+
+
+class GetterAndSetter extends React.Component {
+    static set defaultProps(props: DefaultProps) {
+        // do nothing
+    }
+
+    static get defaultProps(): DefaultProps {
+        return {
+            foo: 3
+        }
+    }
+}
+
+
+class StaticDefaultProps extends React.Component {
+    static defaultProps: DefaultProps = {
+        foo: 3
+    }
+}
