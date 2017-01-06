9fe46d2300365ec6888ca1180a346baa63724eec
Gabe Levi <gabe@fb.com>
2017-01-06 08:42:51 -0800

    Upgrade ./tool convert script
    Summary:
    `./tool convert` converts tests/ to newtests/. After conversion, it
    runs some sanity checks. These sanity checks used to be pretty primitive, since
    tests/ used to use the old output format in .exp files. Now that both tests/
    and newtests/ use the new format, it's easier to compare. So now convert will
    make sure the newtests/ version has the exact same output as the tests/
    version.
    
    I also had to make some changes to the pretty printing of flow errors in
    `./tool` to match some changes made to Flow. It doesn't perfectly mirror Flow,
    due to some info that is not reflected in the JSON output, but it's pretty
    close!
    
    `./tool convert` will also attempt to set `no_flowlib` for every converted
    test, to let that test run faster. If the sanity check fails, then it will set
    `no_flowlib=false`.
    
    Reviewed By: jeffmo
    
    Differential Revision: D4366741
    
    fbshipit-source-id: d50ba3e769f122ae74989ea1483c71d6246923ef

--- tsrc/convert/convertRunner.js
+++ tsrc/convert/convertRunner.js
@@ -4,13 +4,32 @@ import colors from 'colors/safe';
 import {basename, dirname, extname, join, normalize, relative} from 'path';
 import {format} from 'util';
 
-import {glob, mkdirp, ncp, readdir, readFile, rename, rimraf, unlink, writeFile} from './../async';
+import {
+  exists,
+  glob,
+  mkdirp,
+  ncp,
+  readdir,
+  readFile,
+  rename,
+  rimraf,
+  unlink,
+  writeFile,
+} from './../async';
 import runRecord from '../record/recordRunner';
 import {getTestsDir, defaultFlowConfigName} from '../constants';
+
+import simpleDiffAssertion from '../test/assertions/simpleDiffAssertion';
 
 import type {Args} from './convertCommand';
 
 async function shouldConvert(source, log? = () => {}) {
+  const source_exists = await exists(source);
+  if (!source_exists) {
+    log("'%s' doesn't exist", source);
+    return false;
+  }
+
   const contents = await readdir(source);
   if (contents.find(filename => filename == ".testconfig")) {
     log("Sorry, can't convert legacy tests that use .testconfig");
@@ -44,26 +63,31 @@ async function shouldConvert(source, log? = () => {}) {
   return true;
 }
 
+function logWithSource(source: string, ...args: any) {
+  const padding = Array(Math.max(25 - source.length, 2)).join(" ");
+  console.log("[%s]%s%s", source, padding, format(...args));
+}
+
 async function convert(
+  no_flowlib: "true" | "false",
   results: {[key: string]: ?string},
   source: string,
 ): Promise<?string> {
   results[source] = null;
-  function log(...args: any) {
-    console.log("[%s]\t\t%s", source, format(...args));
-  }
+  const log = logWithSource.bind(null, source);
 
   const suiteName = join("legacy", basename(source));
   const dest = join(getTestsDir(), suiteName);
 
-  log("Removing directory `%s`", dest);
-  await rimraf(dest);
   log("Converting test to `%s`", dest);
-
   const goodToGo = await shouldConvert(source, log);
   if (!goodToGo) {
     return;
   }
+
+  log("Removing directory `%s`", dest);
+  await rimraf(dest);
+
 
   log("Creating directory `%s`", dest);
   await mkdirp(dest);
@@ -91,12 +115,12 @@ async function convert(
       file,
       fileContents
         .replace(/@flow/, "@thisWillBeFlowInTest")
-        .replace(/[\/\\]test(['"])/g, "/legacy_test$1")
-        .replace(/[\/\\]test\.js/, "/legacy_test.js"),
+        .replace(/[\/\\]test(['"])/g, "/test\.legacy$1")
+        .replace(/[\/\\]test\.js/, "/test\.legacy.js"),
     );
 
     if (basename(file) == "test.js") {
-      const new_file_name = join(dirname(file), "legacy_test.js");
+      const new_file_name = join(dirname(file), "test.legacy.js");
       converted_files.push(relative(dest, new_file_name));
       await rename(file, new_file_name);
     } else if (basename(file) == ".flowconfig") {
@@ -104,9 +128,16 @@ async function convert(
       let newConfig = config.toString();
       // Old tests ran flow check --all, so automatically add that option
       if (newConfig.match(/\[options\]/)) {
-        newConfig = newConfig.replace(/\[options\]/, "[options]\nall=true\n");
+        newConfig = newConfig.replace(
+          /\[options\]/,
+          format("[options]\nall=true\nno_flowlib=%s\n", no_flowlib),
+        );
       } else {
-        newConfig = newConfig + "\n[options]\nall=true\n";
+        newConfig = format(
+          "%s\n[options]\nall=true\nno_flowlib=%s\n",
+          newConfig,
+          no_flowlib,
+        );
       }
       // Rename .flowconfig to _flowconfig. This will be automatically included
       // in the test.
@@ -145,16 +176,21 @@ export default suite(({addFiles}) => [
 async function sanityCheck(sourceToSuiteMap) {
   console.log("Running sanity checks...");
   let passed = 0;
-  let failed = 0;
+  let failed = new Set();
   let skipped = 0;
   for (const source in sourceToSuiteMap) {
     const suiteName = sourceToSuiteMap[source];
     if (suiteName == null ) {
-      console.log("%s: %s", colors.yellow.bold("[-] SKIP"), colors.blue(source));
+      console.log(
+        "%s: %s",
+        colors.yellow.bold("[-] SKIP"),
+        colors.blue(source),
+      );
       skipped++;
       continue;
     }
-    const expfile = join(source, suiteName.replace(/legacy[\/\\](.*)/, "$1.exp"));
+    const expfile =
+      join(source, suiteName.replace(/legacy[\/\\](.*)/, "$1.exp"));
     const [exp, testFileContentsBuffer] = await Promise.all([
       readFile(expfile),
       readFile(join(getTestsDir(), suiteName, "test.js")),
@@ -164,69 +200,144 @@ async function sanityCheck(sourceToSuiteMap) {
     if (!matches) {
       console.log(
         "%s: %s %s",
-        colors.red.bold("[✗] FAIL"),
+        colors.red.bold("[\u{2717}] FAIL"),
         colors.blue(suiteName),
         format("Couldn't find `Found _ errors` in %s", expfile),
       );
-      failed++;
+      failed.add(suiteName);
       continue;
     }
     const numErrors = Number(matches[1]);
-    if (testFileContents.match(/noNewErrors\(\)/) && numErrors != 0) {
-      console.log(
-        "%s: %s %s",
-        colors.red.bold("[✗] FAIL"),
-        colors.blue(suiteName),
-        format("%s says %d errors, but %s uses noNewErrors()", expfile, numErrors, suiteName),
-      );
-      failed++;
-      continue;
-    } else if (matches = testFileContents.match(/newErrors\(\n`((.|[\n])*)`,\n\)/)) {
-      const numNewErrors =
-        matches[1]
-          .split("\n\n")
-          .length;
-      if (numNewErrors != numErrors) {
+    if (numErrors === 0) {
+      if (testFileContents.match(/noNewErrors\(\)/) == null) {
         console.log(
           "%s: %s %s",
-          colors.red.bold("[✗] FAIL"),
+          colors.red.bold("[\u{2717}] FAIL"),
           colors.blue(suiteName),
-          format("%s says %d errors, but %s has %d errors", expfile, numErrors, suiteName, numNewErrors),
+          format(
+            "%s says 0 errors, but %s doesn't use noNewErrors()",
+            expfile,
+            suiteName,
+          ),
         );
-        failed++;
-        continue
+        failed.add(suiteName);
+        continue;
+      }
+    } else {
+      matches = testFileContents.match(/newErrors\(\n *`((.|[\n])*)`,\n *\)/);
+      if (matches == null) {
+        console.log(
+          "%s: %s %s",
+          colors.red.bold("[\u{2717}] FAIL"),
+          colors.blue(suiteName),
+          format(
+            "%s says %d errors, but %s doesn't use newErrors()",
+            expfile,
+            numErrors,
+            suiteName,
+          ),
+        );
+        failed.add(suiteName);
+        continue;
+      } else {
+        // There are a few things we need to do to make the old test output match
+        // * Remove the "Found N errors" message
+        // * Rename <BUILTINS>/ to [LIB] (which is what the JSON outputs)
+        // * Escape things for template strings
+        // * Do the test -> test.legacy rename
+        const actual = matches[1]
+          .replace(/test\.legacy/g, "test")
+          .split("\n")
+          .map(s => s.trim());
+        const expected = exp.toString()
+          .replace(/Found .* errors?/, "")
+          .replace(/<BUILTINS>\//g, "[LIB] ")
+          .replace(/See lib: ([^[])/g, 'See lib: [LIB] $1')
+          .replace(/`/g, '\\`')
+          .replace(/\$/g, '\\$')
+          .split("\n")
+          .map(s => s.trim());
+        let doesNotMatch = false;
+
+        for (let eidx = 0, aidx = 0; eidx < expected.length && aidx < actual.length;) {
+          if (expected[eidx] === "") {
+            eidx++;
+            continue;
+          }
+          if (actual[aidx] === "") {
+            aidx++;
+            continue;
+          }
+          if (actual[aidx] != expected[eidx]) {
+            console.log(
+              "%s: %s %s",
+              colors.red.bold("[\u{2717}] FAIL"),
+              colors.blue(suiteName),
+              format(
+                "Line %d of %s doesn't seem to match the recorded test.js\n"+
+                  "Expected `%s`\nActual `%s`",
+                eidx+1,
+                expfile,
+                expected[eidx],
+                actual[aidx],
+              ),
+            );
+            failed.add(suiteName);
+            doesNotMatch = true;
+            break;
+          }
+          eidx++;
+          aidx++;
+        }
+        if (doesNotMatch === true) {
+          continue;
+        }
       }
     }
-    console.log("%s: %s", colors.green.bold("[✓] PASS"), colors.blue(suiteName));
+    console.log("%s: %s", colors.green.bold("[\u{2713}] PASS"), colors.blue(suiteName));
     passed++;
   }
   console.log(
     colors.green("%d passed") + ", " + colors.red("%d failed") + ", " + colors.yellow("%d skipped"),
     passed,
-    failed,
+    failed.size,
     skipped
   );
+
+  return failed;
 }
+
+async function convertAll(
+  args: Args,
+  no_flowlib: "true" | "false",
+  dirs: Array<string>,
+  sourceToSuiteMap: {[key: string]: ?string},
+): Promise<void> {
+  console.log("Converting with no_flowlib=%s", no_flowlib);
+  const converted: Array<?string> = await Promise.all(
+    dirs.map(convert.bind(null, no_flowlib, sourceToSuiteMap))
+  );
+  const suites = new Set();
+  converted.forEach(suiteName => suiteName != null && suites.add(suiteName));
+
+  await runRecord({
+    suites,
+    bin: args.bin,
+    parallelism: args.parallelism,
+    errorCheckCommand: args.errorCheckCommand,
+    rerun: null,
+  });
+}
+
 
 export default async function(args: Args): Promise<void> {
   const sourceToSuiteMap = {};
+  const suiteToSourceMap = {};
 
   const dirs = Array.from(args.dirs).map(dir => normalize(dir.trim()));
 
   if (!args.sanity) {
-    const converted: Array<?string> = await Promise.all(
-      dirs.map(convert.bind(null, sourceToSuiteMap))
-    );
-    const suites = new Set();
-    converted.forEach(suiteName => suiteName != null && suites.add(suiteName));
-
-    await runRecord({
-      suites,
-      bin: args.bin,
-      parallelism: args.parallelism,
-      errorCheckCommand: args.errorCheckCommand,
-      rerun: null,
-    });
+    await convertAll(args, "true", dirs, sourceToSuiteMap);
   } else {
     // Fake like we generate the tests
     console.log("Skipping straight to the sanity check due to `--sanity` flag");
@@ -236,5 +347,39 @@ export default async function(args: Args): Promise<void> {
     }
   }
 
-  await sanityCheck(sourceToSuiteMap);
+  for (const dir in sourceToSuiteMap) {
+    suiteToSourceMap[sourceToSuiteMap[dir]] = dir;
+  }
+
+  let failed = await sanityCheck(sourceToSuiteMap);
+
+  if (!args.sanity && failed.size > 0) {
+    console.log("Trying again...");
+
+    const failed_dirs = Array.from(failed)
+      .map(suiteName => suiteToSourceMap[suiteName]);
+
+    await convertAll(args, "false", failed_dirs, sourceToSuiteMap);
+    failed = await sanityCheck(sourceToSuiteMap);
+  }
+
+  if (!args.sanity) {
+    await Promise.all(
+      dirs.map(async (dir) => {
+        const suiteName = sourceToSuiteMap[dir];
+        if (suiteName == null) {
+          logWithSource(dir, "Convert skipped");
+        } else {
+          if (failed.has(suiteName)) {
+            const dest = join(getTestsDir(), suiteName);
+            logWithSource(dir, "Convert failed, removing dest `%s`", dest);
+            return rimraf(dest);
+          } else {
+            logWithSource(dir, "Convert succeeded, removing source `%s`", dir);
+            await rimraf(dir);
+          }
+        }
+      }),
+    );
+  }
 }
