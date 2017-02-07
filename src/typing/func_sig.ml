d3887c226901cee0fad4d6058651c74e46bc3c14
Jon Bretman <jon.bretman@lyst.com>
2017-02-07 14:29:15 -0800

    [PR] Fix for class method default parameters that use `this`
    Summary:
    Example of bug - https://flowtype.org/try/#0PQKgBAAgZgNg9gdzCYAoVBjGBDAzrsAQTAG9UwxyxsAuMXAFwCcBLAOwHMqqAjACgxgAvGAYALFrgB02AJSkqFKgF9uyoA
    
    Fixes #3353
    Fixes #1234
    
    Closes https://github.com/facebook/flow/pull/3334
    
    Reviewed By: samwgoldman
    
    Differential Revision: D4523083
    
    Pulled By: jeffmo
    
    fbshipit-source-id: 6e8be65711263182d8eef568ae34a46d77b399a3

--- src/typing/func_sig.ml
+++ src/typing/func_sig.ml
@@ -233,6 +233,13 @@ let toplevels id cx this super ~decls ~stmts ~expr
   (* push the scope early so default exprs can reference earlier params *)
   Env.push_var_scope cx function_scope;
 
+  (* add `this` and `super` before looking at parameter bindings as when using
+   * `this` in default parameter values it refers to the function scope and
+   * `super` should resolve to the method's [[HomeObject]]
+  *)
+  Scope.add_entry (internal_name "this") this function_scope;
+  Scope.add_entry (internal_name "super") super function_scope;
+
   (* bind type params *)
   SMap.iter (fun name t ->
     let r = reason_of_t t in
@@ -290,8 +297,6 @@ let toplevels id cx this super ~decls ~stmts ~expr
     new_entry yield_t, new_entry next_t, new_entry return_t
   ) in
 
-  Scope.add_entry (internal_name "this") this function_scope;
-  Scope.add_entry (internal_name "super") super function_scope;
   Scope.add_entry (internal_name "yield") yield function_scope;
   Scope.add_entry (internal_name "next") next function_scope;
   Scope.add_entry (internal_name "return") return function_scope;
