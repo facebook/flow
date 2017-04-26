90ae274996a348e593d45d9ad03818890182b549
Marshall Roch <mroch@fb.com>
2017-04-25 21:22:42 -0700

    use Yarn in CI
    Reviewed By: nmote
    
    Differential Revision: D4904983
    
    fbshipit-source-id: a4cf3a5a7fd80fe951f82d49c9a2db344560dd79

--- resources/appveyor/install.ps1
+++ resources/appveyor/install.ps1
@@ -5,9 +5,9 @@ echo "Build folder is: $env:APPVEYOR_BUILD_FOLDER"
 echo "Installing npm dependencies..."
 pushd $PSScriptRoot\..\..
 try {
-  npm install 2>&1 | %{ "$_" }
+  yarn install 2>&1 | %{ "$_" }
   if ($LASTEXITCODE -gt 0) {
-    Throw "npm install exited with the error code: $LASTEXITCODE"
+    Throw "yarn install exited with the error code: $LASTEXITCODE"
   }
 } finally {
   popd
