df3ae5d78008eacba4d0ee5feb3488e06c2ec268
Gabe Levi <gabe@fb.com>
2017-05-10 07:13:03 -0700

    v0.46.0
    Reviewed By: samwgoldman
    
    Differential Revision: D5022249
    
    fbshipit-source-id: 905d76a694e795a3e02e477331b6e50e6acef963

--- tests/include_suppressed/test.sh
+++ tests/include_suppressed/test.sh
@@ -8,9 +8,11 @@ printf "\n\nWith --include-suppressed\n"
 "$FLOW" check --all --strip-root --include-suppressed
 
 printf "\n\nJSON without --include-suppressed\n"
-"$FLOW" check --all --strip-root --json --pretty
+"$FLOW" check --all --strip-root --json --pretty \
+  | grep -v '^ *"flowVersion":.*'
 
 printf "\n\nJSON with --include-suppressed\n"
-"$FLOW" check --all --strip-root --include-suppressed --json --pretty
+"$FLOW" check --all --strip-root --include-suppressed --json --pretty \
+  | grep -v '^ *"flowVersion":.*'
 
 printf "\n"
