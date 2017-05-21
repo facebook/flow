c76f2c43d5b836a580003c595a1ddf5d56376c7f
Tim Popham <popham@uw.edu>
2017-05-21 10:52:22 -0700

    Move interfaces with statics across module boundaries

--- tests/export_type/importer.js
+++ tests/export_type/importer.js
@@ -23,10 +23,22 @@ import type {talias4} from "./cjs_with_types";
 var i: talias4 = 42;
 var j: talias4 = 'asdf'; // Error: string ~> number
 
-import {IFoo, IFoo2} from "./types_only";
+import {IFoo, IFoo2, IFoo3} from "./types_only";
 
 var k: IFoo = {prop: 42};
 var l: IFoo = {prop: 'asdf'}; // Error: {prop:string} ~> {prop:number}
 
 var m: IFoo2 = {prop: 'asdf'};
 var n: IFoo2 = {prop: 42}; // Error: {prop:number} ~> {prop:string}
+
+class Foo31 {
+  static prop: string;
+}
+var o: IFoo3 = new Foo31;
+var P: Class<IFoo3> = Foo31;
+
+class Foo32 {
+  static prop: number;
+}
+var q: IFoo3 = new Foo32; // Error {static prop: number} ~> {static prop: string}
+var R: Class<IFoo3> = Foo32; // Error {static prop: number} ~> {static prop: string}
