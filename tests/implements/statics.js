7da71651c81669eddb780b38c5ac146ffd8a986c
Tim Popham <popham@uw.edu>
2017-05-21 12:09:43 -0700

    Goofy cases that currently don't work

--- tests/implements/statics.js
+++ tests/implements/statics.js
@@ -1,4 +1,4 @@
-/* @noflow */
+/* @flow */
 
 interface IFoo {
   foo: string;
@@ -10,3 +10,21 @@ class C2 implements IFoo {
   foo: string;
   static bar: string; //ng
 }
+
+/*
+type Goofy = Class<IFoo>;
+class C3 implements Goofy {
+  name: string;
+  bar: number;
+} // Unreachable foo => ng
+
+interface Flat1 {
+  static foo: string;
+}
+
+type FlatGoofy = Class<Flat1>;
+class C4 implements FlatGoofy {
+  name: string;
+  foo: string;
+} // okish
+*/
