cb747693c413cbca0c43666648be2f1ae05df5f3
Marshall Roch <mroch@fb.com>
2017-05-31 11:36:52 -0700

    [travis] use build phases to build js and ocp in parallel

--- resources/travis/setup_opam.sh
+++ resources/travis/setup_opam.sh
@@ -4,3 +4,7 @@ INSTALL_DIR="$HOME/.flow_cache/ocaml-${OCAML_VERSION}_opam-${OPAM_VERSION}_${PLA
 export PATH="$INSTALL_DIR/usr/bin:$PATH"
 export OPAMROOT="$INSTALL_DIR/.opam"
 eval "$(opam config env)"
+
+printf "Using ocaml %s from %s\n  and opam %s from %s\n" \
+  "$(ocaml -vnum)" "$(which ocaml)" \
+  "$(opam --version)" "$(which opam)"
