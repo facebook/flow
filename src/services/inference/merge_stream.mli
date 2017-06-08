8e2bf87f2833b1a6bba571a9441092d57d05bd61
Nat Mote <nmote@fb.com>
2017-06-08 01:35:28 -0700

    Add merge_stream.mli
    Summary: Was poking around here and felt like this exercise would aid my understanding
    
    Reviewed By: avikchaudhuri
    
    Differential Revision: D5192022
    
    fbshipit-source-id: 686e880b8819f95a2f0c47f98531aeac053a4a22

---
+++ src/services/inference/merge_stream.mli
@@ -0,0 +1,35 @@
+(**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the "flow" directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ *)
+
+open Utils_js
+
+type element =
+| Skip of filename
+| Component of filename list
+
+val make :
+  (* dependency graph *)
+  FilenameSet.t FilenameMap.t ->
+  (* leader map *)
+  filename FilenameMap.t ->
+  (* component map *)
+  filename list FilenameMap.t ->
+  (* recheck_leader_map *)
+  bool FilenameMap.t ->
+  unit ->
+  element list MultiWorker.bucket
+
+val join :
+  (* merged, unchanged *)
+  (filename * Errors.ErrorSet.t) list * filename list ->
+  (* accumulators *)
+  (filename * Errors.ErrorSet.t) list * filename list ->
+  (* accumulated results *)
+  (filename * Errors.ErrorSet.t) list * filename list
