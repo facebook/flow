6526e320e12c5553cfa943167453413245263774
Marshall Roch <mroch@fb.com>
2017-08-23 12:31:39 -0700

    [parser] make parse_test262 print all test names in verbose mode
    Summary:
    in verbose mode, print the name of the test before running it. in
    non-verbose mode, only print the name of the test if it fails.
    
    Reviewed By: avikchaudhuri
    
    Differential Revision: D5690759
    
    fbshipit-source-id: 6fc2c9d371c9ab5f3e545225dc99ab51614a4cc2

--- src/parser/test/parse_test262.ml
+++ src/parser/test/parse_test262.ml
@@ -100,7 +100,7 @@ let files_of_path path =
   ) []
   |> List.rev
 
-let print_error ~strip_root (filename, use_strict) err =
+let print_name ~strip_root (filename, use_strict) =
   let filename = match strip_root with
   | Some root ->
     let len = String.length root in
@@ -109,12 +109,14 @@ let print_error ~strip_root (filename, use_strict) err =
   in
   let strict = if use_strict then "(strict mode)" else "(default)" in
   let cr = if Unix.isatty Unix.stdout then "\r" else "" in
+  Printf.printf "%s%s %s\n%!" cr filename strict
+
+let print_error err =
   match err with
   | Missing_parse_error ->
-    Printf.printf "%s%s %s\n  Missing parse error\n%!" cr filename strict
+    Printf.printf "  Missing parse error\n%!"
   | Unexpected_parse_error (loc, err) ->
-    Printf.printf "%s%s %s\n  %s at %s\n%!"
-      cr filename strict (Parse_error.PP.error err) (Loc.to_string loc)
+    Printf.printf "  %s at %s\n%!" (Parse_error.PP.error err) (Loc.to_string loc)
 
 module Frontmatter = struct
   type t = {
@@ -307,16 +309,19 @@ let incr_result (passed, failed) did_pass =
   if did_pass then succ passed, failed
   else passed, succ failed
 
-let fold_test ~strip_root ~bar
+let fold_test ~verbose ~strip_root ~bar
     (passed_acc, failed_acc, features_acc)
     (name, frontmatter, content) =
+
+  if verbose then print_name ~strip_root name;
   let passed =
     let { name; result } = run_test (name, frontmatter, content) in
     match result with
     | Ok _ -> true
     | Error err ->
       Option.iter ~f:(Progress_bar.clear (passed_acc, failed_acc)) bar;
-      print_error ~strip_root name err;
+      if not verbose then print_name ~strip_root name;
+      print_error err;
       false
   in
   let passed_acc, failed_acc = incr_result (passed_acc, failed_acc) passed in
@@ -361,7 +366,7 @@ let main () =
     else Some (Progress_bar.make ~chunks:40 ~frequency:0.1 test_count) in
 
   let (passed, failed, results_by_feature) = List.fold_left
-    (fold_test ~strip_root ~bar)
+    (fold_test ~verbose ~strip_root ~bar)
     (0, 0, SMap.empty)
     tests in
 
