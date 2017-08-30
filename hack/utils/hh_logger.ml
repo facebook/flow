09cb3f36ebc932507283a64f43d81ed2dbaa78b6
Sylvan Zheng <sylv@fb.com>
2017-08-30 08:35:52 -0700

    [hack] Leverage HH_logger.level to partially clean up integration_ml test output
    Summary: Reconfigure methods in Hh_logger so that calls to log and log_duration go through the Level module at a default severity of Info. This way applications that wish to silence the output of the server test (ie, integration_ml tests) can simply set the level to OFF without affecting logging behavior in production.
    
    Differential Revision: D5690336
    
    fbshipit-source-id: bdb925cb7a1837e993f84ca87431963dffc157c2

--- hack/utils/hh_logger.ml
+++ hack/utils/hh_logger.ml
@@ -15,20 +15,20 @@ let timestamp_string () =
   Printf.sprintf "[%d-%02d-%02d %02d:%02d:%02d]"
     year (tm.tm_mon + 1) tm.tm_mday tm.tm_hour tm.tm_min tm.tm_sec
 
-let log_raw s =
+let print_raw s =
   Printf.eprintf "%s %s%!" (timestamp_string ()) s
 
-(* wraps log_raw in order to take a format string & add a newline *)
-let log fmt = Printf.ksprintf log_raw (fmt^^"\n")
+(* wraps print_raw in order to take a format string & add a newline *)
+let print fmt = Printf.ksprintf print_raw (fmt^^"\n")
 
-let log_duration name t =
-  log_raw (name ^ ": ");
+let print_duration name t =
+  print_raw (name ^ ": ");
   let t2 = Unix.gettimeofday() in
   Printf.eprintf "%f\n%!" (t2 -. t);
   t2
 
 let exc ?(prefix="") e =
-  log_raw (prefix ^ Printexc.to_string e ^ "\n");
+  print_raw (prefix ^ Printexc.to_string e ^ "\n");
   Printexc.print_backtrace stderr;
   ()
 
@@ -43,6 +43,7 @@ module Level : sig
   val min_level : unit -> t
   val set_min_level : t -> unit
   val log : t -> ('a, unit, string, string, string, unit) format6 -> 'a
+  val log_duration : t -> string -> float -> float
 end = struct
   type t =
     | Off
@@ -66,9 +67,19 @@ end = struct
 
   let log level fmt =
     if int_of_level level >= int_of_level !min_level_ref
-    then log fmt
+    then print fmt
     else Printf.ifprintf () fmt
+
+  let log_duration level fmt t =
+    if int_of_level level >= int_of_level !min_level_ref
+    then print_duration fmt t
+    else t
+
 end
+
+(* Default log instructions to INFO level *)
+let log ?(lvl=Level.Info) fmt = Level.log lvl fmt
+let log_duration fmt t = Level.log_duration Level.Info fmt t
 
 let fatal fmt = Level.log Level.Fatal fmt
 let error fmt = Level.log Level.Error fmt
