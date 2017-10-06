f0d7f309a124504e83f43974f43b9788a1804951
Nat Mote <nmote@fb.com>
2017-10-06 10:10:35 -0700

    Add missing license header to nel.ml
    Reviewed By: samwgoldman
    
    Differential Revision: D5996676
    
    fbshipit-source-id: 324b450287709c2bc89f04a76908c438bdba73b0

--- src/common/utils/nel.ml
+++ src/common/utils/nel.ml
@@ -1,3 +1,10 @@
+(**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *)
+
 (* Non-empty list *)
 
 type 'a t = 'a * 'a list
