9f015d339a769a455221e3ecd304e6a1a022b5c1
Gabe Levi <gabe@fb.com>
2017-11-01 12:59:17 -0700

    don't read stderr in gen_build_id script
    Summary: if git or hg write to stderr but don't error, we don't want to capture the stderr output.
    
    Reviewed By: samwgoldman
    
    Differential Revision: D6209542
    
    fbshipit-source-id: 1c7e4be6a974a94815117fc086665a9f5991eb82

--- scripts/script_utils.ml
+++ scripts/script_utils.ml
@@ -8,7 +8,7 @@
 let with_pipe f =
   let fd_r, fd_w = Unix.pipe () in
   try
-    let res = f fd_r fd_w in
+    let res = f (fd_r, fd_w) in
     Unix.close fd_r;
     Unix.close fd_w;
     res
@@ -27,12 +27,9 @@ let with_out_channel filename f =
   try let res = f oc in close_out oc; res
   with exn -> close_out oc; raise exn
 
-(* Read the first line in stdout or stderr of an external command. *)
-let read_process_output name args =
-  with_pipe @@ fun in_r _in_w ->
-  with_pipe @@ fun out_r out_w ->
+let read_process name args (in_r, _in_w) (out_r, out_w) (err_r, err_w) =
   let pid =
-    try Unix.create_process name args in_r out_w out_w
+    try Unix.create_process name args in_r out_w err_w
     with Unix.Unix_error (Unix.ENOENT, _, _) ->
       (* On Windows, this is what happens if you call create_process
        * non_existant_thing *)
@@ -46,13 +43,26 @@ let read_process_output name args =
        * non_existant_thing *)
       raise (Failure (name ^ ": command not found"))
   | _, Unix.WEXITED 128 ->
-      raise (Failure (input_line (Unix.in_channel_of_descr out_r)))
+      raise (Failure (input_line (Unix.in_channel_of_descr err_r)))
   | _, Unix.WEXITED code ->
       raise (Failure (name ^ ": exited code "^(string_of_int code)))
   | _, Unix.WSIGNALED signal ->
       raise (Failure (name ^ ": killed by signal " ^ (string_of_int signal)))
   | _, Unix.WSTOPPED signal ->
       raise (Failure (name ^ ": stopped by signal " ^ (string_of_int signal)))
+
+(* Read the first line in stdout or stderr of an external command. *)
+let read_process_output name args =
+  with_pipe @@ fun in_pipe ->
+  with_pipe @@ fun out_pipe ->
+  read_process name args in_pipe out_pipe out_pipe
+
+(* Read the first line in stdout of an external command. *)
+let read_process_stdout name args =
+  with_pipe @@ fun in_pipe ->
+  with_pipe @@ fun out_pipe ->
+  with_pipe @@ fun err_pipe ->
+  read_process name args in_pipe out_pipe err_pipe
 
 let string_of_file filename =
   with_in_channel filename @@ fun ic ->
