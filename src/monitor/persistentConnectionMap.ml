e26b22441dba9184aab251af96c8309b3ae34475
Gabe Levi <gabe@fb.com>
2017-11-07 13:28:14 -0800

    PersistentConnectionMap
    Summary:
    A thread-safe map of persistent connection client IDs to the
    persistent connections.
    
    Reviewed By: samwgoldman
    
    Differential Revision: D6248998
    
    fbshipit-source-id: 5fdeec49f3b12313b177b1aa532cc610c45b7bd0

---
+++ src/monitor/persistentConnectionMap.ml
@@ -0,0 +1,30 @@
+(**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *)
+
+(* This module keeps track of the persistent connections. Since the Flow server monitor is designed
+ * to be super thin, the Flow server manages most of the persistent connection state. All the
+ * monitor really needs to do is keep track of which connection goes with which ID *)
+
+let mutex = Lwt_mutex.create ()
+let map = ref IMap.empty
+
+let add ~client_id ~client =
+  Lwt_mutex.with_lock mutex (fun () ->
+    map := IMap.add client_id client !map;
+    Lwt.return_unit
+  )
+
+let get ~client_id =
+  Lwt_mutex.with_lock mutex (fun () ->
+    Lwt.return (IMap.get client_id !map)
+  )
+
+let remove ~client_id =
+  Lwt_mutex.with_lock mutex (fun () ->
+    map := IMap.remove client_id !map;
+    Lwt.return_unit
+  )
