0fd429806e693f3a58e8027d7907ae629e2c75a3
Nat Mote <nmote@fb.com>
2017-12-13 18:01:55 -0800

    Pull out code to add files to the focused set into a new module
    Reviewed By: gabelevi
    
    Differential Revision: D6560017
    
    fbshipit-source-id: 9e9b13ae35f1925b6a9e007fa94c9313e290fefc

--- src/server/commandHandler.ml
+++ src/server/commandHandler.ml
@@ -470,29 +470,13 @@ let handle_persistent genv env client_id msg =
              * If the newly opened file was already checked, then we'll just send the errors to
              * the client
              *)
-            let file_options = Options.file_options options in
-            let focused = Nel.fold_left
-              (fun acc fn ->
-                FilenameSet.add (Files.filename_from_string ~options:file_options fn) acc)
-              FilenameSet.empty
-              filenames in
-            let checked_files = CheckedSet.add ~focused env.checked_files in
-
-            let new_focused_files = focused
-              |> Fn.flip FilenameSet.diff (CheckedSet.focused env.checked_files)
-              |> Fn.flip FilenameSet.diff (CheckedSet.dependents env.checked_files) in
-
-            let env = { env with checked_files } in
-            if not (FilenameSet.is_empty new_focused_files)
-            then
-              (* Rechecking will send errors to the clients *)
-              Rechecker.recheck genv env new_focused_files
-            else begin
+            let env, triggered_recheck = Lazy_mode_utils.focus_and_check genv env filenames in
+            if not triggered_recheck then begin
               (* This open doesn't trigger a recheck, but we'll still send down the errors *)
               let errors, warnings, _ = ErrorCollator.get_with_separate_warnings env in
-              Persistent_connection.send_errors_if_subscribed ~client ~errors ~warnings;
-              env
-            end
+              Persistent_connection.send_errors_if_subscribed ~client ~errors ~warnings
+            end;
+            env
           | Some Options.LAZY_MODE_FILESYSTEM
           | None ->
             (* In filesystem lazy mode or in non-lazy mode, the only thing we need to do when
