e21122a70499200252db6fa2ff634b086bed1440
Tim Popham <popham@uw.edu>
2017-12-29 22:11:42 -0800

    Expect static named methods

--- src/parser/test/flow/types/object/unexpected_static.js
+++ src/parser/test/flow/types/object/unexpected_static.js
@@ -13,8 +13,8 @@ type D = { static: X }
 // error: static prop named static
 type E = { static static: X }
 
-// error: parsed as a static callable (TODO: consider parsing as method named static?)
+// ok: method named static
 type F = { static(): R }
 
-// error: parsed as static callable (TODO: consider parsing as method named static?)
+// ok: generic method named static
 type G = { static<X>(): R }
