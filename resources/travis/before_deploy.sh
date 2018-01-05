bacec7d3392a56eded36b01938a171073d43ef41
Marshall Roch <mroch@fb.com>
2018-01-05 15:10:31 -0800

    [parser] upload libflowparser to github on releases
    Summary: D6668679 builds libflowparser.zip, this diff makes it get uploaded to the release for Mac and Linux
    
    Reviewed By: gabelevi
    
    Differential Revision: D6669327
    
    fbshipit-source-id: 2caa4e7e7e73c238eb331f43e81f6404df7ea974

--- resources/travis/before_deploy.sh
+++ resources/travis/before_deploy.sh
@@ -4,6 +4,9 @@ if [[ "$TRAVIS_TAG" = "" ]]; then exit 0; fi
 
 PLATFORM=$([ "$TRAVIS_OS_NAME" == "linux" ] && echo "linux64" || echo "$TRAVIS_OS_NAME")
 
+cp src/parser/dist/libflowparser.zip \
+  "$TRAVIS_BUILD_DIR/libflowparser-$PLATFORM-$TRAVIS_TAG.zip"
+
 mkdir -p "$HOME/release"
 rm -rf "$HOME/release/flow"
 cp -R bin "$HOME/release/flow"
