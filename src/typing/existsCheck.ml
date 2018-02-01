ea478fa1ac73710a9dd3ca723ca73b777b17ff25
Nat Mote <nmote@fb.com>
2018-02-01 15:52:00 -0800

    ExistsCheck.to_string
    Reviewed By: samwgoldman
    
    Differential Revision: D6764614
    
    fbshipit-source-id: b453cde0ac71857f82aa49043cc15babc8561bca

--- src/typing/existsCheck.ml
+++ src/typing/existsCheck.ml
@@ -20,3 +20,20 @@ let empty = {
   number_loc = None;
   mixed_loc = None;
 }
+
+let to_string t =
+  let string_of_loc_option = function
+    | None -> "None"
+    | Some loc -> Loc.to_string ~include_source:true loc
+  in
+  [
+    ("null_loc", t.null_loc);
+    ("bool_loc", t.bool_loc);
+    ("string_loc", t.string_loc);
+    ("number_loc", t.number_loc);
+    ("mixed_loc", t.mixed_loc);
+  ]
+  |> List.map (fun (name, loc_opt) -> (name, string_of_loc_option loc_opt))
+  |> List.map (fun (name, loc) -> Printf.sprintf "  %s: %s;\n" name loc)
+  |> String.concat ""
+  |> Printf.sprintf "{\n%s}"
