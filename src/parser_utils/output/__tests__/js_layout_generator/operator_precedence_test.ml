210155cc29a537620c4861a08f02515684305baa
Marshall Roch <mroch@fb.com>
2018-02-08 16:09:56 -0800

    ast printer
    Summary: lowers a flow AST to a `Layout` which encodes a tree of "atoms" (strings) as well as whitespace and line wrapping rules. two `Layout` printers are included, one that attempts a reasonable pretty print with whitespace and wrapped to 80 cols, and another that attempts to ugly print as compactly as possible.
    
    Reviewed By: panagosg7
    
    Differential Revision: D6940946
    
    fbshipit-source-id: d100f356c69094356343748e9ff386dee842fa17

---
+++ src/parser_utils/output/__tests__/js_layout_generator/operator_precedence_test.ml
@@ -0,0 +1,242 @@
+(**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *)
+
+open Ast_builder
+open Layout_test_utils
+
+module S = Ast_builder.Statements
+module E = Ast_builder.Expressions
+module L = Layout_builder
+
+let test ctxt =
+  let x, y, z = E.identifier "x", E.identifier "y", E.identifier "z" in
+  let str = E.literal (Literals.string "a") in
+  let (&&) a b = E.logical_and a b in
+  let (||) a b = E.logical_or a b in
+  let (+) a b = E.binary ~op:Ast.Expression.Binary.Plus a b in
+  let (-) a b = E.binary ~op:Ast.Expression.Binary.Minus a b in
+
+  (* nested logical expressions *)
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [
+      expression (x && y); pretty_space; atom "&&";
+      sequence ~break:Layout.Break_if_needed ~inline:(false, true) [
+        fused [flat_pretty_space; expression z];
+      ];
+    ]))
+    ((x && y) && z);
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [
+      expression x; pretty_space; atom "&&";
+      sequence ~break:Layout.Break_if_needed ~inline:(false, true) [
+        fused [flat_pretty_space; wrap_in_parens (expression (y && z))];
+      ];
+    ]))
+    (x && (y && z));
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [
+      expression (x && y); pretty_space; atom "||";
+      sequence ~break:Layout.Break_if_needed ~inline:(false, true) [
+        fused [flat_pretty_space; expression z];
+      ];
+    ]))
+    ((x && y) || z);
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [
+      expression x; pretty_space; atom "&&";
+      sequence ~break:Layout.Break_if_needed ~inline:(false, true) [
+        fused [flat_pretty_space; wrap_in_parens (expression (y || z))];
+      ];
+    ]))
+    (x && (y || z));
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [
+      expression (x || y); pretty_space; atom "||";
+      sequence ~break:Layout.Break_if_needed ~inline:(false, true) [
+        fused [flat_pretty_space; expression z]
+      ];
+    ]))
+    ((x || y) || z);
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [
+      expression x; pretty_space; atom "||";
+      sequence ~break:Layout.Break_if_needed ~inline:(false, true) [
+        fused [flat_pretty_space; wrap_in_parens (expression (y || z))];
+      ];
+    ]))
+    (x || (y || z));
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [
+      wrap_in_parens (expression (x || y));
+      pretty_space; atom "&&";
+      sequence ~break:Layout.Break_if_needed ~inline:(false, true) [
+        fused [flat_pretty_space; expression z];
+      ];
+    ]))
+    ((x || y) && z);
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [
+      expression x; pretty_space; atom "||";
+      sequence ~break:Layout.Break_if_needed ~inline:(false, true) [
+        fused [flat_pretty_space; expression (y && z)];
+      ];
+    ]))
+    (x || (y && z));
+
+  (* nested binary expressions *)
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [expression (x + y); pretty_space; atom "+"; pretty_space; expression z]))
+    ((x + y) + z);
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [
+      expression x; pretty_space; atom "+"; pretty_space;
+      wrap_in_parens (expression (y + z));
+    ]))
+    (x + (y + z));
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [expression (x + y); pretty_space; atom "-"; pretty_space; expression z]))
+    ((x + y) - z);
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [
+      expression x; pretty_space; atom "+"; pretty_space;
+      wrap_in_parens (expression (y - z));
+    ]))
+    (x + (y - z));
+
+  (* logical expressions and binary expressions *)
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [
+      expression (x + y); pretty_space; atom "&&";
+      sequence ~break:Layout.Break_if_needed ~inline:(false, true) [
+        fused [flat_pretty_space; expression z];
+      ];
+    ]))
+    ((x + y) && z);
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [
+      expression x; pretty_space; atom "+"; pretty_space;
+      wrap_in_parens (expression (y && z));
+    ]))
+    (x + (y && z));
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [
+      wrap_in_parens (expression (x && y));
+      pretty_space; atom "+"; pretty_space; expression z;
+    ]))
+    ((x && y) + z);
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [
+      expression x; pretty_space; atom "&&";
+      sequence ~break:Layout.Break_if_needed ~inline:(false, true) [
+        fused [flat_pretty_space; expression (y + z)]
+      ];
+    ]))
+    (x && (y + z));
+
+  (* literals *)
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [
+      expression str; pretty_space; atom "&&";
+      sequence ~break:Layout.Break_if_needed ~inline:(false, true) [
+        fused [flat_pretty_space; expression x];
+      ];
+    ]))
+    (str && x);
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [
+      expression x; pretty_space; atom "&&";
+      sequence ~break:Layout.Break_if_needed ~inline:(false, true) [
+        fused [flat_pretty_space; expression str];
+      ];
+    ]))
+    (x && str);
+
+  (* function *)
+
+  let fn = (Loc.none, Ast.Expression.Function (
+    Functions.make ~id:None ~expression:true ~params:[] ())
+  ) in
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [
+      expression fn; pretty_space; atom "&&";
+      sequence ~break:Layout.Break_if_needed ~inline:(false, true) [
+        fused [flat_pretty_space; expression x];
+      ];
+    ]))
+    (fn && x);
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [
+      expression x; pretty_space; atom "&&";
+      sequence ~break:Layout.Break_if_needed ~inline:(false, true) [
+        fused [flat_pretty_space; expression fn];
+      ];
+    ]))
+    (x && fn);
+
+  (* sequence *)
+
+  let seq = E.sequence [x; y] in
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [
+      wrap_in_parens (expression seq); pretty_space; atom "&&";
+      sequence ~break:Layout.Break_if_needed ~inline:(false, true) [
+        fused [flat_pretty_space; expression z];
+      ];
+    ]))
+    (seq && z);
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (fused [
+      expression z; pretty_space; atom "&&";
+      sequence ~break:Layout.Break_if_needed ~inline:(false, true) [
+        fused [flat_pretty_space; wrap_in_parens (expression seq)];
+      ];
+    ]))
+    (z && seq);
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (sequence ~break:Layout.Break_if_needed ~inline:(true, true) ~indent:0 [
+      sequence ~break:Layout.Break_if_needed ~inline:(true, true) ~indent:0 [
+        fused [
+          expression z;
+          Layout.IfBreak ((atom ","), (fused [atom ","; pretty_space]));
+        ];
+        wrap_in_parens (expression seq);
+      ];
+    ]))
+    (E.sequence [z; seq]);
+
+  assert_layout_of_expression ~ctxt
+    L.(loc (sequence ~break:Layout.Break_if_needed ~inline:(true, true) ~indent:0 [
+      sequence ~break:Layout.Break_if_needed ~inline:(true, true) ~indent:0 [
+        fused [
+          wrap_in_parens (expression seq);
+          Layout.IfBreak ((atom ","), (fused [atom ","; pretty_space]));
+        ];
+        expression z;
+      ];
+    ]))
+    (E.sequence [seq; z]);
---
+++ src/parser_utils/output/js_layout_generator.mli
@@ -0,0 +1,31 @@
+(**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *)
+
+type expression_context = {
+  left: expression_context_left;
+  group: expression_context_group;
+}
+and expression_context_left =
+  | Normal_left
+  | In_expression_statement
+  | In_tagged_template
+  | In_plus_op
+  | In_minus_op
+and expression_context_group =
+  | Normal_group
+  | In_arrow_func
+  | In_for_init
+
+val normal_context: expression_context
+
+val program: preserve_docblock:bool -> checksum:string option -> Loc.t Ast.program -> Layout.layout_node
+val expression: ?ctxt:expression_context -> Loc.t Ast.Expression.t -> Layout.layout_node
+val statement:
+  ?allow_empty:bool ->
+  ?pretty_semicolon:bool ->
+  Loc.t Ast.Statement.t -> Layout.layout_node
+val object_property: Loc.t Ast.Expression.Object.property -> Layout.layout_node
