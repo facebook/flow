e530f45e9b315f73f4ab970e07192a359e6238ae
Nikos Gorogiannis <nikosgorogiannis@fb.com>
2018-02-13 04:29:29 -0800

    [hack] Dead code elimination, 2nd pass
    Summary: Using D6819419, detect more dead code and remove.
    
    Reviewed By: dabek
    
    Differential Revision: D6934259
    
    fbshipit-source-id: 9e0c74be004946fa7737ba28f402d4dccdbb2b8e

--- hack/monitor/serverProcess.ml
+++ hack/monitor/serverProcess.ml
@@ -29,21 +29,3 @@ type server_process =
    * that crash info to the next hh_client that connects. We keep that info
    * here. *)
   | Died_unexpectedly of Unix.process_status * bool
-
-
-let process_status_to_string (status: Unix.process_status) : string =
-  match status with
-  | Unix.WEXITED i -> Printf.sprintf "WEXITED(%d)" i
-  | Unix.WSIGNALED i -> Printf.sprintf "WSIGNALED(%d)" i
-  | Unix.WSTOPPED i -> Printf.sprintf "WSTOPPED(%d)" i
-
-let process_data_to_string (process_data: process_data) : string =
-  Printf.sprintf "%s, PID=%d" process_data.name process_data.pid
-
-let server_process_to_string (server_process: server_process) : string =
-  match server_process with
-  | Not_yet_started -> "Not_yet_started"
-  | Alive process_data -> Printf.sprintf "Alive(%s)" (process_data_to_string process_data)
-  | Informant_killed -> "Informant_killed"
-  | Died_unexpectedly (status, was_oom) -> Printf.sprintf "Died unexpectedly(%s, was_oom=%B)"
-      (process_status_to_string status) was_oom
