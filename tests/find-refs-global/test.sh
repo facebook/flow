8e23a25d9de113cc17160c1915193d9889288b6e
Mayank Patke <fishythefish@fb.com>
2018-02-13 09:50:56 -0800

    Integrate CJS global find-refs with local find-refs
    Summary: The previous diffs in the stack added file_sig support for CJS exports/imports and updated find-refs to start making use of that information. This diff finishes up by fixing symbol resolution and connecting up all the CJS pieces in find-refs.
    
    Reviewed By: nmote
    
    Differential Revision: D6928618
    
    fbshipit-source-id: 832bbdc865af675ea3868cda47139c4548f2b141

--- tests/find-refs-global/test.sh
+++ tests/find-refs-global/test.sh
@@ -20,3 +20,27 @@ assert_ok "$FLOW" find-refs --global --json --pretty --strip-root es6-2.js 16 4
 
 echo "class method that overrides a superclass implementation:"
 assert_ok "$FLOW" find-refs --global --json --pretty --strip-root extendsFoo.js 7 3
+
+echo "local exported as CJS property:"
+assert_ok "$FLOW" find-refs --global --json --pretty --strip-root cjs-1.js 2 7
+
+echo "CJS property exporting local:"
+assert_ok "$FLOW" find-refs --global --json --pretty --strip-root cjs-1.js 3 16
+
+echo "CJS property exporting literal:"
+assert_ok "$FLOW" find-refs --global --json --pretty --strip-root cjs-1.js 4 16
+
+echo "local exported in CJS object:"
+assert_ok "$FLOW" find-refs --global --json --pretty --strip-root cjs-2.js 2 7
+
+echo "CJS object exporting local:"
+assert_ok "$FLOW" find-refs --global --json --pretty --strip-root cjs-2.js 4 19
+
+echo "CJS object exporting literal:"
+assert_ok "$FLOW" find-refs --global --json --pretty --strip-root cjs-2.js 4 27
+
+echo "CJS object exporting shorthand:"
+assert_ok "$FLOW" find-refs --global --json --pretty --strip-root cjs-2.js 4 37
+
+echo "CJS ident exports/imports:"
+assert_ok "$FLOW" find-refs --global --json --pretty --strip-root cjs-3.js 2 7
