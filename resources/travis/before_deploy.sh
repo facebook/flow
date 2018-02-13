0c001070ea4d6ff8d41b01f17adb71685dc2bfa8
Marshall Roch <mroch@fb.com>
2018-02-13 11:29:39 -0800

    [travis] set up opam in deploy scripts
    Summary: deploys run in a separate shell from earlier scripts. now that we call `make dist/flow.zip` which calls `build-flow` which uses ocamlbuild (and realizes that nothing's changed), we need to set up opam so that it can find `ocaml` and `opam`.
    
    Reviewed By: gabelevi
    
    Differential Revision: D6974597
    
    fbshipit-source-id: 1e2a80fa461925357f2e128306ffeb494f78c187

--- resources/travis/before_deploy.sh
+++ resources/travis/before_deploy.sh
@@ -2,6 +2,9 @@
 
 if [[ "$TRAVIS_TAG" = "" ]]; then exit 0; fi
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/setup_opam.sh
+
 PLATFORM=$([ "$TRAVIS_OS_NAME" == "linux" ] && echo "linux64" || echo "$TRAVIS_OS_NAME")
 
 cp src/parser/dist/libflowparser.zip \
