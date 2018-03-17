3ed83d5cb315eefee760e5236745e6446de20883
Gabe Levi <gabe@fb.com>
2018-03-16 17:13:00 -0700

    Switch FlowServerMonitorLogger from buffered channels to fds
    Summary:
    I noticed that the monitor process would sometimes take like 7
    seconds to exit (like after `Pervasives.exit` is called) when the
    machine was under heavy load. I believe the root cause is the extra
    `at_exit` hooks which Lwt installs. That said, there's only really 1
    Lwt hook, which is calling `Lwt_io.flush_all`. This flushes all the Lwt
    buffered channels, similarly to how `Pervasives.flush_all` flushes all
    the OCaml buffered channels.
    
    We only use `Lwt_io` and Lwt buffered channels in 1 place:
    FlowServerMonitorLogger. And we only really use them there because when
    I wrote it I was new to Lwt and didn't know how to allow multiple
    threads to log without interleaving output. Futhermore, while debugging
    this slowness, I noticed I could get Lwt into a deadlock by calling
    either `Lwt_io.flush_all ()` or `Lwt_io.(flush stderr)` as we exit. I
    think this is due to the `Lwt_io.atomic` call in the logger.
    
    So the best way to avoid this potential deadlock is to avoid any sort of
    locking behavior. Instead, we can just use a stream and a single writing
    thread. Much easier!
    
    Reviewed By: samwgoldman
    
    Differential Revision: D7212061
    
    fbshipit-source-id: a2295af2aa5a57d54fe852342e90f59c04dbfbf2

--- src/monitor/logger/flowServerMonitorLogger.ml
+++ src/monitor/logger/flowServerMonitorLogger.ml
@@ -6,7 +6,18 @@
  *)
 
 (* This module makes Lwt's logger easily available for use. Lwt's logger is nice because it
- * can easily log exceptions, format messages, and interact with Lwt. *)
+ * can easily log exceptions, format messages, and interact with Lwt.
+ *
+ * Initially I used Lwt_io for this module's implementation. It was nice, because Lwt_io.atomic
+ * guaranteed that I wouldn't have multiple threads interleaving their logs. However, I found that
+ * Lwt_io.flush_all which is called by at_exit could take awhile. Furthermore, I was able to
+ * trigger deadlocks by logging & flushing around exit time.
+ *
+ * So now the implementation uses fds exclusively.
+ *
+ * 1. Multiple threads write to the msg_stream with their logs
+ * 2. A single thread (WriteLoop) reads the messages and writes them to the various fds directly
+ **)
 
 let (>>=) = Lwt.(>>=)
 
@@ -18,20 +29,48 @@ type 'a logger_fn =
   ('a, unit, string, unit) format4 ->
   'a
 
+let msg_stream, push_to_msg_stream = Lwt_stream.create ()
+
+module WriteLoop = LwtLoop.Make (struct
+  type acc = Lwt_unix.file_descr list
+
+  (* Given a list of messages and a fd, write them serially to the fd *)
+  let write_msgs msgs fd =
+    Lwt_list.iter_s
+      (fun msg -> Lwt_unix.write_string fd msg 0 (String.length msg) >>= (fun _ -> Lwt.return_unit))
+      msgs
+
+  (* Get a list of messages, write the list in parallel to each fd *)
+  let main fds =
+    Lwt_stream.next msg_stream
+    >>= (fun msgs -> Lwt_list.iter_p (write_msgs msgs) fds)
+    >>= (fun () -> Lwt.return fds)
+
+  (* If we failed to write to an fd throw an exception and exit. I'm not 100% sure this is the
+   * best behavior - should logging errors cause the monitor (and server) to crash? *)
+  let catch _ exn =
+    Printf.eprintf
+      "Logger.WriteLoop exception:\n%s\n%s"
+      (Printexc.to_string exn)
+      (Printexc.get_backtrace ());
+    raise exn
+end)
+
+let initialized = ref false
+
 (* Creates a default logger and sets the minimum logger level. The logger will log every message
  * that passes the minimum level to stderr. If log_fd is provided, each message will be logged
  * to it as well *)
 let init_logger ?log_fd min_level =
-  let template = "$(date).$(milliseconds) [$(level)] $(message)" in
-  let log_oc = Option.map ~f:(Lwt_io.of_unix_fd ~mode:Lwt_io.output) log_fd in
+  if !initialized then failwith "Cannot initialized FlowServerMonitorLogger more than once";
+  initialized := true;
 
-  (* Given a list of formatted messages, atomically write them to the channel and flush it *)
-  let atomic_write_list formatted_messages =
-    Lwt_io.atomic begin fun oc ->
-      Lwt_list.iter_s (Lwt_io.write oc) formatted_messages
-      >>= fun () -> Lwt_io.flush oc
-    end
-  in
+  let template = "$(date).$(milliseconds) [$(level)] $(message)" in
+
+  let log_fd = Option.map log_fd ~f:Lwt_unix.of_unix_file_descr in
+
+  let fds = Lwt_unix.stderr :: (Option.value_map log_fd ~default:[] ~f:(fun fd -> [fd])) in
+  Lwt.async (fun () -> WriteLoop.run fds);
 
   (* Format the messages and write the to the log and stderr *)
   let output section level messages =
@@ -42,14 +81,12 @@ let init_logger ?log_fd min_level =
       Buffer.add_char buffer '\n';
       Buffer.contents buffer
     ) messages in
-    Lwt.join [
-      atomic_write_list formatted_messages Lwt_io.stderr;
-      Option.value_map ~default:Lwt.return_unit ~f:(atomic_write_list formatted_messages) log_oc;
-    ]
+    push_to_msg_stream (Some formatted_messages);
+    Lwt.return_unit
   in
 
   (* Just close the log *)
-  let close () = Option.value_map ~default:Lwt.return_unit ~f:Lwt_io.close log_oc in
+  let close () = Option.value_map ~default:Lwt.return_unit ~f:Lwt_unix.close log_fd in
 
   (* Set the default logger *)
   Lwt_log.default := Lwt_log_core.make ~output ~close;
