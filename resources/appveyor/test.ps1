520c7cfd625912885421630621721910669e066c
Gabe Levi <gabe@fb.com>
2018-03-27 10:01:22 -0700

    Allow up to 20% of test to error on AppVeyor
    Summary:
    If no tests fail and at least 80% of the tests pass without
    erroring, then AppVeyor shouldn't error. This should make AppVeyor more
    stable until we can make Flow on Windows more stable.
    
    Reviewed By: samwgoldman
    
    Differential Revision: D7415245
    
    fbshipit-source-id: 9704566678fb22354de161ebecc08fd60a5a810d

--- resources/appveyor/test.ps1
+++ resources/appveyor/test.ps1
@@ -2,7 +2,7 @@ pushd "C:\projects\flow"
 try {
   echo "Running tool tests"
   echo "Using node version $(node --version)"
-  node packages\flow-dev-tools\bin\tool test --parallelism 1 2>&1 | %{ "$_" }
+  node packages\flow-dev-tools\bin\tool test --parallelism 1 --max-errored-tests-pct 20 2>&1 | %{ "$_" }
   if ($LASTEXITCODE -gt 0) {
     Throw "node tool test exited with error code: $LASTEXITCODE"
   }
