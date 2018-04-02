27d0483640f5c042d2fe14863d4469af631721a9
Panagiotis Vekris <pvekris@fb.com>
2018-04-01 22:02:37 -0700

    [type-at-pos][tests] Adds more function tests
    Summary: This adds tests that exercise tracking of type parameters in polymorphic functions.
    
    Reviewed By: samwgoldman
    
    Differential Revision: D7385276
    
    fbshipit-source-id: 8ce7e8fd51ca1151a9c2f420ed7e962b39748bda

--- tests/type-at-pos/test.sh
+++ tests/type-at-pos/test.sh
@@ -161,15 +161,66 @@ assert_ok "$FLOW" type-at-pos facebookism.js 3 8 --strip-root --pretty
 # printf "facebookism.js:3:14 = "
 # assert_ok "$FLOW" type-at-pos facebookism.js 3 14 --strip-root --pretty
 
-# function_expressions.js
-printf "function_expression.js:4:3 = "
-assert_ok "$FLOW" type-at-pos function_expressions.js 4 3 --strip-root --pretty
-printf "function_expression.js:8:3 = "
-assert_ok "$FLOW" type-at-pos function_expressions.js 8 3 --strip-root --pretty
-printf "function_expression.js:12:3 = "
-assert_ok "$FLOW" type-at-pos function_expressions.js 12 3 --strip-root --pretty
-printf "function_expression.js:16:3 = "
-assert_ok "$FLOW" type-at-pos function_expressions.js 16 3 --strip-root --pretty
+# function.js
+printf "function.js:4:3 = "
+assert_ok "$FLOW" type-at-pos function.js 4 3 --strip-root --pretty
+printf "function.js:8:3 = "
+assert_ok "$FLOW" type-at-pos function.js 8 3 --strip-root --pretty
+printf "function.js:12:3 = "
+assert_ok "$FLOW" type-at-pos function.js 12 3 --strip-root --pretty
+printf "function.js:16:3 = "
+assert_ok "$FLOW" type-at-pos function.js 16 3 --strip-root --pretty
+
+# function-poly-0.js
+printf "function-poly-0.js:3:10 = "
+assert_ok "$FLOW" type-at-pos function-poly-0.js 3 10 --strip-root --pretty
+printf "function-poly-0.js:3:30 = "
+assert_ok "$FLOW" type-at-pos function-poly-0.js 3 30 --strip-root --pretty
+# TODO fix leaking of 'T'
+printf "function-poly-0.js:4:7 = "
+assert_ok "$FLOW" type-at-pos function-poly-0.js 4 7 --strip-root --pretty
+
+# function-poly-1.js
+printf "function-poly-1.js:3:10 = "
+assert_ok "$FLOW" type-at-pos function-poly-1.js 3 10 --strip-root --pretty
+printf "function-poly-1.js:3:3 = "
+assert_ok "$FLOW" type-at-pos function-poly-1.js 3 33 --strip-root --pretty
+printf "function-poly-1.js:4:7 = "
+assert_ok "$FLOW" type-at-pos function-poly-1.js 4 7 --strip-root --pretty
+
+# function-poly-2.js
+printf "function-poly-2.js:3:10 = "
+assert_ok "$FLOW" type-at-pos function-poly-2.js 3 10 --strip-root --pretty
+printf "function-poly-2.js:4:12 = "
+assert_ok "$FLOW" type-at-pos function-poly-2.js 4 12 --strip-root --pretty
+printf "function-poly-2.js:5:5 = "
+assert_ok "$FLOW" type-at-pos function-poly-2.js 5 5 --strip-root --pretty
+# TODO shadowing Ts
+printf "function-poly-2.js:6:5 = "
+assert_ok "$FLOW" type-at-pos function-poly-2.js 6 5 --strip-root --pretty
+printf "function-poly-2.js:7:12 = "
+assert_ok "$FLOW" type-at-pos function-poly-2.js 7 12 --strip-root --pretty
+printf "function-poly-2.js:9:13 = "
+# TODO shadowing Ts
+assert_ok "$FLOW" type-at-pos function-poly-2.js 9 13 --strip-root --pretty
+printf "function-poly-2.js:11:12 = "
+assert_ok "$FLOW" type-at-pos function-poly-2.js 11 12 --strip-root --pretty
+
+# function-poly-3.js
+printf "function-poly-3.js:8:1 = "
+assert_ok "$FLOW" type-at-pos function-poly-3.js 8 1 --strip-root --pretty
+
+# function-poly-4.js
+printf "function-poly-4.js:3:11 = "
+assert_ok "$FLOW" type-at-pos function-poly-4.js 3 11 --strip-root --pretty
+printf "function-poly-4.js:7:7 = "
+assert_ok "$FLOW" type-at-pos function-poly-4.js 7 7 --strip-root --pretty
+printf "function-poly-4.js:9:7 = "
+assert_ok "$FLOW" type-at-pos function-poly-4.js 9 7 --strip-root --pretty
+
+# function-poly-5.js
+printf "function-poly-5.js:3:10 = "
+assert_ok "$FLOW" type-at-pos function-poly-5.js 3 10 --strip-root --pretty
 
 # generics.js
 printf "generics.js:5:1 = "
