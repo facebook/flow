4e95eaf8d9a08ff802d7d6edd2bfd83b07fde8cc
Marshall Roch <mroch@fb.com>
2018-04-10 13:45:12 -0700

    publish to npm from circle
    Summary:
    Circle is successfully packing flow-parser and flow-parser-bin. This diff makes Circle publish both to npm, instead of Travis which only builds flow-parser.
    
    This is untested until we tag a commit, but if it fails we can manually `npm publish` the packed artifact.
    
    Reviewed By: gabelevi
    
    Differential Revision: D7573981
    
    fbshipit-source-id: bb2c0e396aa42783ae2a1395dbf1afbae3d04d57

---
+++ .circleci/deploy_npm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+# make sure -x (debugging) is off so we don't print the token in the logs
+set +x
+
+# only run on tags
+if [[ "$CIRCLE_TAG" = "" ]]; then exit 0; fi
+
+if [ -f ~/.npmrc ]; then mv ~/.npmrc ~/.npmrc.bak; fi
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
+
+echo "Publishing flow-parser-bin@${TAG_V}";
+npm publish "dist/npm-flow-parser-bin.tgz";
+
+echo "Publishing flow-parser@${TAG_V}";
+npm publish "dist/npm-flow-parser.tgz";
+
+if [ -f ~/.npmrc.bak ]; then mv ~/.npmrc.bak ~/.npmrc; fi
