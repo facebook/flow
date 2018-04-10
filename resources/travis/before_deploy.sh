93f8ad52a20a78df24009fad361ded8847bb2916
Marshall Roch <mroch@fb.com>
2018-04-10 11:14:13 -0700

    remove OS X travis build
    Summary: this is done by Circle now
    
    Reviewed By: panagosg7
    
    Differential Revision: D7554832
    
    fbshipit-source-id: 0d9a444bfa36c923a6b9233fd3d90ca6d791ee90

--- resources/travis/before_deploy.sh
+++ resources/travis/before_deploy.sh
@@ -2,22 +2,9 @@
 
 if [[ "$TRAVIS_TAG" = "" ]]; then exit 0; fi
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $DIR/setup_opam.sh
-
-PLATFORM=$([ "$TRAVIS_OS_NAME" == "linux" ] && echo "linux64" || echo "$TRAVIS_OS_NAME")
-
-cp src/parser/dist/libflowparser.zip \
-  "$TRAVIS_BUILD_DIR/libflowparser-$PLATFORM-$TRAVIS_TAG.zip"
-
-make dist/flow.zip
-cp dist/flow.zip "$TRAVIS_BUILD_DIR/flow-$PLATFORM-$TRAVIS_TAG.zip"
-
 # We don't deploy the whole website on tags, but we do upload flow.js and the
 # libs. This sets up the directory we'll upload. See the deploy section of
 # .travis.yml.
-if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-  mkdir -p "$HOME/static/$TRAVIS_TAG"
-  cp "bin/flow.js" "$HOME/static/$TRAVIS_TAG/flow.js"
-  cp -r "lib" "$HOME/static/${TRAVIS_TAG}/flowlib"
-fi
+mkdir -p "$HOME/static/$TRAVIS_TAG"
+cp "bin/flow.js" "$HOME/static/$TRAVIS_TAG/flow.js"
+cp -r "lib" "$HOME/static/${TRAVIS_TAG}/flowlib"
