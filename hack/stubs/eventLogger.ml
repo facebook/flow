efe4d3814fff1018dabd004eec67fddd8ffadf90
Greg Nisbet <gregorynisbet@fb.com>
2018-04-13 10:12:32 -0700

    [hack] scuba logger exit_on_parent_exit
    Summary:
    Start a new OS thread outside the OCaml runtime in the scuba logger process that checks whether the scuba logger process had been reparented onto init once every ten seconds. If this condition is reached, then wait 60 seconds before terminating.
    
    Reparenting onto init only happens when the parent process has gone away.
    
    There aren't any circumstances under which the scuba logger process should outlive the parent.
    
    10 second interval, 60 seconds grace
    
    Reviewed By: dabek
    
    Differential Revision: D7541181
    
    fbshipit-source-id: cb07738f6450b8c36ca4a42634be04b138075ee6

--- hack/stubs/eventLogger.ml
+++ hack/stubs/eventLogger.ml
@@ -19,7 +19,7 @@ type init_mode =
   | Event_logger_fake
   | Event_logger_real of init_settings
 
-let init ?log_pid:_ ?init_id:_ _ _ = ()
+let init ?exit_on_parent_exit:_ ?log_pid:_ ?init_id:_ _ _ = ()
 let disable_logging _ = ()
 let log _ = ()
 let logger_pid () = None
