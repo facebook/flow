5265439bb5d5bd145be795bcff220823c0c7a4de
Marshall Roch <mroch@fb.com>
2018-04-18 17:30:09 -0700

    fixes for CircleCI npm publish script
    Summary: i guess `npm publish` only thinks it's a tarball if it looks like a relative path, otherwise it assumes it's a github repo. also had to fix the executable permission.
    
    Reviewed By: fishythefish
    
    Differential Revision: D7626924
    
    fbshipit-source-id: d146e5bf2d1dc52a11df0d645b0dc13fc9dac104

--- .circleci/deploy_npm.sh
+++ .circleci/deploy_npm.sh
@@ -9,10 +9,10 @@ if [[ "$CIRCLE_TAG" = "" ]]; then exit 0; fi
 if [ -f ~/.npmrc ]; then mv ~/.npmrc ~/.npmrc.bak; fi
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
 
-echo "Publishing flow-parser-bin@${TAG_V}";
-npm publish "dist/npm-flow-parser-bin.tgz";
+echo "Publishing flow-parser-bin";
+npm publish ./dist/npm-flow-parser-bin.tgz;
 
-echo "Publishing flow-parser@${TAG_V}";
-npm publish "dist/npm-flow-parser.tgz";
+echo "Publishing flow-parser";
+npm publish ./dist/npm-flow-parser.tgz;
 
 if [ -f ~/.npmrc.bak ]; then mv ~/.npmrc.bak ~/.npmrc; fi
