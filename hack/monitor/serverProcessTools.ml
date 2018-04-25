08c7e37ed67066c0b3c744be4a36bbfb28463c2d
Alexander Chow <achow@fb.com>
2018-04-24 17:47:05 -0700

    [hack] Worker_oomed also logged as is_oom
    Summary: Worker_oomed also logged as is_oom
    
    Reviewed By: dabek
    
    Differential Revision: D7734421
    
    fbshipit-source-id: bec0a87d7ee051af8a304d7a96fac19877931a27

--- hack/monitor/serverProcessTools.ml
+++ hack/monitor/serverProcessTools.ml
@@ -16,7 +16,12 @@ let check_exit_status proc_stat process monitor_config  =
   | _ ->
     let exit_kind, exit_code = Exit_status.unpack proc_stat in
     Hh_logger.log "typechecker %s with exit code %d\n" exit_kind exit_code;
-    let is_oom = try Sys_utils.check_dmesg_for_oom process.pid "hh_server" with _ -> false in
+    let is_oom = match proc_stat with
+      | Unix.WEXITED i when i = (Exit_status.exit_code Exit_status.Worker_oomed) -> true
+      | _ -> false
+    in
+    let is_oom = is_oom || try
+      Sys_utils.check_dmesg_for_oom process.pid "hh_server" with _ -> false in
     let time_taken = Unix.time () -. process.start_t in
     HackEventLogger.bad_exit
       time_taken proc_stat
