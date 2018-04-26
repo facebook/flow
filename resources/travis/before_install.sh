aab7913c003c70b847c438be07fa62766cb986d5
Marshall Roch <mroch@fb.com>
2018-04-25 23:57:15 -0700

    Remove Travis
    Summary: I moved everything over to Circle
    
    Reviewed By: nmote
    
    Differential Revision: D7754244
    
    fbshipit-source-id: 7f34ef4679b990bfb33afc72975069d9b6564a27

--- resources/travis/before_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source "$DIR/setup_node.sh"
--- resources/travis/setup_node.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source "$HOME/.nvm/nvm.sh"
-nvm install 6
-nvm use 6
