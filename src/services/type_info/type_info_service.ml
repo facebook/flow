19fc6ad3d7ed3b8a636f77617b53abfa4b75a768
Panagiotis Vekris <pvekris@fb.com>
2018-04-27 17:40:10 -0700

    Refactor Lwt and Result integration in type_info_service.ml
    Summary:
    Use `Lwt.(>|=)` to replace the pattern:
    ```
    let%lwt result = lwt_op in
    result >>| process_result
    |> Lwt.return
    ```
    with the more concise:
    ```
    lwt_op >|= map ~f:process_result
    ```
    including some variants of this.
    
    Reviewed By: samwgoldman
    
    Differential Revision: D7778094
    
    fbshipit-source-id: 89bffa855316b15c20a9592267fb4fbcad0a774b

--- src/services/type_info/type_info_service.ml
+++ src/services/type_info/type_info_service.ml
@@ -6,11 +6,13 @@
  *)
 
 open Core_result
+let (>|=) = Lwt.(>|=)
 
 let type_at_pos ~options ~workers ~env ~profiling file content line col =
-  let%lwt result = Types_js.basic_check_contents ~options ~workers ~env ~profiling content file in
-  map_error ~f:(fun str -> str, None) result
-  >>| (fun (cx, _info) ->
+  Types_js.basic_check_contents ~options ~workers ~env ~profiling content file >|=
+  function
+  | Error str -> Error (str, None)
+  | Ok (cx, _info) ->
     let loc = Loc.make file line col in
     let json_data, loc, ty =
       let mk_data result_str loc ty_json = Hh_json.JSON_Object [
@@ -20,47 +22,42 @@ let type_at_pos ~options ~workers ~env ~profiling file content line col =
       ] in
       Query_types.(match query_type cx loc with
         | FailureNoMatch ->
-          Hh_json.JSON_Object ["result", Hh_json.JSON_String "FAILURE_NO_MATCH"], Loc.none, None
+          Hh_json.JSON_Object [
+            "result", Hh_json.JSON_String "FAILURE_NO_MATCH"
+          ], Loc.none, None
         | FailureUnparseable (loc, gt, _) ->
-          mk_data "FAILURE_UNPARSEABLE" loc (Hh_json.JSON_String (Type.string_of_ctor gt)), loc, None
+          let json = Hh_json.JSON_String (Type.string_of_ctor gt) in
+          mk_data "FAILURE_UNPARSEABLE" loc json, loc, None
         | Success (loc, ty) ->
           (* TODO use Ty_debug.json_of_t after making it faster using
              count_calls *)
-          mk_data "SUCCESS" loc (Hh_json.JSON_String (Ty_printer.string_of_t ty)),
-            loc, Some ty
-      )
-    in
+          let json = Hh_json.JSON_String (Ty_printer.string_of_t ty) in
+          mk_data "SUCCESS" loc json, loc, Some ty
+      ) in
+
     let ty = Option.map ~f:Ty_printer.string_of_t ty in
-    (loc, ty), Some json_data
-  ) |> Lwt.return
+    Ok ((loc, ty), Some json_data)
 
 let dump_types ~options ~workers ~env ~profiling file content =
   (* Print type using Flow type syntax *)
   let printer = Ty_printer.string_of_t in
+  Types_js.basic_check_contents ~options ~workers ~env ~profiling content file >|=
+  map ~f:(fun (cx, _info) -> Query_types.dump_types ~printer cx)
 
-  let%lwt result = Types_js.basic_check_contents ~options ~workers ~env ~profiling content file in
-  result
-  >>| (fun (cx, _info) -> Query_types.dump_types ~printer cx)
-  |> Lwt.return
 
 let coverage ~options ~workers ~env ~profiling ~force file content =
   let should_check =
-    if force then
-      true
-    else
+    if force then true else
       let (_, docblock) =
         Parsing_service_js.(parse_docblock docblock_max_tokens file content) in
       Docblock.is_flow docblock
   in
-  let%lwt result = Types_js.basic_check_contents ~options ~workers ~env ~profiling content file in
-  result
-  >>| (fun (cx, _) -> Query_types.covered_types cx ~should_check)
-  |> Lwt.return
+  Types_js.basic_check_contents ~options ~workers ~env ~profiling content file >|=
+  map ~f:(fun (cx, _) -> Query_types.covered_types cx ~should_check)
 
 let suggest ~options ~workers ~env ~profiling file content =
-  let%lwt result = Types_js.typecheck_contents ~options ~workers ~env ~profiling content file in
-  Lwt.return @@
-  match result with
+  Types_js.typecheck_contents ~options ~workers ~env ~profiling content file >|=
+  function
   | (Some (cx, ast), tc_errors, tc_warnings) ->
     let cxs = Query_types.suggest_types cx in
     let visitor = new Suggest.visitor ~cxs in
