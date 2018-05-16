c6c96d60bc3aa4b0777affd195c617dcef3508cc
Nat Mote <nmote@fb.com>
2018-05-15 17:42:30 -0700

    Find-refs tests for ES6 default exports/imports
    Summary:
    Once I'm finished, all of the newly-added test cases will return the same
    results.
    
    Reviewed By: panagosg7
    
    Differential Revision: D7958042
    
    fbshipit-source-id: 9319fb3a03396a18e0934c369fc848e1bd35493d

--- tests/find-refs-global/test.sh
+++ tests/find-refs-global/test.sh
@@ -5,6 +5,36 @@ FLOW=$1
 echo "simple ES6 exports/imports:"
 assert_ok "$FLOW" find-refs --global --json --pretty --strip-root es6-1.js 3 17
 
+echo "ES6 default export:"
+# export default class Bar {}
+#           ^
+assert_ok "$FLOW" find-refs --global --json --pretty --strip-root es6-1.js 7 11
+
+echo "ES6 default export from the identifier:"
+# export default class Bar {}
+#                      ^
+assert_ok "$FLOW" find-refs --global --json --pretty --strip-root es6-1.js 7 22
+
+echo "ES6 default export from a use in the file where it is defined:"
+# new Bar();
+#     ^
+assert_ok "$FLOW" find-refs --global --json --pretty --strip-root es6-1.js 25 5
+
+echo "ES6 default import:"
+# import Bar, {foo, Foo} from './es6-1';
+#        ^
+assert_ok "$FLOW" find-refs --global --json --pretty --strip-root es6-2.js 3 8
+
+echo "Use of ES6 default import:"
+# new Bar();
+#      ^
+assert_ok "$FLOW" find-refs --global --json --pretty --strip-root es6-2.js 19 6
+
+echo "Use of ES6 default import imported through import *"
+# new all.default();
+#          ^
+assert_ok "$FLOW" find-refs --global --json --pretty --strip-root es6-2.js 20 10
+
 echo
 
 echo "shadowing an export:"
