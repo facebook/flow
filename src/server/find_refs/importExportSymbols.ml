d275957751d78e2f13075f9734bbe3378b364c2b
Nat Mote <nmote@fb.com>
2018-05-16 12:56:12 -0700

    Include local uses of ES6 default imports
    Summary:
    See D7961883 for more context. Now, all the tests introduced in D7958042 return
    the same results, as desired.
    
    Reviewed By: panagosg7
    
    Differential Revision: D8000218
    
    fbshipit-source-id: be3dc26f8795b107ed7cae4f224246026d3c9796

--- src/server/find_refs/importExportSymbols.ml
+++ src/server/find_refs/importExportSymbols.ml
@@ -17,12 +17,33 @@ let find_related_symbol_from_export loc = function
       None
   | _ -> None
 
-let find_related_symbol file_sig loc =
-  match file_sig.module_sig.module_kind with
+let find_related_symbol_from_module_kind loc = function
   | CommonJS _ -> None
   | ES {named; _} ->
     let exports = SMap.values named in
     ListUtils.first_some_map (find_related_symbol_from_export loc) exports
+
+let find_related_symbol_from_require loc = function
+  | Import {named; _} ->
+    begin match SMap.get "default" named with
+      | None -> None
+      | Some local_name_map ->
+        let locs =
+          SMap.values local_name_map
+          |> List.map (Nel.to_list)
+          |> List.concat
+        in
+        if List.mem loc locs then Some loc else None
+    end
+  | _ -> None
+
+let find_related_symbol_from_requires loc requires =
+  ListUtils.first_some_map (find_related_symbol_from_require loc) requires
+
+let find_related_symbol file_sig loc =
+  match find_related_symbol_from_module_kind loc file_sig.module_sig.module_kind with
+    | Some _ as result -> result
+    | None -> find_related_symbol_from_requires loc file_sig.module_sig.requires
 
 let find_related_symbols file_sig starting_locs =
   List.map (find_related_symbol file_sig) starting_locs
