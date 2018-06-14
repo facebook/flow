8076766434a0b8eee6b8bb4484bd14c7cd56ea40
Gabe Levi <gabe@fb.com>
2018-06-14 14:37:43 -0700

    [hack] Buffered_line_reader_lwt
    Summary:
    Buffered_line_reader exists because it's difficult to mix buffered channels with Unix.select.
    
    I'm introducing Buffered_line_reader_lwt because it's difficult to mix lwt buffered channels with Lwt_unix.wait_read
    
    I plan to use this in Watchman_lwt (Watchman uses Buffered_line_reader)
    
    Reviewed By: avikchaudhuri
    
    Differential Revision: D8249206
    
    fbshipit-source-id: 50d3305e10300d5be273bd6c1f47349d2481c12f

--- hack/utils/buffered_line_reader.mli
+++ hack/utils/buffered_line_reader.mli
@@ -7,39 +7,7 @@
  *
 *)
 
-(**
- * This module is needed because Unix.select doesn't play well with
- * input_line on Ocaml channels.. i.e., when a buffered read into an
- * Ocaml channel consumes two complete lines from the file descriptor, the next
- * select will say there is nothing to read when in fact there is
- * something in the channel. This wouldn't be a problem if Ocaml channel's API
- * supported a "has buffered content" call, so you could check if the
- * buffer contains something as well as doing a Unix select to know for real if
- * there is content coming.
- *
- * The "has_buffered_content" method below does exactly that.
- *
- * is_readable is a friendly wrapper around "has_buffered_content" and
- * non-blocking Unix.select.
- *)
+include Buffered_line_reader_sig.S with type 'a result = 'a and type fd = Unix.file_descr
 
-type t
-
-val create: Unix.file_descr -> t
-
-val get_null_reader: unit -> t
-
-val has_buffered_content: t -> bool
-
-(**
-  * Returns true if and only if there is content to be read (does not know if
-  * the incoming content is newline-terminated. So we can't actually know
-  * if get_next_line will be non-blocking.
-  *)
-val is_readable : t -> bool
-
-val get_fd: t -> Unix.file_descr
-
-val get_next_line: ?approx_size: int -> t -> string
-
-val get_next_bytes: t -> int -> string
+module Functor : functor (Reader: Buffered_line_reader_sig.READER)
+  -> Buffered_line_reader_sig.S with type 'a result = 'a Reader.result and type fd = Reader.fd
