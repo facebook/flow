3b08afc96aed8a0a310ad2ff5ca78730385e51a3
Moti Zilberman <motiz88@gmail.com>
2018-06-23 12:09:27 +0100

    Add React lifecycles getDerivedStateFromProps, getSnapshotBeforeUpdate
    Closes #6101, closes #6104.

---
+++ tests/react_16.3_lifecycles/test.js
@@ -0,0 +1,49 @@
+import * as React from "react";
+
+class A extends React.Component<{foo: string}, {bar: string}> {
+    static getDerivedStateFromProps(props, state) {
+        let num: number;
+        num = props.foo; // error: string ~> number
+        num = state.bar; // error: string ~> number
+
+        let str: string;
+        str = props.foo; // ok
+        str = state.bar; // ok
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        let num: number;
+        num = prevProps.foo; // error: string ~> number
+        num = prevState.bar; // error: string ~> number
+
+        let str: string;
+        str = prevProps.foo; // ok
+        str = prevState.bar; // ok
+    }
+}
+
+class B extends React.Component<void, {foo: number, bar: string}> {
+    static getDerivedStateFromProps(props, state) {
+        return {foo: 1}; // ok
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        return null; // ok
+    }
+}
+
+class C extends React.Component<void, {foo: number, bar: string}> {
+    static getDerivedStateFromProps(props, state) {
+        return {foo: "nope"}; // error: string ~> number
+    }
+}
+
+class D extends React.Component<{foo: string}, {bar: string}> {
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        return 123; // ok
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        let x: number = snapshot; // ok
+    }
+}
