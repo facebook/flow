829f54f535bf27449ddc67d62881c16b28bc103d
Nat Mote <nmote@fb.com>
2018-06-26 17:58:58 -0700

    Add additional destructuring example
    Summary:
    Currently this use of `x` is not included in the list of references. This will
    be fixed in an upcoming diff. This avoids the noise of line number adjustments
    later.
    
    Reviewed By: panagosg7
    
    Differential Revision: D8626867
    
    fbshipit-source-id: 0c1ffa55e6d1da641a928ee488838135cbf479c7

--- tests/find-refs-local/test.sh
+++ tests/find-refs-local/test.sh
@@ -39,7 +39,7 @@ assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 38 7
 # assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 50 4
 
 printf "\nImports:\n"
-assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 55 2
+assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 57 2
 # This is a type, which doesn't work yet
 # assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 55 9
 
@@ -47,22 +47,22 @@ assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 55 2
 # assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 58 9
 
 printf "\nExports:\n"
-assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 64 20
-assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 65 6
+assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 66 20
+assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 67 6
 
 printf "\nMethods and properties:\n\n"
 printf "Method declaration: "
-assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 70 3
+assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 72 3
 printf "Property declaration: "
-assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 71 5
+assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 73 5
 printf "Method call (finds definition and other references): "
-assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 80 13
+assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 82 13
 
 printf "Method call on an imported class (finds other references but not the definition since it's in another file): "
-assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 90 16
+assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 92 16
 
 printf "Method call within a class that has type params: "
-assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 96 10
+assert_ok "$FLOW" find-refs --json --pretty --strip-root locals.js 98 10
 
 printf "Instance method on a superclass: "
 assert_ok "$FLOW" find-refs --json --pretty --strip-root classInheritance.js 5 3
