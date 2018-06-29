fc0f2277a7b64073967c2ed37bed2f1f1a0117e4
Nat Mote <nmote@fb.com>
2018-06-29 10:25:28 -0700

    Add string_of_ref_kind to FindRefsTypes
    Summary: This has been useful for debugging. Might as well commit it.
    
    Reviewed By: panagosg7
    
    Differential Revision: D8628372
    
    fbshipit-source-id: be56548270e8106535e23f3db2e52254985ad3b7

--- src/server/find_refs/findRefsTypes.ml
+++ src/server/find_refs/findRefsTypes.ml
@@ -11,6 +11,12 @@ type ref_kind =
   | Local
   | Other
 
+let string_of_ref_kind = function
+  | PropertyDefinition -> "PropertyDefinition"
+  | PropertyAccess -> "PropertyAccess"
+  | Local -> "Local"
+  | Other -> "Other"
+
 type single_ref = ref_kind * Loc.t
 type find_refs_found = (string * single_ref list)
 type find_refs_ok = find_refs_found option
