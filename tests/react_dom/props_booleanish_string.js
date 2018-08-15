5363e88216c5a6c4c5e6d9ed51d7360a48ca5b34
Moti Zilberman <motiz88@gmail.com>
2018-08-15 15:56:42 +0100

    Remove empty string from React DOM booleans/enums
    This is a design choice documented in
    https://github.com/facebook/flow/pull/6727#user-content-forbid-empty-string

--- tests/react_dom/props_booleanish_string.js
+++ tests/react_dom/props_booleanish_string.js
@@ -7,6 +7,6 @@ import React from "react";
 <div contentEditable={null} />; // Error: Expected booleanish string
 <div contentEditable="true" />; // OK
 <div contentEditable="false" />; // OK
-<div contentEditable="" />; // OK
+<div contentEditable="" />; // Error: Expected booleanish string
 <div contentEditable="yes" />; // Error: Expected booleanish string
 <div contentEditable="contentEditable" />; // Error: Expected booleanish string
