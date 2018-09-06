87ea87bec92cf0144f9846126fd5e490ee401c1c
Aurélien Le Masson <ojisama.san@gmail.com>
2018-09-06 19:33:53 +0200

    fix(react_functional): add missing render and fix typo

--- tests/react_functional/test.js
+++ tests/react_functional/test.js
@@ -1,12 +1,12 @@
 import * as React from "react";
 
-function F(props: { foo: string }) {}
+function F(props: { foo: string }) { return null }
 <F />; // error: missing `foo`
 <F foo={0} />; // error: number ~> string
 <F foo="" />; // ok
 
 // props subtyping is property-wise covariant
-function G(props: { foo: string|numner }) {}
+function G(props: { foo: string|number }) { return null }
 <G foo="" />; // ok
 
 var Z = 0;
