13a5571fc33fa391ae785f77e4d7a0f0cc70ab74
Sinan Cepel <sinancepel@fb.com>
2018-09-05 17:20:12 -0700

    introduce functions to save virtual memory state

--- hack/heap/hh_shared_sqlite.h
+++ hack/heap/hh_shared_sqlite.h
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2015, Facebook, Inc.
  * All rights reserved.
- *
+*
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the "hack" directory of this source tree.
  *
@@ -10,26 +10,12 @@
 #ifndef HH_SHARED_SQLITE_H
 #define HH_SHARED_SQLITE_H
 
-#ifdef _WIN32
-#include <windows.h>
-#else
-#include <stdint.h>
-#endif
-
 #ifndef NO_SQLITE3
 #include <sqlite3.h>
-#endif
 
-
-#ifndef NO_SQLITE3
 typedef sqlite3 *sqlite3_ptr;
-#else
-typedef void *sqlite3_ptr;
-#endif
 
-#ifndef NO_SQLITE3
 #define assert_sql(x, y) (assert_sql_with_line((x), (y), __LINE__))
-#endif
 
 
 void assert_sql_with_line(
@@ -37,7 +23,6 @@ void assert_sql_with_line(
   int correct_result,
   int line_number);
 
-#ifndef NO_SQLITE3
 void make_all_tables(sqlite3 *db);
 
 void hhfi_insert_row(
@@ -47,7 +32,6 @@ void hhfi_insert_row(
   int64_t kind,
   const char *filespec
 );
-#endif
 
 char *hhfi_get_filespec(
   sqlite3_ptr db,
@@ -57,4 +41,5 @@ char *hhfi_get_filespec(
 void hhfi_init_db(const char *path);
 void hhfi_free_db(void);
 sqlite3_ptr hhfi_get_db(void);
-#endif
+#endif /* NO_SQLITE3 */
+#endif /* HH_SHARED_SQLITE_H */
