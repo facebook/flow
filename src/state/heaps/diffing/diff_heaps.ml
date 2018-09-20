262776d3d51c130bccf39776802d33ad8a3f46ed
Paco Estevez Garcia <festevezga@fb.com>
2018-09-20 05:54:26 -0700

    Add heaps for diff information
    Summary: This diff adds a heap to store the patches necessary to update a file after a codemod. This includes the offset start, end, and the string to be inserted.
    
    Reviewed By: pvdz
    
    Differential Revision: D9886306
    
    fbshipit-source-id: 890b1a7ff190f75535f5d6ba2970f78eb9265318

---
+++ src/state/heaps/diffing/diff_heaps.ml
@@ -0,0 +1,31 @@
+(**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *)
+
+(*
+ * The Diff Heap is specialized to store the patches used by the Replacement_printer
+ * when updating files that have been transformed by a worker process.
+ *)
+type patch = (int * int * string) list
+
+type key = File_key.t
+
+module DiffPatchHeap =
+  SharedMem_js.NoCache
+    (File_key)
+    (struct
+      type t = patch
+
+      let prefix = Prefix.make ()
+
+      let description = "DiffPatch"
+
+      let use_sqlite_fallback () = false
+    end)
+
+let set_diff = Expensive.wrap DiffPatchHeap.add
+
+let get_diff = DiffPatchHeap.get
---
+++ src/state/heaps/diffing/diff_heaps.mli
@@ -0,0 +1,14 @@
+(**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *)
+
+type key = File_key.t
+
+type patch = (int * int * string) list
+
+val set_diff : (key -> patch -> unit) Expensive.t
+
+val get_diff : key -> patch option
