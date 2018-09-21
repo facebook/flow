9261aa353c0c14418ec0f03001334ea7e1cdc333
Gabe Levi <gabe@fb.com>
2018-09-21 06:41:18 -0700

    Flow_build_id
    Reviewed By: pakoito
    
    Differential Revision: D9663488
    
    fbshipit-source-id: 3d64ea6bf6e56ac32083ac89fe6d8dd373f81a50

---
+++ src/common/build_id/flow_build_id.ml
@@ -0,0 +1,18 @@
+(**
+ * Copyright (c) 2018-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *)
+
+let build_id = ref None
+
+let get_build_id () =
+  match !build_id with
+  | None ->
+    let state = Xx.init () in
+    Xx.update state (Sys_utils.cat Sys.executable_name);
+    let hash = Xx.digest state |> Xx.to_string in
+    build_id := Some hash;
+    hash
+  | Some hash -> hash
--- src/common/xx/xx.ml
+++ src/common/xx/xx.ml
@@ -1,7 +1,12 @@
 type state
-type hash (* = string *)
+type hash
 
 external init: unit -> state = "caml_xx_init"
 external update: state -> string -> unit = "caml_xx_update" [@@noalloc]
 external update_int: state -> 'a (* int *) -> unit = "caml_xx_update_int" [@@noalloc]
 external digest: state -> hash = "caml_xx_digest"
+
+(* Returns a 16 character hex string representation of the hash *)
+external to_string: hash -> string = "caml_xx_to_string"
+
+let foo x = x
