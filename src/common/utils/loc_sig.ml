c29bea586fcac6cdbf9c3785535193ae67224128
Nat Mote <nmote@fb.com>
2018-10-30 12:59:09 -0700

    Loc_Sig.S.debug_to_string
    Summary: Explained in the comment
    
    Reviewed By: jbrown215
    
    Differential Revision: D12809316
    
    fbshipit-source-id: a2ef0855b8e97411a9066b3be68ae91b957fe600

--- src/common/utils/loc_sig.ml
+++ src/common/utils/loc_sig.ml
@@ -9,6 +9,9 @@ module type S = sig
   type t
   val compare: t -> t -> int
   val equal: t -> t -> bool
+  (* Exposes the underlying representation of the location. Use for debugging purposes only. Do not
+   * expose these results in user output or make typecheker behavior depend on it. *)
+  val debug_to_string: ?include_source:bool -> t -> string
 
   module LMap: (MyMap.S with type key = t)
   module LSet: (Set.S with type elt = t)
@@ -18,6 +21,7 @@ module LocS = struct
   type t = Loc.t
   let compare = Loc.compare
   let equal = Loc.equal
+  let debug_to_string = Loc.to_string
 
   module LMap = Utils_js.LocMap
   module LSet = Utils_js.LocSet
@@ -27,6 +31,7 @@ module ALocS = struct
   type t = ALoc.t
   let compare = ALoc.compare
   let equal = ALoc.equal
+  let debug_to_string = ALoc.to_string
 
   module LMap = Utils_js.ALocMap
   module LSet = Utils_js.ALocSet
