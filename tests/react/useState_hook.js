2a834d8a4a7e45f2355873d77ffc3bab5bb81c33
Jordan Brown <jmbrown@fb.com>
2018-11-19 09:40:11 -0800

    [react] Get rid of polymorphic type aliases in react hooks api
    Summary:
    Polymorphic type aliases have a known bug where they may cause unnecessary unification. This causes a [problem for the react hooks API types](https://flow.org/try/#0CYUwxgNghgTiAEAXAngBwQISgZwJZgGVEpEQBBMRXAewDsAeAgPngF54AKAtlggSngAfeAQDcAKFCRYCFOngARXNlQkwAC3pkW7Mj3gA3armASp0OEjQIAslGQAjEAGEIIKLQCqqAGK028AD8HBwCrCwAtrgAHiDAfKLw4uIA9CnwAHLUiPAArni0AOZW8lAQuDgg2JLgFggg0ajUMDkAZrm0lDT++SBEJCAAjIxMHOLwE-C4tLhUZf2kAFycofr8QiIANOPwfMsA2gSbK9zhIgLC62dGJgC6EqnpngXFcghlFdhVNdKWDU0teDtTpUOh5L4LEAAJhGY0mUxmcwgkOWITCvAuWx2e3gh2OShUak0WDwhGIpAooIYzCY9x28PEwK6YNI2EQqwA3jswHQ2biAI65EAwZCDY5fRAARSFIshg1uAV6cpCtFyEAgy0CHOErWo1GWqoiThgQgAvnwEjsJdLhchlRzdfr4ABmc0PCY82h8-aC21Q8UgKUyu3k6EK9hK0NQlVqjVBbVAvUG3JG4Vmi0SCbW4OQ6MOpMut3iU1AA).
    
    To get around this, we can get rid of the type aliases until this bug is fixed. The lack of unification made one test have confusing behavior-- I have a comment inline explaining why it errored before and why it requires an explicit type argument to retain its behavior.
    
    Reviewed By: samwgoldman
    
    Differential Revision: D13083694
    
    fbshipit-source-id: 4a328618119e68c8801fa225a221929f7275b016

--- tests/react/useState_hook.js
+++ tests/react/useState_hook.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 React.useState(); // Error: function requires another argument.
 
-const [count, setCount] = React.useState(1);
+const [count, setCount] = React.useState<number>(1);
 
 (count: number); // Ok
 (count: string); // Error: number is incompatible with string
