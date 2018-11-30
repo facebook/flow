0991601249f05ef18971eefa38c9585d8f9da1bb
Jordan Brown <jmbrown@fb.com>
2018-11-30 12:31:14 -0800

    [react] AbstractComponent DefaultProps can be accessed via .defaultProps
    Summary: DefaultProps mirrors the type of the .defaultProps field on the component. Allowing users to get/set that property opens up new exciting opportunities for HOCs, which I will show in a stacked commit.
    
    Reviewed By: samwgoldman
    
    Differential Revision: D13027633
    
    fbshipit-source-id: 1b905a38413700b2961ede2bf140cbc0c0cb8800

---
+++ tests/react_abstract_component/rw_default_props.js
@@ -0,0 +1,12 @@
+//@flow
+
+declare var x: React$AbstractComponent<{foo: number}, {foo: number}, void>;
+
+(x.defaultProps: {foo: number});
+x.defaultProps = {foo: 3};
+
+(x.defaultProps: {foo: number, bar: number}); // Error, bar not in defaultProps.
+x.defaultProps = {foo: 3, bar: 3}; // Ok, default props still hold, but won't be reflected in
+                                   // config calculation.
+
+x.defaultProps = {}; // error, missing foo.
