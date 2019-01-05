7ff44dc334ba4a5e3dee470972e246e0472a24d2
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2019-01-05 03:52:43 +0100

    Introduce gitignore matching and exclusion logic for patterns in ignored, untyped and declarations sections.

--- src/commands/commandUtils.ml
+++ src/commands/commandUtils.ml
@@ -461,6 +461,11 @@ let collect_flowconfig_flags main ignores_str untyped_str declarations_str inclu
   let raw_lint_severities = list_of_string_arg lints_str in
   main { ignores; includes; libs; raw_lint_severities; untyped; declarations; }
 
+let remove_exclusion pattern =
+  if Base.String.is_prefix pattern "!" then (
+    String.sub pattern 1 ((String.length pattern) - 1)
+  ) else pattern
+
 let file_options =
   let default_lib_dir ~no_flowlib tmp_dir =
     let root = Path.make (Tmp.temp_dir tmp_dir "flowlib") in
@@ -477,6 +482,7 @@ let file_options =
      let root = Path.to_string root
        |> Sys_utils.normalize_filename_dir_sep in
      let reg = s
+	   |> remove_exclusion
        |> Str.split_delim Files.project_root_token
        |> String.concat root
        |> Str.regexp in
