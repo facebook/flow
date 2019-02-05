eba9109c8602871c8c6fda846664e4e9fa81febd
Braden Napier <brady.napier@dashos.net>
2019-02-04 22:43:44 -0800

    resolve_alias extra tests to handle more cases

--- tests/config_module_system_node_resolve_alias/toplevel.js
+++ tests/config_module_system_node_resolve_alias/toplevel.js
@@ -1,6 +1,12 @@
 // @flow
 
-import {name} from "testproj";
+import { name as one } from "testproj";
+import { name as two } from "testproj2";
 
-(name: "custom_resolve_dir/testproj"); 
-(name: "node_modules/testproj"); // Error: Resolve from resolve_alias first!
+(one: "custom_resolve_dir/testproj");
+(one: "node_modules/testproj"); // Error: Resolve from resolve_alias first!
+
+// at base level we resolve local testproj2
+// as opposed to the local custom_resolve_dir
+// in the subdir test
+(two: "custom_resolve_dir/testproj2");
