022aa1c86c0273378f7c92d30f9fc5dcee34efc1
Braden Napier <brady.napier@dashos.net>
2019-02-04 22:43:44 -0800

    resolve_alias extra tests to handle more cases

--- tests/config_module_system_node_resolve_alias/subdir/sublevel.js
+++ tests/config_module_system_node_resolve_alias/subdir/sublevel.js
@@ -1,7 +1,17 @@
 // @flow
+import { name as one } from "testproj";
+import { name as two } from "testproj2";
+import { name as three } from "testproj3";
+import { name as four } from "testproj4";
 
-import {name} from "testproj2";
+(one: "custom_resolve_dir/testproj");
 
-(name: "subdir/custom_resolve_dir/testproj2");
-(name: "custom_resolve_dir/testproj2"); // Error: Resolve from sibling 'custom_resolve_dir' first!
-(name: "node_modules/testproj2"); // Error: Resolve from sibling 'custom_resolve_dir' first!
+(two: "subdir/custom_resolve_dir/testproj2");
+(two: "custom_resolve_dir/testproj2"); // Error: Resolve from sibling 'custom_resolve_dir' first!
+(two: "node_modules/testproj2"); // Error: Resolve from sibling 'custom_resolve_dir' first!
+
+// should still resolve node_modules
+(three: "node_modules/testproj3");
+
+// should still resolve custom_node_modules
+(four: "custom_node_modules/testproj4");
