36e42eadd62f0cbb81daea963a74817a4038c6f0
Daniel Caceres Salas <danielcaceres@fb.com>
2019-02-12 02:25:32 -0800

    Add PPX for constant expressions
    Summary: Add a new entry point to the parser from the Expression node.
    
    Reviewed By: pakoito
    
    Differential Revision: D13901245
    
    fbshipit-source-id: 79fadfc329606da2018011e0e27b3e5e99377ddc

--- src/parser/parser_flow.ml
+++ src/parser/parser_flow.ml
@@ -343,6 +343,17 @@ let do_parse env parser fail =
   then raise (Error.Error error_list);
   ast, error_list
 
+(* Makes the input parser expect EOF at the end. Use this to error on trailing
+ * junk when parsing non-Program nodes. *)
+let with_eof parser =
+  fun env ->
+    let ast = parser env in
+    Expect.token env T_EOF;
+    ast
+
+let parse_expression env fail =
+  do_parse env (with_eof Parse.expression) fail
+
 let parse_program fail ?(token_sink=None) ?(parse_options=None) filename content =
   let env = init_env ~token_sink ~parse_options filename content in
   do_parse env Parse.program fail
