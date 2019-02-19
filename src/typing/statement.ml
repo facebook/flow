fee1a755b9de13b8308fdac0d6082fafec277346
Sam Goldman <samgoldman@fb.com>
2019-02-19 14:41:58 -0800

    Remove unused ignore
    Summary:
    This TODO was added during Typed AST development, because recovering ASTs for
    interface/declare class through generate_tests was not yet done. This is now
    accomplished through a callback and refs.
    
    Reviewed By: pakoito
    
    Differential Revision: D14139494
    
    fbshipit-source-id: a04d2b35562c5a32b8f5fd5441f5ddfa2162698a

--- src/typing/statement.ml
+++ src/typing/statement.ml
@@ -471,10 +471,10 @@ and statement cx : 'a -> (ALoc.t, ALoc.t * Type.t) Ast.Statement.t = Ast.Stateme
 
   let interface_helper cx loc (iface_sig, self) =
     let def_reason = mk_reason (desc_of_t self) loc in
-    iface_sig |> Class_sig.generate_tests cx (fun iface_sig ->
+    Class_sig.generate_tests cx (fun iface_sig ->
       Class_sig.check_super cx def_reason iface_sig;
       Class_sig.check_implements cx def_reason iface_sig
-    ) |> ignore (* TODO(vijayramamurthy) *);
+    ) iface_sig;
     let t = Class_sig.classtype ~check_polarity:false cx iface_sig in
     Flow.unify cx self t;
     Type_table.set (Context.type_table cx) loc t;
