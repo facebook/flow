51c52859024e57ce67156abd1268d7023235aa0a
Vincent Siles <vsiles@fb.com>
2019-02-22 09:28:02 -0800

    [hack] Update dune build to use third-party-buck instead of system lib
    Summary:
    Current dune build relies on pkg-config and system libs for lz4 and sqlite3.
    This patch instruments cmake scripts to thread the correct information to
    dune. In case the libs are not found in third-party-buck, the system will
    fallback to pkg-config.
    
    Reviewed By: fredemmott
    
    Differential Revision: D14103613
    
    fbshipit-source-id: 8cdb38c2ba197ec5c95ca21aefef8784f79df102

--- hack/heap/config/discover.ml
+++ hack/heap/config/discover.ml
@@ -4,6 +4,32 @@ https://jbuilder.readthedocs.io/en/latest/configurator.html *)
 module C = Configurator.V1
 module P = C.Pkg_config
 
+(* cmake should have prepared some information for us in the env:
+  EXTRA_INCLUDE_PATHS
+  EXTRA_LIB_PATHS
+  EXTRA_NATIVE_LIBRARIES
+*)
+let query_env s =
+  match Sys.getenv s with
+  | t -> Some t
+  | exception Not_found -> None
+
+let process_env () =
+  let open Option in
+  let open String in
+  let split = split_on_char ' ' in
+  let includes = (query_env "EXTRA_INCLUDE_PATHS") >>| split
+  and libs = (query_env "EXTRA_LIB_PATHS") >>| split
+  and names = (query_env "EXTRA_NATIVE_LIBRARIES") >>| split in
+  let includes = includes >>| (List.map (fun s -> "-I" ^ s))
+  and libs = libs >>| (List.map (fun s -> "-L" ^ s))
+  and names = names >>| (List.map (fun s -> "-l" ^ s)) in
+  match includes, libs, names with
+  | Some includes, Some libs, Some names ->
+    Some (includes, libs @ names)
+  | _ , _, _ -> None
+
+(* In case cmake didn't found anything, fall back to pkg-config *)
 let query_pkg (c : C.t) (name : string) : P.package_conf =
   let default : P.package_conf =
     { P.libs  =  [ "-l" ^ name ]
@@ -26,9 +52,14 @@ let concat_confs
 
 let () =
   C.main ~name:"heap" (fun (c : C.t) ->
-    let lz4_conf : P.package_conf = query_pkg c "lz4" in
-    let squilte3_conf : P.package_conf = query_pkg c "sqlite3" in
-    let conf = concat_confs lz4_conf squilte3_conf in
-
-    C.Flags.write_sexp "c_flags.sexp" conf.P.cflags;
-    C.Flags.write_sexp "c_library_flags.sexp" conf.P.libs)
+      let env_info = process_env () in
+      let (cflags, libs) = match env_info with
+        | Some (cflags, libs) -> (cflags, libs)
+        | None -> (* fallback to pkg-config *)
+          let lz4_conf : P.package_conf = query_pkg c "lz4" in
+          let squilte3_conf : P.package_conf = query_pkg c "sqlite3" in
+          let conf = concat_confs lz4_conf squilte3_conf in
+          (conf.P.cflags, conf.P.libs)
+      in
+    C.Flags.write_sexp "c_flags.sexp" cflags;
+    C.Flags.write_sexp "c_library_flags.sexp" libs)
