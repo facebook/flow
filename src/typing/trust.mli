5edfa7f2a835ed2f788a187d839125aab9091c97
Mike Vitousek <mvitousek@fb.com>
2019-02-27 11:50:24 -0800

    [trusted-types] Initial implementation of trust data
    Summary: This diff sets up the `trust.ml` module for actual use (as opposed to being a placeholder before). Trust data describes whether a type is trusted, private, both, or neither--see the discussion in `trust.ml` for details. The details of `trust.ml`'s implementation should be fine to keep hidden; instead, trust information can be generated with several new trust constructors which (in later diffs) will be used in places like `statement.ml` to assign trust to types.
    
    Reviewed By: dsainati1
    
    Differential Revision: D13874139
    
    fbshipit-source-id: ee5dfe73a03a22292989eb40a9c8c8cb568bb2f3

--- src/typing/trust.mli
+++ src/typing/trust.mli
@@ -7,7 +7,15 @@
 
 type trust
 
-val bogus_trust: unit -> trust
+val string_of_trust: trust -> string
+
+val bogus_trust  : unit -> trust
+val literal_trust: unit -> trust
+val annot_trust  : unit -> trust
+val dynamic_trust: unit -> trust
 
 val make_trusted: trust -> trust
 val make_private: trust -> trust
+
+val is_public: trust -> bool
+val is_tainted: trust -> bool
