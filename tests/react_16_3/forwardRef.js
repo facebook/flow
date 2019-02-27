b097c67031d7aa74c06973b4047776921138d178
Jordan Brown <jmbrown@fb.com>
2019-02-26 22:55:24 -0800

    [react] Make current write-only in React.Ref
    Summary:
    Making the ref write-only allows union types for refs. See the test case for an example.
    
    To build an intuition for why this is correct, consider how the code would look for `React.createElement`. After the element is created, React should write the current field of the passed in `ref` with the instance of the component. React does not need to read that ref anywhere!
    
    For more confidence, consider that the ref can also be a function where the argument is write-only by the nature of being a function argument.
    
    Reviewed By: samwgoldman
    
    Differential Revision: D14169874
    
    fbshipit-source-id: 4fb2846c91143b5e515c8e9783f61663cff0c401

--- tests/react_16_3/forwardRef.js
+++ tests/react_16_3/forwardRef.js
@@ -21,3 +21,22 @@ const _e = <FancyButton foo={3} ref={badRef} />; // Incorrect ref type
 
 const _f =  <FancyButton foo={3} ref={x => x} />;
 const _g =  <FancyButton foo={3} ref={(x: null | HTMLDivElement) => x} />; // Incorrect ref type
+
+type FooProps = {|foo: number|};
+
+const UnionRef = React.forwardRef<FooProps, HTMLButtonElement | HTMLAnchorElement>(
+  (props, ref): React.Element<'button' | 'a'> => {
+    if (props.foo === 0) {
+      return <a {...props} ref={ref} />;
+    }
+
+    return <button {...props} ref={ref} />;
+  },
+);
+
+const unionRef = React.createRef<HTMLButtonElement | HTMLAnchorElement>();
+const _h = <UnionRef foo={0} ref={unionRef} />;
+const _i = <UnionRef foo={1} ref={unionRef} />;
+
+const badUnionRef = React.createRef<HTMLButtonElement | HTMLDivElement>();
+const _j = <UnionRef foo={3} ref={badUnionRef} />; // Error bad ref
