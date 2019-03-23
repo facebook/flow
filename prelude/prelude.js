081d1452f7b2620f0233a9ccdadf318b6b5ae86c
andretshurotshka <andwebar@gmail.com>
2019-03-23 08:06:30 +0500

    [lib] Update prelude core types

--- prelude/prelude.js
+++ prelude/prelude.js
@@ -16,9 +16,9 @@ declare class $ReadOnlyArray<+T> {
   @@iterator(): Iterator<T>;
 }
 
-declare class TemplateStringsArray extends $ReadOnlyArray<string> {
-  +raw: $ReadOnlyArray<string>;
-}
+type TemplateStringsArray = interface extends $ReadOnlyArray<string> {
+  +raw: $ReadOnlyArray<string>
+};
 
 declare class Array<T> extends $ReadOnlyArray<T> {}
 
@@ -29,34 +29,34 @@ declare function $await<T>(p: Promise<T> | T): T;
 
 // Iterable/Iterator/Generator
 
-interface $Iterator<+Yield,+Return,-Next> {
-  @@iterator(): $Iterator<Yield,Return,Next>;
+interface $Iterator<+Yield, +Return, -Next> {
+  @@iterator(): $Iterator<Yield, Return, Next>;
 }
-interface $Iterable<+Yield,+Return,-Next> {
-  @@iterator(): $Iterator<Yield,Return,Next>;
+interface $Iterable<+Yield, +Return, -Next> {
+  @@iterator(): $Iterator<Yield, Return, Next>;
 }
-interface Generator<+Yield,+Return,-Next> {
-  @@iterator(): $Iterator<Yield,Return,Next>;
+interface Generator<+Yield, +Return, -Next> {
+  @@iterator(): $Iterator<Yield, Return, Next>;
 }
 
-type Iterator<+T> = $Iterator<T,void,void>;
-type Iterable<+T> = $Iterable<T,void,void>;
+type Iterator<+T> = $Iterator<T, void, void>;
+type Iterable<+T> = $Iterable<T, void, void>;
 
 declare function $iterate<T>(p: Iterable<T>): T;
 
 // Async Iterable/Iterator/Generator
 
-interface $AsyncIterator<+Yield,+Return,-Next> {
-  @@asyncIterator(): $AsyncIterator<Yield,Return,Next>;
+interface $AsyncIterator<+Yield, +Return, -Next> {
+  @@asyncIterator(): $AsyncIterator<Yield, Return, Next>;
 }
-interface $AsyncIterable<+Yield,+Return,-Next> {
-  @@asyncIterator(): $AsyncIterator<Yield,Return,Next>;
+interface $AsyncIterable<+Yield, +Return, -Next> {
+  @@asyncIterator(): $AsyncIterator<Yield, Return, Next>;
 }
-interface AsyncGenerator<+Yield,+Return,-Next> {
-  @@asyncIterator(): $AsyncIterator<Yield,Return,Next>;
+interface AsyncGenerator<+Yield, +Return, -Next> {
+  @@asyncIterator(): $AsyncIterator<Yield, Return, Next>;
 }
 
-type AsyncIterator<+T> = $AsyncIterator<T,void,void>;
-type AsyncIterable<+T> = $AsyncIterable<T,void,void>;
+type AsyncIterator<+T> = $AsyncIterator<T, void, void>;
+type AsyncIterable<+T> = $AsyncIterable<T, void, void>;
 
 declare function $asyncIterator<T>(p: AsyncIterable<T>): T;
