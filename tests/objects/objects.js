5aa2e15799d10e5a0672aebdb273e6b8bdacaa8d
andretshurotshka <andwebar@gmail.com>
2019-03-26 22:57:27 +0500

    [tests] Update object tests

--- tests/objects/objects.js
+++ tests/objects/objects.js
@@ -3,11 +3,11 @@
 var x : {'123': string, bar: string} = {'123': 'val', bar: 'bar'};
 (x.foo : string);     // error, key doesn't exist
 (x['foo'] : string);  // error, key doesn't exist
-(x[123] : boolean);   // TODO: use the number's value to error here
+(x[123] : boolean);   // error, string !~> boolean
 (x.bar: boolean);     // error, string !~> boolean
 (x['123'] : boolean); // error, string !~> boolean
 x['123'] = false;     // error, boolean !~> string
-x[123] = false;       // TODO: use the number's value to error here
+x[123] = false;       // error, boolean !~> string
 x['foo'+'bar'] = 'derp'; // ok since we can't tell
 (x[`foo`]: string);   // error, key doesn't exist
 
