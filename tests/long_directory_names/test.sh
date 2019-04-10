f986ba3a69a07a3ec842af9d999611319e57711b
Gabe Levi <gabe@fb.com>
2019-04-10 14:59:48 -0700

    [PR] Shorten directory path to fix windows
    Summary:
    Appveyor is failing with
    
    ```
    git clone -q https://github.com/facebook/flow.git C:\projects\flow
    fatal: cannot create directory at 'tests/long_directory_names/really_long_path/really_long_path/really_long_path/really_long_path/really_long_path/really_long_path/really_long_path/really_long_path/really_long_path/really_long_path/really_long_path/really_long_path/really_long_path': Filename too long
    ```
    
    I think `MAX_PATH` is 260. After this diff, the long path should only be 243 characters, assuming it's cloned into `C:\projects\flow`
    Pull Request resolved: https://github.com/facebook/flow/pull/7634
    
    Reviewed By: panagosg7
    
    Differential Revision: D14879207
    
    Pulled By: gabelevi
    
    fbshipit-source-id: 22aee85533efcc4438b0f5e97b2b13b085f92681

--- tests/long_directory_names/test.sh
+++ tests/long_directory_names/test.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
+# Hopefully this will be short enough not to upset windows CI
 LONG_PATH="really_long_path/really_long_path/really_long_path/really_long_path/\
 really_long_path/really_long_path/really_long_path/really_long_path/\
-really_long_path/really_long_path/really_long_path/really_long_path/\
-really_long_path/really_long_path/really_long_path/really_long_path/\
-really_long_path/really_long_path/really_long_path/really_long_path/\
-really_long_path/really_long_path/really_long_path/really_long_path/\
-really_long_path"
+really_long_path/really_long_path/really_long_path"
 
 printf "\nFlow check:\n"
 assert_errors "$FLOW" check --strip-root "$LONG_PATH"
