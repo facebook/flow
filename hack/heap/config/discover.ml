8e6b900ba7b1b5d1f5889696fcb19ad19618d87f
Fred Emmott <fe@fb.com>
2019-04-11 18:16:01 -0700

    [hack] Fix Dune open source linking, always use it, clean up legacy stuff from CMake
    Summary:
    This diff
    - adds comments explaining the linking variables
    - adds support for `EXTRA_LINK_OPTS` in the dune build
    - removes OCamlBuild support from cmake
    - removes support for running cmake from `hphp/hack`
    - removes the fallback to pkg-config for third-party stuff. If there's no
      `-llz4`, `EXTRA_LINK_OPTS` points towards it
    - fixes `EXTRA_LINK_PATHS` to be directory paths, as required by `-L` - this
      variable was used for `-L` by both ocamlbuild and dune, so has basically
      always been broken
    
    Reviewed By: vassilmladenov
    
    Differential Revision: D14897200
    
    fbshipit-source-id: 177194d331ce3d2d19963c5310384c2886016f43

--- hack/heap/config/discover.ml
+++ hack/heap/config/discover.ml
@@ -2,12 +2,12 @@
 https://jbuilder.readthedocs.io/en/latest/configurator.html *)
 
 module C = Configurator.V1
-module P = C.Pkg_config
 
 (* cmake should have prepared some information for us in the env:
   EXTRA_INCLUDE_PATHS
   EXTRA_LIB_PATHS
   EXTRA_NATIVE_LIBRARIES
+  EXTRA_LINK_OPTS
 *)
 let query_env s =
   match Sys.getenv s with
@@ -17,49 +17,25 @@ let query_env s =
 let process_env () =
   let open Option in
   let open String in
-  let split = split_on_char ' ' in
+  let split = (fun s -> if s = "" then [] else split_on_char ' ' s) in
   let includes = (query_env "EXTRA_INCLUDE_PATHS") >>| split
-  and libs = (query_env "EXTRA_LIB_PATHS") >>| split
-  and names = (query_env "EXTRA_NATIVE_LIBRARIES") >>| split in
+  and dirs = (query_env "EXTRA_LIB_PATHS") >>| split
+  and names = (query_env "EXTRA_NATIVE_LIBRARIES") >>| split
+  and opaque_opts = (query_env "EXTRA_LINK_OPTS") >>| split in
   let includes = includes >>| (List.map (fun s -> "-I" ^ s))
-  and libs = libs >>| (List.map (fun s -> "-L" ^ s))
+  and dirs = dirs >>| (List.map (fun s -> "-L" ^ s))
   and names = names >>| (List.map (fun s -> "-l" ^ s)) in
-  match includes, libs, names with
-  | Some includes, Some libs, Some names ->
-    Some (includes, libs @ names)
-  | _ , _, _ -> None
-
-(* In case cmake didn't found anything, fall back to pkg-config *)
-let query_pkg (c : C.t) (name : string) : P.package_conf =
-  let default : P.package_conf =
-    { P.libs  =  [ "-l" ^ name ]
-    ; P.cflags = []
-    }
-  in
-  match P.get c with
-  | None -> default
-  | Some pc ->
-    Option.value (P.query pc ~package:("lib" ^ name)) ~default
-    (* TODO die instead of default here? *)
-
-let concat_confs
-  (c1 : P.package_conf)
-  (c2 : P.package_conf)
-  : P.package_conf =
-  P.({ libs =   c1.libs @ c2.libs
-  ; cflags = c1.cflags @ c2.cflags
-  })
+  match includes, dirs, names, opaque_opts with
+  | Some includes, Some dirs, Some names, Some opts ->
+    Some (includes, dirs @ names @ opts)
+  | _ , _, _, _ -> None
 
 let () =
-  C.main ~name:"heap" (fun (c : C.t) ->
+  C.main ~name:"heap" (fun (_ : C.t) ->
       let env_info = process_env () in
-      let (cflags, libs) = match env_info with
-        | Some (cflags, libs) -> (cflags, libs)
-        | None -> (* fallback to pkg-config *)
-          let lz4_conf : P.package_conf = query_pkg c "lz4" in
-          let squilte3_conf : P.package_conf = query_pkg c "sqlite3" in
-          let conf = concat_confs lz4_conf squilte3_conf in
-          (conf.P.cflags, conf.P.libs)
+      let (cflags, cldflags) = match env_info with
+        | Some (cflags, cldflags) -> (cflags, cldflags)
+        | None -> ( [], [] )
       in
     C.Flags.write_sexp "c_flags.sexp" cflags;
-    C.Flags.write_sexp "c_library_flags.sexp" libs)
+    C.Flags.write_sexp "c_library_flags.sexp" cldflags)
