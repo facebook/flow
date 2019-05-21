f249c8dfcf11641850a46cb01d41d2ea4ef690d1
Nat Mote <nmote@fb.com>
2019-05-20 21:10:58 -0700

    Simplify the implementation of replacement_printer
    Summary:
    This gets rid of the `file_info` utility function:
    
    * `lines` has been replaced by a call to `Sys_utils.cat`. There's not point in reading a file line-by-line only to `String.concat "\n"` it. This also necessitated the removal of the `\n` at the end when we print it back out, because `String.concat` does not result in a trailing newline.
    * `line_counts` was basically a worse implementation of Offset_utils (which is defensible, since Offset_utils did not exist when this was first implemented). Therefore, I replaced it with the appropriate calls to `Offset_utils`.
    
    Reviewed By: mvitousek
    
    Differential Revision: D15426095
    
    fbshipit-source-id: b84dc221e3554d2217850ec6ef1860276194b634

--- src/parser_utils/output/printers/replacement_printer.ml
+++ src/parser_utils/output/printers/replacement_printer.ml
@@ -13,33 +13,12 @@ let show_patch p: string =
   ListUtils.to_string ""
     (fun (s, e, p) -> Printf.sprintf "Start: <%d> End: <%d> Patch: <%s>\n" s e p) p
 
-let file_info file_path : string list * (int * int * int) list =
-  let input_channel = open_in file_path in
-  let rec build_list l =
-    match input_line input_channel with
-    | line -> build_list (line :: l)
-    | exception End_of_file -> close_in input_channel ; List.rev l
-  in
-  let lines_read = build_list [] in
-  let lines, line_counts, _ =
-    List.fold_left
-      (fun (lines, acc, prev_offset) s ->
-        let size = String.length s in
-        let new_offset = prev_offset + size in
-        (s :: lines, (size, prev_offset, new_offset) :: acc, new_offset + 1) )
-      ([], [(0, 0, 0)], 0)
-      lines_read
-  in
-  (lines, line_counts)
-
 let mk_patch_ast_differ (diff : Flow_ast_differ.node Flow_ast_differ.change list)
   (ast : (Loc.t, Loc.t) Ast.program) (file_path : string) : patch =
 
-   let _, line_counts = file_info file_path in
-   let line_counts_arr = Array.of_list (List.rev line_counts) in
-   let offset {Loc.line; column; _} =
-     let _, line_start, _ = line_counts_arr.(line) in
-     line_start + column in
+   let contents = Sys_utils.cat file_path in
+   let offset_table = Offset_utils.make contents in
+   let offset loc = Offset_utils.offset offset_table loc in
 
    let attached_comments = Some (Flow_prettier_comments.attach_comments ast) in
    Ast_diff_printer.edits_of_changes attached_comments diff
@@ -50,9 +29,8 @@ let print (patch : patch) (file_path : string) : string =
    (fun (start_one, _, _) (start_two, _, _) -> compare start_one start_two)
    patch
   in
-  let lines, line_counts = file_info file_path in
-  let _, _, file_end = List.hd line_counts in
-  let file_string = String.concat "\n" (List.rev lines) in
+  let file_string = Sys_utils.cat file_path in
+  let file_end = String.length file_string in
   (* Apply the spans to the original text *)
   let result_string_minus_end, last_span =
     List.fold_left
@@ -68,9 +46,9 @@ let print (patch : patch) (file_path : string) : string =
   let last_span_to_end_size = file_end - last_span in
   let result_string =
     if last_span_to_end_size = 0 then
-      Printf.sprintf "%s\n" result_string_minus_end
+      Printf.sprintf "%s" result_string_minus_end
     else
-      Printf.sprintf "%s%s\n" result_string_minus_end
+      Printf.sprintf "%s%s" result_string_minus_end
         (String.sub file_string last_span last_span_to_end_size)
   in
   result_string
