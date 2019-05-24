236342384a5ce71a21936e47152328b625450e21
Kevin Grandon <kevingrandon@yahoo.com>
2019-05-24 16:28:04 -0700

    WIP - Try overloading unhandledRejection process event handlers

--- tests/node_tests/process/process.js
+++ tests/node_tests/process/process.js
@@ -3,3 +3,19 @@
 (process.allowedNodeEnvironmentFlags: Set<string>);
 
 (process.allowedNodeEnvironmentFlags: string); // error
+
+process.once('unhandledRejection', e => {
+  return e + 1; // error
+});
+
+process.once('unhandledRejection', e => {
+  return e + 1; // error
+});
+
+process.on('unhandledRejection', e => {
+  (process.allowedNodeEnvironmentFlags: mixed);
+});
+
+process.once('unhandledRejection', e => {
+  (process.allowedNodeEnvironmentFlags: mixed);
+});
