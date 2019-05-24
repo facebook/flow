6040b0a4be7933b1f82417a43cb19c91bd964eff
Tatiana Racheva <tatianaracheva@fb.com>
2019-05-24 15:19:44 -0700

    [hack] Move get_root into a more common location
    Summary: I would like to use this root-getting logic in another place, and it seems like it belongs in Wwwroot, anyway.
    
    Reviewed By: ljw1004
    
    Differential Revision: D15491499
    
    fbshipit-source-id: 2036f7ff18163aca182b4f83ade3b0450524d617

--- hack/utils/wwwroot.mli
+++ hack/utils/wwwroot.mli
@@ -15,3 +15,4 @@ shape. *)
 
 val is_www_directory : ?config:string -> Path.t -> bool
 val assert_www_directory : ?config:string -> Path.t -> unit
+val get : ?config:string -> string option -> Path.t
