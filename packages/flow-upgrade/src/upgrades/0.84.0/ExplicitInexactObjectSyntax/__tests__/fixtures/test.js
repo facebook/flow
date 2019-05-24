07d5ba2f219d33f72991fbc6a066d2a52923ffdb
Jordan Brown <jmbrown@fb.com>
2019-05-24 18:00:43 -0400

    Fix flow-upgrade

--- packages/flow-upgrade/src/upgrades/0.84.0/ExplicitInexactObjectSyntax/__tests__/fixtures/test.js
+++ packages/flow-upgrade/src/upgrades/0.84.0/ExplicitInexactObjectSyntax/__tests__/fixtures/test.js
@@ -1,67 +1,32 @@
 //@flow
-type T = {...};
-opaque type T: {...} = {...};
+type T = {};
+opaque type T: {} = {};
 
-function test(x: {...}): {...} {}
+function test(x: {}): {} {}
 class A {}
 interface B {}
 declare class C {}
 declare interface D {}
 
-type Nested = { x: {...}, ... };
+type Nested = { x: {},  };
 type Exact = {| |};
 
-type Generic<T: {...}> = T;
-opaque type Generic<T: {...}> = T;
+type Generic<T: {}> = T;
+opaque type Generic<T: {}> = T;
 
 class A {
-  static x: {...} = {}
+  static x: {} = {}
 }
 
 type T = {
   a: number,
   b: string,
-  c: {...},
+  c: {},
   d: string,
 };
 type U = {
   a: number,
-  b: {...},
-  c: { x : {...}, ... },
-  d: { x: { y :{...}, ... }, ... },
-}
-
-
-type T = {...};
-opaque type T: {...} = {...};
-
-function test(x: {...}): {...} {}
-class A {}
-interface B {}
-declare class C {}
-declare interface D {}
-
-type Nested = { x: {...}, ... };
-type Exact = {| |};
-
-type Generic<T: {...}> = T;
-opaque type Generic<T: {...}> = T;
-
-class A {
-  static x: {...} = {}
-}
-
-type T = {
-  a: number,
-  b: string,
-  c: {...},
-  d: string,
-  ...
-};
-type U = {
-  a: number,
-  b: {...},
-  c: { x : {...}, ... },
-  d: { x: { y :{...}, ... }, ... },
-  ...
+  b: {},
+  c: { x : {},  },
+  d: { x: { y :{},  },  },
 }
