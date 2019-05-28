e187b771ed6e1610017ea7782faeaa0ee310c1d8
Jordan Brown <jmbrown@fb.com>
2019-05-28 09:38:07 -0700

    [PR] Fix flow-upgrade
    Summary:
    This PR does a few things:
    1. Upgrades the jscodeshift version (still using my custom fork)
    2. fixes the tests
    3. bumps the version number
    
    After this is merged, I'll deploy this to NPM. Note that I used this version of flow-upgrade to create D15503065
    Pull Request resolved: https://github.com/facebook/flow/pull/7760
    
    Reviewed By: samwgoldman
    
    Differential Revision: D15503742
    
    Pulled By: jbrown215
    
    fbshipit-source-id: f5c00e5a8fdb826de6589891c755f76a864558a1

--- packages/flow-upgrade/src/upgrades/0.84.0/ExplicitInexactObjectSyntax/__tests__/fixtures/test.js
+++ packages/flow-upgrade/src/upgrades/0.84.0/ExplicitInexactObjectSyntax/__tests__/fixtures/test.js
@@ -1,37 +1,37 @@
 //@flow
-type T = {...};
-opaque type T: {...} = {...};
+type T = {};
+opaque type T: {} = {};
 
-function test(x: {...}): {...} {}
+function test(x: {}): {} {}
 class A {}
 interface B {}
 declare class C {}
 declare interface D {}
 
-type Nested = { x: {...}, ... };
+type Nested = { x: {},  };
 type Exact = {| |};
 
-type Generic<T: {...}> = T;
-opaque type Generic<T: {...}> = T;
+type Generic<T: {}> = T;
+opaque type Generic<T: {}> = T;
 
 class A {
-  static x: {...} = {}
+  static x: {} = {}
 }
 
 type T = {
   a: number,
   b: string,
-  c: {...},
+  c: {},
   d: string,
 };
 type U = {
   a: number,
-  b: {...},
-  c: { x : {...}, ... },
-  d: { x: { y :{...}, ... }, ... },
-}
+  b: {},
+  c: { x : {},  },
+  d: { x: { y :{},  },  },
+};
 
-
+// We should also make sure objects already using this syntax aren't broken by the codemod
 type T = {...};
 opaque type T: {...} = {...};
 
@@ -64,4 +64,4 @@ type U = {
   c: { x : {...}, ... },
   d: { x: { y :{...}, ... }, ... },
   ...
-}
+};
