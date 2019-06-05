832e74beb9d7d5d6b4ddeb5b1f0812e81b7f87de
andretshurotshka <andwebar@gmail.com>
2019-06-05 11:03:19 +0500

    [tests] Add delete readonly properties tests

--- tests/unary/delete.js
+++ tests/unary/delete.js
@@ -4,16 +4,68 @@ let tests = [
   function (foo: { a: number }) {
     delete foo.a; // ok
   },
+  function (foo: { a: number }) {
+    delete foo['a']; // ok
+  },
+
+  function (foo: { +a: number }) {
+    delete foo.a; // error
+  },
+  function (foo: { +a: number }) {
+    delete foo['a']; // error
+  },
+  
+  function (foo: { -a: number }) {
+    delete foo.a; // ok
+  },
+  function (foo: { -a: number }) {
+    delete foo['a']; // ok
+  },
+
+  function (foo: number[]) {
+    delete foo[0] // ok
+  },
+  function (foo: [number]) {
+    delete foo[1] // error
+  },
+  function (foo: $ReadOnlyArray<number>) {
+    delete foo[0] // error
+  },
+
+  function (foo: { a?: number }) {
+    delete foo?.a // ok
+  },
+  function (foo: { +a?: number }) {
+    delete foo?.a // TODO
+  },
+  function (foo: { -a?: number }) {
+    delete foo?.a // TODO
+  },
+
+  function (foo: {[key: string]: number}) {
+    delete foo['bar'] // ok
+  },
+  function (foo: {+[key: string]: number}) {
+    delete foo['bar'] // error
+  },
+  function (foo: {-[key: string]: number}) {
+    delete foo['bar'] // ok
+  },
+
+  function (foo: {[key: number]: number}) {
+    delete foo[0] // ok
+  },
+  function (foo: {+[key: number]: number}) {
+    delete foo[1] // error
+  },
+  function (foo: {-[key: number]: number}) {
+    delete foo[1] // ok
+  },
+
   function () {
     delete { a: 1 } // error
   },
   function () {
     delete [1, 2, 3] // error
-  },
-  function (foo: number[]) {
-    delete foo[0] // ok
-  },
-  function (foo: { a?: number }) {
-    delete foo?.a // ok
   },
 ]
