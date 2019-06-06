787156e7205a17417dd333f1c95e3e5976daf1ca
Avik Chaudhuri <avik@fb.com>
2019-06-06 16:57:02 -0700

    tests for assigning properties to module.exports that are polymorphic functions
    Reviewed By: panagosg7
    
    Differential Revision: D15689640
    
    fbshipit-source-id: c293cdf02b37f41f83726b577dab62f41cb3df31

--- tests/types-first-function-statics2/b.js
+++ tests/types-first-function-statics2/b.js
@@ -6,3 +6,11 @@ const a = require('./a');
 (a.g: string);
 (a.g: number);
 (a.other: string);
+
+const poly_a = require('./poly_a');
+
+(poly_a: () => void);
+(poly_a: <T>() => void);
+(poly_a.g: string);
+(poly_a.g: number);
+(poly_a.other: string);
--- tests/types-first-function-statics3/b.js
+++ tests/types-first-function-statics3/b.js
@@ -6,3 +6,8 @@ const Component = require('./a');
 
 <Component/>;
 <Component other={1}/>;
+
+const PolyComponent = require('./poly_a');
+
+<PolyComponent t={0}/>;
+<PolyComponent t={0} other={1}/>;
