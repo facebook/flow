b715a5249af3af26fd40ef47a52b132a7d26187d
Nat Mote <nmote@fb.com>
2019-06-10 23:05:12 -0700

    Add mli for Ast_loc_utils
    Summary: D14091203 will add some implementation details that we want to hide
    
    Reviewed By: avikchaudhuri
    
    Differential Revision: D15689781
    
    fbshipit-source-id: 86d15d2467518c24d3af3eab991217ad03a7e929

---
+++ src/parser_utils/ast_loc_utils.mli
@@ -0,0 +1,10 @@
+(**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *)
+
+(* Converts a (Loc.t, Loc.t) AST into a (ALoc.t, ALoc.t) AST. Leaves the underlying representation
+ * of the contained ALoc.ts concrete. *)
+val loc_to_aloc_mapper: (Loc.t, Loc.t, ALoc.t, ALoc.t) Flow_polymorphic_ast_mapper.mapper
