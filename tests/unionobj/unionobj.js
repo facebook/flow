6e272e2e3f0a93e5df5c9bbed9953d0cb19b3a27
andretshurotshka <andwebar@gmail.com>
2019-06-25 23:47:38 +0500

    [tests] Add unionobj tests

---
+++ tests/unionobj/unionobj.js
@@ -0,0 +1,21 @@
+// @flow
+
+declare function onAction<T, Result>(
+  myEnum: T,
+  pattern: $ObjMap<$UnionObj<T>, () => () => Result>
+): Result;
+
+declare var action: "Action1" | "Action2" | "Action3";
+
+// no error
+const result: string = onAction(action, {
+  Action1: () => "",
+  Action2: () => "",
+  Action3: () => ""
+});
+
+// error
+const result2: number = onAction(action, {
+  Action1: () => 0,
+  Action2: () => 1
+});
