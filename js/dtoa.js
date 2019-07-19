4a82d8d0cb15df34d43b11678ef6127794899490
andretshurotshka <andwebar@gmail.com>
2019-07-19 08:03:05 +0500

    [js] Remove custom dtoa

--- js/dtoa.js
+++
@@ -1,6 +0,0 @@
-//Provides: flow_ecma_string_of_float
-//Requires: caml_js_to_string
-function flow_ecma_string_of_float(float) {
-  var ocaml_string = caml_js_to_string(float.toString());
-  return ocaml_string;
-}
