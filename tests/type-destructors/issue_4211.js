b544e8820d1c3275c3fa227e742ef3ba70eb74e0
andretshurotshka <andwebar@gmail.com>
2019-07-20 19:53:07 +0500

    [tests] Add more tests

--- tests/type-destructors/issue_4211.js
+++ tests/type-destructors/issue_4211.js
@@ -1,12 +1,21 @@
 // @flow
 
 declare var t: [number, string];
-type T = $ElementType<typeof t, 0 | 1>;
+declare var y: 0 | 1;
+type T = $ElementType<typeof t, typeof y>;
+type T1 = $ElementType<typeof t, 0 | 1>;
 declare var v: T;
+declare var v1: T1;
 (v: number | string); // OK, correct
+(v1: number | string);
 
+(1: T); // works
+(2: T1); // works
 ('foo': T); // works
+('foo': T1); // works
+({a: 1}: T); // error
 
 declare var x: string & number;
 
 (x: T); // works
+(x: T1); // works
