41c163e9345293881285f674b084fc7435cf12b1
Sam Goldman <samgoldman@fb.com>
2019-08-02 14:20:16 -0700

    Represent Xx.hash as Int64, provide int64 hash combine util
    Summary:
    Before this change, we stored the digest as a 16-character long string. Due to
    OCaml's string encoding, the storage for the string itself is actually 3 words
    (24 bytes) instead of 2 (16 bytes).
    
    This small inefficiency is no big deal today, since we store relatively few
    hashes -- just one hash per file (FileHashHeap) and one hash per strongly
    connected component (SigHashHeap).
    
    However, I am implementing a finer-grained hashing scheme which should speed up
    signature hashing while also improving the stability of hashes, but it requires
    storing a hash for each top-level name reachable from exports.
    
    The new hashing scheme also benefits from an efficient way to combine hashes, a
    la merkle trees, so I provided a simple function to do so.
    
    Another benefit of using int64 is that OCaml can pass these values unboxed
    between OCaml and C. This makes it possible to avoid the sole allocation in
    caml_xx_digest, so we can generate the digests more cheaply.
    
    Along the way, I noticed that the implementation of Xx.to_string had some
    confusing comments, which I have clarified.
    
    Reviewed By: gabelevi
    
    Differential Revision: D16627740
    
    fbshipit-source-id: 8ad2ee704c2b0dc5dd8003956336365011d96223

--- src/common/xx/xx.ml
+++ src/common/xx/xx.ml
@@ -5,12 +5,14 @@
  * LICENSE file in the root directory of this source tree.
  *)
 type state
-type hash
+type hash = int64
 
 external init: unit -> state = "caml_xx_init"
 external update: state -> string -> unit = "caml_xx_update" [@@noalloc]
 external update_int: state -> 'a (* int *) -> unit = "caml_xx_update_int" [@@noalloc]
-external digest: state -> hash = "caml_xx_digest"
+external update_int64: state -> (int64 [@unboxed]) -> unit = "caml_xx_update_int64" "caml_xx_update_int64_unboxed" [@@noalloc]
+external digest: state -> (hash [@unboxed]) = "caml_xx_digest" "caml_xx_digest_unboxed" [@@noalloc]
 
-(* Returns a 16 character hex string representation of the hash *)
-external to_string: hash -> string = "caml_xx_to_string"
+(* Unlike Int64.to_string, which returns a decimal string, this returns a hex
+ * string which is padded out to the full 16 bytes. *)
+external to_string: (hash [@unboxed]) -> string = "caml_xx_to_string" "caml_xx_to_string_unboxed"
