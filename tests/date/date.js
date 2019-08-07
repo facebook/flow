005ac3a49e507e7c15d154ea5d1e24cde614b41a
andretshurotshka <andwebar@gmail.com>
2019-08-08 01:05:30 +0500

    [tests] update date tests

--- tests/date/date.js
+++ tests/date/date.js
@@ -1,7 +1,7 @@
 var d = new Date(0);
-var x:string = d.getTime(); // expect error
+var x: string = d.getTime(); // expect error
 
-var y:number = d; // expect error
+var y: number = d; // expect error
 
 // valid constructors
 new Date();
@@ -25,8 +25,9 @@ new Date(2015, 6, 18, 11, 55, 42, '999');
 new Date(2015, 6, 18, 11, 55, 42, 999, 'hahaha');
 new Date('2015', 6);
 
-var b:boolean = d > 0;
-var n1:number = d - 12;
-var n2:number = d & 255;
-var n3:number = d + 12; // expect error
-var s:string = d + 12; // fixme? in js this coerces both to string and concats them
+var b: boolean = d > 0;
+var n1: number = d - 12;
+var n2: number = d & 255;
+var n3: number = d + 12; // expect error
+var s: string = d + 12; // fixme? in js this coerces both to string and concats them
+var n4: number = ~d;
