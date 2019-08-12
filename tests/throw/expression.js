8d7778cf0438a798dc24ebf5a87e930eb36e30d5
andretshurotshka <andwebar@gmail.com>
2019-08-12 22:40:39 +0500

    [tests] add logical operators tests

--- tests/throw/expression.js
+++ tests/throw/expression.js
@@ -6,3 +6,20 @@ function x(a: number = throw 'x') {
 
 x()
 x(1)
+
+function f(): number {
+  return throw new Error(); // OK to not return
+}
+
+function g(a: ?number): number {
+  const x = a == null ? throw new Error() : null;
+  return a * 1; // a is not null
+}
+function h(a: ?number): number {
+  const x = a == null && throw new Error();
+  return a * 1; // TODO: a is not null
+}
+function i(a: ?number): number {
+  const x = a != null || throw new Error();
+  return a * 1; // TODO: a is not null
+}
