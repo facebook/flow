f090aa001007141b58b679b7e0b43f03c8a0673a
andretshurotshka <andwebar@gmail.com>
2019-08-17 00:58:05 +0500

    [tests] Update tests

--- tests/unary/delete.js
+++ tests/unary/delete.js
@@ -1,11 +1,17 @@
 //@flow
 
 let tests = [
-  function (foo: { a: number }) {
+  function (foo: { a?: number }) {
     delete foo.a; // ok
   },
-  function (foo: { a: number }) {
+  function (foo: { a?: number }) {
     delete foo['a']; // ok
+  },
+  function (foo: { a: number }) {
+    delete foo.a; // error
+  },
+  function (foo: { a: number }) {
+    delete foo['a']; // error
   },
 
   function (foo: { +a: number }) {
@@ -14,16 +20,25 @@ let tests = [
   function (foo: { +a: number }) {
     delete foo['a']; // error
   },
-  
-  function (foo: { -a: number }) {
+
+  function (foo: { -a?: number }) {
     delete foo.a; // ok
   },
-  function (foo: { -a: number }) {
+  function (foo: { -a?: number }) {
     delete foo['a']; // ok
   },
+  function (foo: { -a: number }) {
+    delete foo.a; // error
+  },
+  function (foo: { -a: number }) {
+    delete foo['a']; // error
+  },
 
-  function (foo: number[]) {
+  function (foo: Array<number | void>) {
     delete foo[0] // ok
+  },
+  function (foo: number[]) {
+    delete foo[0] // error
   },
   function (foo: [number]) {
     delete foo[1] // error
@@ -33,7 +48,7 @@ let tests = [
   },
 
   function (foo: { a?: number }) {
-    delete foo?.a // ok
+    delete foo?.a // TODO
   },
   function (foo: { +a?: number }) {
     delete foo?.a // TODO
@@ -42,30 +57,52 @@ let tests = [
     delete foo?.a // TODO
   },
 
-  function (foo: {[key: string]: number}) {
+  function (foo: { [key: string]: number | void }) {
     delete foo['bar'] // ok
   },
-  function (foo: {+[key: string]: number}) {
+  function (foo: { -[key: string]: number | void }) {
+    delete foo['bar'] // ok
+  },
+  function (foo: { [key: string]: number }) {
     delete foo['bar'] // error
   },
-  function (foo: {-[key: string]: number}) {
-    delete foo['bar'] // ok
+  function (foo: { +[key: string]: number }) {
+    delete foo['bar'] // error
+  },
+  function (foo: { -[key: string]: number }) {
+    delete foo['bar'] // error
   },
 
-  function (foo: {[key: number]: number}) {
+  function (foo: { [key: number]: number | void }) {
     delete foo[0] // ok
+  },
+  function (foo: { [key: number]: number | void }) {
+    delete foo[0] // ok
+  },
+  function (foo: { [key: number]: number }) {
+    delete foo[0] // error
   },
   function (foo: {+[key: number]: number}) {
     delete foo[1] // error
   },
   function (foo: {-[key: number]: number}) {
-    delete foo[1] // ok
+    delete foo[1] // error
   },
 
+  function (a: number) {
+    delete a;
+  },
   function () {
     delete { a: 1 } // error
   },
   function () {
     delete [1, 2, 3] // error
   },
+  function() {
+    class Foo {
+      bar() {
+        delete super.name;
+      }
+    }
+  }
 ]
