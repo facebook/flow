66a4dd4778157d13feb1234844360a5201c5128e
Gabriel Miranda <gabrielrumiranda@gmail.com>
2019-09-06 13:25:45 -0300

    Creating new tests

--- tests/declaration_files_node_not_ignore_dotfiles/test_relative.js
+++ tests/declaration_files_node_not_ignore_dotfiles/test_relative.js
@@ -8,6 +8,15 @@ var A1 = require('./.A');
 var A2 = require('./.A.js');
 (A2.fun(): string); // Error number ~> string
 
+// This will require ./.js.flow
+var A1 = require('./.');
+(A1.fun(): string); // Error number ~> string
+
+// This will require ./.js.flow
+var A2 = require('./.js');
+(A2.fun(): string); // Error number ~> string
+
+
 var CJS = require('./.CJS.js');
 (CJS: string);
 (CJS: number); //
