b33a78d70ec66841366fea343e7786ec575ad03c
George Zahariev <gkz@fb.com>
2019-10-31 18:02:46 -0700

    Add tests for op assignments
    Summary:
    Add tests for assignments with operators (e.g. `+=`, `-=`, etc.)
    
    We will expand on these tests in the future when we fix some things that are broken.
    
    Reviewed By: gabelevi
    
    Differential Revision: D18213984
    
    fbshipit-source-id: edcf8ffc7d3b0fa62c481c2c5a816b22e22b9b2c

---
+++ tests/op_assignment/test.js
@@ -0,0 +1,57 @@
+// @flow
+
+///////////
+// Valid //
+///////////
+{
+  // Let binding
+  let x = 1;
+  x += 2; // Ok
+  x -= 2; // Ok
+  x *= 2; // Ok
+  x /= 2; // Ok
+  x **= 2; // Ok
+  x %= 2; // Ok
+
+  let sx = "a";
+  sx += "b"; // Ok
+}
+
+{
+  // Regular object
+  const o: {|p: number|} = {p: 1};
+  o.p += 2; // Ok
+  o.p -= 2; // Ok
+  o.p *= 2; // Ok
+  o.p /= 2; // Ok
+  o.p **= 2; // Ok
+  o.p %= 2; // Ok
+}
+
+////////////
+// Errors //
+////////////
+{
+  // Const binding
+  const x = 1;
+  x += 2; // Error: cannot reassign constant
+  x -= 2; // Error: cannot reassign constant
+  x *= 2; // Error: cannot reassign constant
+  x /= 2; // Error: cannot reassign constant
+  x **= 2; // Error: cannot reassign constant
+  x %= 2; // Error: cannot reassign constant
+}
+
+{
+  // Const binding, string value
+  const sx = "a";
+  sx += "b"; // Error: cannot reassign constant
+}
+
+{
+  // Write-only property
+  const o: {|-p: number|} = {p: 1};
+  o.p += 2; // Error: property is non-readable
+  // TODO(T56716039): If you read a write-only property after it is written, there is no error
+  // o.p -= 2; // Error: property is non-readable
+}
