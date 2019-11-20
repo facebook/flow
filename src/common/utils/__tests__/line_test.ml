471d5957606b4df49776f1164462a45fea5c3b23
Marshall Roch <mroch@fb.com>
2019-11-20 06:54:01 -0800

    fix autocomplete at end of file
    Summary:
    autocomplete currently fails at the very end of a file, like this:
    
    ```
    // flow
    
    const x = { foo: 123 };
    
    x.|
    ```
    
    This is because of an off-by-one error. we think that the 5th line is just `x` instead of `x.`, which causes `AutocompleteService_js.add_autocomplete_token` to not insert the token.
    
    Reviewed By: dsainati1
    
    Differential Revision: D18596360
    
    fbshipit-source-id: 3d267aaa49b3067cdea5a69993a05228ce675722

---
+++ src/common/utils/__tests__/line_test.ml
@@ -0,0 +1,27 @@
+(**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *)
+
+open OUnit2
+
+let split_nth_printer result =
+  match result with
+  | Some (pre, x, post) -> Printf.sprintf "Some (%S, %S, %S)" pre x post
+  | None -> "None"
+
+let tests =
+  "line"
+  >::: [
+         "split_nth"
+         >::: [
+                ( "last_line" >:: fun ctxt ->
+                  assert_equal
+                    ~ctxt
+                    ~printer:split_nth_printer
+                    (Some ("foo\n", "bar", ""))
+                    (Line.split_nth "foo\nbar" 1) );
+              ];
+       ]
