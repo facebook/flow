f647daf406bde5cc953237347d17eb2b07c5c712
Lucian Wischik <ljw@fb.com>
2019-12-03 22:00:27 -0800

    [hack] clientIdeDaemon supports HackEventLogger
    Summary:
    This diff adds support for HackEventLogger to clientIdeDaemon.
    
    Prior to this diff, ClientIdeDaemon called `EventLogger.init_fake ()` inside set_up_hh_logger_for_client_ide_server, which is called in response to receiving the Initialize_from_saved_state message. This had two effects:
    
    (1) All HackEventLogger telemetry was ignored
    
    (2) Any attempt at HackEventLogger before the Initialize_from_saved_state message was received, would fail.
    
    This diff makes it initialize properly, right at the very start. That way, HackEventLogger will work.
    
    This diff also calls into EventLogger.flush periodically, which is also needed by HackEveentLogger.
    
    Reviewed By: arxanas
    
    Differential Revision: D18755935
    
    fbshipit-source-id: 10a0fbd1c86be6700209cc24fed55f2d60b4562d

---
+++ hack/stubs/logging/common/eventLoggerLwt.ml
@@ -0,0 +1,10 @@
+(*
+ * Copyright (c) 2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the "hack" directory of this source tree.
+ *
+ *)
+
+let flush () = Lwt.return_unit
