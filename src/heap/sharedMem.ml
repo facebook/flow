c39728b761795a439e08f5764016fa820ed8c06d
Marshall Roch <mroch@fb.com>
2020-01-15 07:23:57 -0800

    [PR] Fix dune after Hack fork
    Summary: Pull Request resolved: https://github.com/facebook/flow/pull/8216
    
    Reviewed By: dsainati1
    
    Differential Revision: D18847405
    
    Pulled By: mroch
    
    fbshipit-source-id: 729e3daf8b17ece3c346c2734cefc373afe106d7

--- src/heap/sharedMem.ml
+++ src/heap/sharedMem.ml
@@ -1004,7 +1004,7 @@ end)
         end
         cache;
       Hashtbl.clear cache;
-      l := List.sort (fun (_, x, _) (_, y, _) -> y - x) !l;
+      l := List.sort ~compare:(fun (_, x, _) (_, y, _) -> y - x) !l;
       let i = ref 0 in
       while !i < Config.capacity do
         match !l with
