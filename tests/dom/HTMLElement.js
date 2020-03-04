6adcb27dfa753fc0c8ec693bc888fa4f1cbe5f53
James Ribe <ribe@fb.com>
2020-03-04 10:57:42 -0800

    [dom.js] fix Element.querySelector() and Element.querySelectorAll() overloads
    Summary: `Element.querySelector()` and `Element.querySelectorAll()` are overloaded with several alternate signatures for specific inputs. They both have signatures using `string` before *and* after the specific overloads, which forces all callsites to use the broad signature. This diff deletes the broad signatures that were clobbering the specific signatures and adds a regression test.
    
    Reviewed By: nmote
    
    Differential Revision: D19844224
    
    fbshipit-source-id: 82df3ad9f46f18473642efacbbd54467be06d972

--- tests/dom/HTMLElement.js
+++ tests/dom/HTMLElement.js
@@ -6,25 +6,62 @@ let tests = [
     element.scrollIntoView();
     element.scrollIntoView(false);
     element.scrollIntoView({});
-    element.scrollIntoView({ behavior: 'smooth', block: 'end' });
-    element.scrollIntoView({ block: 'end' });
-    element.scrollIntoView({ behavior: 'smooth' });
+    element.scrollIntoView({behavior: 'smooth', block: 'end'});
+    element.scrollIntoView({block: 'end'});
+    element.scrollIntoView({behavior: 'smooth'});
 
     // fails
-    element.scrollIntoView({ behavior: 'invalid' });
-    element.scrollIntoView({ block: 'invalid' });
+    element.scrollIntoView({behavior: 'invalid'});
+    element.scrollIntoView({block: 'invalid'});
     element.scrollIntoView(1);
+  },
+
+  // functions with specific overloads
+  function(element: HTMLElement) {
+    const str: string = 'a';
+    // broad
+    (element.getElementsByTagName(str): HTMLCollection<HTMLElement>);
+    (element.getElementsByTagNameNS(null, str): HTMLCollection<HTMLElement>);
+    (element.querySelector(str): HTMLElement | null);
+    (element.querySelectorAll(str): NodeList<HTMLElement>);
+
+    // specific
+    (element.getElementsByTagName('a'): HTMLCollection<HTMLAnchorElement>);
+    (element.getElementsByTagNameNS(
+      null,
+      'a',
+    ): HTMLCollection<HTMLAnchorElement>);
+    (element.querySelector('a'): HTMLAnchorElement | null);
+    (element.querySelectorAll('a'): NodeList<HTMLAnchorElement>);
+
+    // overly broad input (fails)
+    (element.getElementsByTagName(str): HTMLCollection<HTMLAnchorElement>);
+    (element.getElementsByTagNameNS(
+      null,
+      str,
+    ): HTMLCollection<HTMLAnchorElement>);
+    (element.querySelector(str): HTMLAnchorElement | null);
+    (element.querySelectorAll(str): NodeList<HTMLAnchorElement>);
+
+    // wrong specific input (fails)
+    (element.getElementsByTagName('div'): HTMLCollection<HTMLAnchorElement>);
+    (element.getElementsByTagNameNS(
+      null,
+      'div',
+    ): HTMLCollection<HTMLAnchorElement>);
+    (element.querySelector('div'): HTMLAnchorElement | null);
+    (element.querySelectorAll('div'): NodeList<HTMLAnchorElement>);
   },
 
   // focus
   function(element: HTMLElement) {
     element.focus();
     element.focus({});
-    element.focus({ preventScroll: true });
-    element.focus({ preventScroll: false });
+    element.focus({preventScroll: true});
+    element.focus({preventScroll: false});
 
     // fails
-    element.focus({ preventScroll: 'invalid' });
+    element.focus({preventScroll: 'invalid'});
     element.focus(1);
-  }
+  },
 ];
