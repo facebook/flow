e3b6b41edffc8f0fb53ef03682bf6ab132e47976
Nat Mote <nmote@fb.com>
2020-03-06 10:42:04 -0800

    Add test for --saved-state-no-fallback flag
    Summary: This test illustrates the issue described in D20236637. We fail to distinguish between "could not find saved state" and "did not look for saved state," so even if the `--saved-state-no-fallback` flag is provided, Flow ignores it and initializes normally if saved state cannot be found.
    
    Reviewed By: mroch
    
    Differential Revision: D20252511
    
    fbshipit-source-id: e6521e877be83bacc8b4e5078fc998a7fa59ebb2

---
+++ tests/saved_state_no_fallback/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# --saved-state-no-fallback should mean that Flow gives up if it is unable to
+# find a saved state file.
+# Exit code 78 indicates that server start failed (start_flow_unsafe adds the
+# --wait flag).
+# TODO should be exit code 78
+assert_exit 0 start_flow_unsafe . --saved-state-fetcher "local" --saved-state-no-fallback
+
+assert_ok "$FLOW" stop
+
+# If there is an invalid saved state file, --saved-state-no-fallback makes Flow
+# give up.
+echo "definitely not a real saved state file" > .flow.saved_state
+echo "" > .flow.saved_state_file_changes
+assert_exit 78 start_flow_unsafe . --saved-state-fetcher "local" --saved-state-no-fallback
+
+assert_ok "$FLOW" stop
