abc554a9b708698aa37a87f7294f505584cb206e
Marshall Roch <mroch@fb.com>
2020-03-10 14:39:57 -0700

    improve url.parse libdef
    Summary: when the 2nd arg to `url.parse` is `true`, the resulting object's `query` param is definitely a dictionary. when it's `false` or unset, `query` is a string or null. if it's an unknown bool, then it could be any of them:  `string | null | { [string]: string }`.
    
    Reviewed By: panagosg7
    
    Differential Revision: D20365810
    
    fbshipit-source-id: 648bc2965f9f3dc75146d5e7cfc698d6efae585c

--- tests/node_tests/url/url.js
+++ tests/node_tests/url/url.js
@@ -1,2 +1,9 @@
 const url = require('url');
 url.format(url.parse('https://example.com/foo'));
+
+(url.parse('http://example.com/?foo=bar').query: empty); // error, string | null
+(url.parse('http://example.com/?foo=bar', true).query: empty); // error, object
+
+function f(parseQueryString: boolean) {
+  (url.parse('http://example.com/?foo=bar', parseQueryString).query: empty); // error, string | null | object
+}
