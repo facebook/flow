1577c813380da12d3d78a53c1fb6b7bc94ffe474
Marshall Roch <mroch@fb.com>
2020-05-27 06:50:50 -0700

    update jekyll and other deps
    Summary:
    updates ruby, jekyll and jekyll-assets to current versions.
    
    one new thing is that the 'asset' Liquid template tag is now more powerful and extensible, so we can use 'path' to get the path and add a 'name' to get the require.js module name, instead of needing separate tags
    
    Reviewed By: panagosg7
    
    Differential Revision: D21725853
    
    fbshipit-source-id: b2bd3218381eb5dfe62d55df0a7f5561e1d87bd6

--- website/_plugins/jekyll_assets_ext.rb
+++ website/_plugins/jekyll_assets_ext.rb
@@ -7,47 +7,15 @@ require 'jekyll-assets'
 
 module Jekyll
   module Assets
-    module Liquid
-      # Custom tag: adds an `asset_name` liquid tag that returns the AMD module
-      # name, and also sets up the dependency info so that Sprockets builds that
-      # file.
-      class AssetNameTag < Tag
-        def render(context)
-          site = context.registers[:site]
-          args = @args.parse_liquid(context)
-          sprockets = site.sprockets
-
-          asset = find_asset(args, sprockets)
-          sprockets.manifest.add(asset)
-          File.basename(asset.logical_path, File.extname(asset.logical_path))
-
-        rescue => error
-          Jekyll.logger.error("", error.to_s)
-          raise error
-        end
-
-        private
-        def find_asset(args, sprockets)
-          sprockets_, file = args[:sprockets] ||= {}, args[:file]
-          if !(out = sprockets.find_asset(file, sprockets_))
-            raise(
-              AssetNotFoundError, args[:file]
-            )
-
-          else
-            out.liquid_tags << self
-            !args.proxies?? out : ProxiedAsset.new(
-              out, args, sprockets, self
-            )
-          end
-        end
+    class Tag
+      def on_name(args, ctx:, asset:)
+        return unless args[:name]
+        env = ctx.registers[:site].sprockets
+        File.basename(asset.logical_path, File.extname(asset.logical_path))
       end
     end
   end
 end
-
-Liquid::Template.register_tag "asset_name", Jekyll::Assets::Liquid::AssetNameTag
-
 
 module StripExtensionFilter
   def strip_extension(input)
