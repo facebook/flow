facebook/flow

commit 1eaa9dba104a8b39bb6dccda6c0b72237e15b50e

update add-comments to support error codes
Summary:
Update the `add-comments` script to include error codes in its added suppressions.

Note that this changes the API of the tool. Previously, when specifying a suppression, it was necessary to include the suppressor in the comment text when running the command, e.g:
 `./tool add-comments --comment="\$FlowFixMe etc etc` would insert a comment of the form `//$FlowFixMe etc etc`

Now that suppression syntax is standardized, and in order to allow for insertion of a code after the suppressor, one only needs to specify the post-suppression text of the comment. For example:

 `./tool add-comments --comment="etc etc` would insert a comment of the form `//$FlowFixMe[CODE] etc etc` for each error code that appears on a line. This means that potentially invoking the comment could insert multiple suppression comments on a single line of code.

Reviewed By: mroch

Differential Revision: D21847076

fbshipit-source-id: bd60ead1b10c97c51f857379af15143aee4b0350

packages/flow-dev-tools/src/comment/add-commentsRunner.js
@@ -35,6 +35,7 @@ export type Suppression = {|
   loc: FlowLoc,
   isError: boolean,
   lints: Set<string>,
+  error_codes: Array<string>,
 |};
 
 const unselectedBox = '[ ]';
@@ -77,6 +78,10 @@ class BlessedError {
     } else {
       return mainSourceLocOfError(this.error);
     }
+  }
+
+  getErrorCodes(): Array<string> {
+    return this.error.error_codes;
   }
 
   getStringOfLocation(): string {
@@ -623,18 +628,16 @@ async function addComments(args: Args, errors: Array<BlessedError>) {
     string,
     Map<number, Suppression>,
   > = new Map();
-  // Filter out errors without a main location, and dedupe errors that share
-  // the same main location
+  // Filter out errors without a main location
   let errorCount = 0;
   for (const error of errors) {
     const loc = error.getLocation();
+    const error_codes = error.getErrorCodes();
     if (loc != null && loc.source != null) {
       const source = loc.source;
       const lineToLocsMap = filenameToLineToLocsMap.get(source) || new Map();
-      const prevValue = lineToLocsMap.get(loc.start.line);
-      const isError =
-        (prevValue && prevValue.isError) || error.error.kind !== 'lint';
-      let lints = (prevValue && prevValue.lints) || new Set();
+      const isError = error.error.kind !== 'lint';
+      let lints = new Set();
       if (error.error.kind === 'lint') {
         // \u0060 is `. using the escape to avoid a syntax highlighting bug in vscode-language-babel
         const match = /\(\u0060([^\u0060]+)\u0060\)$/.exec(
@@ -644,7 +647,27 @@ async function addComments(args: Args, errors: Array<BlessedError>) {
           lints.add(match[1]);
         }
       }
-      const value = {loc, isError, lints};
+      function joinSuppression(
+        prevValue: ?Suppression,
+        newValue: Suppression,
+      ): Suppression {
+        if (!prevValue) {
+          return newValue;
+        }
+        return {
+          loc: newValue.loc,
+          isError: newValue.isError || prevValue.isError,
+          lints: new Set([...newValue.lints, ...prevValue.lints]),
+          error_codes: [...newValue.error_codes, ...prevValue.error_codes],
+        };
+      }
+      const prevValue: ?Suppression = lineToLocsMap.get(loc.start.line);
+      const value = joinSuppression(prevValue, {
+        loc,
+        isError,
+        lints,
+        error_codes,
+      });
       lineToLocsMap.set(loc.start.line, value);
       filenameToLineToLocsMap.set(source, lineToLocsMap);
       errorCount++;
@@ -698,7 +721,7 @@ export async function addCommentsToCode(
   const ast = await getAst(code, flowBinPath);
 
   let commentCount = 0;
-  for (const {loc, isError, lints} of locs) {
+  for (const {loc, isError, lints, error_codes} of locs) {
     const path = getPathToLoc(loc, ast);
 
     if (path != null) {
@@ -711,7 +734,12 @@ export async function addCommentsToCode(
           .join(', ')}`;
         wrap = false;
       }
-      code = addCommentToCode(c, code, loc, path, wrap);
+      const comments = [...new Set(error_codes)].map(
+        error_code => '$FlowFixMe[' + error_code + '] ' + c,
+      );
+      for (c of comments) {
+        code = addCommentToCode(c, code, loc, path, wrap);
+      }
       commentCount++;
     }
   }

packages/flow-dev-tools/src/comment/add-commentsCommand.js
@@ -61,7 +61,7 @@ Queries Flow for the errors for ROOT. Then opens a curses interface to let you s
       {
         type: 'string',
         name: 'comment',
-        argName: '"\\$FlowFixMe foo"',
+        argName: '"foo"',
         description:
           'Comment to add before the selected errors. If not set, you will be prompted later for it',
       },

packages/flow-dev-tools/src/comment/add-commentsRunner.test.js
@@ -44,19 +44,27 @@ const baz = 3;
 `;
 
 const testOutput =
-`/* this is a really long comment that definitely goes over the line length
- * limit so the tool has to wrap it */
+`/* $FlowFixMe[code1] this is a really long comment that definitely goes over
+ * the line length limit so the tool has to wrap it */
+/* $FlowFixMe[code2] this is a really long comment that definitely goes over
+ * the line length limit so the tool has to wrap it */
 const bar = 4;
 const foo = 4;
-/* this is a really long comment that definitely goes over the line length
- * limit so the tool has to wrap it */
+/* $FlowFixMe[code1] this is a really long comment that definitely goes over
+ * the line length limit so the tool has to wrap it */
+/* $FlowFixMe[code2] this is a really long comment that definitely goes over
+ * the line length limit so the tool has to wrap it */
 const baz = 3;
 <>
-  {/* this is a really long comment that definitely goes over the line length
-    * limit so the tool has to wrap it */}
+  {/* $FlowFixMe[code1] this is a really long comment that definitely goes over
+    * the line length limit so the tool has to wrap it */}
+  {/* $FlowFixMe[code2] this is a really long comment that definitely goes over
+    * the line length limit so the tool has to wrap it */}
   <div>
-    {/* this is a really long comment that definitely goes over the line length
-      * limit so the tool has to wrap it */}
+    {/* $FlowFixMe[code1] this is a really long comment that definitely goes
+      * over the line length limit so the tool has to wrap it */}
+    {/* $FlowFixMe[code2] this is a really long comment that definitely goes
+      * over the line length limit so the tool has to wrap it */}
     <span>
       foo
     </span>
@@ -83,5 +91,6 @@ function makeSuppression(line: number): Suppression {
     },
     isError: true,
     lints: new Set(),
+    error_codes:["code2","code1"]
   };
 }

packages/flow-dev-tools/src/flowResult.js
@@ -18,6 +18,7 @@ export type FlowResult = {
 };
 export type FlowError = {
   kind: string,
+  error_codes : Array<string>,
   level: string,
   message: Array<FlowMessage>,
   trace: ?Array<FlowMessage>,

commit 3a525f9808fab701081245a2e07e886eddf5790f

[PR] Fix add-comments empty space after each line
Summary:
Issue was introduced in 1eaa9dba104a8b39bb6dccda6c0b72237e15b50e /cc dsainati1

When we ran `add-comments` on our codebase, we noticed an empty space after each `$FlowFixMe`:

![image](https://user-images.githubusercontent.com/127199/84708085-afb06080-af14-11ea-9d92-2c5974315092.png)

Command we used:
```bash
~/code/flow/tool add-comments . --bin ~/code/repo/node_modules/.bin/flow --all
```
Pull Request resolved: https://github.com/facebook/flow/pull/8402

Reviewed By: dsainati1

Differential Revision: D22067404

Pulled By: mroch

fbshipit-source-id: 97eb4dc813a33b7013ca8bcc4ee687ce0bd63778

packages/flow-dev-tools/src/comment/add-commentsRunner.js
@@ -735,7 +735,7 @@ export async function addCommentsToCode(
         wrap = false;
       }
       const comments = [...new Set(error_codes)].map(
-        error_code => '$FlowFixMe[' + error_code + '] ' + c,
+        error_code => `$FlowFixMe[${error_code}]${c ? ` ${c}` : ''}`,
       );
       for (c of comments) {
         code = addCommentToCode(c, code, loc, path, wrap);