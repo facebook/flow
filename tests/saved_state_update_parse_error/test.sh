83f38053106b571e3c3a6293505469d581bc4bce
Panagiotis Vekris <pvekris@fb.com>
2020-08-04 10:26:06 -0700

    add tests for saved-state incremental bug
    Summary:
    This test trigger a bug in saved state initialization, when parser errors are
    introduced in the `saved_state_file_changes` set. This happens specifically,
    when the set of erroneous files is not part of the checked set. The bug applies
    for both parser and signature verification errors.
    
    Reviewed By: samwgoldman
    
    Differential Revision: D22847292 (https://github.com/facebook/flow/commit/90b7392d48a69ac65ed225744c8aa46ca4e212a8)
    
    fbshipit-source-id: f6a8387ea1e218eba4f2a47342292f13780688a4

---
+++ tests/saved_state_update_parse_error/test.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+assert_ok "$FLOW" stop
+
+start_flow .
+
+assert_ok "$FLOW" save-state --out .flow.saved_state > /dev/null
+
+mv a.js_ a.js
+echo "a.js" > .flow.saved_state_file_changes
+
+assert_ok "$FLOW" stop
+
+assert_ok "$FLOW" start --saved-state-fetcher local --saved-state-no-fallback --lazy-mode fs
+
+assert_ok "$FLOW" force-recheck --focus b.js
+
+echo "Parse error should be reported:"
+echo ""
+assert_ok "$FLOW" # TODO parse error should be reported
+
+echo "(a: empty);" >> b.js
+echo ""
+echo "Parse error, but no typing error in b.js:"
+echo ""
+assert_ok "$FLOW" # TODO this should be a parse error, but not a typing error (because of the parse error in a.js)
---
+++ tests/saved_state_update_whitespace/test.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+assert_ok "$FLOW" stop
+
+start_flow .
+
+assert_ok "$FLOW" save-state --out .flow.saved_state > /dev/null
+
+mv a.js_ a.js
+echo "a.js" > .flow.saved_state_file_changes
+
+assert_ok "$FLOW" stop
+
+assert_ok "$FLOW" start --saved-state-fetcher local --saved-state-no-fallback --lazy-mode fs
+
+assert_ok "$FLOW" force-recheck --focus b.js
+
+assert_errors "$FLOW" # TODO This should not error
