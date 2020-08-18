640e1c2c2e6300777c24a8c9128b4faedd500cd9
Vijay Ramamurthy <vijayramamurthy@fb.com>
2020-08-18 09:04:16 -0700

    rename JSDoc object member test file
    Summary: since these tests are specifically for object members, rename it to "objects-requires.js" so that in the next diff "requires.js" can be used for more general tests relating to requires
    
    Reviewed By: mroch
    
    Differential Revision: D23087818
    
    fbshipit-source-id: 0cde6579d641c8788382d7b619b29671df70df86

--- tests/type_at_pos_jsdoc/test.sh
+++ tests/type_at_pos_jsdoc/test.sh
@@ -8,5 +8,5 @@ queries_in_file "type-at-pos" "variable_declaration.js" --strip-root --pretty
 queries_in_file "type-at-pos" "class_declaration.js" --strip-root --pretty
 queries_in_file "type-at-pos" "function_declaration.js" --strip-root --pretty
 queries_in_file "type-at-pos" "declare.js" --strip-root --pretty
-queries_in_file "type-at-pos" "requires.js" --strip-root --pretty
+queries_in_file "type-at-pos" "objects-requires.js" --strip-root --pretty
 queries_in_file "type-at-pos" "objects.js" --strip-root --pretty
