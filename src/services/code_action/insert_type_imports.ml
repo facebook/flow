4ffa65b8a425d4485f816bc6b1ededaebf7171e0
Panagiotis Vekris <pvekris@fb.com>
2020-09-30 12:55:14 -0700

    fix code-action's imported relative path
    Summary:
    This improves the relative paths of imported types added by code-actions
    (insert_type_imports.ml).
    
    Before, if the relative path was `'..'`, `Path.make` would expand this to the
    absolute path from system root. This change removes the use of `Path.make`
    completely. It also crops the `.js(.flow)` extension.
    
    This was reported by wchargin on discord
    
    Reviewed By: mroch, samwgoldman
    
    Differential Revision: D24012282
    
    fbshipit-source-id: e6d719903ed0de93965f20a84044132c93ca5153

--- src/services/code_action/insert_type_imports.ml
+++ src/services/code_action/insert_type_imports.ml
@@ -36,11 +36,20 @@ module Modules = struct
       let local_file = Filename.basename f in
       let dep_folder = Filename.dirname f in
       let this_folder = Filename.dirname (File_key.to_string file) in
+      (* remove .flow extension if there is one *)
+      let local_file =
+        if Filename.extension local_file = "flow" then
+          Filename.chop_extension local_file
+        else
+          local_file
+      in
+      (* remove .js extension *)
+      let local_file = Filename.chop_extension local_file in
       if dep_folder = this_folder then
         Filename.concat "./" local_file
       else
         let relative_dir = Files.relative_path this_folder dep_folder in
-        Path.to_string (Path.concat (Path.make relative_dir) local_file)
+        Filename.concat relative_dir local_file
 end
 
 module AstHelper = struct
