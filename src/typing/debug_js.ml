c7472cff139b9ba982789ea816e6765702173dd0
Panagiotis Vekris <pvekris@fb.com>
2020-10-22 17:02:42 -0700

    [easy] remove stale openness logging
    Reviewed By: dsainati1
    
    Differential Revision: D24424669
    
    fbshipit-source-id: a0af80d787321c5afe3397afbcd6407e58ee4354

--- src/typing/debug_js.ml
+++ src/typing/debug_js.ml
@@ -893,21 +893,19 @@ and dump_tvar_ (depth, tvars) cx id =
     spf "%d, ^" id
   else
     let stack = ISet.add id tvars in
-    let open_string = "No Openness" in
     Constraint.(
       try
         match Context.find_tvar cx id with
-        | Goto g -> spf "%d, %s, Goto %d" id open_string g
+        | Goto g -> spf "%d, Goto %d" id g
         | Root { constraints = Resolved (_, t) | FullyResolved (_, t); _ } ->
-          spf "%d, %s, Resolved %s" id open_string (dump_t_ (depth - 1, stack) cx t)
+          spf "%d, Resolved %s" id (dump_t_ (depth - 1, stack) cx t)
         | Root { constraints = Unresolved { lower; upper; _ }; _ } ->
           if lower = TypeMap.empty && upper = UseTypeMap.empty then
-            spf "%d, %s" id open_string
+            spf "%d" id
           else
             spf
-              "%d, %s, [%s], [%s]"
+              "%d, [%s], [%s]"
               id
-              open_string
               (String.concat
                  "; "
                  (List.rev
