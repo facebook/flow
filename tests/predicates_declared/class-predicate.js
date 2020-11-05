2ab20b86c993cc455e02dd71754b73c7a032faf9
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2020-11-05 21:34:46 +0100

    Add test for extending classes

--- tests/predicates_declared/class-predicate.js
+++ tests/predicates_declared/class-predicate.js
@@ -9,5 +9,23 @@ class Hello {
 const m = new Hello();
 const foo: string | number = "";
 if (m.test(foo)) {
-  (foo: string);
+  (foo: string); // okay
+}
+
+class D1 extends Hello {
+  test(x: mixed): boolean %checks { // okay
+    return typeof x === "string";
+  }
+}
+
+class D2 extends Hello {
+  test(x: mixed): boolean %checks { // error incompatible predicates
+    return typeof x === "number";
+  }
+}
+
+class D3 extends Hello {
+  test(x: mixed): boolean { // error non-predicate method
+    return typeof x === "string";
+  }
 }
