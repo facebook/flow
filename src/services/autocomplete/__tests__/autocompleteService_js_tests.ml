5b3608a09f9469fc4a101a0dc4a4151fa2dd090a
Marshall Roch <mroch@fb.com>
2020-12-07 08:48:49 -0800

    add test for add_autocomplete_token
    Summary:
    was investigating why sometimes autocomplete can't find the token in the AST, and the `broader_context` doesn't show the token, as if `add_autocomplete_token` is failing to insert it.
    
    added some tests, but it appears to be working as intended.
    
    Reviewed By: gkz
    
    Differential Revision: D25356961
    
    fbshipit-source-id: f43b4ce90e3d389707c4b159d59443395839b72d

---
+++ src/services/autocomplete/__tests__/autocompleteService_js_tests.ml
@@ -0,0 +1,23 @@
+(*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *)
+
+open OUnit2
+open AutocompleteService_js
+
+let tests =
+  [
+    "add_autocomplete_token"
+    >::: [
+           ( "empty_line" >:: fun ctxt ->
+             let contents = "// @flow\n\nfoo\n\nbar" in
+             let expected = "// @flow\n\nfoo\nAUTO332\nbar" in
+             let (actual, broader_context) = add_autocomplete_token contents 4 0 in
+             assert_equal ~ctxt ~printer:(fun x -> x) expected actual;
+             let expected = "foo\nAUTO332\nbar" in
+             assert_equal ~ctxt ~printer:(fun x -> x) expected broader_context );
+         ];
+  ]
