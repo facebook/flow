dbb98bd7cdb433f73582b2fde6671a2905e45f08
Marshall Roch <mroch@fb.com>
2020-12-08 11:36:34 -0800

    fix Ty_normalizer for imported enum types
    Summary:
    normalizes `TypeT (ImportEnumKind, DefT (_, _, EnumT _))` into a `Ty.Decl (Ty.EnumDecl)` instead of a `Ty.Type (Ty.Generic (... EnumKind ...)`
    
    this fixes a bug with autocomplete where `import type`'d enums weren't included in autocomplete for types.
    
    it also fixes bugs with type-at-pos that caused no results when hovering over the `E` in `import type {E} from ...`, `import { type E } from ...`, and any use of that `E`.
    
    --
    
    `TypeT (ImportEnumKind, _)` comes from `import type { E }` or `import { type E }`, which is the type of the enum object, not an enum instance, so I normalize it the same way as a literal `EnumObjectT`.
    
    this also mirrors what we do for classes. see the very similar `        | DefT (_, _, TypeT (ImportClassKind, DefT (r, _, InstanceT (static, super, _, inst)))) ->` line just above these changes in ty_normalizer.ml, which we normalize into a `ClassDecl` even though it's an `InstanceT`
    
    Reviewed By: panagosg7
    
    Differential Revision: D25273372 (https://github.com/facebook/flow/commit/5d92f4b6ba8170e6d82679bbfa9019e22cf5ef6a)
    
    fbshipit-source-id: 5e02ba9e787e66a95373be5978e083df200ca37a

--- tests/type_at_pos_enums/test.js
+++ tests/type_at_pos_enums/test.js
@@ -9,14 +9,36 @@ const x = E.A;
 const y = E.A;
 //          ^
 
+const z: E = E.A;
+//       ^
+
 type T = Class<E>;
 //   ^
 
 import type {F} from './export.js';
+//           ^
 declare var f: F;
 //          ^
+declare var f2: F;
+//              ^
 
 import type G from './export.js';
+//          ^
 declare var g: G;
 //          ^
+declare var g2: G;
+//              ^
 
+import H from './export.js';
+//     ^
+declare var h: H;
+//          ^
+declare var h2: H;
+//              ^
+
+import type {FType} from './export.js';
+//           ^
+declare var i: FType;
+//          ^
+declare var i2: FType;
+//              ^
