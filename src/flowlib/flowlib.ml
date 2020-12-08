35b1787691d95ae9cbb678e33853dd943a7be347
Marshall Roch <mroch@fb.com>
2020-12-08 12:06:01 -0800

    unique flowlib dir per user (fixes "Could not locate flowlib files")
    Summary:
    the files in `/tmp/flow` have default permissions of `644` (rw-r--r--). in v0.138.0, we started using a consistent directory for flowlibs, `/tmp/flow/flowlib_[hash]`, which ends up writable only by the user that first creates it. if multiple users run flow on the same machine, the second user gets a permission error.
    
    to fix, we now include the effective user ID in the path: `/tmp/flow/flowlib_[hash]_[euid]`. the euid is what is used for file permission checks, so the directory is unique per user.
    
    on Windows, the euid is always 1, but we write to `TEMP` which is per-user on Windows.
    
    NOTE: technically, we still have the same problem with all the other files (logs, pid, sockets, etc) but we have things relying on those paths right now so I'm not changing them. it's much less likely for 2 users to run flow on the same directory. the workaround is to use `FLOW_TEMP_DIR` to write everything into a per-user temp directory, say `~/.cache/flow`.
    
    Fixes https://github.com/facebook/flow/issues/8539
    
    Reviewed By: samwgoldman
    
    Differential Revision: D25400628
    
    fbshipit-source-id: 45f0c4f174ace707dd0565409051ad71c7b5f69e

--- src/flowlib/flowlib.ml
+++ src/flowlib/flowlib.ml
@@ -17,8 +17,14 @@ let contents no_flowlib : (string * string) array =
   else
     [%flowlib_contents]
 
+(** [mkdir ~no_flowlib parent_dir] creates a directory under [parent_dir]
+    within which the flowlib files will be extracted. This directory is
+    named uniquely based on the flowlib contents, as well as the effective
+    user ID (euid) of the current process. The euid is used to ensure that
+    the directory is writable by the current user. *)
 let mkdir ~no_flowlib parent_dir =
-  let libdir = Path.concat parent_dir (Printf.sprintf "flowlib_%s" (hash no_flowlib)) in
+  let euid = Unix.geteuid () in
+  let libdir = Path.concat parent_dir (Printf.sprintf "flowlib_%s_%d" (hash no_flowlib) euid) in
   Sys_utils.mkdir_no_fail (Path.to_string parent_dir);
   Sys_utils.mkdir_no_fail (Path.to_string libdir);
   libdir
