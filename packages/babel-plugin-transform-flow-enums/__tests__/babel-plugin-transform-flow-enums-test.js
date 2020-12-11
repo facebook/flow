d503ff4d93593a04c070f049e07a5fe9cd4ebe11
George Zahariev <gkz@fb.com>
2020-12-11 14:02:32 -0800

    Flow Enums transform package
    Summary:
    This diffs moves the existing Flow Enums Babel transform, as defined and used in WWW, to its own package. We copy over the code and tests.
    
    In the future, when we open-source Flow Enums (announce, and move the docs to flow.org), I'll update and expand the README with more information.
    
    Reviewed By: pieterv
    
    Differential Revision: D25475421
    
    fbshipit-source-id: 615abc36c8106dd967a5176c38c1c6e7900df8a7

---
+++ packages/babel-plugin-transform-flow-enums/__tests__/babel-plugin-transform-flow-enums-test.js
@@ -0,0 +1,83 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * @format
+ */
+
+'use strict';
+
+const pluginTester = require('babel-plugin-tester').default;
+const plugin = require('../index');
+
+pluginTester({
+  plugin,
+  pluginName: 'transform-flow-enums',
+  tests: {
+    boolean: {
+      code: `enum E {A = true, B = false}`,
+      output: `const E = require("flow-enums-runtime")({
+  A: true,
+  B: false
+});`,
+    },
+    number: {
+      code: `enum E {A = 1, B = 2}`,
+      output: `const E = require("flow-enums-runtime")({
+  A: 1,
+  B: 2
+});`,
+    },
+    'string-initialized': {
+      code: `enum E {A = 'a', B = 'b'}`,
+      output: `const E = require("flow-enums-runtime")({
+  A: 'a',
+  B: 'b'
+});`,
+    },
+    'string-defaulted': {
+      code: `enum E {A, B}`,
+      output: `const E = require("flow-enums-runtime").Mirrored(["A", "B"]);`,
+    },
+    symbol: {
+      code: `enum E of symbol {A, B}`,
+      output: `const E = require("flow-enums-runtime")({
+  A: Symbol("A"),
+  B: Symbol("B")
+});`,
+    },
+    export: {
+      code: `export enum E {A = 1, B = 2}`,
+      output: `export const E = require("flow-enums-runtime")({
+  A: 1,
+  B: 2
+});`,
+    },
+    export_default: {
+      code: `export default enum E {A = 1, B = 2}`,
+      output: `const E = require("flow-enums-runtime")({
+  A: 1,
+  B: 2
+});
+
+export default E;`,
+    },
+  },
+});
+
+// Test `getRuntime` plugin option
+pluginTester({
+  plugin,
+  pluginName: 'transform-flow-enums',
+  pluginOptions: {
+    getRuntime: t => t.identifier('Enum'),
+  },
+  tests: {
+    getRuntime: {
+      code: `enum E {A = 1, B = 2}`,
+      output: `const E = Enum({
+  A: 1,
+  B: 2
+});`,
+    },
+  },
+});
