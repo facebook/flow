0d997b6f7372bd055ce2ed642916b239ac2834ec
Panagiotis Vekris <pvekris@fb.com>
2021-01-12 11:09:37 -0800

    make lib tests explicitly inexact
    Summary:
    If errors in libs were raised, we would get a lot of `implicit-inexact-object` errors.
    This diff makes these cases explicitly inexact.
    
    Reviewed By: gkz
    
    Differential Revision: D25799624
    
    fbshipit-source-id: 79de2e87012c5257580668fb19f45fce59c354e6

--- tests/react_instance/lib/jsx.js
+++ tests/react_instance/lib/jsx.js
@@ -1,10 +1,13 @@
 type $JSXIntrinsics = {
   foo: {
     props: any,
-    instance: {yep1: true},
+    instance: {yep1: true, ...},
+    ...
   },
   bar: {
     props: any,
-    instance: {yep2: true},
+    instance: {yep2: true, ...},
+    ...
   },
+  ...
 };
