6a39b6edb28594b2c7beeb29dfe32068c2690927
Marshall Roch <mroch@fb.com>
2021-02-04 13:15:30 -0800

    [autocomplete] autoimport react when completing JSX
    Summary: JSX transforms into `React.createElement`, so `React` must be imported any time you use JSX. this does that automatically.
    
    Reviewed By: vrama628
    
    Differential Revision: D26230755 (https://github.com/facebook/flow/commit/bda7742b492a3a9699a0589e05c05a3751d8829a)
    
    fbshipit-source-id: db3369bf30573f10e76aa57e9dca35509dcaaa46

--- newtests/lsp/completion/autoimports/test.js
+++ newtests/lsp/completion/autoimports/test.js
@@ -196,5 +196,97 @@ export default (suite(
         ['textDocument/publishDiagnostics', ...lspIgnoreStatusAndCancellation],
       ),
     ]).flowConfig('_flowconfig_autoimports'),
+
+    test('textDocument/completion with JSX autoimports', [
+      addCode(`function Foo(props: {...}): null {}`),
+      addCode(`(<F`),
+      lspStartAndConnect(),
+      lspRequestAndWaitUntilResponse('textDocument/completion', {
+        textDocument: {uri: '<PLACEHOLDER_PROJECT_URL>/test.js'},
+        position: {line: 4, character: 3},
+        context: {triggerKind: 1},
+      }).verifyAllLSPMessagesInStep(
+        [
+          [
+            'textDocument/completion',
+            JSON.stringify({
+              isIncomplete: false,
+              items: [
+                {
+                  label: 'undefined',
+                  kind: 6,
+                  detail: 'undefined',
+                  sortText: '00000000000000000101',
+                  insertTextFormat: 1,
+                  textEdit: {
+                    range: {
+                      start: {line: 4, character: 2},
+                      end: {line: 4, character: 3},
+                    },
+                    newText: 'undefined',
+                  },
+                  additionalTextEdits: [
+                    {
+                      range: {
+                        start: {line: 2, character: 0},
+                        end: {line: 2, character: 0},
+                      },
+                      newText: 'import * as React from "react";\n\n',
+                    },
+                  ],
+                },
+                {
+                  label: 'Function',
+                  kind: 6,
+                  detail: 'Function',
+                  sortText: '00000000000000000101',
+                  insertTextFormat: 1,
+                  textEdit: {
+                    range: {
+                      start: {line: 4, character: 2},
+                      end: {line: 4, character: 3},
+                    },
+                    newText: 'Function',
+                  },
+                  additionalTextEdits: [
+                    {
+                      range: {
+                        start: {line: 2, character: 0},
+                        end: {line: 2, character: 0},
+                      },
+                      newText: 'import * as React from "react";\n\n',
+                    },
+                  ],
+                },
+                {
+                  label: 'Foo',
+                  kind: 3,
+                  detail: '(props: {...}) => null',
+                  sortText: '00000000000000000000',
+                  insertTextFormat: 1,
+                  textEdit: {
+                    range: {
+                      start: {line: 4, character: 2},
+                      end: {line: 4, character: 3},
+                    },
+                    newText: 'Foo',
+                  },
+                  additionalTextEdits: [
+                    {
+                      range: {
+                        start: {line: 2, character: 0},
+                        end: {line: 2, character: 0},
+                      },
+                      newText: 'import * as React from "react";\n\n',
+                    },
+                  ],
+                },
+              ],
+            }),
+          ],
+        ],
+        ['textDocument/publishDiagnostics', ...lspIgnoreStatusAndCancellation],
+      ),
+    ]).flowConfig('_flowconfig_autoimports'),
   ],
 ): Suite);
