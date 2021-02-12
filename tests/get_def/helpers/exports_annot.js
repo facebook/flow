9b09a3e7b2d955f0f9afb400acf4f39ec8460abd
Marshall Roch <mroch@fb.com>
2021-02-12 13:03:36 -0800

    [chore] add more tests for get-def on annots
    Summary: tests annotations on named exports, and `declare export`
    
    Reviewed By: panagosg7
    
    Differential Revision: D26424394
    
    fbshipit-source-id: 7464003535c788e4a5fd00b8046695b2b0c4b434

--- tests/get_def/helpers/exports_annot.js
+++ tests/get_def/helpers/exports_annot.js
@@ -1,7 +1,15 @@
 // @flow
 
-type Annot = string
+type X = string
 
-const x : Annot = "a string";
+type Y = number
+
+export const x : X = "a string";
+
+export const _y : Y = 123;
 
 export default x;
+
+declare export var y : typeof _y;
+
+declare export var z : boolean;
