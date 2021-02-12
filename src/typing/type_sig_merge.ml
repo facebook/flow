58acf96b1038bfe3dfab7cc5302bf06aba84cbe2
Marshall Roch <mroch@fb.com>
2021-02-11 19:47:27 -0800

    add deriving show to Type_sig_merge
    Reviewed By: panagosg7
    
    Differential Revision: D26407911
    
    fbshipit-source-id: 6e87784c92802bf245602699571fe3bfc5e0ccdb

--- src/typing/type_sig_merge.ml
+++ src/typing/type_sig_merge.ml
@@ -14,7 +14,7 @@ module Fn = Base.Fn
 module Component : sig
   type 'a t
 
-  type index = private int
+  type index = private int [@@deriving show]
 
   val make : 'a Nel.t -> (index -> 'a -> 'b) -> 'b t
 
@@ -30,7 +30,7 @@ module Component : sig
 end = struct
   type 'a t = 'a array
 
-  type index = int
+  type index = int [@@deriving show]
 
   let make (x, xs) f =
     let len = 1 + List.length xs in
@@ -51,22 +51,24 @@ end
 
 type dependency =
   | CyclicDep of Component.index
-  | AcyclicDep of Type.t (* ModuleT *)
+  | AcyclicDep of (Type.t[@opaque]) (* ModuleT *)
   | ResourceDep of string
   | UncheckedDep of string
   | BuiltinDep of string * string
   | UnknownDep of string
   | LegacyUncheckedDepTryBuiltinsFirst of string
+[@@deriving show]
 
 type 'a node =
   | Node of {
-      mutable merged: Type.t option;
+      mutable merged: Type.t option; [@opaque] (* TODO *)
       packed: 'a;
     }
+[@@deriving show]
 
 type file = {
   key: File_key.t;
-  cx: Context.t;
+  cx: Context.t; [@opaque]
   dependencies: (string * dependency) Module_refs.t;
   exports: ALoc.t Pack.exports node;
   export_def: ALoc.t Pack.packed option;
@@ -75,6 +77,7 @@ type file = {
   patterns: ALoc.t Pack.pattern node Patterns.t;
   pattern_defs: ALoc.t Pack.packed Pattern_defs.t;
 }
+[@@deriving show]
 
 let remote_ref_loc = function
   | Pack.Import { id_loc; _ }
