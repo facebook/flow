89fbf76e7bafb3a4f231c4effd4151d373833b81
Marshall Roch <mroch@fb.com>
2021-02-17 05:33:53 -0800

    add Loc.intersects
    Reviewed By: nmote
    
    Differential Revision: D26431855 (https://github.com/facebook/flow/commit/a9c0df5b6459f3635aab7ffa40f2b21e997e74a3)
    
    fbshipit-source-id: 1386c830a89ae1b23500881350c94b05d82d410a

--- src/parser/loc.mli
+++ src/parser/loc.mli
@@ -29,8 +29,15 @@ val first_char : t -> t
 val contains : t -> t -> bool
 (** [contains loc1 loc2] returns true if [loc1] entirely overlaps [loc2] *)
 
+val intersects : t -> t -> bool
+(** [intersects loc1 loc2] returns true if [loc1] intersects [loc2] at all *)
+
 val lines_intersect : t -> t -> bool
-(** [lines_intersect loc1 loc2] returns true if [loc1] intersects [loc2] at all *)
+(** [lines_intersect loc1 loc2] returns true if [loc1] and [loc2] cover any part of
+    the same line, even if they don't actually intersect.
+
+    For example, if [loc1] ends and then [loc2] begins later on the same line,
+    [intersects loc1 loc2] is false, but [lines_intersect loc1 loc2] is true. *)
 
 val pos_cmp : position -> position -> int
 
