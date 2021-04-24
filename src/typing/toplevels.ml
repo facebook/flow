56f12adf72642a68eaed976afc5dc87d34e77744
Mike Vitousek <mvitousek@fb.com>
2021-04-23 19:01:24 -0700

    Functorize toplevels and give a lexical definition
    Summary: This diff starts to build the framework for reordering checking of programs. It functorizes the toplevels module and provides an `Ordering` module used to figure out the order of checking statements. Currently, this just uses the regular lexical ordering of the statements in the program, determined using `compare` on their locs.
    
    Reviewed By: panagosg7
    
    Differential Revision: D27722338
    
    fbshipit-source-id: 0d58336de01fc6d3cb8a78e2c371429ed205a7dc

--- src/typing/toplevels.ml
+++ src/typing/toplevels.ml
@@ -11,44 +11,71 @@ module Ast = Flow_ast
 module Flow = Flow_js
 module Tast_utils = Typed_ast_utils
 
-let toplevels statement =
-  let rec loop acc cx = function
-    | [] -> List.rev acc
-    | (loc, Ast.Statement.Empty empty) :: stmts ->
-      loop ((loc, Ast.Statement.Empty empty) :: acc) cx stmts
-    | stmt :: stmts ->
-      (match Abnormal.catch_stmt_control_flow_exception (fun () -> statement cx stmt) with
-      | (stmt, Some abnormal) ->
-        (* control flow exit out of a flat list:
+module type Ordering = sig
+  type t
+
+  type loc
+
+  val make : (loc, loc) Ast.Statement.t list -> t
+
+  val compare : t -> (loc, loc) Ast.Statement.t -> (loc, loc) Ast.Statement.t -> int
+end
+
+module Toplevels (Order : Ordering with type loc = ALoc.t) = struct
+  let toplevels statement =
+    let rec loop acc cx = function
+      | [] -> List.rev acc
+      | (loc, Ast.Statement.Empty empty) :: stmts ->
+        loop ((loc, Ast.Statement.Empty empty) :: acc) cx stmts
+      | stmt :: stmts ->
+        (match Abnormal.catch_stmt_control_flow_exception (fun () -> statement cx stmt) with
+        | (stmt, Some abnormal) ->
+          (* control flow exit out of a flat list:
            check for unreachable code and rethrow *)
-        let warn_unreachable loc = Flow.add_output cx (Error_message.EUnreachable loc) in
-        let rest =
-          Base.List.map
-            ~f:
-              (let open Ast.Statement in
-              fun stmt ->
-                match stmt with
-                | (_, Empty _) as stmt -> stmt
-                (* function declarations are hoisted, so not unreachable *)
-                | (_, FunctionDeclaration _) -> statement cx stmt
-                (* variable declarations are hoisted, but associated assignments are
+          let warn_unreachable loc = Flow.add_output cx (Error_message.EUnreachable loc) in
+          let rest =
+            Base.List.map
+              ~f:
+                (let open Ast.Statement in
+                fun stmt ->
+                  match stmt with
+                  | (_, Empty _) as stmt -> stmt
+                  (* function declarations are hoisted, so not unreachable *)
+                  | (_, FunctionDeclaration _) -> statement cx stmt
+                  (* variable declarations are hoisted, but associated assignments are
                    not, so skip variable declarations with no assignments.
                    Note: this does not seem like a practice anyone would use *)
-                | (_, VariableDeclaration d) as stmt ->
-                  VariableDeclaration.(
-                    d.declarations
-                    |> List.iter
-                         Declarator.(
-                           function
-                           | (_, { init = Some (loc, _); _ }) -> warn_unreachable loc
-                           | _ -> ()));
-                  Tast_utils.unreachable_mapper#statement stmt
-                | (loc, _) as stmt ->
-                  warn_unreachable loc;
-                  Tast_utils.unreachable_mapper#statement stmt)
-            stmts
-        in
-        Abnormal.throw_stmts_control_flow_exception (List.rev_append acc (stmt :: rest)) abnormal
-      | (stmt, None) -> loop (stmt :: acc) cx stmts)
-  in
-  (fun cx -> loop [] cx)
+                  | (_, VariableDeclaration d) as stmt ->
+                    VariableDeclaration.(
+                      d.declarations
+                      |> List.iter
+                           Declarator.(
+                             function
+                             | (_, { init = Some (loc, _); _ }) -> warn_unreachable loc
+                             | _ -> ()));
+                    Tast_utils.unreachable_mapper#statement stmt
+                  | (loc, _) as stmt ->
+                    warn_unreachable loc;
+                    Tast_utils.unreachable_mapper#statement stmt)
+              stmts
+          in
+          Abnormal.throw_stmts_control_flow_exception (List.rev_append acc (stmt :: rest)) abnormal
+        | (stmt, None) -> loop (stmt :: acc) cx stmts)
+    in
+    fun cx statements ->
+      let ordering = Order.make statements in
+      Base.List.sort statements (Order.compare ordering) |> loop [] cx
+end
+
+module LexicalOrdering : Ordering with type loc = ALoc.t = struct
+  type t = unit
+
+  type loc = ALoc.t
+
+  let make _ = ()
+
+  let compare _ (l1, _) (l2, _) = Loc.compare (ALoc.to_loc_exn l1) (ALoc.to_loc_exn l2)
+end
+
+module LexicalToplevels = Toplevels (LexicalOrdering)
+include LexicalToplevels
