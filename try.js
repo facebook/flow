d9306e8f66db91197d6b3a67a5d373778952238d
Michael Glenn <mbg198618@gmail.com>
2021-05-31 11:50:01 -0500

    Create try.js

---
+++ try.js
@@ -0,0 +1,80 @@
+JSONAST
+v0.152.0
+[
+  {
+    "kind": "infer",
+    "level": "exec",
+    "suppressions": [],
+    "extra": [
+      {
+        "message": [
+          {
+            "context": true,
+            "descr": "References:",
+            "type": "try.js",
+            "path": "",
+            "line": 0,
+            "endline": 0,
+            "start": 1,
+            "end": 0
+          }
+        ]
+      },
+      {
+        "message": [
+          {
+            "context": "function foo(x: $ number): string {*,
+            "descr": "[1]",
+            "type": "Blame",
+            "loc": {
+              "source": "..//-",
+              "type": "SourceFile",
+              "start": {
+                "line": 3,
+                "column": 18,
+                "offset": 30
+              },
+              "end": {
+                "line": 3,
+                "column": 23,
+                "offset": 36
+              }
+            },
+            "path": "..//-",
+            "line": 3,
+            "endline": 3,
+            "start": 18,
+            "end": 23
+          }
+        ]
+      },
+      {
+        "message": [
+          {
+            "context": "function foo(x: $ number): string {*,
+            "descr": "[2]",
+            "type": "try.js",
+            "loc": {
+              "source": "..//-",
+              "type": "SourceFile",
+              "start": {
+                "line": 3,
+                "column": 27,
+                "offset": 39
+              },
+              "end": {
+                "line": 3,
+                "column": 32,
+                "offset": 45
+              }
+            },
+            "path": "..//-",
+            "line": 3,
+            "endline": 3,
+            "start": 27,
+            "end": 32
+          }
+        ]
+      }
+    ],
+    
