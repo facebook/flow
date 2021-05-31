871d325720ed62eaeec8327ff01a35e501badc0a
Michael Glenn <mbg198618@gmail.com>
2021-05-31 13:22:32 -0500

    Create NEXT.js

---
+++ NEXT.js
@@ -0,0 +1,5 @@
+Next.js
+Next.js is a popular and lightweight framework for static and server‑rendered applications built with React. It includes styling and routing solutions out of the box, and assumes that you’re using Node.js as the server environment.
+
+Learn Next.js from its official guide.
+
