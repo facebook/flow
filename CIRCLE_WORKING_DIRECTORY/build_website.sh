29dd61b2df041ef1cc768dfe5353bd33b288bac5
Michael Glenn <mbg198618@gmail.com>
2021-05-31 12:11:06 -0500

    Create build_website.sh

---
+++ CIRCLE_WORKING_DIRECTORY/build_website.sh
@@ -0,0 +1,19 @@
+# <content://com.rarlab.rar.provider/external_files/Android/data/com.rarlab.rar/files/_rartemp_open_1622480753289/build_website.sh>
+#
+$ !/bin/bash -x
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
+PAGES_CHECKOUT="$CIRCLE_WORKING_DIRECTORY/dist/flow.org"
+export BUNDLE_GEMFILE="$CIRCLE_WORKING_DIRECTORY/website/Gemfile"
+#
+GEN_DIR=$([[ "$CIRCLE_TAG" = "" ]] && echo "master" || echo "$CIRCLE_TAG")
+mkdir -p "$PAGES_CHECKOUT"
+mkdir -p "website/static/$GEN_DIR"
+cp "bin/flow.js" "website/static/${GEN_DIR}/flow.js"
+cp -r "lib" "website/static/${GEN_DIR}/flowlib"
+(cd website && env \
+  JEKYLL_ENV=production \
+  make build DEST="$PAGES_CHECKOUT")
