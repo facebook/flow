c5f895f08560bde5397573ed01c872ef8b0f05ab
Michael Glenn <mbg198618@gmail.com>
2021-05-31 13:24:28 -0500

    Create Gatsby.js

---
+++ Gatsby.js
@@ -0,0 +1,12 @@
+Gatsby
+Gatsby is the best way to create static websites with React. It lets you use React components, but outputs pre-rendered HTML and CSS to guarantee the fastest load time.
+
+Learn Gatsby from its official guide and a gallery of starter kits.
+
+More Flexible Toolchains
+The following toolchains offer more flexibility and choice. We recommend them to more experienced users:
+
+Neutrino combines the power of webpack with the simplicity of presets, and includes a preset for React apps and React components.
+Nx is a toolkit for full-stack monorepo development, with built-in support for React, Next.js, Express, and more.
+Parcel is a fast, zero configuration web application bundler that works with React.
+Razzle is a server-rendering framework that doesn’t require any configuration, but offers more flexibility than Next.js.
