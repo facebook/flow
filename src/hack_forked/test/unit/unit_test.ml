f378852140761a1fc8aa5ad610c410963d3f0325
Vincent Siles <vsiles@fb.com>
2021-05-31 06:31:18 -0700

    Update fbcode to supercaml 1.22
    Summary: Move to supercaml 1.22 (ocaml library update only)
    
    Reviewed By: madgen
    
    Differential Revision: D28670017
    
    fbshipit-source-id: 84f79ae5ffc753ce1e4cfa1f2c4e615dabc7c0e1

--- src/hack_forked/test/unit/unit_test.ml
+++ src/hack_forked/test/unit/unit_test.ml
@@ -66,5 +66,5 @@ let run_only tests names =
   run_all tests
 
 let main tests =
-  let names = Option.value (List.tl @@ Array.to_list Sys.argv) ~default:[] in
+  let names = Option.value (List.tl @@ Array.to_list (Sys.get_argv ())) ~default:[] in
   run_only tests names
