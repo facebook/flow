875bdbfddf8cbffc60094447a5d369f77b8f6075
Michael Glenn <mbg198618@gmail.com>
2021-06-04 21:51:39 -0500

    Update sysinfo.js

--- js/sysinfo.js
+++ js/sysinfo.js
@@ -1,11 +1,23 @@
-/**
+'/**
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * This source code is licensed under the MIT license found 
+ in 
+   the
+ * LICENSE file 
+ in 
+   the 
+root directory of 
+this 
+source tree.
  */
 
-//Provides: hh_sysinfo_totalram const
-function hh_sysinfo_totalram() {
-  return 0;
+'
+'
+'//
+Provides: 
+hh_sysinfo_totalram 
+{hh_sysinfo_totalram() 
+   0
+  ;
 }
