15b2e2aa2960731dc0c03bcc387eaec007be3e06
Sam Goldman <samgoldman@fb.com>
2021-06-09 12:23:23 -0700

    Remove old signature hashing code
    Summary:
    The context optimizer is currently used by some temporary implicit instantiation
    code. However, that caller only seems to rely on the side effect of concretizing
    tvars, not signature hashing.
    
    Reviewed By: panagosg7
    
    Differential Revision: D28979095
    
    fbshipit-source-id: 316e8b67cafb94fcc9b6548201f6c619b4f2d41e

--- src/typing/context_optimizer.ml
+++ src/typing/context_optimizer.ml
@@ -62,29 +62,6 @@ class context_optimizer ~no_lowers =
   object (self)
     inherit [Polarity.t] Type_mapper.t_with_uses as super
 
-    val sig_hash = Xx.init 0L
-
-    method sig_hash () = Xx.digest sig_hash
-
-    val mutable next_stable_id = 0
-
-    method fresh_stable_id =
-      let stable_id = next_stable_id in
-      next_stable_id <- next_stable_id + 1;
-      stable_id
-
-    val mutable stable_tvar_ids = IMap.empty
-
-    val mutable stable_trust_var_ids = IMap.empty
-
-    val mutable stable_eval_ids = IMap.empty
-
-    val mutable stable_poly_ids = IMap.empty
-
-    val mutable stable_props_ids = IMap.empty
-
-    val mutable stable_call_prop_ids = IMap.empty
-
     val mutable reduced_module_map = NameUtils.Map.empty
 
     val mutable reduced_graph : Type.Constraint.node IMap.t = IMap.empty
@@ -110,20 +87,14 @@ class context_optimizer ~no_lowers =
     method tvar cx pole r id =
       let (root_id, _) = Context.find_constraints cx id in
       if id == root_id then (
-        if IMap.mem id reduced_graph then (
-          let stable_id = IMap.find root_id stable_tvar_ids in
-          SigHash.add_int sig_hash stable_id;
+        if IMap.mem id reduced_graph then
           id
-        ) else
+        else
           let t = Flow_js_utils.merge_tvar ~no_lowers cx r id in
           let node =
             Root { rank = 0; constraints = Lazy.from_val (FullyResolved (unknown_use, lazy t)) }
           in
           reduced_graph <- IMap.add id node reduced_graph;
-          let () =
-            let stable_id = self#fresh_stable_id in
-            stable_tvar_ids <- IMap.add id stable_id stable_tvar_ids
-          in
           let t = self#type_ cx pole t in
           let node =
             Root { rank = 0; constraints = Lazy.from_val (FullyResolved (unknown_use, lazy t)) }
@@ -140,18 +111,12 @@ class context_optimizer ~no_lowers =
     method trust_var cx pole id =
       let (root_id, constr) = Context.find_trust_constraints cx id in
       if id == root_id then (
-        if IMap.mem id reduced_trust_graph then (
-          let stable_id = IMap.find root_id stable_trust_var_ids in
-          SigHash.add_int sig_hash stable_id;
+        if IMap.mem id reduced_trust_graph then
           id
-        ) else
+        else
           let t = merge_trust_var constr in
           let node = Trust_constraint.new_resolved_root t in
           reduced_trust_graph <- IMap.add id node reduced_trust_graph;
-          let () =
-            let stable_id = self#fresh_stable_id in
-            stable_trust_var_ids <- IMap.add id stable_id stable_trust_var_ids
-          in
           id
       ) else (
         ignore (self#trust_var cx pole root_id);
@@ -162,36 +127,9 @@ class context_optimizer ~no_lowers =
 
     method props cx pole id =
       if Properties.Map.mem id reduced_property_maps then
-        let () =
-          Base.Option.iter
-            ~f:(fun id_int ->
-              let stable_id =
-                if Context.mem_nominal_prop_id cx id_int then
-                  IMap.find id_int stable_props_ids
-                else
-                  id_int
-              in
-              SigHash.add_int sig_hash stable_id)
-            (Properties.id_as_int id)
-        in
         id
       else
-        let () =
-          Base.Option.iter
-            ~f:(fun id_int ->
-              let stable_id =
-                if Context.mem_nominal_prop_id cx id_int then (
-                  let stable_id = self#fresh_stable_id in
-                  stable_props_ids <- IMap.add id_int stable_id stable_props_ids;
-                  stable_id
-                ) else
-                  id_int
-              in
-              SigHash.add_int sig_hash stable_id)
-            (Properties.id_as_int id)
-        in
         let pmap = Context.find_props cx id in
-        let () = SigHash.add_props_map sig_hash pmap in
         reduced_property_maps <- Properties.Map.add id pmap reduced_property_maps;
         let pmap' = NameUtils.Map.ident_map (self#prop cx pole) pmap in
         reduced_property_maps <- Properties.Map.add id pmap' reduced_property_maps;
@@ -199,14 +137,8 @@ class context_optimizer ~no_lowers =
 
     method call_prop cx pole id =
       if IMap.mem id reduced_call_props then
-        let stable_id = IMap.find id stable_call_prop_ids in
-        let () = SigHash.add_int sig_hash stable_id in
         id
       else
-        let () =
-          let stable_id = self#fresh_stable_id in
-          stable_call_prop_ids <- IMap.add id stable_id stable_call_prop_ids
-        in
         let t = Context.find_call cx id in
         reduced_call_props <- IMap.add id t reduced_call_props;
         let t' = self#type_ cx pole t in
@@ -245,20 +177,12 @@ class context_optimizer ~no_lowers =
         reduced_export_maps <- Exports.Map.add id tmap reduced_export_maps;
         let tmap' = NameUtils.Map.ident_map map_pair tmap in
         reduced_export_maps <- Exports.Map.add id tmap' reduced_export_maps;
-        SigHash.add_exports_map sig_hash tmap';
         id
 
     method eval_id cx pole id =
-      if Eval.Map.mem id reduced_evaluated then (
-        Eval.id_as_int id
-        |> Base.Option.iter ~f:(fun int_id ->
-               IMap.find int_id stable_eval_ids |> SigHash.add_int sig_hash);
+      if Eval.Map.mem id reduced_evaluated then
         id
-      ) else
-        let stable_id = self#fresh_stable_id in
-        Eval.id_as_int id
-        |> Base.Option.iter ~f:(fun int_id ->
-               stable_eval_ids <- IMap.add int_id stable_id stable_eval_ids);
+      else
         match Eval.Map.find_opt id (Context.evaluated cx) with
         | None -> id
         | Some t ->
@@ -268,12 +192,9 @@ class context_optimizer ~no_lowers =
           id
 
     method! destructor cx map_cx t =
-      SigHash.add_destructor sig_hash t;
       match t with
       | NonMaybeType -> t
-      | PropertyType s ->
-        SigHash.add_name sig_hash s;
-        t
+      | PropertyType _ -> t
       | ElementType { index_type; is_indexed_access } ->
         let index_type' = self#type_ cx map_cx index_type in
         if index_type' == index_type then
@@ -286,9 +207,7 @@ class context_optimizer ~no_lowers =
           t
         else
           OptionalIndexedAccessNonMaybeType { index_type = index_type' }
-      | OptionalIndexedAccessResultType { void_reason } ->
-        SigHash.add_reason sig_hash void_reason;
-        t
+      | OptionalIndexedAccessResultType _ -> t
       | Bind t' ->
         let t'' = self#type_ cx map_cx t' in
         if t'' == t' then
@@ -297,11 +216,6 @@ class context_optimizer ~no_lowers =
           Bind t''
       | ReadOnlyType -> t
       | SpreadType (options, tlist, acc) ->
-        let () =
-          match options with
-          | Object.Spread.Annot { make_exact } -> SigHash.add_bool sig_hash make_exact
-          | _ -> ()
-        in
         let tlist' = ListUtils.ident_map (self#object_kit_spread_operand cx map_cx) tlist in
         let acc' = OptionUtils.ident_map (self#object_kit_spread_operand_slice cx map_cx) acc in
         if tlist' == tlist && acc == acc' then
@@ -309,15 +223,6 @@ class context_optimizer ~no_lowers =
         else
           SpreadType (options, tlist', acc')
       | RestType (options, x) ->
-        let open Object.Rest in
-        let () =
-          match options with
-          | Sound -> SigHash.add_int sig_hash 1
-          | IgnoreExactAndOwn -> SigHash.add_int sig_hash 2
-          | ReactConfigMerge p ->
-            SigHash.add_int sig_hash 3;
-            SigHash.add sig_hash (Polarity.sigil p)
-        in
         let x' = self#type_ cx map_cx x in
         if x' == x then
           t
@@ -347,22 +252,7 @@ class context_optimizer ~no_lowers =
       | ReactElementRefType ->
         t
 
-    method! dict_type cx pole dicttype =
-      let dicttype' = super#dict_type cx pole dicttype in
-      SigHash.add_polarity sig_hash dicttype'.dict_polarity;
-      dicttype'
-
-    method! enum cx pole e =
-      let { enum_id; members; _ } = e in
-      let e' = super#enum cx pole e in
-      SigHash.add_aloc sig_hash (enum_id :> ALoc.t);
-      Base.List.iter
-        ~f:(SigHash.add sig_hash)
-        (SMap.keys members |> Base.List.stable_sort ~compare:String.compare);
-      e'
-
     method! type_ cx pole t =
-      SigHash.add_reason sig_hash (reason_of_t t);
       begin
         match t with
         | DefT (_, trust, _) when Context.trust_tracking cx && is_ident trust ->
@@ -371,29 +261,6 @@ class context_optimizer ~no_lowers =
       end;
       match t with
       | InternalT _ -> Utils_js.assert_false "internal types should not appear in signatures"
-      | OpenT _ -> super#type_ cx pole t
-      | DefT (_, _, InstanceT (_, _, _, { class_id; _ })) ->
-        SigHash.add_aloc sig_hash (class_id :> ALoc.t);
-        super#type_ cx pole t
-      | OpaqueT (_, { opaque_id; _ }) ->
-        SigHash.add_aloc sig_hash (opaque_id :> ALoc.t);
-        super#type_ cx pole t
-      | DefT (_, _, PolyT { id = poly_id; _ }) ->
-        if Context.mem_nominal_poly_id cx poly_id then
-          Poly.id_as_int poly_id
-          |> Base.Option.iter ~f:(fun poly_id ->
-                 let id =
-                   match IMap.find_opt poly_id stable_poly_ids with
-                   | None ->
-                     let id = self#fresh_stable_id in
-                     stable_poly_ids <- IMap.add poly_id id stable_poly_ids;
-                     id
-                   | Some id -> id
-                 in
-                 SigHash.add_int sig_hash id)
-        else
-          Poly.id_as_int poly_id |> Base.Option.iter ~f:(SigHash.add_int sig_hash);
-        super#type_ cx pole t
       | UnionT (_, rep) ->
         if not (UnionRep.is_optimized_finally rep) then
           UnionRep.optimize
@@ -402,16 +269,10 @@ class context_optimizer ~no_lowers =
             ~flatten:(Type_mapper.union_flatten cx)
             ~find_resolved:(Context.find_resolved cx)
             ~find_props:(Context.find_props cx);
-        let t' = super#type_ cx pole t in
-        SigHash.add_type sig_hash t';
-        t'
-      | _ ->
-        let t' = super#type_ cx pole t in
-        SigHash.add_type sig_hash t';
-        t'
+        super#type_ cx pole t
+      | _ -> super#type_ cx pole t
 
     method! use_type cx pole use =
-      SigHash.add_reason sig_hash (reason_of_use_t use);
       match use with
       | UseT (u, t) ->
         let t' = self#type_ cx Polarity.Neutral t in
@@ -419,9 +280,7 @@ class context_optimizer ~no_lowers =
           use
         else
           UseT (u, t')
-      | _ ->
-        SigHash.add_use sig_hash use;
-        super#use_type cx pole use
+      | _ -> super#use_type cx pole use
 
     method! choice_use_tool =
       (* Any choice kit constraints should be fully
@@ -431,11 +290,6 @@ class context_optimizer ~no_lowers =
        * context. This makes it possible to clear the type graph before storing
        * in the heap. *)
       Utils_js.assert_false "choice kit uses should not appear in signatures"
-
-    (* We need to make sure to hash the keys in any spread intermediate types! *)
-    method! object_kit_spread_operand_slice cx map_cx slice =
-      NameUtils.Map.iter (fun k _ -> SigHash.add_name sig_hash k) slice.Object.Spread.prop_map;
-      super#object_kit_spread_operand_slice cx map_cx slice
 
     method get_reduced_module_map = reduced_module_map
 
@@ -455,4 +309,4 @@ class context_optimizer ~no_lowers =
 let reduce_context cx ~no_lowers module_refs =
   let reducer = new context_optimizer ~no_lowers in
   Base.List.iter ~f:(reducer#reduce cx) module_refs;
-  (reducer#sig_hash (), reducer)
+  reducer
