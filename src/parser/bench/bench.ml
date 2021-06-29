b5b532b09d410a0799ee68861e0c817555e613aa
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2021-06-29 10:11:17 +0800

    rename test data extension to .data to avoid some tooling issues
    update the benchmark

--- src/parser/bench/bench.ml
+++ src/parser/bench/bench.ml
@@ -1,8 +1,5 @@
-
-
-
 (* let filename = "react.development.js" *)
-let filename = "./flow_parser_dot_js.bc.js"
+let filename = "./flow_parser_dot_js.data"
 
 let read_whole_file filename =
   let ch = open_in_bin filename in
@@ -10,15 +7,20 @@ let read_whole_file filename =
   close_in ch;
   s
 
-;; assert (Sys.file_exists filename)
+;;
+assert (Sys.file_exists filename)
 
 let content = read_whole_file filename
+
 let start = Unix.gettimeofday ()
 
+let parse content = Parser_flow.program_file ~fail:true content None
 
-let (_,errors) = Parser_flow.program_file ~fail:true content None 
+let () =
+  for i = 0 to 30 do
+    ignore (parse content)
+  done;
+  (* let ch = open_out_gen [Open_append; Open_creat] 0o666 "time.txt" in *)
+  output_string stdout (Printf.sprintf "%f" (Unix.gettimeofday () -. start))
 
-
-;; assert (errors = [])
-
-;; Format.eprintf "consumed %f@." (Unix.gettimeofday () -. start)
+(* close_out ch *)
