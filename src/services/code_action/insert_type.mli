2365670bbd69dcfc14bb76bd980ebba16775a40d
Sam Zhou <samzhou19815@fb.com>
2021-07-07 18:38:29 -0700

    [lsp][easy] Create interface for `insert_type.ml`
    Summary: `insert_type.ml` has too many internal helper methods, which makes it harder to understand what's being used elsewhere, and what's considered as internal. Create an interface so that we know what's external and we can easily see the parameter of types without IDE help.
    
    Reviewed By: gkz
    
    Differential Revision: D29590971
    
    fbshipit-source-id: ae8e48f9169e8cc498ffc79bf2f4dbdc40229484

---
+++ src/services/code_action/insert_type.mli
@@ -0,0 +1,86 @@
+(*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *)
+
+type unexpected =
+  | UnknownTypeAtPoint of Loc.t
+  | NoFileInLocation of Loc.t
+  | FailedToSerialize of {
+      ty: Ty.t;
+      error_message: string;
+    }
+  | FailedToNormalizeNoMatch
+
+type expected =
+  | TypeAnnotationAtPoint of {
+      location: Loc.t;
+      type_ast: (Loc.t, Loc.t) Flow_ast.Type.t;
+    }
+  | InvalidAnnotationTarget of Loc.t
+  | UnsupportedAnnotation of {
+      location: Loc.t;
+      error_message: string;
+    }
+  | MulipleTypesPossibleAtPoint of {
+      generalized: (Loc.t, Loc.t) Flow_ast.Type.t;
+      specialized: (Loc.t, Loc.t) Flow_ast.Type.t;
+    }
+  | FailedToValidateType of {
+      error: Insert_type_utils.Error.validation_error;
+      error_message: string;
+    }
+  | FailedToTypeCheck of Errors.ConcreteLocPrintableErrorSet.t
+  | FailedToNormalize of (Loc.t * string)
+  | FailedToImport of Insert_type_utils.Error.import_error
+
+type errors =
+  | Unexpected of unexpected
+  | Expected of expected
+
+exception FailedToInsertType of errors
+
+exception FoundAmbiguousType
+
+val simplify : Ty.t -> Ty.t
+
+val path_of_loc : ?error:(string, string) result -> Loc.t -> (string, string) result
+
+val error_to_string : errors -> string
+
+val add_statement_after_directive_and_type_imports :
+  (Loc.t, Loc.t) Flow_ast.Statement.t list ->
+  (Loc.t, Loc.t) Flow_ast.Statement.t list ->
+  (Loc.t, Loc.t) Flow_ast.Statement.t list
+
+val add_imports :
+  Insert_type_imports.ImportsHelper.remote_converter ->
+  (Loc.t, Loc.t) Flow_ast.Statement.t list ->
+  (Loc.t, Loc.t) Flow_ast.Statement.t list
+
+val insert_type :
+  full_cx:Context.t ->
+  file_sig:File_sig.With_Loc.t ->
+  typed_ast:(ALoc.t, ALoc.t * Type.t) Flow_ast.Program.t ->
+  expand_aliases:bool ->
+  omit_targ_defaults:bool ->
+  strict:bool ->
+  ambiguity_strategy:Autofix_options.ambiguity_strategy ->
+  ?remote_converter:Insert_type_imports.ImportsHelper.remote_converter ->
+  (Loc.t, Loc.t) Flow_ast_mapper.Ast.Program.t ->
+  Loc.t ->
+  (Loc.t, Loc.t) Flow_ast_mapper.Ast.Program.t
+
+val mk_diff :
+  (Loc.t, Loc.t) Flow_ast.Program.t ->
+  (Loc.t, Loc.t) Flow_ast.Program.t ->
+  Flow_ast_differ.node Flow_ast_differ.changes
+
+val mk_patch :
+  opts:Js_layout_generator.opts ->
+  (Loc.t, Loc.t) Flow_ast.Program.t ->
+  (Loc.t, Loc.t) Flow_ast.Program.t ->
+  string ->
+  Replacement_printer.patch
