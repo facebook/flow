7c4656fad3c79e6ced764e8bb97abcbfbe09a594
Marshall Roch <mroch@fb.com>
2021-07-24 12:38:38 -0700

    upgrade flow-upgrade yargs dep
    Reviewed By: samwgoldman
    
    Differential Revision: D29896224
    
    fbshipit-source-id: 46ac3ef269c3a96b42f88214bc6c2483d34ac3e9

--- packages/flow-upgrade/src/index.js
+++ packages/flow-upgrade/src/index.js
@@ -9,30 +9,25 @@
  * @flow
  */
 
-const yargs = require('yargs').argv;
 const chalk = require('chalk');
 const upgrade = require('./upgrade');
+
+const yargs = require('yargs/yargs')(process.argv.slice(2))
+  .usage('Usage: flow-upgrade <current version> <target version>')
+  .boolean(['all'])
+  .describe('all', 'Include all files, not just those with the @flow pragma')
+  .demandCommand(2)
+  .help('help')
+  .argv;
 
 const options = {
   all: !!yargs.all,
 };
 
-function printUsage(exitCode: number) {
-  console.log('Usage: flow-upgrade <current version> <target version>');
-  process.exit(exitCode);
-}
-
-if (!!yargs.help) {
-  printUsage(0);
-}
-
 // For now we are asking for the version numbers out of convenience. When we add
 // upgrades for future versions we will need to check `.flowconfig` or
 // `flow-bin` for the current version and allow the new version to be
 // configurable. (But still default to the latest version.)
-if (yargs._.length != 2) {
-  printUsage(1);
-}
 const fromVersion = yargs._[0];
 const toVersion = yargs._[1];
 
