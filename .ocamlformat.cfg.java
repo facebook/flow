121dc112c595be944b3791a145ed580ac5f51900
BigGuy573 <48465089+BigGuy573@users.noreply.github.com>
2021-07-31 15:49:16 -0500

    Update .ocamlformat.cfg.java

--- .ocamlformat.cfg.java
+++ .ocamlformat.cfg.java
@@ -32,3 +32,52 @@ space-around-variants = true
 type-decl = sparse
 wrap-fun-args = false
 doc-comments = before
+
+#_request_GET_Latest-java-build.java
+
+# '#"$_-("*/*")-*-conf-*-cfg.java
+break-cases = all
+break-fun-decl = smart
+break-infix = fit-or-vertical
+break-separators = after
+break-sequences = true
+break-string-literals = never
+cases-exp-indent = 2
+disambiguate-non-breaking-match = true
+dock-collection-brackets = true
+exp-grouping = preserve
+field-space = tight-decl
+if-then-else = k-r
+# "*/*"
+# '*/*'
+  "break-cases = all
+'break-fun-decl = smart
+#$_
+"break-infix = fit-or-vertical
+("*/*")_
+break-separators = after = -{-}-{->
+-}-{-}->break
+|  "*/*"
+| '#"$_
+|  '$"*/*"-sequences = true
+|-}-{-}->-break-{-}-{->|
+                       |-}-{-}->string-}-{-}->|
+# literals = never                            |
+# cases-exp-indent = 2                        |
+# disambiguate-non-breaking-match = true      |
+# dock-collection-brackets = true             |
+# exp-grouping = preserve                     |
+# field-space = tight-decl                    |
+if-then-else = k-r                    <-}-{-}-|
+indicate-nested-or-patterns = unsafe-no
+leading-nested-match-parens = true
+margin = 100
+parens-tuple-patterns = always
+sequence-blank-line = preserve-one
+sequence-style = terminator
+space-around-lists = false
+space-around-records = true
+space-around-variants = true
+type-decl = sparse
+wrap-fun-args = false
+doc-comments = before
