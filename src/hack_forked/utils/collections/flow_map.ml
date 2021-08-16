b65f0176a1cfbb8a46e7064a31d409d040fb1141
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2021-08-16 14:04:53 +0800

    expand the types to make it more stable
    The signature may vary differently across different stdlib versions

--- src/hack_forked/utils/collections/flow_map.ml
+++ src/hack_forked/utils/collections/flow_map.ml
@@ -1,2 +1,57 @@
-module type S = Map.S
-module Make = Map.Make
+module type S = sig
+  type key
+
+  type +'a t
+
+  val empty : 'a t
+
+  val is_empty : 'a t -> bool
+
+  val mem : key -> 'a t -> bool
+
+  val add : key -> 'a -> 'a t -> 'a t
+
+  val update : key -> ('a option -> 'a option) -> 'a t -> 'a t
+
+  val singleton : key -> 'a -> 'a t
+
+  val remove : key -> 'a t -> 'a t
+
+  val merge : (key -> 'a option -> 'b option -> 'c option) -> 'a t -> 'b t -> 'c t
+
+  val union : (key -> 'a -> 'a -> 'a option) -> 'a t -> 'a t -> 'a t
+
+  val compare : ('a -> 'a -> int) -> 'a t -> 'a t -> int
+
+  val equal : ('a -> 'a -> bool) -> 'a t -> 'a t -> bool
+
+  val iter : (key -> 'a -> unit) -> 'a t -> unit
+
+  val fold : (key -> 'a -> 'b -> 'b) -> 'a t -> 'b -> 'b
+
+  val for_all : (key -> 'a -> bool) -> 'a t -> bool
+
+  val exists : (key -> 'a -> bool) -> 'a t -> bool
+
+  val filter : (key -> 'a -> bool) -> 'a t -> 'a t
+
+  val partition : (key -> 'a -> bool) -> 'a t -> 'a t * 'a t
+
+  val cardinal : 'a t -> int
+
+  val bindings : 'a t -> (key * 'a) list
+
+  val choose : 'a t -> key * 'a
+
+  val choose_opt : 'a t -> (key * 'a) option
+
+  val find : key -> 'a t -> 'a
+
+  val find_opt : key -> 'a t -> 'a option
+
+  val map : ('a -> 'b) -> 'a t -> 'b t
+
+  val mapi : (key -> 'a -> 'b) -> 'a t -> 'b t
+end
+
+module Make : functor (Ord : Set.OrderedType) -> S with type key = Ord.t = Map.Make
