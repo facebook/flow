db23bf9224afced71d83f30ef8b837ef5abaefc4
Justin Grant <justingrant@users.noreply.github.com>
2021-08-16 20:13:36 -0700

    Allow mixed args to string.prototype.concat()
    According to MDN docs for the `concat()` method of the `String` type:
    
    > If the arguments are not of the type string, they are converted to string
    > values before concatenating.
    
    Passing numbers, objects, etc to `String.prototype.concat` is is valid JS
    behavior but currently causes an error in Flow. It also may be blocking
    facebook/react#22064.
    
    This commit changes the arg type: `Array<string>` -> `Array<mixed>`.

--- newtests/autocomplete/test.js
+++ newtests/autocomplete/test.js
@@ -158,7 +158,7 @@ export default (suite(({addFile, flowCmd}) => [
              },
              {
                "name": "concat",
-               "type": "(...strings: Array<string>) => string"
+               "type": "(...values: Array<mixed>) => string"
              },
              {
                "name": "endsWith",
