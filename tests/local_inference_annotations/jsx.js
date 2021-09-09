7c421c15937bd9d4f9fb1c984e9ac845bdbed465
Evan Yeung <evanyeung@fb.com>
2021-09-09 13:13:08 -0700

    Do not require LTI annotations on annotated JSX children
    Summary:
    This is a quick fix to allow for callbacks as JSX children to avoid
    needing annotations. See the included test for an example
    
    Changelog: [internal]
    
    Reviewed By: panagosg7
    
    Differential Revision: D30819049
    
    fbshipit-source-id: 7be789a428c6c5e9d1b4a3f7abbc12d879173812

--- tests/local_inference_annotations/jsx.js
+++ tests/local_inference_annotations/jsx.js
@@ -9,3 +9,9 @@ function Component(props: {| foo: number => number |}) { return null }
   const y = (x) => 3; // error, required annot
   return 3;
 }} />;
+
+function Component2(props: {| children: number => React.Node |}) {
+  return null
+}
+  <Component2>{(x) => <div />}</Component2> // no annot
+
