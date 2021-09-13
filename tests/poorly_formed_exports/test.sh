485b7fae9ce7853358d82ea74a91207135f5cf08
Marshall Roch <mroch@fb.com>
2021-09-13 10:33:57 -0700

    fix shellcheck lints in poorly_formed_exports test
    Reviewed By: pieterv
    
    Differential Revision: D30848498
    
    fbshipit-source-id: a402553e36cf068ec0a061ec1f4bd4e59c6fc798

--- tests/poorly_formed_exports/test.sh
+++ tests/poorly_formed_exports/test.sh
@@ -4,10 +4,12 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# shellcheck disable=SC2094
+
 echo "FLOW STATUS:"
 
-assert_errors $FLOW status
+assert_errors "$FLOW" status
 
 echo "FLOW CHECK-CONTENTS:"
 
-assert_errors $FLOW check-contents bad-named-export.js < bad-named-export.js
+assert_errors "$FLOW" check-contents bad-named-export.js < bad-named-export.js
