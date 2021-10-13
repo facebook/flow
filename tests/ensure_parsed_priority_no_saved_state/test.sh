3cfbe96ab65427d8871988272fe4dd225007d29c
Marshall Roch <mroch@fb.com>
2021-10-13 12:02:23 -0700

    fix ensure_parsed with prioritize_dependency_checks
    Summary:
    during a recheck, we call `ensure_parsed_or_trigger_recheck` to verify that files haven't changed on disk. that can request that more files get rechecked and then calls `raise Lwt.Canceled` to cancel the current recheck.
    
    when `experimental.prioritize_dependency_checks` is enabled, we do a recheck just of prioritized files (unchecked deps) and defer the rest for a subsequent recheck.
    
    the problem is, those extra `ensure_parsed` files weren't prioritized and so we end up in an infinite loop: a prioritized recheck cancels itself to add some `ensure_parsed` files. the next recheck prioritizes the same files but defers the ensure_parsed files, so it cancels again, ad nauseum.
    
    the fix is to track these extra files separately (`files_to_prioritize`) and include them in the next recheck.
    
    Reviewed By: vrama628
    
    Differential Revision: D31506304
    
    fbshipit-source-id: cabb63f5cbb7c44a94387485075123557e792bcf

---
+++ tests/ensure_parsed_priority_no_saved_state/test.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# This test is intended to complement the ensure_parsed_saved_state test and
+# demonstrate the different behavior of ensure_parsed when saved state is not
+# used
+printf "==== No errors at start====\\n"
+assert_ok "$FLOW" status --no-auto-start
+
+cp B.js B.js.orig
+cp C.js C.js.orig
+
+# Simulate a file watcher race condition. Let's say it notices A.js changed
+# but not that B.js changed
+cp B.js.with_parse_error B.js
+assert_ok "$FLOW" force-recheck --focus A.js
+
+printf "\\n\\n==== Still don't notice the error ====\\n"
+# The parse error should not be noticed yet. The server does recheck B.js, but
+# it will use the AST stored in shared memory. If we started up with saved state
+# we wouldn't have the AST in memory and would notice the error when we try to
+# read B.js from disk and parse it
+assert_ok "$FLOW" status --no-auto-start
+
+assert_ok "$FLOW" force-recheck --focus B.js
+
+printf "\\n\\n==== Now we see the parse error ====\\n"
+# The parse error should now be noticed
+assert_errors "$FLOW" status --no-auto-start
+
+
+# Reset the parse error
+cp B.js.orig B.js
+cp C.js.with_new_dependency C.js
+assert_ok "$FLOW" force-recheck --focus A.js B.js # But not C.js
+
+printf "\\n\\n==== We don't see the unknown module exception yet ====\\n"
+# The error should not be noticed yet. The server does recheck C.js, but
+# it will use the AST stored in shared memory. If we started up with saved state
+# we wouldn't have the AST in memory and would notice the error when we try to
+# read C.js from disk, parse it, and recheck the changed file
+assert_ok "$FLOW" status --no-auto-start
+
+assert_ok "$FLOW" force-recheck --focus C.js
+
+printf "\\n\\n==== Now we see the unknown module error ====\\n"
+# The error should now be noticed
+assert_errors "$FLOW" status --no-auto-start
---
+++ tests/ensure_parsed_priority_saved_state/test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# This test is primarily intended to test that Types_js.ensure_parsed properly
+# parses things which were not originally parsed due to saved_state
+
+printf "==== No errors at start====\\n"
+assert_ok "$FLOW" status --no-auto-start
+
+cp B.js B.js.orig
+cp C.js C.js.orig
+
+# Simulate a file watcher race condition. Let's say it notices A.js changed
+# but not that B.js changed
+cp B.js.with_parse_error B.js
+assert_ok "$FLOW" force-recheck --focus A.js
+
+printf "\\n\\n==== ensure_parsed notices that B has changed ====\\n"
+# Focusing on A.js requires B.js to be checked, so ensure_parsed tries to
+# parse it. But it notices it has changed and triggers a recheck, which finds
+# the parse errors
+assert_errors "$FLOW" status --no-auto-start
+
+# Reset the parse error
+cp B.js.orig B.js
+assert_ok "$FLOW" stop
+start_flow . --lazy-mode fs --file-watcher none
+
+printf "==== No errors after restart====\\n"
+assert_ok "$FLOW" status --no-auto-start
+
+cp C.js.with_new_dependency C.js
+cp A.js.modified A.js
+# Tell the server that A.js has changed but don't tell it about C
+assert_ok "$FLOW" force-recheck --focus A.js
+
+printf "\\n\\n==== ensure_parsed notices that C has changed ====\\n"
+# Focusing on A.js requires C.js to be checked, so ensure_parsed tries to
+# parse it. But it notices it has changed and triggers a recheck, which finds
+# the error
+assert_errors "$FLOW" status --no-auto-start
