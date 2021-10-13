94a769d1a1e9db2272c3e12f17f1ebd377a1a3b5
Mike Vitousek <mvitousek@fb.com>
2021-10-13 16:37:46 -0700

    Add information in the annotate-declarations codemod about possibly renamable variables
    Summary:
    The interplay between the annotate-declarations codemod and the rename-redefinitions codemod is such that it might be useful for the former to suggest that the latter be run. This diff adds stats information to the annotate-declarations codemod about how many variables should perhaps be renamed rather than annotated. It's a little imprecise: `var x;` will count towards the number of assignments that should be renamed according to this diff, but won't actually be renamed by the rename-redefinitions codemod.
    
    Changelog: [internal]
    
    Reviewed By: panagosg7
    
    Differential Revision: D31453076
    
    fbshipit-source-id: f8a1c0608788ad8f915caee636c37c78764f9eeb

--- src/codemods/annotate_declarations.ml
+++ src/codemods/annotate_declarations.ml
@@ -9,19 +9,31 @@ module Ast = Flow_ast
 module LMap = Loc_collections.LocMap
 module LSet = Loc_collections.LocSet
 open Insert_type_utils
+open Loc_collections
 
 module ErrorStats = struct
-  type t = { num_total_errors: int }
+  type t = {
+    num_error_vars: int;
+    num_renamable_vars: int;
+  }
 
-  let empty = { num_total_errors = 0 }
+  let empty = { num_error_vars = 0; num_renamable_vars = 0 }
 
-  let combine c1 c2 = { num_total_errors = c1.num_total_errors + c2.num_total_errors }
+  let combine c1 c2 =
+    {
+      num_error_vars = c1.num_error_vars + c2.num_error_vars;
+      num_renamable_vars = c1.num_renamable_vars + c2.num_renamable_vars;
+    }
 
   let serialize s =
     let open Utils_js in
-    [spf "total_errors: %d" s.num_total_errors]
+    [spf "total_errors: %d" s.num_error_vars; spf "renamable_vars: %d" s.num_renamable_vars]
 
-  let report s = [string_of_row ~indent:2 "Number of constrained write errors" s.num_total_errors]
+  let report s =
+    [
+      string_of_row ~indent:2 "Number of vars with write errors" s.num_error_vars;
+      string_of_row ~indent:2 "Number of possibly renamable vars" s.num_renamable_vars;
+    ]
 end
 
 module Codemod_declaration_annotator = Codemod_annotator.Make (ErrorStats)
@@ -45,6 +57,32 @@ let mapper ~preserve_literals ~max_type_size ~default_any (cctx : Codemod_contex
   let suppress_types = Options.suppress_types options in
   let exact_by_default = Options.exact_by_default options in
   let flowfixme_ast = Builtins.flowfixme_ast ~lint_severities ~suppress_types ~exact_by_default in
+  let cx = Codemod_context.Typed.context cctx in
+  let errors = Context.errors cx in
+  let loc_error_set =
+    Flow_error.ErrorSet.fold
+      (fun error ->
+        LSet.fold
+          (fun loc -> LSet.add loc)
+          (Codemod_constrained_write_utils.declaration_locs_of_constrained_write_error cx error))
+      errors
+      LSet.empty
+  in
+  let extractable_assignments loc =
+    (* See if we should run the rename-redefinitions codemod by seeing if any write to this variable is extractable *)
+    let is_extractable =
+      Codemod_constrained_write_utils.is_extractable_assignment cx loc_error_set
+    in
+    let { Loc_env.var_info = { Env_api.scopes; ssa_values; _ }; _ } = Context.environment cx in
+    let uses = Scope_api.With_ALoc.uses_of_use scopes loc in
+    ALocSet.fold
+      (fun use acc ->
+        match ALocMap.find_opt use ssa_values with
+        | None (* is a write *) when is_extractable use -> ALocSet.add use acc
+        | _ -> acc)
+      uses
+      ALocSet.empty
+  in
   object (this)
     inherit
       Codemod_declaration_annotator.mapper
@@ -57,11 +95,16 @@ let mapper ~preserve_literals ~max_type_size ~default_any (cctx : Codemod_contex
         ~default_any
         cctx as super
 
-    val mutable loc_error_map = LMap.empty
+    val mutable renamable = ALocSet.empty
 
     method private post_run () =
-      this#add_unannotated_loc_warnings loc_error_map;
-      ErrorStats.{ num_total_errors = LMap.cardinal loc_error_map }
+      this#add_unannotated_loc_warnings
+        (LSet.fold (fun loc -> LMap.add loc ()) loc_error_set LMap.empty);
+      ErrorStats.
+        {
+          num_error_vars = LSet.cardinal loc_error_set;
+          num_renamable_vars = ALocSet.cardinal renamable;
+        }
 
     method! variable_declarator_pattern ~kind ((ploc, patt) : ('loc, 'loc) Ast.Pattern.t) =
       let get_annot ty annot =
@@ -70,7 +113,7 @@ let mapper ~preserve_literals ~max_type_size ~default_any (cctx : Codemod_contex
         this#opt_annotate ~f ~error ~expr:None ploc ty annot
       in
 
-      if LMap.mem ploc loc_error_map then (
+      if LSet.mem ploc loc_error_set then (
         let ty_result = Codemod_annotator.get_ty cctx ~preserve_literals ~max_type_size ploc in
         match patt with
         | Ast.Pattern.Object Ast.Pattern.Object.{ annot; properties; comments } ->
@@ -81,6 +124,8 @@ let mapper ~preserve_literals ~max_type_size ~default_any (cctx : Codemod_contex
           (ploc, Ast.Pattern.Array Ast.Pattern.Array.{ annot = annot'; elements; comments })
         | Ast.Pattern.Identifier Ast.Pattern.Identifier.{ annot; name; optional } ->
           let annot' = get_annot ty_result annot in
+          renamable <- ALocSet.union renamable (extractable_assignments (ALoc.of_loc ploc));
+
           (ploc, Ast.Pattern.Identifier Ast.Pattern.Identifier.{ annot = annot'; name; optional })
         | Ast.Pattern.Expression _ ->
           (* No such thing as a pattern expression *)
@@ -91,18 +136,7 @@ let mapper ~preserve_literals ~max_type_size ~default_any (cctx : Codemod_contex
         super#variable_declarator_pattern ~kind (ploc, patt)
 
     method! program prog =
-      let errors = Context.errors @@ Codemod_context.Typed.context cctx in
-      loc_error_map <-
-        Flow_error.ErrorSet.fold
-          (fun error ->
-            LSet.fold
-              (fun loc -> LMap.add loc ())
-              (Codemod_constrained_write_utils.declaration_locs_of_constrained_write_error
-                 (Codemod_context.Typed.context cctx)
-                 error))
-          errors
-          LMap.empty;
-      if LMap.is_empty loc_error_map then
+      if LSet.is_empty loc_error_set then
         prog
       else
         super#program prog
