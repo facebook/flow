90af48ff4009a25eba5f3ab25551f9a361c4e880
Greg Price <gnprice@gmail.com>
2021-10-16 22:18:43 -0700

    Add tests for subtyping of PolyT ~> PolyT
    This tests for the unsoundness issue #8766.  It consists mainly
    of the tests from #8767, with the expected results adapted to the
    fact the issue isn't yet fixed.
    
    As discussed at #8767, simply dropping the shortcut case and
    relying on the general case for flows _ ~> PolyT isn't yet viable
    because it causes some common code patterns to encounter a
    different bug, producing large volumes of errors.  The plan is to
    instead replace the shortcut with a different rule, from the
    literature, which should be sound.
    
    That rule fixes these unsoundness cases, and those are marked with
    TODO comments.  It won't fix some other cases, where we reject valid
    programs that would be natural to accept, because it still relies on
    lining up the parameter lists against each other in order.
    
    (Effectively the rule interprets the polymorphic types `<X: Sb>S`
    and `<Y: Tb>T` as type *functions*, and asks whether the one
    function lies below the other pointwise.  The general GenericT
    machinery would instead take them as logical formulas headed by
    the quantifier "∀", and seek to prove that the formula "∀X:Sb, S"
    implies "∀Y:Tb, T".  That allows the lower tparam X to be inferred
    as something other than Y, when that's what makes the proof work.)
    
    We include tests for some of those cases too, marked as ideally OK
    but known errors.

---
+++ tests/poly/subtyping.js
@@ -0,0 +1,72 @@
+// @flow
+//
+// Tests for subtyping PolyT ~> PolyT.
+
+// Unsoundness issue #8766: we'd accept when the subtyping looked OK
+// with the upper tparams set to their bounds, even if it wasn't OK
+// for other possible arguments.
+{
+  (o: <T>() => mixed): (<S>() => S) => o; // TODO should error
+
+  function coerce<A, B>(x: A): B {
+    const g = (o: <T>() => A): (<S>() => S) => o; // TODO should error
+    return g(<T>() => x)();
+  }
+
+  const a: number = coerce('a');
+  a.toFixed();
+}
+
+// Test issue #8766, round 2.
+{
+  (f: <T>(T) => mixed): (<S>(mixed) => S) => f; // TODO should error
+
+  function coerce<A, B>(x: A): B {
+    const g = (f: <T>(T) => mixed): (<S>(mixed) => S) => f; // TODO should error
+    return g(<T>(y: T) => x)();
+  }
+
+  (coerce('a'): empty);
+  coerce('a').toFixed();
+}
+
+
+// Conversely, we reject (but ideally shouldn't) a subtyping when it
+// requires setting the lower tparams to particular types, or types in
+// a particular relation to the upper tparams, other than setting the
+// lower tparams to just equal the upper tparams.
+{
+  // This rejects because it requires (T => T) ~> (number => number)
+  // for a generic T.
+  (f: <T>(T) => T): (<S>(number) => number) => f; // should be ok, currently isn't
+
+  // The same example would work with the upper type monomorphized
+  // by substituting its parameter S, for any type S we choose:
+  (f: <T>(T) => T): (number => number) => f; // ok
+
+  // This rejects because it requires it to work with T = S,
+  // whereas it can only work with T = S[].
+  (f: <T>(T) => T): (<S>(S[]) => S[]) => f; // should be ok, currently isn't
+}
+
+
+// Test subtyping where lower has fewer or more parameters than upper.
+// Currently these don't work; see #8767.
+(f: <R, S>(R, S) => S): (<T>(T, T) => T) => f; // ideally ok, known error
+(f: <R, S>(R, S) => S): (<T>(T, T) => boolean) => f; // error
+(f: <R>(R, mixed) => R): (<S, T>(S, T) => S) => f; // ideally ok, known error
+(f: <R>(R, mixed) => R): (<S, T>(S, T) => T) => f; // error
+
+
+// Test subtyping where lower and upper have same number of parameters,
+// but they don't line up when paired off in order.  These are rejected
+// by the same approximation that causes us to reject fewer or more parameters.
+(f: <S: string, T: number>() => { a: S, b: T })
+  : <T: number, S: string>() => { a: S, b: T } => f; // ideally ok, known error
+
+
+// Test subtyping where only one side is polymorphic.
+(f: mixed => boolean): (<T>(T) => boolean) => f; // ok
+(f: (number, number) => number): (<T>(T, T) => T) => f; // error
+(f: <T>(T, T) => T): ((number, number) => number) => f; // ok
+(f: <T>(T, T) => T): ((number, string) => number) => f; // error
