648482505bca28307c1102326ae61bd1a9e83107
Noah Yamamoto <nry@fb.com>
2021-10-20 20:00:13 -0700

    Fix navigator.clipboard.write type and add ClipboardItem
    Summary:
    Pull Request resolved: https://github.com/facebook/flow/pull/8763
    
    I noticed that the type for clipboard.write was expecting a DataTransfer, when this doesn't seem to be the case on any browser. I updated it according to the spec by adding the new ClipboardItem type.
    
    I essentially just took the types from the [W3C spec](https://w3c.github.io/clipboard-apis/#clipboarditem) and the [Typescript definitions](https://github.com/microsoft/TypeScript/blob/80e1a2924853f87926fc13d684920813499193e4/lib/lib.dom.d.ts#L3548) and copied them here in Flow.
    
    I'm by no means a Flow expert so please let me know if any changes are needed!
    
    Reviewed By: panagosg7
    
    Differential Revision: D31668972
    
    fbshipit-source-id: a7b8c97e00c68b2178d1a0983287cd10bfbd5008

--- tests/clipboard/clipboard.js
+++ tests/clipboard/clipboard.js
@@ -6,5 +6,5 @@ const t: EventTarget = navigator.clipboard;
 navigator.clipboard.read().then(function(data: DataTransfer) {});
 navigator.clipboard.readText().then(function(data: string) {});
 
-const p: Promise<void> = navigator.clipboard.write(new DataTransfer());
+const p: Promise<void> = navigator.clipboard.write([new ClipboardItem({'image/png': new Blob()})]);
 const p2: Promise<void> = navigator.clipboard.writeText('test');
