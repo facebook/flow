a552a639ccd4bb7521df0919b2bcd9bd599c9202
Mike Vitousek <mvitousek@fb.com>
2021-10-27 13:51:14 -0700

    Additional test cases for constrained writes
    Summary:
    This diff adds a number of constrained writes test cases for the last few diffs. It tests switch statements, constrained parameters and destructured annotated declarations
    
    Changelog: [internal]
    
    Reviewed By: panagosg7
    
    Differential Revision: D31811727
    
    fbshipit-source-id: b410f95bad8729168402e514d23e9e02558b45bc

--- tests/constrain_writes/test.js
+++ tests/constrain_writes/test.js
@@ -21,3 +21,47 @@ w = "a";
 var z;
 z = 42;
 z = "a";
+
+function ff(x: number) {
+  x = "Hello"; // only regular failure
+}
+
+
+function ff2(x) {
+  x = "Hello";
+  x = 42; // no failure, parameters don't have post-inference checks
+}
+ff2(true); // give x a lb
+
+function pos() {
+  class X {foo: number;};
+  const x = new X;
+  var a : string;
+  var {...a} = x; // no post-check here
+}
+
+function destruct() {
+  var {p}: T = {p: "foo"};
+  p = 42; // error: number ~> string, but no post-infer
+  type T = {p: string};
+}
+
+function ifs(x: mixed) {
+  let a = "";
+  if (x) {
+      let a;
+      a = 0; // doesn't add lower bound to outer a
+  }
+  (a : string); // OK
+}
+
+
+function switch_scope(x: mixed) {
+  let a = "";
+  switch (x) {
+    case "foo":
+      let a;
+      a = 0; // doesn't add lower bound to outer a
+  }
+  (a : string); // OK
+}
