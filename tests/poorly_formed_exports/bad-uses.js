c08f8b9232f50c044968c288c77425120db52cc8
George Zahariev <gkz@fb.com>
2021-10-29 11:20:44 -0700

    Improve poorly formed exports tests
    Summary:
    Improve poorly formed exports bad uses tests by:
    - Adding `// ERROR`/`// OK` comments for readability. This makes it easier to see in the test file what is expected to be an error and what is not.
    - Adding the `no_flowlib=false` option, as before we were getting unintended `cannot-resolve-name` errors
    - Add example of reading from `module.exports.foo`
    - Move tests of reads to the top, so they are not affected by writes
    
    Changelog: [internal]
    
    Reviewed By: samwgoldman
    
    Differential Revision: D32009605
    
    fbshipit-source-id: 8ed7330ad162eed384ed8b7365eca1d3c854cffd

--- tests/poorly_formed_exports/bad-uses.js
+++ tests/poorly_formed_exports/bad-uses.js
@@ -1,5 +1,8 @@
 // @flow
 
+exports.foo; // OK
+module.exports.foo; // OK
+
 // These functions are provided just to illustrate the danger of aliasing `module` and
 // `module.exports`.
 function addsAProp(x) {
@@ -10,52 +13,50 @@ function addsAPropToExports(x) {
   x.exports.bar = '42';
 }
 
-addsAProp(module.exports);
-addsAProp(module['exports']);
-addsAProp(exports);
-addsAPropToExports(module);
-
-module.exports['foo'] = 42;
-module['exports'] = {};
-exports['foo'] = 42;
-
-(5, exports).foo = 1;
-(5, module).exports.foo = 34;
+addsAProp(module.exports); // ERROR
+addsAProp(module['exports']); // ERROR
+addsAProp(exports); // ERROR
+addsAPropToExports(module); // ERROR
 
 function h() {
-  exports['foo'] = 42;
-  exports.foo = 5;
-  exports.foo;
-  module.exports.foo;
+  exports.foo; // OK
+  module.exports.foo; // OK
+  exports['foo'] = 42; // ERROR
+  exports.foo = 5; // ERROR
 }
+
+module.exports['foo'] = 42; // ERROR
+module['exports'] = {}; // ERROR
+exports['foo'] = 42; // ERROR
+
+(5, exports).foo = 1; // ERROR
+(5, module).exports.foo = 34; // ERROR
 
 function g() {
   // We don't know when this function may be called, clobbering `module` and `exports`, so we can't
   // allow this.
-  module = {};
-  exports = {};
+  module = {}; // ERROR
+  exports = {}; // ERROR
 }
 
 function f() {
   let module = {};
   let exports = {};
   // These should all be fine, since `module` and `exports` have been shadowed.
-  module.exports = {};
-  exports = {};
-  exports.foo = 42;
-  module.exports.foo = 42;
-  addsAPropToExports(module);
+  module.exports = {}; // OK
+  exports = {}; // OK
+  exports.foo = 42; // OK
+  module.exports.foo = 42; // OK
+  addsAPropToExports(module); // OK
 }
 
 // This is a pattern recommended by Node, and shold be allowed.
-if (require.main === module) { }
+if (require.main === module) { } // OK
 // This doesn't need to be allowed, the above can be special-cased.
-if (require.main === (5, module)) { }
+if (require.main === (5, module)) { } // ERROR
 
 // This is okay since `module` is rebound. Previously it errored due to a (now fixed) bug in
 // scope_builder.
-switch ('') { case '': const module = ''; module; };
+switch ('') { case '': const module = ''; module; }; // OK
 
-// Property accesses should be okay since they cannot alias or mutate `module` or `exports`.
-module.id;
-exports.foo;
+module.id; // OK
