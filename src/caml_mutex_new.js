f98f6cce563dc5f0b19a558b3a7af29d011039f9
Shayne Fletcher <shaynefletcher@fb.com>
2021-11-11 15:43:31 -0800

    supercaml-1.28
    Summary:
    allow-large-files
    
    - rules.py
      - lwt.unix to depend on threads
    - new packages:
      - landmarks
      - landmarks-ppx
      - memtrace
      - uuidm
    
    Reviewed By: samwgoldman
    
    Differential Revision: D32359959
    
    fbshipit-source-id: 0e74a12a9fcbdb47408ed6beeeb61686f02c80e3

---
+++ src/caml_mutex_new.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+//Provides: caml_mutex_new const
+
+function caml_mutex_new() {
+    throw 'caml_mutex_new: not implemented'
+}
