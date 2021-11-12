cefa6b0f6d8ca5da2d16efbab0d213bffc216616
Shayne Fletcher <shaynefletcher@fb.com>
2021-11-12 08:51:30 -0800

    release supercaml-1.28 into fbcode
    Summary: flips the switch to release supercaml-1.28 into fbcode
    
    Reviewed By: samwgoldman
    
    Differential Revision: D32374258
    
    fbshipit-source-id: 0ff292409fefc3d95232191f97f228c9249f1000

---
+++ src/caml_thread_cleanup.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+//Provides: caml_thread_cleanup const
+
+function caml_thread_cleanup() {
+    throw 'caml_thread_cleanup: not implemented'
+}
