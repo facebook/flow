8c99a9f0233077dd06d097e4cb2ebd687b8cb635
Marshall Roch <mroch@fb.com>
2021-12-01 11:50:43 -0800

    consume messages from the monitor eagerly
    Summary:
    the server process runs two Lwt loops in parallel: `ServerMonitorListener.ListenLoop`, and `Server.serve`
    
    the former listens for messages over the socket from the monitor, and writes them to streams in `ServerMonitorListenerState`. the latter consumes `ServerMonitorListenerState` streams.
    
    since `LwtLoop` yields between each iteration of the loop, we end up consuming one message at a time from the monitor and then trying to process it.
    
    instead, we should read from the monitor as quickly as possible and queue them in `ServerMonitorListenerState`. `ServerMonitorListenerState` buckets together events.
    
    Reviewed By: samwgoldman
    
    Differential Revision: D31906966
    
    fbshipit-source-id: 120ea68f6cc2bd955271cbf46b2daed1f64b35a6

--- src/server/monitor_listener/serverMonitorListener.ml
+++ src/server/monitor_listener/serverMonitorListener.ml
@@ -7,87 +7,80 @@
 
 open ServerEnv
 
-(* This is a thread that just keeps looping in the background. It reads messages from the
- * monitor process and adds them to a stream *)
-module ListenLoop = LwtLoop.Make (struct
-  type acc = genv
+let kill_workers () =
+  (* TODO - find a way to gracefully kill the workers. At the moment, if the workers are in the
+     * middle of a job this will lead to some log spew. We probably should send SIGTERM to each
+     * worker and set up a signal handler to kill the fork and exit gracefully. Might also want
+     * to use the SharedMem_js.cancel thingy *)
+  Hh_logger.info "Killing the worker processes";
+  WorkerController.killall ()
 
-  let kill_workers () =
-    (* TODO - find a way to gracefully kill the workers. At the moment, if the workers are in the
-       * middle of a job this will lead to some log spew. We probably should send SIGTERM to each
-       * worker and set up a signal handler to kill the fork and exit gracefully. Might also want
-       * to use the SharedMem_js.cancel thingy *)
-    Hh_logger.info "Killing the worker processes";
-    WorkerController.killall ()
-
-  let handle_message genv = function
-    | MonitorProt.Request (request_id, command) ->
-      CommandHandler.enqueue_or_handle_ephemeral genv (request_id, command)
-    | MonitorProt.PersistentConnectionRequest (client_id, request) ->
-      CommandHandler.enqueue_persistent genv client_id request
-    | MonitorProt.NewPersistentConnection (client_id, lsp_init_params) ->
-      (* Immediately register the new client *)
-      Persistent_connection.add_client client_id lsp_init_params;
-      ServerMonitorListenerState.push_new_env_update (fun env ->
-          {
-            env with
-            connections = Persistent_connection.add_client_to_clients env.connections client_id;
-          }
-      );
+let handle_message genv = function
+  | MonitorProt.Request (request_id, command) ->
+    CommandHandler.enqueue_or_handle_ephemeral genv (request_id, command)
+  | MonitorProt.PersistentConnectionRequest (client_id, request) ->
+    CommandHandler.enqueue_persistent genv client_id request
+  | MonitorProt.NewPersistentConnection (client_id, lsp_init_params) ->
+    (* Immediately register the new client *)
+    Persistent_connection.add_client client_id lsp_init_params;
+    ServerMonitorListenerState.push_new_env_update (fun env ->
+        {
+          env with
+          connections = Persistent_connection.add_client_to_clients env.connections client_id;
+        }
+    );
+    Lwt.return_unit
+  | MonitorProt.DeadPersistentConnection client_id ->
+    (* Immediately remove the dead client *)
+    Persistent_connection.remove_client client_id;
+    ServerMonitorListenerState.push_new_env_update (fun env ->
+        {
+          env with
+          connections = Persistent_connection.remove_client_from_clients env.connections client_id;
+        }
+    );
+    Lwt.return_unit
+  | MonitorProt.FileWatcherNotification { files = changed_files; metadata; initial } ->
+    let open LspProt in
+    let file_count = SSet.cardinal changed_files in
+    if initial then (
+      let reason = Lazy_init_typecheck in
+      ServerMonitorListenerState.push_files_to_force_focused_and_recheck ~reason changed_files;
       Lwt.return_unit
-    | MonitorProt.DeadPersistentConnection client_id ->
-      (* Immediately remove the dead client *)
-      Persistent_connection.remove_client client_id;
-      ServerMonitorListenerState.push_new_env_update (fun env ->
-          {
-            env with
-            connections = Persistent_connection.remove_client_from_clients env.connections client_id;
-          }
-      );
-      Lwt.return_unit
-    | MonitorProt.FileWatcherNotification { files = changed_files; metadata; initial } ->
-      let open LspProt in
-      let file_count = SSet.cardinal changed_files in
-      if initial then (
-        let reason = Lazy_init_typecheck in
-        ServerMonitorListenerState.push_files_to_force_focused_and_recheck ~reason changed_files;
-        Lwt.return_unit
-      ) else
-        let reason =
-          match metadata with
-          | Some { MonitorProt.changed_mergebase = Some true; _ } -> Rebased { file_count }
-          | Some { MonitorProt.missed_changes = true; _ } -> File_watcher_missed_changes
-          | _ when file_count = 1 ->
-            Single_file_changed { filename = SSet.elements changed_files |> List.hd }
-          | _ -> Many_files_changed { file_count }
-        in
-        ServerMonitorListenerState.push_files_to_recheck ?metadata ~reason changed_files;
-        Lwt.return_unit
-    | MonitorProt.PleaseDie please_die_reason ->
-      kill_workers ();
-      let msg =
-        match please_die_reason with
-        | MonitorProt.MonitorExiting (monitor_exit_status, monitor_msg) ->
-          Utils_js.spf
-            "Monitor is exiting with status %s (%s)"
-            (Exit.to_string monitor_exit_status)
-            monitor_msg
+    ) else
+      let reason =
+        match metadata with
+        | Some { MonitorProt.changed_mergebase = Some true; _ } -> Rebased { file_count }
+        | Some { MonitorProt.missed_changes = true; _ } -> File_watcher_missed_changes
+        | _ when file_count = 1 ->
+          Single_file_changed { filename = SSet.elements changed_files |> List.hd }
+        | _ -> Many_files_changed { file_count }
       in
-      Exit.(exit ~msg Killed_by_monitor)
-
-  let main genv =
-    (* read a message from the monitor *)
-    let%lwt message =
-      try%lwt MonitorRPC.read () with
-      | End_of_file ->
-        let () = kill_workers () in
-        let msg = "Connection to monitor closed unexpectedly" in
-        Exit.(exit ~msg Killed_by_monitor)
+      ServerMonitorListenerState.push_files_to_recheck ?metadata ~reason changed_files;
+      Lwt.return_unit
+  | MonitorProt.PleaseDie please_die_reason ->
+    kill_workers ();
+    let msg =
+      match please_die_reason with
+      | MonitorProt.MonitorExiting (monitor_exit_status, monitor_msg) ->
+        Utils_js.spf
+          "Monitor is exiting with status %s (%s)"
+          (Exit.to_string monitor_exit_status)
+          monitor_msg
     in
-    let%lwt () = handle_message genv message in
-    Lwt.return genv
+    Exit.(exit ~msg Killed_by_monitor)
 
-  let catch _ exn = Exception.reraise exn
-end)
-
-let listen_for_messages genv = ListenLoop.run genv
+(** This is a thread that keeps reading messages from the monitor process and
+    adding them to a stream. It runs in parallel with the [Server.serve] loop, which
+    consumes the stream. *)
+let rec listen_for_messages genv =
+  (* read a message from the monitor *)
+  let%lwt message =
+    try%lwt MonitorRPC.read () with
+    | End_of_file ->
+      let () = kill_workers () in
+      let msg = "Connection to monitor closed unexpectedly" in
+      Exit.(exit ~msg Killed_by_monitor)
+  in
+  let%lwt () = handle_message genv message in
+  listen_for_messages genv
