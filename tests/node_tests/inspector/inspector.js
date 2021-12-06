b1f6a78984f4c16710f5b5e51efa01190f56c508
Dima Dovgopolyi <dovgopolyi@fb.com>
2021-12-06 14:51:51 -0800

    Add node inspector module
    Summary:
    Add support for node inspector
    https://nodejs.org/docs/latest-v17.x/api/inspector.html
    
    Reviewed By: vrama628
    
    Differential Revision: D32808057
    
    fbshipit-source-id: 2bfa008d3bf8f1d310a7a1f3c81e409fe382e334

---
+++ tests/node_tests/inspector/inspector.js
@@ -0,0 +1,90 @@
+const inspector = require('inspector');
+
+/* open */
+
+inspector.open();
+inspector.open(8080);
+inspector.open(8080, 'http://localhost');
+inspector.open(8080, 'http://localhost', true);
+
+inspector.open('8080'); // error
+inspector.open(8080, 127001); // error
+inspector.open(8080, 'http://localhost', 1000); // error
+
+(inspector.open() : void);
+
+(inspector.open() : string); // error
+
+/* close */
+
+inspector.close();
+
+(inspector.close() : void);
+
+(inspector.close() : string); // error
+
+/* console */
+
+(inspector.console : Object);
+
+/* url */
+
+inspector.url();
+
+(inspector.url() : string | void);
+
+(inspector.url() : number); // error
+
+/* waitForDebugger */
+
+inspector.waitForDebugger();
+
+(inspector.waitForDebugger() : void);
+
+(inspector.waitForDebugger() : number); // error
+
+/* Session.connect */
+
+var session = new inspector.Session();
+
+session.connect();
+
+(session.connect() : void);
+
+(inspector.connect() : number); // error
+
+/* Session.events */
+
+session.on('foo', () => {});
+
+/* Session.connectToMainThread */
+
+var session = new inspector.Session();
+
+session.connectToMainThread();
+
+(session.connectToMainThread() : void);
+
+(inspector.connectToMainThread() : number); // error
+
+/* Session.disconnect */
+
+var session = new inspector.Session();
+
+session.disconnect();
+
+(session.disconnect() : void);
+
+(inspector.disconnect() : number); // error
+
+/* Session.post */
+
+session.post('Profiler.enable');
+session.post('Runtime.evaluate', { expression: '2 + 2' });
+session.post('Profiler.enable', null, () => {});
+
+session.post(); // error
+
+(session.post('Profiler.enable') : void);
+
+(session.post() : string); // error
