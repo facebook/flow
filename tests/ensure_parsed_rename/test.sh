01b161e5a44966870559d37156769825ec0d959d
Marshall Roch <mroch@fb.com>
2021-12-10 18:35:05 -0800

    fix crash when moving a module
    Summary: when moving a file, its haste module name ends up in both `to_remove` (from the old file) and `to_replace` (from the new one). this caused it to be oldified twice, which for unknown reasons (D1458889) completely deletes the entry, leading to a crash.
    
    Reviewed By: samwgoldman
    
    Differential Revision: D33029984
    
    fbshipit-source-id: f8fba4779b72abf84052e4b44a603e5176b0e9b0

---
+++ tests/ensure_parsed_rename/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+assert_ok "$FLOW" status --no-auto-start
+
+# rename Dependency and make it slow, so we can interrupt the recheck
+mv dependency.js dependency.js.ignored
+mv dependency_slow.js.ignored dependency_slow.js
+
+# start a very slow recheck
+assert_ok "$FLOW" force-recheck dependency.js dependency_slow.js
+
+sleep 2
+
+# modify Dependency to cancel the recheck. we overwrite it with
+# the fast version, so the test doesn't take forever.
+mv dependency.js.ignored dependency_slow.js
+
+assert_ok "$FLOW" force-recheck dependency_slow.js
+
+assert_ok "$FLOW" status --no-auto-start
