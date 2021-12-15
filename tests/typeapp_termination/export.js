ba533d42433056c12d8750df93278fbc8b364084
Panagiotis Vekris <pvekris@fb.com>
2021-12-14 16:27:26 -0800

    add typeapp termination test
    Summary:
    An earlier version of the next diff (D33013513 (https://github.com/facebook/flow/commit/b384de3b60e46eeb78edf2afa12fa1364dd96dd1)) would cause a "recursion limit
    exceeded" error. The test added here is a mini-repro of that error.
    
    The change that caused this behavior was calling `UnionRep.optimize` in
    `union_optimization_guard`. While calling this optimization shouldn't make a
    difference in the checker, some of the constraints in subtyping_kit.ml are handled
    differently based on whether the union has been optimized.
    
    Changelog: [internal]
    
    Reviewed By: samwgoldman
    
    Differential Revision: D33040733
    
    fbshipit-source-id: 640a79efdcd5fc4aea7711e2b2926c7a28c889ba

---
+++ tests/typeapp_termination/export.js
@@ -0,0 +1,7 @@
+// @flow
+
+export interface PromiseLike<T> {
+  then<TResult1 = T, TResult2 = empty>(
+    onfulfilled?: (value: T) => TResult1 | PromiseLike<TResult1>
+  ): PromiseLike<TResult1 | TResult2>;
+}
---
+++ tests/typeapp_termination/main.js
@@ -0,0 +1,14 @@
+// @flow
+
+import { PromiseLike as RemotePromiseLike } from './export';
+
+// Same definition as in export.js
+export interface PromiseLike<T> {
+  then<TResult1 = T, TResult2 = empty>(
+    onfulfilled?: (value: T) => TResult1 | PromiseLike<TResult1>
+  ): PromiseLike<TResult1 | TResult2>;
+}
+
+declare var p: PromiseLike<mixed>;
+// This check exercises Flow's termination strategies
+(p: RemotePromiseLike<mixed>);
