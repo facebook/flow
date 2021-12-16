dcfcbf00aa4eac351ef85a2f2cb6a689ca151ca6
Panagiotis Vekris <pvekris@fb.com>
2021-12-16 14:05:52 -0800

    rearrange annotate-lti tests
    Summary:
    Moves tests added in D31913925 (https://github.com/facebook/flow/commit/9e2b913c0c1039cf2a36180e4e2aebf347265189) to a new folder tests/annotate_lti_heuristics so that we can enable the `--preserve-literals auto` flag.
    
    Also adds test that shows how the above heuristic is not optimal.
    
    Changelog: [internal]
    
    Reviewed By: evanyeung
    
    Differential Revision: D33152283
    
    fbshipit-source-id: 0812b2a0400e312311013d072c15f024a864baaa

---
+++ tests/annotate_lti_heuristics/file.js
@@ -0,0 +1,21 @@
+// @flow
+
+// Should be annotated with `NumType`, not `NumType | number`
+function g(x) {}
+type NumType = number;
+const y: NumType = 1;
+g(y);
+g(2);
+
+// Should be annotated with `NumType | StringType`, not `NumType | StringType | string`
+function g2(x) {}
+type StringType = string;
+const z: NumType | StringType = 1;
+g2(z);
+g2('test');
+
+// Should be annotated with `?StringType` (TODO)
+function g3(x) {}
+declare var w: ?StringType;
+g3(w);
+g3('test');
