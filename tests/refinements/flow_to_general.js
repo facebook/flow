77760d03013d43627a25a86df07e1c0b98bcc7aa
Mike Vitousek <mvitousek@fb.com>
2022-02-21 13:38:02 -0800

    [ez] Document existing refinement behavior with test
    Summary:
    Document the behavior of refinements for the next diff
    
    Changelog: [internal]
    
    Reviewed By: panagosg7
    
    Differential Revision: D34324391
    
    fbshipit-source-id: f5a6e6fcf09f6ed1b4d18f39d6703b46ddbcd882

---
+++ tests/refinements/flow_to_general.js
@@ -0,0 +1,14 @@
+//@flow
+
+function f(x) {
+  if (typeof x === 'number') { }
+  (x: empty);
+};
+f((42: any));
+
+type T = 'A' | 'B'
+
+declare var x: { p?: ?T };
+let { p } = x;
+
+if (p === 'C') { }
