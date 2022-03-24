771cdf13e2bc8d4537c0301f965447dd035a69bb
Sam Goldman <samgoldman@fb.com>
2022-03-24 13:37:19 -0700

    Move module name candidates hashtbl into Module_js
    Summary:
    Per the comment in Module_hashtables, this bit of state does not need to be
    here. This is the only code remaining in this folder, so let's move it and
    delete a bunch of files/folders.
    
    Reviewed By: mroch
    
    Differential Revision: D35030029
    
    fbshipit-source-id: 3ff87ff20c91e470c8a739cdb3b81e706ad01ed4

--- src/state/locals/module/module_hashtables.mli
+++
@@ -1,8 +0,0 @@
-(*
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *)
-
-val memoize_with_module_name_candidates_cache : f:(string -> string list) -> string -> string list
