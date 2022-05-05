9df9f63f29284fe8aab968a494900059695428b2
Marshall Roch <mroch@fb.com>
2022-05-05 16:10:21 -0700

    remove unused NameUtils helpers
    Summary:
    this helps prepare for ocamlformat 0.19
    
    Changelog: [internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D36168205
    
    fbshipit-source-id: 161177ff7f19c2d3202adadcb37104f372de4289

--- src/common/nameUtils.ml
+++ src/common/nameUtils.ml
@@ -19,24 +19,7 @@ module Set = Flow_set.Make (struct
   let compare = compare_name
 end)
 
-(* We might have an SMap that consists only of ordinary names. We only need to use a NameUtils.Map if
-  * the map might contain internal names. If we can prove by construction that a map only contains
-  * ordinary names, we should use an SMap to avoid losing that information. *)
-let smap_mem name map =
-  match name with
-  | OrdinaryName str -> SMap.mem str map
-  | InternalName _
-  | InternalModuleName _ ->
-    false
-
-let smap_find_opt name map =
-  match name with
-  | OrdinaryName str -> SMap.find_opt str map
-  | InternalName _
-  | InternalModuleName _ ->
-    None
-
-(* Converts all the names to display string. Not to be used to determine typechecker behavior. *)
+(** Converts all the names to display string. Not to be used to determine typechecker behavior. *)
 let display_smap_of_namemap map =
   Map.fold (fun name v smap -> SMap.add (display_string_of_name name) v smap) map SMap.empty
 
--- src/common/nameUtils.mli
+++ src/common/nameUtils.mli
@@ -9,10 +9,6 @@ module Map : WrappedMap.S with type key = Reason.name
 
 module Set : Flow_set.S with type elt = Reason.name
 
-val smap_mem : Reason.name -> 'a SMap.t -> bool
-
-val smap_find_opt : Reason.name -> 'a SMap.t -> 'a option
-
 val display_smap_of_namemap : 'a Map.t -> 'a SMap.t
 
 val namemap_of_smap : 'a SMap.t -> 'a Map.t
