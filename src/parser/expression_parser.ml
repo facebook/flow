30e59baea97f8cdf84065d312df52d98fdb2e1d1
Marshall Roch <mroch@fb.com>
2022-05-17 20:25:16 -0700

    [parser] parse RegExp d flag
    Summary:
    adds parser support for the `d` RegExp flag in ES2022 (https://github.com/tc39/proposal-regexp-match-indices)
    
    Changelog: [parser] Add parser support for ES2022 `d` RegExp flag (see [RegExp Match Indices]((https://github.com/tc39/proposal-regexp-match-indices)))
    
    Reviewed By: gkz
    
    Differential Revision: D36466228
    
    fbshipit-source-id: 7ba86649a3fd85bf827d2504b0fcb8cac8f7746c

--- src/parser/expression_parser.ml
+++ src/parser/expression_parser.ml
@@ -1565,7 +1565,7 @@ module Expression
     let filtered_flags = Buffer.create (String.length raw_flags) in
     String.iter
       (function
-        | ('g' | 'i' | 'm' | 's' | 'u' | 'y') as c -> Buffer.add_char filtered_flags c
+        | ('d' | 'g' | 'i' | 'm' | 's' | 'u' | 'y') as c -> Buffer.add_char filtered_flags c
         | _ -> ())
       raw_flags;
     let flags = Buffer.contents filtered_flags in
