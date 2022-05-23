13c4441f56ede889e8efdcc019456ccaa435f7a7
Marshall Roch <mroch@fb.com>
2022-05-23 14:41:08 -0700

    minor refactor to SocketAcceptor
    Summary:
    - unindent
    - make sure all fields of the handshake are used by destructuring it
    
    Changelog: [internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D36596723
    
    fbshipit-source-id: 7d28a5076475717f3a164d2d80d32766d1d025e3

--- src/monitor/socketAcceptor.ml
+++ src/monitor/socketAcceptor.ml
@@ -173,100 +173,105 @@ let close client_fd () =
 (* Well...I mean this is a pretty descriptive function name. It performs the handshake and then
  * returns the client's side of the handshake *)
 let perform_handshake_and_get_client_handshake ~client_fd =
-  SocketHandshake.(
-    let server_build_id = build_revision in
-    let server_bin = Sys.executable_name in
-    (* handshake step 1: client sends handshake *)
-    let%lwt (wire : client_handshake_wire) = Marshal_tools_lwt.from_fd_with_preamble client_fd in
-    let client_handshake =
-      try fst wire |> Hh_json.json_of_string |> json_to__client_to_monitor_1 with
-      | exn ->
-        Logger.error ~exn "Failed to parse JSON section of handshake: %s" (fst wire);
-        default_client_to_monitor_1
-    in
-    let client =
-      if client_handshake.client_build_id <> server_build_id then
-        None
-      else
-        Some (Marshal.from_string (snd wire) 0 : client_to_monitor_2)
-    in
-    (* handshake step 2: server sends back handshake *)
-    let respond server_intent server2 =
-      assert (server2 = None || client_handshake.client_build_id = server_build_id);
-
-      (* the client will trust our invariant that server2=Some means the client *)
-      (* can certainly deserialize server2. *)
-      let server_version = Flow_version.version in
-      let server1 = { server_build_id; server_bin; server_intent; server_version } in
-      let wire : server_handshake_wire =
-        ( server1 |> monitor_to_client_1__to_json |> Hh_json.json_to_string,
-          Base.Option.map server2 ~f:(fun server2 -> Marshal.to_string server2 [])
-        )
-      in
-      let%lwt _ = Marshal_tools_lwt.to_fd_with_preamble client_fd wire in
-      Lwt.return_unit
-    in
-    let error_client () =
-      let%lwt () = respond Server_will_hangup None in
-      Logger.error "Build mismatch, so rejecting attempted connection";
-      Lwt.return None
-    in
-    let stop_server () =
-      let%lwt () = respond Server_will_exit None in
-      let msg = "Client and server are different builds. Flow server is out of date. Exiting" in
-      FlowEventLogger.out_of_date ();
-      Logger.fatal "%s" msg;
-      Exit.exit ~msg Exit.Build_id_mismatch
-    in
-    let fd_as_int = client_fd |> Lwt_unix.unix_file_descr |> Obj.magic in
-    if client_handshake.is_stop_request then
-      (* Stop request *)
-      let%lwt () = respond Server_will_exit None in
-      let%lwt () = close client_fd () in
-      Server.stop Server.Stopped
-    else if client_handshake.client_build_id <> build_revision then
-      (* Binary version mismatch *)
-      match client_handshake.version_mismatch_strategy with
-      | Always_stop_server -> stop_server ()
-      | Stop_server_if_older ->
-        if Semver.compare Flow_version.version client_handshake.client_version < 0 then
-          stop_server ()
-        (* server < client *)
-        else
-          error_client ()
-      | Error_client -> error_client ()
-    else if Sys.unix && fd_as_int > 500 then (
-      (* Too many clients *)
-      (* We currently rely on using Unix.select, which doesn't work for fds >= FD_SETSIZE (1024).
-       * So we can't have an unlimited number of clients. So if the new fd is too large, let's
-       * reject it.
-       * TODO(glevi): Figure out whether this check is needed for Windows *)
-      let%lwt () = respond Server_will_hangup (Some Server_has_too_many_clients) in
-      Logger.error "Too many clients, so rejecting new connection (%d)" fd_as_int;
-      Lwt.return None
-    ) else if not (StatusStream.ever_been_free ()) then
-      (* Server still initializing *)
-      let client = Base.Option.value_exn client in
-      let status = StatusStream.get_status () in
-      if client_handshake.server_should_hangup_if_still_initializing then (
-        let%lwt () = respond Server_will_hangup (Some (Server_still_initializing status)) in
-        (* In the case of Persistent, lspCommand will retry a second later. *)
-        (* The message we log here solely goes to the logs, not the user. *)
-        let (server_status, watchman_status) = status in
-        Logger.info
-          "Server still initializing -> hangup. server_status=%s watchman_status=%s"
-          (ServerStatus.string_of_status server_status)
-          (FileWatcherStatus.string_of_status watchman_status);
-        Lwt.return None
-      ) else
-        let%lwt () = respond Server_will_continue (Some (Server_still_initializing status)) in
-        Lwt.return (Some client)
-    (* Success *)
+  let open SocketHandshake in
+  let server_build_id = build_revision in
+  let server_bin = Sys.executable_name in
+  (* handshake step 1: client sends handshake *)
+  let%lwt (wire : client_handshake_wire) = Marshal_tools_lwt.from_fd_with_preamble client_fd in
+  let {
+    client_build_id;
+    client_version;
+    is_stop_request;
+    server_should_hangup_if_still_initializing;
+    version_mismatch_strategy;
+  } =
+    try fst wire |> Hh_json.json_of_string |> json_to__client_to_monitor_1 with
+    | exn ->
+      Logger.error ~exn "Failed to parse JSON section of handshake: %s" (fst wire);
+      default_client_to_monitor_1
+  in
+  let client =
+    if client_build_id <> server_build_id then
+      None
     else
-      let client = Base.Option.value_exn client in
-      let%lwt () = respond Server_will_continue (Some Server_ready) in
+      Some (Marshal.from_string (snd wire) 0 : client_to_monitor_2)
+  in
+  (* handshake step 2: server sends back handshake *)
+  let respond server_intent server2 =
+    assert (server2 = None || client_build_id = server_build_id);
+
+    (* the client will trust our invariant that server2=Some means the client *)
+    (* can certainly deserialize server2. *)
+    let server_version = Flow_version.version in
+    let server1 = { server_build_id; server_bin; server_intent; server_version } in
+    let wire : server_handshake_wire =
+      ( server1 |> monitor_to_client_1__to_json |> Hh_json.json_to_string,
+        Base.Option.map server2 ~f:(fun server2 -> Marshal.to_string server2 [])
+      )
+    in
+    let%lwt _ = Marshal_tools_lwt.to_fd_with_preamble client_fd wire in
+    Lwt.return_unit
+  in
+  let error_client () =
+    let%lwt () = respond Server_will_hangup None in
+    Logger.error "Build mismatch, so rejecting attempted connection";
+    Lwt.return None
+  in
+  let stop_server () =
+    let%lwt () = respond Server_will_exit None in
+    let msg = "Client and server are different builds. Flow server is out of date. Exiting" in
+    FlowEventLogger.out_of_date ();
+    Logger.fatal "%s" msg;
+    Exit.exit ~msg Exit.Build_id_mismatch
+  in
+  let fd_as_int = client_fd |> Lwt_unix.unix_file_descr |> Obj.magic in
+  if is_stop_request then
+    (* Stop request *)
+    let%lwt () = respond Server_will_exit None in
+    let%lwt () = close client_fd () in
+    Server.stop Server.Stopped
+  else if client_build_id <> build_revision then
+    (* Binary version mismatch *)
+    match version_mismatch_strategy with
+    | Always_stop_server -> stop_server ()
+    | Stop_server_if_older ->
+      if Semver.compare Flow_version.version client_version < 0 then
+        stop_server ()
+      (* server < client *)
+      else
+        error_client ()
+    | Error_client -> error_client ()
+  else if Sys.unix && fd_as_int > 500 then (
+    (* Too many clients *)
+    (* We currently rely on using Unix.select, which doesn't work for fds >= FD_SETSIZE (1024).
+     * So we can't have an unlimited number of clients. So if the new fd is too large, let's
+     * reject it.
+     * TODO(glevi): Figure out whether this check is needed for Windows *)
+    let%lwt () = respond Server_will_hangup (Some Server_has_too_many_clients) in
+    Logger.error "Too many clients, so rejecting new connection (%d)" fd_as_int;
+    Lwt.return None
+  ) else if not (StatusStream.ever_been_free ()) then
+    (* Server still initializing *)
+    let client = Base.Option.value_exn client in
+    let status = StatusStream.get_status () in
+    if server_should_hangup_if_still_initializing then (
+      let%lwt () = respond Server_will_hangup (Some (Server_still_initializing status)) in
+      (* In the case of Persistent, lspCommand will retry a second later. *)
+      (* The message we log here solely goes to the logs, not the user. *)
+      let (server_status, watchman_status) = status in
+      Logger.info
+        "Server still initializing -> hangup. server_status=%s watchman_status=%s"
+        (ServerStatus.string_of_status server_status)
+        (FileWatcherStatus.string_of_status watchman_status);
+      Lwt.return None
+    ) else
+      let%lwt () = respond Server_will_continue (Some (Server_still_initializing status)) in
       Lwt.return (Some client)
-  )
+  (* Success *)
+  else
+    let client = Base.Option.value_exn client in
+    let%lwt () = respond Server_will_continue (Some Server_ready) in
+    Lwt.return (Some client)
 
 let catch close exn =
   (* We catch all exceptions, since one bad connection shouldn't kill the whole monitor *)
