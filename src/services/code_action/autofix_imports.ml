aaf6a87dfbdf77b7354fc7d4aec2112e6e2f9b68
Marshall Roch <mroch@fb.com>
2022-07-14 13:23:03 -0700

    fix extra line when autoimporting
    Summary:
    if an autoimport is inserted at the bottom of a section and there's another section below it, we incorrectly include an extra blank line.
    
    we scan the existing imports looking for the best place to insert the new import. when we find it, we previously inserted the new one above the next import. this is a problem when the next import is in a new section and already has a blank line above it.
    
    now we insert the new import below the previous import in this case.
    
    Changelog: [fix] Fix inconsistent blank lines around auto-imports
    
    Reviewed By: samwgoldman
    
    Differential Revision: D37861656
    
    fbshipit-source-id: 96811deb7dd279f0318db32cd46d5bf371e7baca

--- src/services/code_action/autofix_imports.ml
+++ src/services/code_action/autofix_imports.ml
@@ -373,31 +373,27 @@ let sorted_insertion_point =
      whether `import` should be inserted `Above` it, in which case we're done,
      or somewhere `Below`. If `Below`, we keep going until we go "too far"
      or run out of imports. *)
-  let rec helper acc import imports =
-    match imports with
-    | [] -> acc
+  let rec helper best import = function
+    | [] -> best
     | current :: rest ->
-      let sorted =
-        match rest with
-        | [] -> true
-        | next :: _ -> compare_imports current next <= 0
-      in
-      if sorted then
-        let acc =
-          match acc with
-          | None
-          | Some (_, Below _) ->
-            Some (fst current, relative_placement import current)
-          | Some (_, Above _)
-          | Some (_, Replace) ->
-            acc
-        in
-        helper acc import rest
-      else
-        (* imports are not sorted, so give up *)
-        None
+      let placement = relative_placement import current in
+      (match placement with
+      | Above { skip_line = true } ->
+        (match best with
+        | Some _ -> best
+        | None -> Some (fst current, placement))
+      | Above { skip_line = false }
+      | Replace ->
+        Some (fst current, placement)
+      | Below _ ->
+        let best = Some (fst current, placement) in
+        helper best import rest)
   in
-  (fun import imports -> helper None import imports)
+  fun import imports ->
+    if Base.List.is_sorted ~compare:compare_imports imports then
+      helper None import imports
+    else
+      None
 
 let rec last_loc = function
   | [] -> failwith "empty list"
