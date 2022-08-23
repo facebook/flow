b957736464ff1ec907fbcc76fae8d0ef5217f7ab
Sam Zhou <samzhou19815@fb.com>
2022-08-22 22:25:52 -0700

    [cleanup][EZ] Compiler-guided deletions of unused env.ml functions
    Summary:
    Changelog: [internal]
    
    env.ml is only used in new_env.ml as fallback, like `Old_env.unify_declared_type`. Therefore, a lot of functions are actually unused, but since it still implements the Env_sig.S interface, we can't easily delete them yet.
    
    In this diff, I make env.ml implement a copy of Env_sig.S. Then I start to delete functions in the signature that are not used by `Old_env.xyz` calls. Then the ocaml compiler will tell me all the unused functions, and I delete them one by one.
    
    Reviewed By: mvitousek
    
    Differential Revision: D38927023
    
    fbshipit-source-id: cebf1bdd18f546f2f2fd582ddad132097a941c70

--- src/typing/env.ml
+++ src/typing/env.ml
@@ -17,6 +17,128 @@ open TypeUtil
 open Reason
 open Scope
 module Flow = Flow_js
+module LookupMode = Env_sig.LookupMode
+
+module type S = sig
+  type scope
+
+  type t = scope list
+
+  val in_toplevel_scope : Context.t -> bool
+
+  val in_global_scope : Context.t -> bool
+
+  val var_scope_kind : Context.t -> Scope.var_scope_kind
+
+  val in_async_scope : Context.t -> bool
+
+  val in_predicate_scope : Context.t -> bool
+
+  val get_global_value_type : Context.t -> Reason.name -> Reason.t -> Type.t
+
+  val find_entry :
+    Context.t -> Reason.name -> ?desc:Reason.reason_desc -> ALoc.t -> Scope.t * Entry.t
+
+  val push_var_scope : Context.t -> Scope.t -> Scope.var_scope_kind
+
+  val pop_var_scope : Context.t -> Scope.var_scope_kind -> unit
+
+  val in_lex_scope : (unit -> 'a) -> 'a
+
+  val env_depth : unit -> int
+
+  val trunc_env : int -> unit
+
+  val init_env : ?exclude_syms:NameUtils.Set.t -> Context.t -> ALoc.t -> Scope.t -> unit
+
+  (***)
+
+  val bind_class : Context.t -> Type.class_binding -> unit
+
+  val bind_implicit_let :
+    ?state:State.t ->
+    Entry.let_binding_kind ->
+    Context.t ->
+    Reason.name ->
+    Type.annotated_or_inferred ->
+    ALoc.t ->
+    unit
+
+  val bind_fun : ?state:State.t -> Context.t -> Reason.name -> Type.t -> ALoc.t -> unit
+
+  val bind_declare_fun : Context.t -> predicate:bool -> Reason.name -> Type.t -> ALoc.t -> unit
+
+  val init_var :
+    Context.t -> use_op:Type.use_op -> Reason.name -> has_anno:bool -> Type.t -> ALoc.t -> unit
+
+  val init_let :
+    Context.t -> use_op:Type.use_op -> Reason.name -> has_anno:bool -> Type.t -> ALoc.t -> unit
+
+  val init_implicit_let :
+    Entry.let_binding_kind ->
+    Context.t ->
+    use_op:Type.use_op ->
+    Reason.name ->
+    has_anno:bool ->
+    Type.t ->
+    ALoc.t ->
+    unit
+
+  val init_fun : Context.t -> use_op:Type.use_op -> Reason.name -> Type.t -> ALoc.t -> unit
+
+  val init_const :
+    Context.t -> use_op:Type.use_op -> Reason.name -> has_anno:bool -> Type.t -> ALoc.t -> unit
+
+  val init_implicit_const :
+    Entry.const_binding_kind ->
+    Context.t ->
+    use_op:Type.use_op ->
+    Reason.name ->
+    has_anno:bool ->
+    Type.t ->
+    ALoc.t ->
+    unit
+
+  val get_var_declared_type :
+    ?lookup_mode:LookupMode.t ->
+    ?is_declared_function:bool ->
+    Context.t ->
+    Reason.name ->
+    ALoc.t ->
+    Type.t
+
+  val constraining_type : default:Type.t -> Context.t -> Reason.name -> ALoc.t -> Type.t
+
+  val unify_declared_type :
+    ?lookup_mode:LookupMode.t ->
+    ?is_func:bool ->
+    Context.t ->
+    Reason.name ->
+    ALoc.t ->
+    Type.t ->
+    unit
+
+  val read_declared_type :
+    ?lookup_mode:LookupMode.t ->
+    ?is_func:bool ->
+    Context.t ->
+    Reason.name ->
+    Reason.t ->
+    ALoc.t ->
+    Type.t
+
+  val unify_declared_fun_type : Context.t -> Reason.name -> ALoc.t -> Type.t -> unit
+
+  val query_var :
+    ?lookup_mode:LookupMode.t ->
+    Context.t ->
+    Reason.name ->
+    ?desc:Reason.reason_desc ->
+    ALoc.t ->
+    Type.t
+
+  val valid_declaration_check : Context.t -> Reason.name -> ALoc.t -> unit
+end
 
 (* lookup modes:
 
@@ -50,8 +172,8 @@ module Flow = Flow_js
       rule #2, hence the need for a special mode.
 *)
 
-module Env : Env_sig.S = struct
-  open Env_sig.LookupMode
+module Env : S = struct
+  open LookupMode
 
   (****************)
   (* Environment *)
@@ -167,8 +289,6 @@ module Env : Env_sig.S = struct
     pop_lex_scope ();
     result
 
-  let in_class_scope _ _ = in_lex_scope
-
   (* depth of current env *)
   let env_depth () = List.length !scopes
 
@@ -257,20 +377,6 @@ module Env : Env_sig.S = struct
     Scope.add_entry name entry global_scope;
     (global_scope, entry)
 
-  let local_scope_entry_exists _ _ name =
-    let name = OrdinaryName name in
-    let rec loop = function
-      | [] -> assert_false "empty scope list"
-      | scope :: scopes ->
-        (match Scope.get_entry name scope with
-        | Some _ -> true
-        | None ->
-          (match scopes with
-          | [] -> false
-          | _ -> loop scopes))
-    in
-    loop !scopes
-
   (* Look for scope that holds binding for a given name. If found,
      return scope and entry. Note that anything we don't resolve
      otherwise, we add to the global scope after generating a
@@ -286,53 +392,6 @@ module Env : Env_sig.S = struct
           (match scopes with
           | [] -> cache_global cx name ?desc loc scope
           | _ -> loop scopes))
-    in
-    loop !scopes
-
-  let get_class_entries _ =
-    let rec loop class_bindings = function
-      | [] -> assert_false "empty scope list"
-      | scope :: scopes ->
-        (match Scope.get_entry (internal_name "class") scope with
-        | Some entry -> loop (entry :: class_bindings) scopes
-        | None ->
-          (* keep looking until we're at the global scope *)
-          (match scopes with
-          | [] -> class_bindings
-          | _ -> loop class_bindings scopes))
-    in
-    let class_bindings = loop [] !scopes in
-    let to_class_record = function
-      | Entry.Class c -> c
-      | _ -> assert_false "Internal Error: Non-class binding stored with .class"
-    in
-    Base.List.map ~f:to_class_record class_bindings
-
-  (* Search for the scope which binds the given name, through the
-     topmost LexScopes and up to the first VarScope. If the entry
-     is not found, return the VarScope where we terminated. *)
-  let find_entry_in_var_scope name =
-    let rec loop = function
-      | [] -> assert_false "empty scope list"
-      | scope :: scopes ->
-        (match (Scope.get_entry name scope, scope.kind) with
-        | (Some entry, _) -> (scope, Some entry)
-        | (None, VarScope _) -> (scope, None)
-        | (None, LexScope) -> loop scopes)
-    in
-    loop !scopes
-
-  (* Search for the scope which holds the given refinement, through
-     the topmost LexScopes and up to the first VarScope. If the
-     entry is not found, return None. *)
-  let find_refi_in_var_scope key =
-    let rec loop = function
-      | [] -> assert_false "empty scope list"
-      | scope :: scopes ->
-        (match (Scope.get_refi key scope, scope.kind) with
-        | (Some refi, _) -> Some (scope, refi)
-        | (None, VarScope _) -> None
-        | (None, LexScope) -> loop scopes)
     in
     loop !scopes
 
@@ -423,10 +482,6 @@ module Env : Env_sig.S = struct
     Flow.add_output cx (Error_message.EBindingError (msg, loc, name, Entry.entry_loc entry))
 
   let already_bound_error = binding_error Error_message.ENameAlreadyBound
-
-  let is_provider cx id_loc =
-    let { Loc_env.var_info = { Env_api.providers; _ }; _ } = Context.environment cx in
-    Env_api.Provider_api.is_provider providers id_loc
 
   let constrain_by_provider _cx ~use_op:_ _t _name _loc = ()
 
@@ -521,41 +576,6 @@ module Env : Env_sig.S = struct
   (* bind class entry *)
   let bind_class cx x = bind_entry cx (internal_name "class") (Entry.Class x) ALoc.none
 
-  (* bind var entry *)
-  let bind_var_to_name ?(state = State.Declared) cx name t loc =
-    let (spec, closure_writes, provider) =
-      mk_havoc cx name loc (TypeUtil.type_t_of_annotated_or_inferred t) Entry.Havocable
-    in
-    bind_entry cx name (Entry.new_var t ~loc ~state ~spec ?closure_writes ~provider) loc
-
-  let bind_var ?state cx name t loc = bind_var_to_name ?state cx (OrdinaryName name) t loc
-
-  (* bind let entry *)
-  let bind_let ?(state = State.Undeclared) cx name t loc =
-    let (spec, closure_writes, provider) =
-      mk_havoc
-        cx
-        (OrdinaryName name)
-        loc
-        (TypeUtil.type_t_of_annotated_or_inferred t)
-        Entry.Havocable
-    in
-    bind_entry
-      cx
-      (OrdinaryName name)
-      (Entry.new_let t ~loc ~state ~spec ?closure_writes ~provider)
-      loc
-
-  let bind_function_this _ _ _ = ()
-
-  let bind_class_instance_this _ _ _ = ()
-
-  let bind_class_static_this _ _ _ = ()
-
-  let bind_class_instance_super _ _ _ = ()
-
-  let bind_class_static_super _ _ _ = ()
-
   (* bind implicit let entry *)
   let bind_implicit_let ?(state = State.Undeclared) kind cx name t loc =
     let (spec, closure_writes, provider) =
@@ -565,19 +585,6 @@ module Env : Env_sig.S = struct
 
   let bind_fun ?(state = State.Declared) cx name t =
     bind_implicit_let ~state Entry.FunctionBinding cx name (Inferred t)
-
-  (* bind const entry *)
-  let bind_const ?(state = State.Undeclared) cx name t loc =
-    bind_entry cx (OrdinaryName name) (Entry.new_const t ~loc ~state) loc
-
-  (* bind implicit const entry *)
-  let bind_implicit_const ?(state = State.Undeclared) kind cx name t loc =
-    bind_entry cx (OrdinaryName name) (Entry.new_const t ~kind ~loc ~state) loc
-
-  let bind_this_tparam ~state cx t loc =
-    bind_entry cx (OrdinaryName "this") (Entry.new_type t ~loc ~state) loc
-
-  let bind_class_self_type cx _class_loc self class_t_internal = Flow.unify cx self class_t_internal
 
   (* bind entry for declare function *)
   let bind_declare_fun =
@@ -817,22 +824,8 @@ module Env : Env_sig.S = struct
   (* get current env entry for name, if it exists *)
   let get_current_env_entry name = get_env_entry name !scopes
 
-  (* get env refi for key, if it exists *)
-  let get_env_refi key = seek_env (Scope.get_refi key)
-
-  (* get current env refi for name, if it exists *)
-  let get_current_env_refi key = get_env_refi key !scopes
-
-  (* get var's specific type (and track the reference) *)
-  let get_var ?(lookup_mode = ForValue) cx name loc =
-    read_entry ~lookup_mode ~specific:true ?desc:None cx (OrdinaryName name) loc
-
   (* query var's specific type *)
   let query_var ?(lookup_mode = ForValue) = read_entry ~lookup_mode ~specific:true
-
-  let get_internal_var cx name loc = query_var cx (internal_name name) loc
-
-  let get_module_exports cx loc = get_internal_var cx "exports" loc
 
   (* get var's general type - for annotated vars, this is the
      annotated type, and for others it's the union of all
@@ -889,215 +882,4 @@ module Env : Env_sig.S = struct
 
   let read_declared_type ?lookup_mode ?is_func cx name reason loc =
     Tvar.mk_where cx reason (unify_declared_type ?lookup_mode ?is_func cx name loc)
-
-  let is_global_var _cx name _ =
-    let rec loop = function
-      | [] -> true
-      | scope :: scopes ->
-        (match Scope.get_entry (OrdinaryName name) scope with
-        | Some _ -> Scope.is_global scope
-        | None -> loop scopes)
-    in
-    loop !scopes
-
-  (* get var type, with given location used in type's reason *)
-  let var_ref ?(lookup_mode = ForValue) cx ?desc name loc =
-    let t = query_var ~lookup_mode cx name ?desc loc in
-    Flow.reposition cx loc t
-
-  let init_import ~lookup_mode cx name loc t =
-    let t_generic = get_var_declared_type ~lookup_mode cx name loc in
-    Flow.unify cx t t_generic
-
-  (* get refinement entry *)
-  let get_refinement cx key loc =
-    match find_refi_in_var_scope key with
-    | Some (_, { refined; _ }) -> Some (Flow.reposition cx loc refined)
-    | _ -> None
-
-  (* Types-First signature extraction only inspects the initial binding for let-like
-     definitions. Any subsequent assignment is ignored. This would cause a discrepancy
-     between what classic mode views as the exported value, and what types-first does.
-     To avoid this, we disallow updates on classes and functions. Let-bound variables
-     would follow the same rule, but Types-First requires an annotation on their
-     definition. This mitigates the problem, since in case of a reassigment the updated
-     value needs to respect the annotation type, so we would only miss a potential
-     refinement rather a strong type update.
-
-     We also ban such reassignments in non-exported functions and classes in order to
-     allow their types to be eagerly resolved.
-  *)
-  let has_illegal_let_bound_reassignment op cx name entry loc =
-    let open Entry in
-    match (op, entry) with
-    | ( Changeset.Write,
-        Value
-          {
-            Entry.kind =
-              Let
-                Entry.
-                  ( ( (ClassNameBinding | FunctionBinding | DeclaredFunctionBinding _) as
-                    binding_kind
-                    ),
-                    _
-                  );
-            value_declare_loc;
-            _;
-          }
-      ) ->
-      let reason = mk_reason (RType name) value_declare_loc in
-      Flow.add_output
-        cx
-        Error_message.(EAssignConstLikeBinding { loc; definition = reason; binding_kind });
-      true
-    | _ -> false
-
-  (* helper: update let or var entry *)
-  let update_var op cx ~use_op name specific loc =
-    let (scope, entry) = find_entry cx name loc in
-    if has_illegal_let_bound_reassignment op cx name entry loc then
-      None
-    else
-      Entry.(
-        match entry with
-        | Value ({ Entry.kind = Let _ as kind; value_state = State.Undeclared; _ } as v)
-          when (not (allow_forward_ref kind)) && same_activation scope ->
-          tdz_error cx name loc v;
-          None
-        | Value ({ Entry.kind = Let _ | Var _; closure_writes; general; _ } as v) ->
-          let change = (scope.id, name, op) in
-          begin
-            match (closure_writes, op) with
-            | (_, Changeset.Refine) -> ()
-            | (_, Changeset.Read) -> assert_false "read op during variable update"
-            | (Some (writes_by_closure, t, _), Changeset.Write)
-              when ALocSet.mem loc writes_by_closure ->
-              Flow.flow cx (specific, UseT (use_op, t))
-            | (_, Changeset.Write) ->
-              Flow.flow cx (specific, UseT (use_op, Entry.general_of_value v))
-          end;
-
-          begin
-            match (general, op) with
-            | (Inferred _, Changeset.Write) -> constrain_by_provider cx ~use_op specific name loc
-            | _ -> ()
-          end;
-
-          (* add updated entry *)
-          let update =
-            Entry.Value
-              { v with Entry.value_state = State.Initialized; specific; value_assign_loc = loc }
-          in
-          Scope.add_entry name update scope;
-          Some change
-        | Value { Entry.kind = Const ConstVarBinding; _ } ->
-          let msg = Error_message.EConstReassigned in
-          binding_error msg cx name entry loc;
-          None
-        | Value { Entry.kind = Const EnumNameBinding; _ } ->
-          let msg = Error_message.EEnumReassigned in
-          binding_error msg cx name entry loc;
-          None
-        | Value { Entry.kind = Const ConstImportBinding; _ } ->
-          let msg = Error_message.EImportReassigned in
-          binding_error msg cx name entry loc;
-          None
-        | Value { Entry.kind = Const ConstParamBinding; _ } ->
-          (* TODO: remove extra info when surface syntax is added *)
-          let msg = Error_message.EConstParamReassigned in
-          binding_error msg cx name entry loc;
-          None
-        | Type _ ->
-          let msg = Error_message.ETypeAliasInValuePosition in
-          binding_error msg cx name entry loc;
-          None
-        | Class _ -> assert_false "Internal error: update_var called on Class"
-      )
-
-  (* update var by direct assignment *)
-  let set_var cx ~use_op name t loc =
-    update_var Changeset.Write cx ~use_op (OrdinaryName name) t loc |> ignore
-
-  let set_internal_var cx name t loc =
-    update_var Changeset.Write cx ~use_op:unknown_use (internal_name name) t loc |> ignore
-
-  let set_module_exports cx loc t = set_internal_var cx "exports" t loc
-
-  (* The protocol around havoc has changed a few times.
-     The following function used to do most of the work, but is now subsumed by
-     havoc_ctx, and is now used only to clear the environment when looking at
-     a function body. Also see below. *)
-
-  let add_heap_refinement op key refi_loc refined original =
-    let refi = { refi_loc; refined; original } in
-    let (base, _) = key in
-    let (scope, _) = find_entry_in_var_scope base in
-    let change = (scope.id, key, op) in
-    Scope.add_refi key refi scope;
-    change
-
-  let set_expr _cx k l ~refined:t1 ~original:t2 =
-    add_heap_refinement Changeset.Write k l t1 t2 |> ignore
-
-  let record_expression_type_if_needed _ _ _ _ = ()
-
-  let discriminant_after_negated_cases cx _switch_loc refinement_key_opt discriminant =
-    match discriminant with
-    | (loc, Flow_ast.Expression.Identifier (_, { Flow_ast.Identifier.name; _ })) ->
-      Some (query_var cx (OrdinaryName name) loc)
-    | _ ->
-      refinement_key_opt
-      |> Base.Option.bind ~f:get_current_env_refi
-      |> Base.Option.map ~f:(fun refi -> refi.Scope.refined)
-
-  let get_next cx loc = get_internal_var cx "next" loc
-
-  let init_class_self_type cx _loc reason = Tvar.mk cx reason
-
-  let init_declare_module_synthetic_module_exports cx ~export_type loc reason module_scope =
-    match Context.module_kind cx with
-    | Module_info.ES _ -> ()
-    | Module_info.CJS clobbered ->
-      Scope.(
-        Entry.(
-          let () =
-            match clobbered with
-            | Some _ -> ()
-            | None ->
-              let props =
-                NameUtils.Map.fold
-                  (fun x entry acc ->
-                    match entry with
-                    | Value { specific; _ } when x <> internal_name "exports" ->
-                      let loc = Some (entry_loc entry) in
-                      Properties.add_field x Polarity.Positive loc specific acc
-                    | _ -> acc)
-                  module_scope.entries
-                  NameUtils.Map.empty
-              in
-              let proto = ObjProtoT reason in
-              let t = Obj_type.mk_with_proto cx reason ~obj_kind:Exact ~props proto in
-              set_module_exports cx loc t
-          in
-          NameUtils.Map.iter
-            (fun x entry ->
-              match entry with
-              | Type { type_; type_binding_kind = TypeBinding; _ } ->
-                (* TODO we may want to provide a location here *)
-                export_type cx x None type_
-              | Type { type_binding_kind = ImportTypeBinding; _ }
-              | Value _
-              | Class _ ->
-                ())
-            module_scope.entries
-        )
-      )
-
-  let init_builtins_from_libdef cx module_scope =
-    (module_scope
-    |> Scope.(
-         iter_entries Entry.((fun name entry -> Flow_js.set_builtin cx name (actual_type entry)))
-       )
-    );
-    NameUtils.Map.keys Scope.(module_scope.entries)
 end
