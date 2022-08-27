7c776eb1642a94216ccda69af845c2b4333a9660
George Zahariev <gkz@fb.com>
2022-08-26 19:04:45 -0700

    Add [string]: mixed indexer to `import.meta` to allow for additional property lookup
    Summary:
    "The import.meta object exposes context-specific metadata to a JavaScript module."
    This means it can contain arbitrary data. To allow our users to access this data in a safe way, we can add a `[string]: mixed` indexer.
    
    Requested in GitHub: https://github.com/facebook/flow/pull/6958#issuecomment-1223909153
    
    Changelog: [feature] Allow access of arbitrary properties from `import.meta`. They are typed as `mixed`
    
    Reviewed By: SamChou19815
    
    Differential Revision: D39072638
    
    fbshipit-source-id: 099dd4b17755210d8a893edf2c2c9a94ae38bd74

--- tests/meta_property/import_meta.js
+++ tests/meta_property/import_meta.js
@@ -2,6 +2,7 @@
 
 (import.meta: mixed); // OK
 (import.meta.url: ?string); // OK
+(import.meta.XXX: mixed); // OK
 
 (import.meta: 1); // Error
-import.meta.xxx; // Error
+(import.meta.XXX: 1); // Error
