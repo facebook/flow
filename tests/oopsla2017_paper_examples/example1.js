d454d0f9e33356f2c365c7fc851daa4b738ee386
Sam Zhou <samzhou19815@fb.com>
2022-09-15 09:17:59 -0700

    [EZ] Delete oopsla2017_paper_examples test suite
    Summary:
    These test cases test Flow's global type inference algorithm, which will soon be replaced by LTI. It doesn't make sense to annotate these examples, since those behaviors are already tested elsewhere. Therefore, deleting them might be the best option.
    
    Changelog: [internal]
    
    Reviewed By: panagosg7
    
    Differential Revision: D39523140
    
    fbshipit-source-id: f6bf3082b7197cadc2725b3059ca84b860d3ec6e

--- tests/oopsla2017_paper_examples/example1.js
+++
@@ -1,6 +0,0 @@
-// @flow
-
-function pipe(x, f) { f(x); }
-let hello = (s) => console.log("hello", s);
-pipe("world", hello);
-pipe("hello", null); // error
--- tests/oopsla2017_paper_examples/example2.js
+++
@@ -1,8 +0,0 @@
-// @flow
-
-function pipe(x, f) {
-  if (f != null) { f(x); }
-}
-let hello = (s) => console.log("hello", s);
-pipe("world", hello);
-pipe("hello", null); // ok
