bcae780ffc652be2fdbcc9862aab440dda92ff00
Sam Zhou <samzhou19815@meta.com>
2022-11-03 14:15:10 -0700

    [lti] Consider RReactChildren reason to be fresh array
    Summary:
    Similar to array literals, a list of react children is also literal like. Therefore, they should be considered as fresh as well. If not, we will fail in the following case in LTI:
    
    ```
    declare function Wrap({children: [mixed, mixed]}): React.Node;
    
    (<Wrap>
      {true ? <div></div> : <div></div>}
      <div></div>
    </Wrap>);
    ```
    
    We will seal all expressions tvars, so we will do `[React.Element, React.Element] ~> [mixed, mixed]`, which triggers `React.Element = mixed` and failed. We have to treat it as fresh so that it only triggers a subtyping flow.
    
    Changelog: [internal]
    
    Reviewed By: mvitousek
    
    Differential Revision:
    D40987949 (https://github.com/facebook/flow/commit/b1993500a5af9f9f932231e082f3b973d33a66df)
    
    ------------------------------------------------------------------------
    (from 975e25cf7407f32f7fbfbe9ca7a3db5a5b50059f)
    
    fbshipit-source-id: 2c1646018a63b5fd87962a881adbf241cd05d992

--- tests/sealed_tvars/react.js
+++ tests/sealed_tvars/react.js
@@ -4,3 +4,10 @@ import * as React from 'react';
 const RC = <T />
 
 function T(): void {}
+
+declare function Wrap({children: [mixed, mixed]}): React.Node;
+
+(<Wrap>
+  {/* ok */ true ? <div></div> : <div></div>}
+  <div></div>
+</Wrap>);
