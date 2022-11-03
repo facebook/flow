1e5ac33ff4201275ab473b6e655eed17d9aa20bd
Sam Zhou <samzhou19815@meta.com>
2022-11-03 00:13:37 -0700

    [lti] Reverse spread in upper bounds
    Summary:
    In this diff, I reverse spread in implicit instantiation upper bound with rest. For every slice or type in spread state, I convert them to a type and do a $Rest operation. It doesn't even matter if we have all the types right, we just need to get the property names right, since we are only removing properties from tout.
    
    Changelog: [internal]
    
    Reviewed By: jbrown215
    
    Differential Revision:
    D40964617 (https://github.com/facebook/flow/commit/c2f795517fe161df402e15c9395393bbc2f702da)
    
    ------------------------------------------------------------------------
    (from 38d4a3f7cb189f56238ec6ce20cf387931f32331)
    
    fbshipit-source-id: e7114d039b05d94fa006b6edc1db699e1fa8d492

--- tests/post_inference_implicit_instantiation/upper_non_t.js
+++ tests/post_inference_implicit_instantiation/upper_non_t.js
@@ -5,7 +5,7 @@ declare function HOC<OwnProps: {}>(
     Component: ({|...OwnProps, ...OtherProps|}) => mixed,
 ): OwnProps => mixed;
 
-const x = HOC((x: {| foo: number, bar: number |}) => null);
+const x = HOC((x: {| foo: number, bar: number |}) => null); // ok
 
 declare function ReposLowerTRegressionTest<T>({o?: T}): T;
 ReposLowerTRegressionTest({}) // Error: T under constrained.
@@ -35,3 +35,44 @@ declare function ElementConfig<T>(React$ElementConfig<T>): T;
 (ElementConfig({foo: 3}): React$AbstractComponent<{foo: number}, mixed>); // ok
 declare function Exact<T>($Exact<T>): T;
 Exact({foo: 3}); // ok
+
+function spreads() {
+  type T = {|bar: number, baz: number|};
+
+  declare function spread1<Props>({...Props}): Props;
+  const s1 = spread1({foo: 3, bar: 2, baz: 1}); // ok
+  (s1.foo: number);
+  (s1.bar: number);
+  (s1.baz: number);
+  s1.bad; // error in LTI
+  declare function spread2<Props>({...Props, bar: number, baz: number}): Props;
+  const s2 = spread2({foo: 3, bar: 2, baz: 1}); // ok
+  (s2.foo: number);
+  s2.bad; // error in LTI
+  s2.bar; // error in LTI
+  s2.baz; // error in LTI
+  declare function spread3<Props>({...Props, ...T}): Props;
+  const s3 = spread3({foo: 3, bar: 2, baz: 1}); // non-Type.t upper bound ReposUseT
+  (s3.foo: number);
+  s3.bad; // error in LTI
+  s3.bar; // error in LTI
+  s3.baz; // error in LTI
+  declare function spread4<Props>({baz: number, bar: number, ...Props}): Props;
+  const s4 = spread4({foo: 3, bar: 2, baz: 1}); // ok
+  (s4.foo: number);
+  s4.bad; // error in LTI
+  s4.bar; // error in LTI
+  s4.baz; // error in LTI
+  declare function spread5<Props>({...T, ...Props}): Props;
+  const s5 = spread5({foo: 3, bar: 2, baz: 1}); // ok
+  (s5.foo: number);
+  s5.bad; // error in LTI
+  s5.bar; // error in LTI
+  s5.baz; // error in LTI
+  declare function spread6<Props>({hhh: number, ...Props, ...T}): Props;
+  const s6 = spread6({foo: 3, bar: 2, baz: 1, hhh: 0}); // non-Type.t upper bound ReposUseT
+  (s6.foo: number);
+  s6.bad; // error in LTI
+  s6.bar; // error in LTI
+  s6.baz; // error in LTI
+}
