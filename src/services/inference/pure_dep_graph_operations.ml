8376d8976dda6469dab83a0e35b51ca7995ab200
Sam Goldman <samgoldman@meta.com>
2022-12-07 11:47:16 -0800

    Remove unused calc_all_dependencies function
    Reviewed By: panagosg7
    
    Differential Revision: D41721732
    
    fbshipit-source-id: f1d8baa5b19d31369d52c31273c86d01cd4d701c

--- src/services/inference/pure_dep_graph_operations.ml
+++ src/services/inference/pure_dep_graph_operations.ml
@@ -7,33 +7,23 @@
 
 open Utils_js
 
-type graph_direction =
-  | Forward
-  | Backward
-
-(* `closure graph files` returns all files in `graph` which are reachable from `files`, directly or
- * indirectly. *)
-let closure =
-  let rec helper graph find_opt files acc =
+(* `rdep_closure graph files` returns all files in `graph` which are reachable
+ * from `files`, directly or indirectly. *)
+let rdep_closure =
+  let rec loop graph files acc =
     FilenameSet.fold
       (fun file acc ->
-        let entry = find_opt file graph in
+        let entry = FilenameGraph.find_backward_opt file graph in
         match entry with
         | Some files ->
           let files = FilenameSet.diff files acc in
           let acc = FilenameSet.union files acc in
-          helper graph find_opt files acc
+          loop graph files acc
         | None -> acc)
       files
       acc
   in
-  fun graph direction files ->
-    let find_opt =
-      match direction with
-      | Forward -> FilenameGraph.find_opt
-      | Backward -> FilenameGraph.find_backward_opt
-    in
-    helper graph find_opt files files
+  (fun graph files -> loop graph files files)
 
 (* `calc_direct_dependencies graph files` will return the set of direct dependencies of
    `files`. This set includes `files`. *)
@@ -45,11 +35,6 @@ let calc_direct_dependencies dependency_graph files =
       | None -> acc)
     files
     files
-
-(* `calc_all_dependencies graph files` will return the set of direct and transitive dependencies
- * of `files`. This set does include `files`.
- *)
-let calc_all_dependencies dependency_graph files = closure dependency_graph Forward files
 
 (** [calc_direct_dependents graph files] will return the set of direct dependents of
   [files]. This set includes [files]. *)
@@ -68,7 +53,7 @@ let calc_direct_dependents dependency_graph files =
   A file is a dependent of [files] whenever its code depends on any file whose *signature*, in
   turn, directly or transitively depends on [files]. *)
 let calc_all_dependents ~sig_dependency_graph ~implementation_dependency_graph files =
-  let all_sig_dependents = closure sig_dependency_graph Backward files in
+  let all_sig_dependents = rdep_closure sig_dependency_graph files in
   let all_dependents = calc_direct_dependents implementation_dependency_graph all_sig_dependents in
   (all_sig_dependents, all_dependents)
 
