f7b52f367c2e50116f0b45b63eed68b235c216b8
Sam Zhou <samzhou19815@meta.com>
2022-12-13 10:12:45 -0800

    [tests] Fix call_caching1 test to make it compatible with LTI
    Summary:
    Under old inference mode, value of `tasksPerStatusMap` can widen while in LTI it can't. This diff adds explicit cast to force the widening in both mode, so the test can pass in both modes.
    
    Changelog: [internal]
    
    Reviewed By: panagosg7
    
    Differential Revision: D41976187
    
    fbshipit-source-id: be7ac7a6f3c8c876a3eb1b4f88265ad50a726e2f

--- tests/call_caching1/test.js
+++ tests/call_caching1/test.js
@@ -3,7 +3,7 @@
 const Immutable = require('immutable');
 
 const tasksPerStatusMap = new Map(
-  ([]: Array<string>).map(taskStatus => [taskStatus, new Map<string, string>()]),
+  ([]: Array<string>).map(taskStatus => [taskStatus, (new Map(): Map<string, string> | Immutable.Map<string, string>)]),
 );
 for (let [taskStatus, tasksMap] of tasksPerStatusMap) {
   tasksPerStatusMap.set(taskStatus, Immutable.Map(tasksMap));
