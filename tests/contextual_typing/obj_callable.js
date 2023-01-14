0ccf7685cdffbe055396a70cacb2cecc7a5d99a8
Sam Zhou <samzhou19815@meta.com>
2023-01-13 19:08:32 -0800

    [lti] Special case handling of polymorphic callables on ThisClassT statics
    Summary:
    Similar to the object callable case, we need to deal with class statics callables in cases like
    
    ```
    declare class C<T> {
      static <X>(values: Array<X>): C<X>;
    }
    const x1: C<string> = C([]);
    ```
    
    Otherwise we will be doing hint decomposition on some polymorphic values and get some bad results.
    
    In this fix, I basically do what the constrain system will do when encountering `PolyT ThisClassT ~> CallT`, we will eliminate the targs from the outer PolyT first, then fix_this_class, and finally recursively call `handle_poly`, so that the existing object callable handler can deal with it.
    
    Changelog: [internal]
    
    Reviewed By: panagosg7
    
    Differential Revision: D42488707
    
    fbshipit-source-id: cdcc04433900ad7afbb5acb5cbb34ecbeb88d8fe

--- tests/contextual_typing/obj_callable.js
+++ tests/contextual_typing/obj_callable.js
@@ -5,4 +5,11 @@ declare var c: {
     (): void,
 }
 
-c<number>(42, (x) => {})
+c<number>(42, (x) => {}) // ok
+
+declare class C<T> {
+  static <X>(values: Array<X>): C<X>;
+}
+const x1: C<string> = C([]); // ok
+type Enum = 'A' | 'B';
+const x2 = C<Enum>([]); // ok
