3b579dc20b481bc8493c3441246c33ea1c49f67d
Sam Zhou <samzhou19815@meta.com>
2023-01-20 08:51:52 -0800

    [tests] Re-record refinements test suite for LTI
    Summary:
    Error diff:
    
    ```
     --- refinements.exp
    +++ refinements.out
    @@ -217,7 +217,7 @@
    
     Error ----------------------------------------------------------------------------------------------- cond_prop.js:11:25
    
    -Cannot call `getTypeASTName` with `typeAST.type` bound to `typeAST` because `BadType` [1] is incompatible with
    +Cannot call `getTypeASTName` with `typeAST.type` bound to `typeAST` because object type [1] is incompatible with
     `Type` [2]. [incompatible-call]
    
        cond_prop.js:11:25
    @@ -3017,6 +3017,24 @@
                               ^^^^^ [2]
    
    +Error ---------------------------------------------------------------------------------------------- issue-7104.js:22:14
    +
    +`T` [1] is underconstrained by async return [2]. Either add explicit type arguments or cast the expression to your
    +expected type. [underconstrained-implicit-instantiation]
    +
    +   issue-7104.js:22:14
    +     22|       return (box: empty).value; // underconstrained-implicit-instantiation error in LTI due to lack of empty propagation
    +                      ^^^^^^^^^^^^^^^^^^
    +
    +References:
    +   <BUILTINS>/core.js:1939:25
    +   1939| declare function $await<T>(p: Promise<T> | T): T;
    +                                 ^ [1]
    +   issue-7104.js:22:7
    +     22|       return (box: empty).value; // underconstrained-implicit-instantiation error in LTI due to lack of empty propagation
    +               ^^^^^^^^^^^^^^^^^^^^^^^^^^ [2]
    +
    +
     Error ---------------------------------------------------------------------------------------------- issue-7104.js:22:15
    
     Cannot cast `box` to empty because object type [1] is incompatible with empty [2]. [incompatible-cast]
    @@ -4798,7 +4816,7 @@
    
     Error --------------------------------------------------------------------------------------------- tagged_union.js:40:8
    
    -Cannot get `x.raw` because property `raw` is missing in `Orange` [1]. [prop-missing]
    +Cannot get `x.raw` because property `raw` is missing in object type [1]. [prop-missing]
    
        tagged_union.js:40:8
        40|     (x.raw: 'Yes' | 'No'); // 2 errors:
    @@ -6277,7 +6295,7 @@
    
    -Found 416 errors
    +Found 417 errors
    
     Only showing the most relevant union/intersection branches.
     To see all branches, re-run Flow with --show-all-branches
    
    ```
    
    Slight message difference, and additional LTI error due to lack of empty propagation
    
    Reviewed By: gkz
    
    Differential Revision: D42624596
    
    fbshipit-source-id: 34c88aa380b12a4776cbcdd12a9dc7df48442586

--- tests/refinements/issue-7104.js
+++ tests/refinements/issue-7104.js
@@ -19,7 +19,7 @@ async function coerce(t: string): Promise<number> {
     sentinel.flag = true;
     await p;
     if (box.type !== "GOOD") {
-      return (box: empty).value;
+      return (box: empty).value; // underconstrained-implicit-instantiation error in LTI due to lack of empty propagation
     }
   }
   throw new Error("Unreachable.");
