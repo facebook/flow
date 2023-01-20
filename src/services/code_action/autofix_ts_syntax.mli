b5e919cef85e6f0c4453978940f80ec4c0f52fda
George Zahariev <gkz@meta.com>
2023-01-20 13:06:52 -0800

    [ts-syntax] Quick fix to convert `in`/`out` variance annotations to `+`/`-`
    Summary:
    Quick fix to convert `in`/`out` variance annotations to `+`/`-`, to help people familiar with TS syntax
    
    Changelog: [internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D42614512
    
    fbshipit-source-id: 14ac8c724a46dfbbacf7ee124f01b3f8c5b360ff

--- src/services/code_action/autofix_ts_syntax.mli
+++ src/services/code_action/autofix_ts_syntax.mli
@@ -35,6 +35,21 @@ val convert_readonly_variance :
   Loc.t ->
   (Loc.t, Loc.t) Flow_ast_mapper.Ast.Program.t
 
+val convert_in_variance :
+  (Loc.t, Loc.t) Flow_ast_mapper.Ast.Program.t ->
+  Loc.t ->
+  (Loc.t, Loc.t) Flow_ast_mapper.Ast.Program.t
+
+val convert_out_variance :
+  (Loc.t, Loc.t) Flow_ast_mapper.Ast.Program.t ->
+  Loc.t ->
+  (Loc.t, Loc.t) Flow_ast_mapper.Ast.Program.t
+
+val remove_in_out_variance :
+  (Loc.t, Loc.t) Flow_ast_mapper.Ast.Program.t ->
+  Loc.t ->
+  (Loc.t, Loc.t) Flow_ast_mapper.Ast.Program.t
+
 val convert_as_expression :
   (Loc.t, Loc.t) Flow_ast_mapper.Ast.Program.t ->
   Loc.t ->
