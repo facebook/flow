fe3bf3c3cbcc060dcf7ccbd5061cfdc1fe9871aa
Panagiotis Vekris <pvekris@meta.com>
2023-02-01 01:38:16 -0800

    include evaluated cache in cache snapshot
    Summary:
    Include the EvalT cache to the set of caches that need to be included in the cache snapshot.
    
    Changelog: [fix] Fixed a bug in LTI where some errors in utility types were not properly shown (e.g. [try-Flow](https://flow.org/try/#0CYUwxgNghgTiAEAzArgOzAFwJYHtVIEYAeAeQD4 (https://github.com/facebook/flow/commit/22ae4fc735d17bab0076f2adf61ab7a1d69468d7)AKHALnhIEpaASABRhwAcQYMBPAFV5ciAbwC+AGngByDtLIBueAHpl8AKIx2MeAGcAFjmQRg8DjiyoM8fdxAA6AFCPQkWAgBuseDXjiFjhSIBFSM8CAAthx89AFAA))
    
    Reviewed By: SamChou19815
    
    Differential Revision: D42789415
    
    fbshipit-source-id: 89813992d545b27d88b025d3050fcc769e81d26b

--- src/typing/context.ml
+++ src/typing/context.ml
@@ -847,6 +847,7 @@ type cache_snapshot = {
   snapshot_eval_repos_cache: Type.t Type.EvalReposCacheMap.t;
   snapshot_fix_cache: Type.t Type.FixCacheMap.t;
   snapshot_spread_cache: Spread_cache.t;
+  snapshot_evaluated: Type.t Type.Eval.Map.t;
 }
 
 let take_cache_snapshot cx =
@@ -859,6 +860,7 @@ let take_cache_snapshot cx =
     snapshot_eval_repos_cache = !(cx.ccx.eval_repos_cache);
     snapshot_fix_cache = !(cx.ccx.fix_cache);
     snapshot_spread_cache = !(cx.ccx.spread_cache);
+    snapshot_evaluated = cx.ccx.sig_cx.evaluated;
   }
 
 let restore_cache_snapshot cx snapshot =
@@ -870,6 +872,7 @@ let restore_cache_snapshot cx snapshot =
     snapshot_eval_repos_cache;
     snapshot_fix_cache;
     snapshot_spread_cache;
+    snapshot_evaluated;
   } =
     snapshot
   in
@@ -881,7 +884,8 @@ let restore_cache_snapshot cx snapshot =
   id_cache := snapshot_id_cache;
   cx.ccx.eval_repos_cache := snapshot_eval_repos_cache;
   cx.ccx.fix_cache := snapshot_fix_cache;
-  cx.ccx.spread_cache := snapshot_spread_cache
+  cx.ccx.spread_cache := snapshot_spread_cache;
+  set_evaluated cx snapshot_evaluated
 
 let run_and_rolled_back_cache cx f =
   let cache_snapshot = take_cache_snapshot cx in
