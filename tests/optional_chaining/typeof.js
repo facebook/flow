8abfbf88979efd5df4e674d00557dec8a1a625a8
Sam Zhou <samzhou19815@meta.com>
2023-02-10 17:10:57 -0800

    [tests] Re-record optional chaining test suite for LTI
    Summary: Changelog: [internal]
    
    Reviewed By: gkz
    
    Differential Revision:
    D43129765 (https://github.com/facebook/flow/commit/1555d61f9dee413a32929b2d0861f5b7f50fd467)
    
    ------------------------------------------------------------------------
    (from a149c3f754121cf4bd0dfeebe43802226ecbae3e)
    
    fbshipit-source-id: 71320c75251119ef25ca01a46e94141683ea57f5

--- tests/optional_chaining/typeof.js
+++ tests/optional_chaining/typeof.js
@@ -3,5 +3,6 @@
 function foo() {
   const x: {a?: {b: $ReadOnlyArray<{c: number}>}} = {};
   const y = x.a?.b;
-  type T = $ElementType<typeof y, 0>;
+  type T = $ElementType<typeof y, 0>; // error: undefined does not have properties
+  const z: T = 1;
 }
