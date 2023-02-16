19b444a26145bd13af31e304f61530c149cb5f7f
George Zahariev <gkz@meta.com>
2023-02-15 17:34:41 -0800

    [flow-upgrade] Add 0.84.0 to upgrades list
    Summary:
    0.84.0 was missing from the upgrades list - add it.
    
    Changelog: [internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D43333334
    
    fbshipit-source-id: 1e6620ffc24dbd6953879e3744557e4f6f0cee42

--- packages/flow-upgrade/src/upgrades/index.js
+++ packages/flow-upgrade/src/upgrades/index.js
@@ -10,6 +10,7 @@
 
 import type {Upgrade} from '../Types';
 
+import v0_84_0 from './0.84.0';
 import v0_170_0 from './0.170.0';
 import v0_176_0 from './0.176.0';
 
@@ -17,4 +18,8 @@ import v0_176_0 from './0.176.0';
  * Holds all of the upgrades that need to be run to upgrade to each version from
  * the last version of Flow.
  */
-export const VERSION_UPGRADES: $ReadOnlyArray<Upgrade> = [v0_170_0, v0_176_0];
+export const VERSION_UPGRADES: $ReadOnlyArray<Upgrade> = [
+  v0_84_0,
+  v0_170_0,
+  v0_176_0,
+];
