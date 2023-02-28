65b0d031e412f4ea21817c92365e0c30669969c8
David Richey <davidrichey@meta.com>
2023-02-28 15:01:01 -0800

    Move hover_evaluate newtests to tests
    Summary:
    These tests are easier to deal with and type-at-pos does the same thing.
    
    Changelog: [internal]
    
    Reviewed By: mroch
    
    Differential Revision: D43507270
    
    fbshipit-source-id: ba72aba1e85197e996b0f46a95ed0642d27c46d4

--- tests/type_at_pos_evaluate/test.js
+++ tests/type_at_pos_evaluate/test.js
@@ -1,25 +1,33 @@
 // @flow
 
 type Foo = {
-    bar: {
-        baz: ?{
-            qux: $ReadOnlyArray<string>;
-        }
-    }
+  bar: {
+      baz: ?{
+          qux: $ReadOnlyArray<string>;
+      }
+  }
 }
 
 type T1 = Foo;
+//   ^
 
 type T2 = Foo["bar"];
+//   ^
 
 type T3 = Foo["bar"]["baz"]
+//   ^
 
 type T4 = Foo["bar"]["baz"]?.["qux"];
+//   ^
 
 type T5 = Foo["bar"]["baz"]?.["qux"][number];
+//   ^
 
 type T6 = $NonMaybeType<Foo["bar"]["baz"]?.["qux"][number]>;
+//   ^
 
 type T7 = $Keys<Foo>;
+//   ^
 
 type T8 = $Keys<{foo: string, bar: number, baz: bigint}>;
+//   ^
