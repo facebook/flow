09a5fa032e96f88d4d341ce0aec87db224e51ce9
Sam Zhou <samzhou19815@meta.com>
2023-03-23 13:01:07 -0700

    [conditional-type] Rename estree-translator names
    Summary:
    Make it more consistent with other annotations. Found the issue when I'm working on prettier integration.
    
    Changelog: [parser] The json output of flow parser will produce "ConditionalTypeAnnotation" and "InferTypeAnnotation" instead of "ConditionalType" and "InferType" as the type name for conditional type and infer type.
    
    Reviewed By: mroch
    
    Differential Revision:
    D44343113
    
    ------------------------------------------------------------------------
    (from 43a59e99d2ba75d1ed8fb0240e708e26748ccebc)
    
    fbshipit-source-id: 96b67e0a10284ac3498647960ca4e4d36046059b

--- src/parser/estree_translator.ml
+++ src/parser/estree_translator.ml
@@ -1684,7 +1684,7 @@ with type t = Impl.t = struct
         loc { Type.Conditional.check_type; extends_type; true_type; false_type; comments } =
       node
         ?comments
-        "ConditionalType"
+        "ConditionalTypeAnnotation"
         loc
         [
           ("checkType", _type check_type);
@@ -1693,7 +1693,7 @@ with type t = Impl.t = struct
           ("falseType", _type false_type);
         ]
     and infer_type loc { Type.Infer.tparam; comments } =
-      node ?comments "InferType" loc [("typeParameter", type_param tparam)]
+      node ?comments "InferTypeAnnotation" loc [("typeParameter", type_param tparam)]
     and generic_type_qualified_identifier (loc, { Type.Generic.Identifier.id; qualification }) =
       let qualification =
         match qualification with
