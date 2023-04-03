b92a880a53b9b135a203a05dad51f075cd91d52e
David Richey <davidrichey@meta.com>
2023-04-03 09:59:57 -0700

    Inline signatureHelp newtest helper
    Summary:
    The snapshot recorder doesn't play nice with `verifySignatureHelp`. Imo, the value from being able to do `./tool record` is greater than the value of the helper.
    
    I also enabled formatting for this file.
    
    Changelog: [internal]
    
    Reviewed By: mroch
    
    Differential Revision: D44585127
    
    fbshipit-source-id: 3319c7ac44b684fb111914ffacda6eeca7685704

--- newtests/lsp/signatureHelp/test.js
+++ newtests/lsp/signatureHelp/test.js
@@ -1,8 +1,9 @@
 /*
  * @flow
+ * @format
  */
 
-import type {Suite} from "../../../packages/flow-dev-tools/src/test/Suite";
+import type {Suite} from '../../../packages/flow-dev-tools/src/test/Suite';
 const {suite, test} = require('flow-dev-tools/src/test/Tester');
 
 module.exports = (suite(
@@ -14,19 +15,6 @@ module.exports = (suite(
     lspRequestAndWaitUntilResponse,
     lspIgnoreStatusAndCancellation,
   }) => {
-    let verifySignatureHelp = function(
-      uri: string,
-      position: {|line: number, character: number|},
-      response: string,
-    ) {
-      return lspRequestAndWaitUntilResponse('textDocument/signatureHelp', {
-        textDocument: {uri: uri},
-        position: position,
-      }).verifyAllLSPMessagesInStep(
-        [['textDocument/signatureHelp', response]],
-        ['textDocument/publishDiagnostics', ...lspIgnoreStatusAndCancellation],
-      );
-    };
     return [
       test('textDocument/signatureHelp', [
         addFiles(
@@ -36,191 +24,370 @@ module.exports = (suite(
           'tupleRestParam.js',
         ),
         lspStartAndConnect(),
-        verifySignatureHelp(
-          '<PLACEHOLDER_PROJECT_URL>/nestedClasses.js',
-          {line: 4, character: 2}, // `f(|class {})`
-          JSON.stringify({
-            signatures: [
-              {label: '(x: mixed): void', parameters: [{label: 'x: mixed'}]},
-            ],
-            activeSignature: 0,
-            activeParameter: 0,
-          }),
-        ),
-        verifySignatureHelp(
-          '<PLACEHOLDER_PROJECT_URL>/nestedClasses.js',
-          {line: 4, character: 7}, // `f(class| {})`
-          JSON.stringify({
-            signatures: [
-              {label: '(x: mixed): void', parameters: [{label: 'x: mixed'}]},
-            ],
-            activeSignature: 0,
-            activeParameter: 0,
-          }),
-        ),
-        verifySignatureHelp(
-          '<PLACEHOLDER_PROJECT_URL>/nestedClasses.js',
-          {line: 4, character: 9}, // `f(class {|})`
-          '{null}',
-        ),
-        verifySignatureHelp(
-          '<PLACEHOLDER_PROJECT_URL>/nestedFunctions.js',
-          {line: 6, character: 2},
-          JSON.stringify({
-            signatures: [
-              {
-                label: '(a: string, f: F, b: number): void',
-                parameters: [
-                  {label: 'a: string'},
-                  {label: 'f: F'},
-                  {label: 'b: number'},
-                ],
-              },
-            ],
-            activeSignature: 0,
-            activeParameter: 0,
-          }),
-        ),
-        verifySignatureHelp(
-          '<PLACEHOLDER_PROJECT_URL>/nestedFunctions.js',
-          {line: 8, character: 2}, // inside nested function body
-          '{null}',
-        ),
-        verifySignatureHelp(
-          '<PLACEHOLDER_PROJECT_URL>/tupleRestParam.js',
-          {line: 6, character: 1},
-          JSON.stringify({
-            signatures: [
-              {
-                label: '(arg[0]: string, arg[1]: number): void',
-                parameters: [
-                  {label: 'arg[0]: string'},
-                  {label: 'arg[1]: number'},
-                ],
-              },
-            ],
-            activeSignature: 0,
-            activeParameter: 0,
-          }),
-        ),
-        verifySignatureHelp(
-          '<PLACEHOLDER_PROJECT_URL>/tupleRestParam.js',
-          {line: 12, character: 1},
-          JSON.stringify({
-            signatures: [
-              {
-                label: '(params[0]: string, params[1]: number): void',
-                parameters: [
-                  {label: 'params[0]: string'},
-                  {label: 'params[1]: number'},
-                ],
-              },
-            ],
-            activeSignature: 0,
-            activeParameter: 0,
-          }),
-        ),
-        verifySignatureHelp(
-          '<PLACEHOLDER_PROJECT_URL>/tupleRestParam.js',
-          {line: 18, character: 1},
-          JSON.stringify({
-            signatures: [
-              {
-                label: '(foo: string, bar: number): void',
-                parameters: [
-                  {label: 'foo: string'},
-                  {label: 'bar: number'},
-                ],
-              },
-            ],
-            activeSignature: 0,
-            activeParameter: 0,
-          }),
-        ),
-        verifySignatureHelp(
-          '<PLACEHOLDER_PROJECT_URL>/tupleRestParam.js',
-          {line: 24, character: 1},
-          JSON.stringify({
-            signatures: [
-              {
-                label: '(foo: string, bar: number): void',
-                parameters: [
-                  {label: 'foo: string'},
-                  {label: 'bar: number'},
-                ],
-              },
-            ],
-            activeSignature: 0,
-            activeParameter: 0,
-          }),
-        ),
-        verifySignatureHelp(
-          '<PLACEHOLDER_PROJECT_URL>/tupleRestParam.js',
-          {line: 30, character: 1},
-          JSON.stringify({
-            signatures: [
-              {
-                label: '(foo: string, arg[1]: number): void',
-                parameters: [
-                  {label: 'foo: string'},
-                  {label: 'arg[1]: number'},
-                ],
-              },
-            ],
-            activeSignature: 0,
-            activeParameter: 0,
-          }),
-        ),
-        verifySignatureHelp(
-          '<PLACEHOLDER_PROJECT_URL>/tupleRestParam.js',
-          {line: 36, character: 1},
-          JSON.stringify({
-            signatures: [
-              {
-                label: '(foo: string, params[1]: number): void',
-                parameters: [
-                  {label: 'foo: string'},
-                  {label: 'params[1]: number'},
-                ],
-              },
-            ],
-            activeSignature: 0,
-            activeParameter: 0,
-          }),
-        ),
-        verifySignatureHelp(
-          '<PLACEHOLDER_PROJECT_URL>/paramDocumentation.js',
-          {line: 12, character: 5},
-          JSON.stringify({
-            signatures: [
-              {
-                label: '(bar: string, baz: number): void',
-                documentation: {
-                  kind: 'markdown',
-                  value: 'foo\n\n**@unrecognized** this tag is unrecognized',
-                },
-                parameters: [
+        lspRequestAndWaitUntilResponse('textDocument/signatureHelp', {
+          textDocument: {uri: '<PLACEHOLDER_PROJECT_URL>/nestedClasses.js'},
+          position: {line: 4, character: 2}, // `f(|class {})`
+        }).verifyAllLSPMessagesInStep(
+          [
+            {
+              method: 'textDocument/signatureHelp',
+              result: {
+                signatures: [
                   {
-                    label: 'bar: string',
-                    documentation: {
-                      kind: 'markdown',
-                      value: 'bar - the first summand',
-                    },
+                    label: '(x: mixed): void',
+                    parameters: [
+                      {
+                        label: 'x: mixed',
+                      },
+                    ],
                   },
+                ],
+                activeSignature: 0,
+                activeParameter: 0,
+              },
+            },
+          ],
+          [
+            'textDocument/publishDiagnostics',
+            'window/showStatus',
+            '$/cancelRequest',
+          ],
+        ),
+        lspRequestAndWaitUntilResponse('textDocument/signatureHelp', {
+          textDocument: {uri: '<PLACEHOLDER_PROJECT_URL>/nestedClasses.js'},
+          position: {line: 4, character: 7}, // `f(class| {})`
+        }).verifyAllLSPMessagesInStep(
+          [
+            {
+              method: 'textDocument/signatureHelp',
+              result: {
+                signatures: [
                   {
-                    label: 'baz: number',
+                    label: '(x: mixed): void',
+                    parameters: [
+                      {
+                        label: 'x: mixed',
+                      },
+                    ],
+                  },
+                ],
+                activeSignature: 0,
+                activeParameter: 0,
+              },
+            },
+          ],
+          [
+            'textDocument/publishDiagnostics',
+            'window/showStatus',
+            '$/cancelRequest',
+          ],
+        ),
+        lspRequestAndWaitUntilResponse('textDocument/signatureHelp', {
+          textDocument: {uri: '<PLACEHOLDER_PROJECT_URL>/nestedClasses.js'},
+          position: {line: 4, character: 9}, // `f(class {|})`
+        }).verifyAllLSPMessagesInStep(
+          [
+            {
+              method: 'textDocument/signatureHelp',
+              result: null,
+            },
+          ],
+          [
+            'textDocument/publishDiagnostics',
+            'window/showStatus',
+            '$/cancelRequest',
+          ],
+        ),
+        lspRequestAndWaitUntilResponse('textDocument/signatureHelp', {
+          textDocument: {uri: '<PLACEHOLDER_PROJECT_URL>/nestedFunctions.js'},
+          position: {line: 6, character: 2},
+        }).verifyAllLSPMessagesInStep(
+          [
+            {
+              method: 'textDocument/signatureHelp',
+              result: {
+                signatures: [
+                  {
+                    label: '(a: string, f: F, b: number): void',
+                    parameters: [
+                      {
+                        label: 'a: string',
+                      },
+                      {
+                        label: 'f: F',
+                      },
+                      {
+                        label: 'b: number',
+                      },
+                    ],
+                  },
+                ],
+                activeSignature: 0,
+                activeParameter: 0,
+              },
+            },
+          ],
+          [
+            'textDocument/publishDiagnostics',
+            'window/showStatus',
+            '$/cancelRequest',
+          ],
+        ),
+        lspRequestAndWaitUntilResponse('textDocument/signatureHelp', {
+          textDocument: {uri: '<PLACEHOLDER_PROJECT_URL>/nestedFunctions.js'},
+          position: {line: 8, character: 2}, // inside nested function body
+        }).verifyAllLSPMessagesInStep(
+          [
+            {
+              method: 'textDocument/signatureHelp',
+              result: null,
+            },
+          ],
+          [
+            'textDocument/publishDiagnostics',
+            'window/showStatus',
+            '$/cancelRequest',
+          ],
+        ),
+        lspRequestAndWaitUntilResponse('textDocument/signatureHelp', {
+          textDocument: {uri: '<PLACEHOLDER_PROJECT_URL>/tupleRestParam.js'},
+          position: {line: 6, character: 1},
+        }).verifyAllLSPMessagesInStep(
+          [
+            {
+              method: 'textDocument/signatureHelp',
+              result: {
+                signatures: [
+                  {
+                    label: '(arg[0]: string, arg[1]: number): void',
+                    parameters: [
+                      {
+                        label: 'arg[0]: string',
+                      },
+                      {
+                        label: 'arg[1]: number',
+                      },
+                    ],
+                  },
+                ],
+                activeSignature: 0,
+                activeParameter: 0,
+              },
+            },
+          ],
+          [
+            'textDocument/publishDiagnostics',
+            'window/showStatus',
+            '$/cancelRequest',
+          ],
+        ),
+        lspRequestAndWaitUntilResponse('textDocument/signatureHelp', {
+          textDocument: {uri: '<PLACEHOLDER_PROJECT_URL>/tupleRestParam.js'},
+          position: {line: 12, character: 1},
+        }).verifyAllLSPMessagesInStep(
+          [
+            {
+              method: 'textDocument/signatureHelp',
+              result: {
+                signatures: [
+                  {
+                    label: '(params[0]: string, params[1]: number): void',
+                    parameters: [
+                      {
+                        label: 'params[0]: string',
+                      },
+                      {
+                        label: 'params[1]: number',
+                      },
+                    ],
+                  },
+                ],
+                activeSignature: 0,
+                activeParameter: 0,
+              },
+            },
+          ],
+          [
+            'textDocument/publishDiagnostics',
+            'window/showStatus',
+            '$/cancelRequest',
+          ],
+        ),
+        lspRequestAndWaitUntilResponse('textDocument/signatureHelp', {
+          textDocument: {uri: '<PLACEHOLDER_PROJECT_URL>/tupleRestParam.js'},
+          position: {line: 18, character: 1},
+        }).verifyAllLSPMessagesInStep(
+          [
+            {
+              method: 'textDocument/signatureHelp',
+              result: {
+                signatures: [
+                  {
+                    label: '(foo: string, bar: number): void',
+                    parameters: [
+                      {
+                        label: 'foo: string',
+                      },
+                      {
+                        label: 'bar: number',
+                      },
+                    ],
+                  },
+                ],
+                activeSignature: 0,
+                activeParameter: 0,
+              },
+            },
+          ],
+          [
+            'textDocument/publishDiagnostics',
+            'window/showStatus',
+            '$/cancelRequest',
+          ],
+        ),
+        lspRequestAndWaitUntilResponse('textDocument/signatureHelp', {
+          textDocument: {uri: '<PLACEHOLDER_PROJECT_URL>/tupleRestParam.js'},
+          position: {line: 24, character: 1},
+        }).verifyAllLSPMessagesInStep(
+          [
+            {
+              method: 'textDocument/signatureHelp',
+              result: {
+                signatures: [
+                  {
+                    label: '(foo: string, bar: number): void',
+                    parameters: [
+                      {
+                        label: 'foo: string',
+                      },
+                      {
+                        label: 'bar: number',
+                      },
+                    ],
+                  },
+                ],
+                activeSignature: 0,
+                activeParameter: 0,
+              },
+            },
+          ],
+          [
+            'textDocument/publishDiagnostics',
+            'window/showStatus',
+            '$/cancelRequest',
+          ],
+        ),
+        lspRequestAndWaitUntilResponse('textDocument/signatureHelp', {
+          textDocument: {uri: '<PLACEHOLDER_PROJECT_URL>/tupleRestParam.js'},
+          position: {line: 30, character: 1},
+        }).verifyAllLSPMessagesInStep(
+          [
+            {
+              method: 'textDocument/signatureHelp',
+              result: {
+                signatures: [
+                  {
+                    label: '(foo: string, arg[1]: number): void',
+                    parameters: [
+                      {
+                        label: 'foo: string',
+                      },
+                      {
+                        label: 'arg[1]: number',
+                      },
+                    ],
+                  },
+                ],
+                activeSignature: 0,
+                activeParameter: 0,
+              },
+            },
+          ],
+          [
+            'textDocument/publishDiagnostics',
+            'window/showStatus',
+            '$/cancelRequest',
+          ],
+        ),
+        lspRequestAndWaitUntilResponse('textDocument/signatureHelp', {
+          textDocument: {uri: '<PLACEHOLDER_PROJECT_URL>/tupleRestParam.js'},
+          position: {line: 36, character: 1},
+        }).verifyAllLSPMessagesInStep(
+          [
+            {
+              method: 'textDocument/signatureHelp',
+              result: {
+                signatures: [
+                  {
+                    label: '(foo: string, params[1]: number): void',
+                    parameters: [
+                      {
+                        label: 'foo: string',
+                      },
+                      {
+                        label: 'params[1]: number',
+                      },
+                    ],
+                  },
+                ],
+                activeSignature: 0,
+                activeParameter: 0,
+              },
+            },
+          ],
+          [
+            'textDocument/publishDiagnostics',
+            'window/showStatus',
+            '$/cancelRequest',
+          ],
+        ),
+        lspRequestAndWaitUntilResponse('textDocument/signatureHelp', {
+          textDocument: {
+            uri: '<PLACEHOLDER_PROJECT_URL>/paramDocumentation.js',
+          },
+          position: {line: 12, character: 5},
+        }).verifyAllLSPMessagesInStep(
+          [
+            {
+              method: 'textDocument/signatureHelp',
+              result: {
+                signatures: [
+                  {
+                    label: '(bar: string, baz: number): void',
                     documentation: {
                       kind: 'markdown',
                       value:
-                        'baz - the second and third summands\nbaz.x (optional)  - the second summand\nbaz.y (optional, defaults to 0)  - the third summand',
+                        'foo\n\n**@unrecognized** this tag is unrecognized',
                     },
+                    parameters: [
+                      {
+                        label: 'bar: string',
+                        documentation: {
+                          kind: 'markdown',
+                          value: 'bar - the first summand',
+                        },
+                      },
+                      {
+                        label: 'baz: number',
+                        documentation: {
+                          kind: 'markdown',
+                          value:
+                            'baz - the second and third summands\nbaz.x (optional)  - the second summand\nbaz.y (optional, defaults to 0)  - the third summand',
+                        },
+                      },
+                    ],
                   },
                 ],
+                activeSignature: 0,
+                activeParameter: 0,
               },
-            ],
-            activeSignature: 0,
-            activeParameter: 0,
-          }),
+            },
+          ],
+          [
+            'textDocument/publishDiagnostics',
+            'window/showStatus',
+            '$/cancelRequest',
+          ],
         ),
       ]),
     ];
