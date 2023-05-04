c1676a8372a40643736a928b64ae8b1b1959fa46
George Zahariev <gkz@meta.com>
2023-05-04 10:00:01 -0700

    Move type at post tests for opaque types to use queries_in_file
    Summary:
    Move type at post tests for opaque types to use queries_in_file.
    
    Changelog: [internal]
    
    Reviewed By: panagosg7
    
    Differential Revision: D45550346
    
    fbshipit-source-id: 8cf22591a52e4970de276ab6f85a71506c103547

--- tests/type_at_pos_types/opaque.js
+++ tests/type_at_pos_types/opaque.js
@@ -1,24 +1,62 @@
 // @flow
 
 export opaque type Foo = number;
+//                 ^
+
 declare var foo: Foo;
+//                ^
+foo;
+//^
 
 declare opaque type Bar: number;
+//                   ^
+
 declare var bar: Bar;
+//               ^
+bar;
+//^
 
 declare opaque type Baz;
+//                   ^
+
 declare var baz: Baz;
+//               ^
+baz;
+//^
 
 opaque type Bak<A> = number | A;
+//           ^
+
 declare var bak: Bak<string>;
+//                ^
+bak;
+//^
 
 declare opaque type Bam<A>
+//                   ^
+
 declare var bam: Bam<string>;
+//                ^
+bam;
+//^
 
 import type { Opaque, PolyTransparent, PolyOpaque } from './opaque-lib';
+
 declare var opaque: Opaque;
+//                   ^
+opaque;
+// ^
+
 declare var polyTransparent: PolyTransparent<string>;
+//                               ^
+polyTransparent;
+// ^
+
 declare var polyOpaque: PolyOpaque<string>;
+//                        ^
+polyOpaque;
+// ^
 
 import { fOpaque } from './opaque-lib';
 const o = fOpaque();
+//    ^
