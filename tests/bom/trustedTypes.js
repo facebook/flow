30855cad7ee6e6117b2b495d005ee95e1a11f9b4
Vincent Lee <vincentlee@meta.com>
2023-05-19 16:05:16 -0700

    Add Trusted Types stubs to bom.js
    Summary:
    We're working on rolling out Trusted Types, a security feature that helps prevent DOM XSS.
    It would be nice to have the types be a part of Flow instead of needing to carry our own stubs around, so this diff does just that.
    
    The types are specified [here](https://w3c.github.io/trusted-types/dist/spec/#trusted-types), with a more human-readable version [here](https://developer.mozilla.org/en-US/docs/Web/API/Trusted_Types_API).
    
    Reviewed By: panagosg7
    
    Differential Revision: D46007012
    
    fbshipit-source-id: 06541f998a5f9417793be3920772003eaa53468b

---
+++ tests/bom/trustedTypes.js
@@ -0,0 +1,28 @@
+/* @flow */
+
+declare var trustedTypes: TrustedTypePolicyFactory;
+
+const policy: TrustedTypePolicy = trustedTypes.createPolicy('insecurePassthroughPolicy', {
+  createHTML: s => s,
+  createScript: s => s,
+});
+
+const html: TrustedHTML = policy.createHTML("<what></what>");
+const script: TrustedScript = policy.createScript("<what></what>");
+const script_url: TrustedScriptURL = policy.createScriptURL("<what></what>");
+
+// Check that type predicates refine the types
+const html_mixed: mixed = html;
+if (trustedTypes.isHTML(html_mixed)) {
+  (html_mixed: TrustedHTML);
+}
+
+const script_mixed: mixed = script;
+if (trustedTypes.isScript(script_mixed)) {
+  (script_mixed: TrustedScript);
+}
+
+const script_url_mixed: mixed = script_url;
+if (trustedTypes.isScriptURL(script_url_mixed)) {
+  (script_url_mixed: TrustedScriptURL);
+}
