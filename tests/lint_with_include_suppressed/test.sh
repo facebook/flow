fe42c21bc999f7ea2c5f721dd5b5d35fa25009b1
Vincent Lee <vincentlee@meta.com>
2023-05-26 14:03:54 -0700

    Add TrustedTypes sink annotations to bom.js and dom.js
    Summary:
    As a follow up to D46007012, this diff modifies the type declarations of XSS DOM sinks to accept Trusted Type objects as well.
    
    Annoyingly, the spec does not fully enumerate the list of sensitive surfaces. I took some hints from https://github.com/microsoft/TypeScript-DOM-lib-generator/pull/1246/ and manual inspection, but left out some of the more ambiguous hints.
    
    Changelog: [new] Updated dom libdefs to allow Trusted Type objects
    
    Reviewed By: SamChou19815
    
    Differential Revision: D46085621
    
    fbshipit-source-id: d10bf667849560319ad69edc639090a9ddd35f9f

--- tests/lint_with_include_suppressed/test.sh
+++ tests/lint_with_include_suppressed/test.sh
@@ -7,4 +7,4 @@
 printf "Without --include-suppressed:\n"
 assert_ok "$FLOW" check
 printf "With --include-suppressed:\n"
-assert_errors "$FLOW" check --include-suppressed
+assert_errors "$FLOW" check --include-suppressed --strip-root
