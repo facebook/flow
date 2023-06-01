0b5c699de410aa08767bc5db28680d31730f2a10
Jordan Brown <jmbrown@meta.com>
2023-06-01 13:47:30 -0700

    [react] Add support for React's second type argument to React.Element
    Summary: Title! No reason to hide the second type argument in React.Element.
    
    Reviewed By: NicolasRoelandt
    
    Differential Revision:
    D46362435
    
    ------------------------------------------------------------------------
    (from 0adfb01230e04e715986db699f2100759e6c076d)
    
    fbshipit-source-id: 07471b451d0cc3de6fc1812d14b01378a70128e2

--- tests/react/element_with_component_type.js
+++ tests/react/element_with_component_type.js
@@ -4,3 +4,6 @@ class Foo extends React.Component<{a: number}> {}
 
 (<Foo a={42}/>: React.Element<React.ComponentType<{a: number}>>); // OK
 (<Foo a={42}/>: React.Element<React.ComponentType<{b: number}>>); // Error
+
+// React.Element can support 2 type arguments, like React$Element
+type El = React.Element<any, any>; // No errors!
