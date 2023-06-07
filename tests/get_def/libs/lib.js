054e72dabc1f8f1cbe6035e6283a262603f14726
Sam Zhou <samzhou19815@meta.com>
2023-06-06 18:17:24 -0700

    [get-def] Add more require-related get-def tests
    Summary:
    The added tests demonstrates the existing undesirable behavior that get-def for requires don't jump to names.
    
    Changelog: [internal]
    
    Reviewed By: samwgoldman
    
    Differential Revision: D46410788
    
    fbshipit-source-id: fa91c3c1321e9ac79558d98e9676a56d72f81dde

--- tests/get_def/libs/lib.js
+++ tests/get_def/libs/lib.js
@@ -1,5 +1,13 @@
 declare module 'test_lib' {
   declare export var foo: string;
+  declare export var bar: {baz: string};
+}
+
+declare module 'test_lib_cjs' {
+  declare module.exports: {
+    foo: string,
+    bar: {baz: string},
+  }
 }
 
 declare module 'Unknown_LibDeclared' {
--- tests/get_def/require.js
+++ tests/get_def/require.js
@@ -11,3 +11,41 @@ require("Untyped");
 
 require("Untyped_LibDeclared");
 //       ^
+
+{
+const {foo} = require('test_lib');
+//      ^
+const {foo: fooo} = require('test_lib');
+//      ^
+const {foo: boo} = require('test_lib');
+//           ^
+const {bar} = require('test_lib');
+//      ^
+const {bar: baaaar} = require('test_lib');
+//      ^
+const {bar: far} = require('test_lib');
+//           ^
+const {bar: {baz}} = require('test_lib');
+//            ^
+const {bar: {baz: boz}} = require('test_lib');
+//                 ^
+}
+
+{
+const {foo} = require('test_lib_cjs');
+//      ^
+const {foo: fooo} = require('test_lib_cjs');
+//      ^
+const {foo: boo} = require('test_lib_cjs');
+//           ^
+const {bar} = require('test_lib_cjs');
+//      ^
+const {bar: baaaar} = require('test_lib_cjs');
+//      ^
+const {bar: far} = require('test_lib_cjs');
+//           ^
+const {bar: {baz}} = require('test_lib_cjs');
+//            ^
+const {bar: {baz: boz}} = require('test_lib_cjs');
+//                 ^
+}
