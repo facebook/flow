804e4fc444eea9725300e365544f81821a358264
Sam Zhou <samzhou19815@meta.com>
2023-06-09 08:13:24 -0700

    [get-def] Rewrite tests to enclose separate tests with functions
    Summary:
    I didn't realize that type aliases are function scoped. We should probably fix that...
    
    Changelog: [internal]
    
    Reviewed By: samwgoldman
    
    Differential Revision: D46581653
    
    fbshipit-source-id: 7a4f1888c78ed81d843eb6c7f67def035b1bd45e

--- tests/get_def2/utilities.js
+++ tests/get_def2/utilities.js
@@ -2,49 +2,49 @@ import * as React from 'react';
 
 type SourceObj = { foo: string, bar: number };
 
-{
+let f = () => {
   declare const Comp: React.ComponentType<SourceObj>;
   <Comp foo={''} bar={3} />;
 //       ^
 }
 
-{
+f = () => {
   declare const Comp: React.ComponentType<$ReadOnly<SourceObj>>;
   <Comp foo={''} bar={3} />;
 //       ^
 }
 
-{
+f = () => {
   declare const Comp: React.ComponentType<$Diff<SourceObj, {bar: number}>>;
   <Comp foo={''} />;
 //       ^
 }
 
-{
+f = () => {
   declare const Comp: React.ComponentType<$Rest<SourceObj, {bar: number}>>;
   <Comp foo={''} />;
 //       ^
 }
 
-{
+f = () => {
   declare const Comp: React.ComponentType<$ObjMap<SourceObj, ((string)=>number) & ((number)=>string)>>;
   <Comp foo={3} bar={''} />;
 //       ^
 }
 
-{
+f = () => {
   declare const Comp: React.ComponentType<$ObjMapConst<SourceObj, string>>;
   <Comp foo={''} bar={''} />;
 //       ^
 }
 
-{
+f = () => {
   declare const Comp: React.ComponentType<{+[key in keyof SourceObj]: SourceObj[key]}>;
   <Comp foo={''} bar={3} />;
 //       ^
 }
 
-{
+f = () => {
   declare const BaseComp: React.ComponentType<SourceObj>;
   type $RelayProps<T> = $ObjMap<
     $Diff<T, {relayProp: string | void}>,
@@ -56,42 +56,42 @@ type SourceObj = { foo: string, bar: number };
 //       ^
 }
 
-{
+f = () => {
   type MapType<T> = $ReadOnly<T>
   declare const Comp: React.ComponentType<MapType<SourceObj>>;
   <Comp foo={''} bar={3} />;
 //       ^
 }
 
-{
+f = () => {
   type MapType<T> = $Diff<T, {bar: number}>;
   declare const Comp: React.ComponentType<MapType<SourceObj>>;
   <Comp foo={''} />;
 //       ^
 }
 
-{
+f = () => {
   type MapType<T> = $Rest<T, {bar: number}>;
   declare const Comp: React.ComponentType<MapType<SourceObj>>;
   <Comp foo={''} />;
 //       ^
 }
 
-{
+f = () => {
   type MapType<T> = $ObjMap<T, ((string)=>number) & ((number)=>string)>
   declare const Comp: React.ComponentType<MapType<SourceObj>>;
   <Comp foo={3} bar={''} />;
 //       ^
 }
 
-{
+f = () => {
   type MapType<T> = $ObjMapConst<T, string>
   declare const Comp: React.ComponentType<MapType<SourceObj>>;
   <Comp foo={''} bar={''} />;
 //       ^
 }
 
-{
+f = () => {
   type MapType<T> = {+[key in keyof T]: SourceObj[key]};
   declare const Comp: React.ComponentType<MapType<SourceObj>>;
   <Comp foo={''} bar={3} />;
