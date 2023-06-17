385c91963137ce3634d20750f8542ad912431096
Marshall Roch <mroch@meta.com>
2023-06-16 19:20:34 -0700

    add test for autocomplete keyword sorting
    Summary: demonstrates current sorting of keywords starting with `c`
    
    Reviewed By: SamChou19815
    
    Differential Revision: D46811662
    
    fbshipit-source-id: dd9cecb713aabb42531eab85d1af08b8dba84aee

--- tests/autocomplete/test.sh
+++ tests/autocomplete/test.sh
@@ -149,3 +149,5 @@ query_at_pos autocomplete "jsdoc_comment.js" 8 4 --lsp
 query_at_pos autocomplete "jsdoc_comment.js" 11 4 --lsp
 queries_in_file autocomplete "super_member.js" --pretty
 queries_in_file autocomplete "switch.js" --pretty
+queries_in_file autocomplete "keywords_c.js" --pretty
+queries_in_file autocomplete "keywords_f.js" --pretty
