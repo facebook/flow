42d8088aaa189333de29a44f2a6be9571b233180
Marshall Roch <mroch@meta.com>
2023-06-27 10:10:30 -0700

    update license for code derived from OCaml
    Summary:
    flow_map.ml and flow_set.ml are derived from OCaml and are LGPLv2.1 with a special static linking exception.
    
    this diff copies https://github.com/ocaml/ocaml/blob/4.14/LICENSE into our LICENSE file
    
    Reviewed By: zertosh, bigfootjon
    
    Differential Revision: D44168840
    
    fbshipit-source-id: bd133e307055487e323f654b9190b1f4b1960561

--- src/hack_forked/utils/collections/third-party/flow_set.ml
+++ src/hack_forked/utils/collections/third-party/flow_set.ml
@@ -1,15 +1,17 @@
-(***********************************************************************)
-(*                                                                     *)
-(*                           Objective Caml                            *)
-(*                                                                     *)
-(*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         *)
-(*                                                                     *)
-(*  Copyright 1996 Institut National de Recherche en Informatique et   *)
-(*  en Automatique.  All rights reserved.  This file is distributed    *)
-(*  under the terms of the GNU Library General Public License, with    *)
-(*  the special exception on linking described in file LICENSE.        *)
-(*                                                                     *)
-(***********************************************************************)
+(* Portions Copyright (c) Meta Platforms, Inc. and affiliates. *)
+
+(***********************************************************************
+ *                                                                     *
+ *                           Objective Caml                            *
+ *                                                                     *
+ *            Xavier Leroy, projet Cristal, INRIA Rocquencourt         *
+ *                                                                     *
+ *  Copyright 1996 Institut National de Recherche en Informatique et   *
+ *  en Automatique.  All rights reserved.  This file is distributed    *
+ *  under the terms of the GNU Library General Public License, with    *
+ *  the special exception on linking described in file LICENSE.        *
+ *                                                                     *
+ ***********************************************************************)
 
 (* This module has been inspired from the OCaml standard library.
  * There are some modifications to make it run fast.
