5edad1c215bcb27afd6732a1e88c2fd75f64e074
Jordan Brown <jmbrown@meta.com>
2023-07-11 12:27:05 -0700

    [EZ] Fix error in Abstract Component tests
    Summary:
    This line should not have had 3 type arguments
    
    Changelog: [internal]
    
    Reviewed By: gkz
    
    Differential Revision: D47372915
    
    fbshipit-source-id: 116f617280ec285805121bd8ad2c4a5df822469e

--- tests/react_abstract_component/classes_lower.js
+++ tests/react_abstract_component/classes_lower.js
@@ -12,7 +12,7 @@ class ComponentNarrower extends React.Component<{foo: number, bar: number}> {
   static defaultProps: { foo: number } = {foo: 3};
 }
 
-(ComponentNarrower: React$AbstractComponent<any, any, any>);
+(ComponentNarrower: React$AbstractComponent<any, any>);
 (ComponentNarrower: React$AbstractComponent<{+foo?: number, +bar: number}, any>);
 (ComponentNarrower: React$AbstractComponent<{}, any>); // Error missing foo and bar in config
 (ComponentNarrower: React$AbstractComponent<{+foo?: number}, any>); // Error missing bar in config
