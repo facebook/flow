dedc1c66779ddcb2d818175a918262ae3ce39adf
George Zahariev <gkz@meta.com>
2023-08-12 11:52:46 -0700

    [flow][try] Set suppress types in Try Flow to different suppression types
    Summary:
    Previously, no suppression types were allowed in Try Flow.
    Rather than make this a configurable setting which I don't think is super useful, just make the default the 4 types of suppressions we have.
    
    Changelog: [internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D48285055
    
    fbshipit-source-id: f1683799436db6e32ce6a5e2237ff788890feed3

--- src/flow_dot_js.ml
+++ src/flow_dot_js.ml
@@ -150,7 +150,7 @@ let stub_metadata ~root ~checked =
     strict_es6_import_export = false;
     strict_es6_import_export_excludes = [];
     strip_root = true;
-    suppress_types = SSet.empty;
+    suppress_types = SSet.of_list ["$FlowFixMe"; "$FlowIssue"; "$FlowIgnore"; "$FlowExpectedError"];
     trust_mode = Options.NoTrust;
     use_mixed_in_catch_variables = false;
   }
