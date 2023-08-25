0f5f9825f2b51f8b2b5dc2937e2ee6cd9be6ea0f
Sam Zhou <samzhou19815@meta.com>
2023-08-25 16:15:22 -0700

    [flow][tests] Change `incremental_duplicate_delete` to use haste name reducers
    Summary:
    It doesn't help a lot to test the providesModule behavior, since it's rarely used. This diff switches it to use name reducer, so that providesModule can be killed in the future.
    
    We keep testing the same behavior by replacing the providesModule change with rename change.
    
    Changelog: [internal]
    
    Reviewed By: samwgoldman
    
    Differential Revision: D48666985
    
    fbshipit-source-id: be9c9b82823fb5813113c6c6aa6576be357ebdd2

--- tests/incremental_duplicate_delete/test.sh
+++ tests/incremental_duplicate_delete/test.sh
@@ -7,9 +7,11 @@
 printf "\nInitial status:\n"
 assert_ok "$FLOW" status --no-auto-start --strip-root .
 
-printf "\nCopy A.js to B.js:\n"
-cp A.js B.js
-assert_ok "$FLOW" force-recheck --no-auto-start B.js
+mkdir nested
+
+printf "\nCopy A.js to nested/A.js:\n"
+cp A.js nested/A.js
+assert_ok "$FLOW" force-recheck --no-auto-start nested/A.js
 assert_errors "$FLOW" status --no-auto-start --strip-root .
 
 printf "\nDelete A.js:\n"
@@ -17,9 +19,11 @@ rm A.js
 assert_ok "$FLOW" force-recheck --no-auto-start A.js
 assert_ok "$FLOW" status --no-auto-start --strip-root .
 
-printf "\nMove B.js to A.js:\n"
-mv B.js A.js
-assert_ok "$FLOW" force-recheck --no-auto-start A.js B.js
+printf "\nMove nested/A.js to A.js:\n"
+mv nested/A.js A.js
+assert_ok "$FLOW" force-recheck --no-auto-start A.js nested/A.js
 assert_ok "$FLOW" status --no-auto-start --strip-root .
+
+rm -rf nested
 
 printf "\nDone!\n"
