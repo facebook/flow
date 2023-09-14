bd36e33ab8aeaae3d16279b5907fc2c5c69fc6de
Panagiotis Vekris <pvekris@meta.com>
2023-09-14 12:21:40 -0700

    [flow] fix unit test added in D49247643
    Summary:
    the unit test added in D49247643 had syntax & type errors
    
    Changelog: [internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D49275743
    
    fbshipit-source-id: eaef2f2e342f27e4aba6d68252f1c6ffabb22eed

--- tests/dump_types/elem_call.js
+++ tests/dump_types/elem_call.js
@@ -1,13 +1,13 @@
 declare opaque type T;
 declare var key: string;
 declare var dict0: { [key: string]: () => T, ... };
-dict0[key]("");
+dict0[key]();
 
 declare var dict1: ?{ [key: string]: () => T, ... };
-dict1?.[key]("");
+dict1?.[key]();
 
 declare var dict2: ?{ [key: string]: () => T, ... };
-dict2?.[key]?.("");
+dict2?.[key]?.();
 
 declare var dict3: { [key: string]: <T>(T) => T, ... };
-dict3.[key].("");
+dict3[key]("");
