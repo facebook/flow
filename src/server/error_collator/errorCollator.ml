f44de49f5733816f9254e87084118be66d389254
Panagiotis Vekris <pvekris@meta.com>
2023-10-10 16:53:24 -0700

    [flow] refactor errorCollator.ml
    Summary:
    Pull out functions that will be used by both full and incremental collation in the next diff.
    
    Changelog: [internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision:
    D49944182
    
    ------------------------------------------------------------------------
    (from 670f465df95bb42d93779f2d8ec5902267f3ebf1)
    
    fbshipit-source-id: 7ae53a6f54ab390e6965fd877bccee1409d7139d

--- src/server/error_collator/errorCollator.ml
+++ src/server/error_collator/errorCollator.ml
@@ -8,6 +8,80 @@
 open ServerEnv
 open Utils_js
 
+let add_suppression_warnings root checked unused warnings =
+  let open Flow_errors_utils in
+  (* For each unused suppression, create an warning *)
+  let all_locs = Error_suppressions.all_unused_locs unused in
+  let warnings =
+    Loc_collections.LocSet.fold
+      (fun loc warnings ->
+        let source_file =
+          match Loc.source loc with
+          | Some x -> x
+          | None -> File_key.SourceFile "-"
+        in
+        (* In lazy mode, dependencies are modules which we typecheck not because we care about
+         * them, but because something important (a focused file or a focused file's dependent)
+         * needs these dependencies. Therefore, we might not typecheck a dependency's dependents.
+         *
+         * This means there might be an unused suppression comment warning in a dependency which
+         * only shows up in lazy mode. To avoid this, we'll just avoid raising this kind of
+         * warning in any dependency. *)
+        if not (CheckedSet.mem_dependency source_file checked) then
+          let err =
+            let msg = Error_message.EUnusedSuppression loc in
+            Flow_error.error_of_msg ~trace_reasons:[] ~source_file msg
+            |> Flow_error.make_error_printable Fun.id ~strip_root:(Some root)
+          in
+          let file_warnings =
+            FilenameMap.find_opt source_file warnings
+            |> Base.Option.value ~default:ConcreteLocPrintableErrorSet.empty
+            |> ConcreteLocPrintableErrorSet.add err
+          in
+          FilenameMap.add source_file file_warnings warnings
+        else
+          warnings)
+      all_locs
+      warnings
+  in
+  Error_suppressions.CodeLocSet.fold
+    (fun (code, loc) warnings ->
+      let source_file =
+        match Loc.source loc with
+        | Some x -> x
+        | None -> File_key.SourceFile "-"
+      in
+      let err =
+        Error_message.ECodelessSuppression (loc, code)
+        |> Flow_error.error_of_msg ~trace_reasons:[] ~source_file
+        |> Flow_error.make_error_printable Fun.id ~strip_root:(Some root)
+      in
+      let file_warnings =
+        FilenameMap.find_opt source_file warnings
+        |> Base.Option.value ~default:ConcreteLocPrintableErrorSet.empty
+        |> ConcreteLocPrintableErrorSet.add err
+      in
+      FilenameMap.add source_file file_warnings warnings)
+    (Error_suppressions.universally_suppressed_codes unused)
+    warnings
+
+let collate_duplicate_providers ~update root =
+  let pos = Loc.{ line = 1; column = 0 } in
+  let f module_name (provider_file, provider) acc duplicate =
+    let conflict = Loc.{ source = Some duplicate; start = pos; _end = pos } in
+    let err =
+      Error_message.EDuplicateModuleProvider { module_name; provider; conflict }
+      |> Flow_error.error_of_msg ~trace_reasons:[] ~source_file:duplicate
+      |> Flow_error.make_error_printable Fun.id ~strip_root:(Some root)
+    in
+    update provider_file duplicate err acc
+  in
+  let f module_name (provider_file, duplicates) acc =
+    let provider = Loc.{ source = Some provider_file; start = pos; _end = pos } in
+    Nel.fold_left (f module_name (provider_file, provider)) acc duplicates
+  in
+  SMap.fold f
+
 (* combine error maps into a single error set and a filtered warning map
  *
  * This can be a little expensive for large repositories and can take a couple of seconds.
@@ -25,62 +99,6 @@ let regenerate ~reader ~options =
   let open Flow_errors_utils in
   let root = Options.root options in
   let loc_of_aloc = Parsing_heaps.Reader.loc_of_aloc ~reader in
-  let add_suppression_warnings checked unused warnings =
-    (* For each unused suppression, create an warning *)
-    let all_locs = Error_suppressions.all_unused_locs unused in
-    let warnings =
-      Loc_collections.LocSet.fold
-        (fun loc warnings ->
-          let source_file =
-            match Loc.source loc with
-            | Some x -> x
-            | None -> File_key.SourceFile "-"
-          in
-          (* In lazy mode, dependencies are modules which we typecheck not because we care about
-           * them, but because something important (a focused file or a focused file's dependent)
-           * needs these dependencies. Therefore, we might not typecheck a dependency's dependents.
-           *
-           * This means there might be an unused suppression comment warning in a dependency which
-           * only shows up in lazy mode. To avoid this, we'll just avoid raising this kind of
-           * warning in any dependency. *)
-          if not (CheckedSet.mem_dependency source_file checked) then
-            let err =
-              let msg = Error_message.EUnusedSuppression loc in
-              Flow_error.error_of_msg ~trace_reasons:[] ~source_file msg
-              |> Flow_error.make_error_printable Fun.id ~strip_root:(Some root)
-            in
-            let file_warnings =
-              FilenameMap.find_opt source_file warnings
-              |> Base.Option.value ~default:ConcreteLocPrintableErrorSet.empty
-              |> ConcreteLocPrintableErrorSet.add err
-            in
-            FilenameMap.add source_file file_warnings warnings
-          else
-            warnings)
-        all_locs
-        warnings
-    in
-    Error_suppressions.CodeLocSet.fold
-      (fun (code, loc) warnings ->
-        let source_file =
-          match Loc.source loc with
-          | Some x -> x
-          | None -> File_key.SourceFile "-"
-        in
-        let err =
-          Error_message.ECodelessSuppression (loc, code)
-          |> Flow_error.error_of_msg ~trace_reasons:[] ~source_file
-          |> Flow_error.make_error_printable Fun.id ~strip_root:(Some root)
-        in
-        let file_warnings =
-          FilenameMap.find_opt source_file warnings
-          |> Base.Option.value ~default:ConcreteLocPrintableErrorSet.empty
-          |> ConcreteLocPrintableErrorSet.add err
-        in
-        FilenameMap.add source_file file_warnings warnings)
-      (Error_suppressions.universally_suppressed_codes unused)
-      warnings
-  in
   let acc_fun
       (type a)
       ~options
@@ -104,28 +122,16 @@ let regenerate ~reader ~options =
     let suppressed = List.rev_append file_suppressed suppressed in
     (errors, suppressed, unused)
   in
-  let collate_duplicate_providers =
-    let pos = Loc.{ line = 1; column = 0 } in
-    let f module_name provider acc duplicate =
-      let conflict = Loc.{ source = Some duplicate; start = pos; _end = pos } in
-      let err =
-        Error_message.EDuplicateModuleProvider { module_name; provider; conflict }
-        |> Flow_error.error_of_msg ~trace_reasons:[] ~source_file:duplicate
-        |> Flow_error.make_error_printable Fun.id ~strip_root:(Some root)
-      in
-      Flow_errors_utils.ConcreteLocPrintableErrorSet.add err acc
-    in
-    let f module_name (provider, duplicates) acc =
-      let provider = Loc.{ source = Some provider; start = pos; _end = pos } in
-      Nel.fold_left (f module_name provider) acc duplicates
-    in
-    SMap.fold f
-  in
+
   fun env ->
     MonitorRPC.status_update ~event:ServerStatus.Collating_errors_start;
     let { local_errors; duplicate_providers; merge_errors; warnings; suppressions } = env.errors in
     let collated_errorset =
-      collate_duplicate_providers duplicate_providers ConcreteLocPrintableErrorSet.empty
+      collate_duplicate_providers
+        ~update:(fun _ _ -> Flow_errors_utils.ConcreteLocPrintableErrorSet.add)
+        root
+        duplicate_providers
+        ConcreteLocPrintableErrorSet.empty
     in
     let acc_err_fun = acc_fun ~options suppressions (fun _ -> ConcreteLocPrintableErrorSet.union) in
     let (collated_errorset, collated_suppressed_errors, unused) =
@@ -138,7 +144,7 @@ let regenerate ~reader ~options =
       (FilenameMap.empty, collated_suppressed_errors, unused)
       |> FilenameMap.fold acc_warn_fun warnings
     in
-    let collated_warning_map = add_suppression_warnings env.checked_files unused warnings in
+    let collated_warning_map = add_suppression_warnings root env.checked_files unused warnings in
     { collated_errorset; collated_warning_map; collated_suppressed_errors }
 
 let get_with_separate_warnings_no_profiling ~reader ~options env =
