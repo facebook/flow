9866cfbc2c2cca1a9966903904a30cfce2e0b896
Sam Zhou <samzhou19815@meta.com>
2023-10-23 14:21:54 -0700

    [flow][autocomplete][render-types] Add test demonstrating that autocomplete doesn't quite work for component syntax components
    Summary: Changelog: [internal]
    
    Reviewed By: jbrown215
    
    Differential Revision: D50568042
    
    fbshipit-source-id: 109bdc0ebec69248dc608ce7049d17c96dba892a

--- tests/autocomplete/jsx4.js
+++ tests/autocomplete/jsx4.js
@@ -4,5 +4,8 @@ class Complementarity {}
 const Computerization = (props: empty) => {};
 function Compressibility(props: empty) {}
 
+declare component ComponentSyntaxFoo();
+declare component ComponentSyntaxBar() renders ComponentSyntaxFoo;
+
 <C // request autocomplete on JSX name
 //^
