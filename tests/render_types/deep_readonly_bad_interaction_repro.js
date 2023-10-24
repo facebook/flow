2b7dcf482fa11a44c6783281a555bc3a25a3e800
Sam Zhou <samzhou19815@meta.com>
2023-10-24 16:24:27 -0700

    [flow][render-types][tests] Add repro test for bad interaction with deep readonly
    Summary: Changelog: [internal]
    
    Reviewed By: jbrown215
    
    Differential Revision: D50617791
    
    fbshipit-source-id: bc9951525c87e528bae51ef46ffe6219595d3f1e

---
+++ tests/render_types/deep_readonly_bad_interaction_repro.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+declare component Base();
+
+declare component Wrapper(label: string | renders Base);
+
+component UseWrapper(
+  label: string | renders Base
+) {
+  return <Wrapper label={label} />; // spurious error
+}
