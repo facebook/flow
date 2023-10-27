10738502326504d5070626929f102885d1a51f7a
Sam Zhou <samzhou19815@meta.com>
2023-10-27 13:04:55 -0700

    [flow][normalizer][tests] Repro for broken normalizer for element shorthand typeapp
    Summary: Changelog: [internal]
    
    Reviewed By: jbrown215
    
    Differential Revision: D50747309
    
    fbshipit-source-id: ce971479e911d1b5bc3595f01c4e5147b0a32f1a

--- tests/type_at_pos_reactscript/component.js
+++ tests/type_at_pos_reactscript/component.js
@@ -6,3 +6,9 @@ component Baz(foo: number) renders? number { return 3; }
 //        ^
 component Qux(bar: string) renders* number { return 3; }
 //        ^
+component Poly<T> (foo: T) {return null};
+//        ^
+type PolyElementTypeApp = Poly<string>;
+//        ^
+type ConcretizedPolyComponent = Poly<string> extends React$Element<infer C> ? C : empty;
+//        ^
