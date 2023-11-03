b68a785ea026fdb0b3cb31ada4adeab082c3a194
Sam Goldman <samgoldman@meta.com>
2023-11-03 09:56:38 -0700

    Add `tool ping` command
    Summary:
    This command connects to a running lsp process and sends `telemetry/ping`
    repeatedly. We use this to measure server availability.
    
    Reviewed By: panagosg7
    
    Differential Revision: D50911742
    
    fbshipit-source-id: b9e0d212f5240cd9a85e226f2006848d5d8b3b7a

---
+++ packages/flow-dev-tools/src/ping/pingCommand.js
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import type {Flag} from '../command/Base';
+
+const child_process = require('child_process');
+const rpc = require('vscode-jsonrpc');
+const uri = require('vscode-uri');
+
+const {performance} = require('perf_hooks');
+const path = require('path');
+
+const {commonFlags, default: Base} = require('../command/Base');
+const findFlowBin = require('../command/findFlowBin').default;
+
+type Args = {
+  bin: string,
+  root: string,
+};
+
+const PingRequest = new rpc.RequestType('telemetry/ping');
+
+class PingCommand extends Base<Args> {
+  static processArgv(argv: any): Args {
+    return {
+      bin: findFlowBin(argv.bin),
+      root: path.resolve(process.cwd(), argv.root),
+    };
+  }
+
+  static async run(args: Args): Promise<void> {
+    const rootUri = uri.URI.file(args.root).toString();
+
+    const lsp = child_process.spawn(args.bin, ['lsp', '--autostop']);
+    const conn = rpc.createMessageConnection(
+      new rpc.StreamMessageReader(lsp.stdout),
+      new rpc.StreamMessageWriter(lsp.stdin),
+    );
+    conn.listen();
+
+    const initializeResponse = await conn.sendRequest('initialize', {
+      rootUri: rootUri,
+      capabilities: {},
+    });
+
+    async function doPing(): Promise<number> {
+      const start = performance.now();
+      await conn.sendRequest(PingRequest);
+      return performance.now() - start;
+    }
+
+    async function throttle(ms: number): Promise<void> {
+      await new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    while (true) {
+      const now = Date.now();
+      const [duration, _] = await Promise.all([doPing(), throttle(100)]);
+      console.log(now, ' ', duration.toFixed(3));
+    }
+  }
+
+  static description(): string {
+    return 'Measures IDE command latency';
+  }
+
+  static async usage(): Promise<string> {
+    return `usage: ${process.argv[1]} ping`;
+  }
+
+  static getFlags(): Array<Flag> {
+    return [
+      commonFlags.bin,
+      {
+        type: 'string',
+        name: 'root',
+        argName: 'ROOT',
+        description: 'The Flow root to profile',
+        default: '.',
+      },
+    ];
+  }
+}
+
+module.exports = {
+  default: PingCommand,
+};
