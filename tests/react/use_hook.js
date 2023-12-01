4ebac6f2ad5f01f234961b08f9305d4fe4ccceb7
Jan Kassens <jkassens@meta.com>
2023-11-30 17:23:20 -0800

    [flow] add types for React.use
    Reviewed By: alunyov
    
    Differential Revision: D51716871
    
    fbshipit-source-id: 189d5a33ce01e2925496ce13b9cfcbb410168159

---
+++ tests/react/use_hook.js
@@ -0,0 +1,30 @@
+import {use} from 'react';
+
+declare opaque type Theme;
+declare const ThemeContext: React$Context<Theme>;
+
+declare const AnswerPromise: Promise<42>;
+
+declare const CustomThennable: {
+  then(onFulfil: (string) => void): void;
+};
+
+function Test() {
+  // expected to not error
+  const theme: Theme = use(ThemeContext);
+  const answer: 42 = use(AnswerPromise);
+  const customThen: string = use(CustomThennable);
+
+  // expected to error - invalid return type
+  const notAny: 0 = use(ThemeContext);
+  const notAny2: 0 = use(AnswerPromise);
+  const notAny3: 0 = use(CustomThennable);
+
+  // expected to error - invalid argument
+  use(null);
+  use(undefined);
+  use(0);
+  use({});
+
+  return null;
+}
