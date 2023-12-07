d38ea59ef41280287912b0f120dfbc06353fa305
Panos Vekris <pvekris@meta.com>
2023-12-07 12:02:51 -0800

    [flow] add type hover test
    Summary:
    The test shows that when importing a name with a different alias we keep the remotely defined name.
    
    In the next diff this changes to the local alias.
    
    Changelog: [internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D51923462
    
    fbshipit-source-id: c36eee88df6ccacaac5f97d7db39539cc8255c97

---
+++ tests/type_at_pos_imported_as/exports.js
@@ -0,0 +1,11 @@
+// @flow
+
+export class C {}
+
+export type T = C;
+
+export interface I {}
+
+export enum E {}
+
+declare export component Comp();
---
+++ tests/type_at_pos_imported_as/main.js
@@ -0,0 +1,24 @@
+// @flow
+
+import { C as CLocal } from './exports';
+
+import type { T as TLocal } from './exports';
+
+import type { I as ILocal } from './exports';
+
+import { E as ELocal } from './exports';
+
+import { Comp as CompLocal } from './exports';
+
+type __ = [
+    CLocal,
+//  ^
+    TLocal,
+//  ^
+    ILocal,
+//  ^
+    ELocal,
+//  ^
+    CompLocal,
+//  ^
+];
