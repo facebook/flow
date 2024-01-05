f7209fec0fe7f898fa4c9acd65c057d3a912d667
Panos Vekris <pvekris@meta.com>
2024-01-05 01:01:20 -0800

    [flow] restore test
    Summary:
    The change in D37156921 had introduced the unintended [name-already-bound] error.
    
    Changelog: [internal]
    
    Reviewed By: gkz
    
    Differential Revision: D52551786
    
    fbshipit-source-id: 2d3644c3de44b5ddac7b8de563a93080f0e430d0

--- tests/resolved_env/aliases.js
+++ tests/resolved_env/aliases.js
@@ -4,6 +4,6 @@ const x: T = "hello world"; // err
 type T = S;
 type S = number;
 
-const x: A = "hello world"; // err
+const y: A = "hello world"; // err
 opaque type A: S = B;
 opaque type B: T = T;
