3a363ab59a7944e9dd14c0d08c742db431e76a74
Sam Zhou <samzhou19815@meta.com>
2024-01-29 21:18:29 -0800

    [flow] Add test of fundamental mini-repro of homebrew test failure
    Summary:
    This diff adds a test that repros the weird behavior we see in https://github.com/Homebrew/homebrew-core/pull/160348. The issue is the most likely to trigger on mac, where the default tmp dir `/tmp` is actually a symlink to `/private/tmp`, but the underlying issue can be triggered on all OSes. We just need to pass in a path that has two parts `${path that's symlinked to another realpath}/${non-existing directory yet}`. This diff adds a test to show the broken behavior, which will be fixed in the next diff.
    
    Changelog: [internal]
    
    Reviewed By: panagosg7
    
    Differential Revision: D53199030
    
    fbshipit-source-id: dd31d3c70ab4e7155a3639f4c0471176525e2b09

---
+++ tests/symlink-tmp-dir/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+mkdir link-src-tmp
+ln -s link-src-tmp link-dest-tmp
+
+printf "Default .flowconfig with bad code should not typecheck:\n"
+# TODO: should error
+assert_ok "$FLOW" check test.js --temp-dir link-dest-tmp/flow
+printf "Default .flowconfig with bad code should not typecheck when run again with the same config:\n"
+assert_errors "$FLOW" check test.js --temp-dir link-dest-tmp/flow
