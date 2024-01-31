a86333f96977be75608d3f441a81159bc5e7c512
Panos Vekris <pvekris@meta.com>
2024-01-30 19:50:50 -0800

    [flow] modernize tests/type_at_pos_interface
    Summary:
    * Use `queries_in_file` instead of manually adding the lines and columns
    * Uncomment TODO test cases that now work as expected
    * Add a new TODO use case fixed in the next diff
    
    Changelog: [internal]
    
    Reviewed By: gkz, SamChou19815
    
    Differential Revision: D53251630
    
    fbshipit-source-id: 88d007537093769d4c09a6473d626aee9dbd7da3

--- tests/type_at_pos_interface/interface.js
+++ tests/type_at_pos_interface/interface.js
@@ -1,24 +1,44 @@
 // @flow
 
 interface IA {
+//        ^
   mm(x: string): string;
   mm(x: number): number;
   mf : ((x: number) => number) & ((x: string) => string);
 }
 
-declare var i_a: IA;
+declare var i_a:
+//          ^
+    IA;
+//  ^
+
 i_a.mm("x");
+//  ^
 i_a.mm(1);
+//  ^
 
 i_a.mf("x");
+//  ^
 i_a.mf(1);
 
 interface I {
   get y(): string;
+//    ^
   set y(x: number): void;
+//    ^
 }
 
 function* gen(): Generator<number, void, number> {
 //        ^
   yield 1;
 }
+
+/*
+ * Aliased polymorphic anonymous interface
+ */
+type PolyAnonAlias<X> = interface {
+  f: X;
+}
+
+declare var polyAnonAlias: PolyAnonAlias<number>; // TODO
+//          ^
