934ae954d80d21c736e5c87b839712b628cf8aa7
Mike Vitousek <mvitousek@meta.com>
2024-02-01 09:26:45 -0800

    [flow] Make HooklikeT handle more incoming types
    Summary: Fixes HooklikeT to correctly make PolyTs and callable ObjTs hooklike
    
    Reviewed By: SamChou19815
    
    Differential Revision:
    D52922283
    
    ------------------------------------------------------------------------
    (from 48628a59dd9ea6a51d5d65655cca800f63977deb)
    
    fbshipit-source-id: a67b8825c2c88f286ff518a309d86c56870ae48d

--- tests/hook_syntax_compatibility/test.js
+++ tests/hook_syntax_compatibility/test.js
@@ -6,6 +6,11 @@ import { useW } from './exp2';
 import useU from './exp2';
 import * as useV from './exp2';
 
+function useT<T>() {}
+declare const useS: {
+    <T>(x: T): T;
+}
+
 component C(...{ useY }: { useY: () => void}) {
     useY(); // no error
     useX(); // error for calling number but no hook error
@@ -13,5 +18,9 @@ component C(...{ useY }: { useY: () => void}) {
     useW(); // no error
     useU(); // no error
     useV(); // error for calling module but no hook error
+    useT(); // no error
+    useT<mixed>(); // no error
+    useS(); // no error
+    useS<mixed>(); // no error
     return 42;
 }
