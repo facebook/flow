4c90e121f6881081f3252f34ff848507ecabd4c9
Sam Zhou <samzhou19815@meta.com>
2024-02-02 16:52:20 -0800

    [flow] Simplify the type reference transform from React.AbstractComponent to React.Element
    Summary:
    This diff aligns the behavior with annotation inference, where we don't pass in the props type argument, and let the type app does its job of computing the default on its own.
    
    It shouldn't affect the behavior of user code, since the underlying computation is the same.
    
    Changelog: [internal]
    
    Reviewed By: panagosg7
    
    Differential Revision: D53333988
    
    fbshipit-source-id: f75289608b6038861476e6a5dd7676f863391c76

--- src/typing/subtyping_kit.ml
+++ src/typing/subtyping_kit.ml
@@ -1993,22 +1993,12 @@ module Make (Flow : INPUT) : OUTPUT = struct
     | (DefT (reasonl, ReactAbstractComponentT _), DefT (r, TypeT (_, t))) ->
       (* a component syntax value annotation becomes an element of that component *)
       let elem =
-        let props =
-          let reason = update_desc_new_reason (fun desc -> RPropsOfComponent desc) reasonl in
-          mk_possibly_evaluated_destructor
-            cx
-            use_op
-            reason
-            l
-            ReactElementPropsType
-            (Eval.generate_id ())
-        in
         let elem_reason =
           let desc = react_element_desc_of_component_reason reasonl in
           let annot_loc = loc_of_reason r in
           annot_reason ~annot_loc (replace_desc_reason desc r)
         in
-        get_builtin_typeapp cx ~use_desc:true elem_reason (OrdinaryName "React$Element") [l; props]
+        get_builtin_typeapp cx ~use_desc:true elem_reason (OrdinaryName "React$Element") [l]
       in
       rec_unify cx trace ~use_op elem t
     (* non-class/function values used in annotations are errors *)
