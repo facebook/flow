6d814997e82a760fb67a7bea6f9bf37ea909dc90
Sam Zhou <samzhou19815@meta.com>
2024-02-21 12:42:04 -0800

    [flow][multiplatform] Implement support for ambient support modules through flowconfig
    Reviewed By: panagosg7
    
    Differential Revision: D53788047
    
    fbshipit-source-id: b8689ee87fff6dcd8a297d199187475d2a1017ac

--- src/common/platform_set.ml
+++ src/common/platform_set.ml
@@ -27,7 +27,23 @@ let available_platforms ~file_options ~filename ~explicit_available_platforms : 
       Some bitset
     | None ->
       (match explicit_available_platforms with
-      | None -> Some (Bitset.all_one (Base.List.length multi_platform_extensions))
+      | None ->
+        let match_directory_override (path, platform) =
+          let normalized_filename = Sys_utils.normalize_filename_dir_sep filename in
+          Base.Option.some_if (Base.String.is_prefix ~prefix:path normalized_filename) platform
+        in
+        (match
+           Base.List.find_map
+             file_options.multi_platform_ambient_supports_platform_directory_overrides
+             ~f:match_directory_override
+         with
+        | None -> Some (Bitset.all_one (Base.List.length multi_platform_extensions))
+        | Some supported_platforms_from_directory_overrides ->
+          Some
+            (available_platforms_to_bitset
+               ~multi_platform_extensions
+               supported_platforms_from_directory_overrides
+            ))
       | Some explicit_available_platforms ->
         Some (available_platforms_to_bitset ~multi_platform_extensions explicit_available_platforms))
 
---
+++ tests/multiplatform_ambient_supports_platform/mobile-and-win.windows.js
@@ -0,0 +1,1 @@
+declare export const foo: number; // doesn't conflict with common interface file, because it doesn't include windows
