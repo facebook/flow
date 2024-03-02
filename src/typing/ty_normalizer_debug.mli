5de0664218c7d872bbf15bf5649334fd81ef1d9d
Panos Vekris <pvekris@meta.com>
2024-03-01 16:15:25 -0800

    [flow] delete Ty_normalizer_debug
    Summary:
    I'm planning to make Ty_normalizer_imports call into Type_operation_utils to get information about imports instead of looking them up in the typed AST. Type_operation_utils depends on Flow_js. Instead of trying to come up with alternatives to Type_operation_utils functions that don't rely on Flow_js to pass in Ty_normalizer_debug, I decided to delete it for now, since it's not used much anyway.
    
    Changelog: [internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D54332600
    
    fbshipit-source-id: 95182c9f27f2622a96ba5f3ef7cb97f0fc19e7db

--- src/typing/ty_normalizer_debug.mli
+++
@@ -1,8 +0,0 @@
-(*
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *)
-
-val string_of_t : Context.t -> Type.t -> string
