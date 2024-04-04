4cd054e59cd891e3f61119aca020a99b070800c4
George Zahariev <gkz@meta.com>
2024-04-03 23:41:41 -0700

    [flow][tuples] Update tuple spread tests to use as-casting
    Summary:
    Update tuple spread tests to use as-casting.
    
    Changelog: [internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D55721532
    
    fbshipit-source-id: 8e7ab901062918c9e64d5339bd96466951c80b8d

--- tests/tuples/spread.js
+++ tests/tuples/spread.js
@@ -3,26 +3,26 @@ type A = [1, 2];
 
 type Same = [...A]; // OK
 {
-  ([1, 2]: Same); // OK
+  [1, 2] as Same; // OK
   declare const x: Same;
-  (x: [1, 2]); // OK
+  x as [1, 2]; // OK
 
-  ([1]: Same); // ERROR: arity
-  (x: [1, 2, 3]); // ERROR: arity
+  [1] as Same; // ERROR: arity
+  x as [1, 2, 3]; // ERROR: arity
 
-  ([1, 99]: Same); // ERROR: element type
+  [1, 99] as Same; // ERROR: element type
 }
 
 type Middle = [0, ...A, 3]; // OK
 {
-  ([0, 1, 2, 3]: Middle); // OK
+  [0, 1, 2, 3] as Middle; // OK
   declare const x: Middle;
-  (x: [0, 1, 2, 3]); // OK
+  x as [0, 1, 2, 3]; // OK
 
-  ([0, 1, 2, 3, 4]: Middle); // ERROR: arity
-  (x: [0, 1, 2]); // ERROR: arity
+  [0, 1, 2, 3, 4] as Middle; // ERROR: arity
+  x as [0, 1, 2]; // ERROR: arity
 
-  ([0, 1, 99, 3]: Middle); // ERROR: element type
+  [0, 1, 99, 3] as Middle; // ERROR: element type
 }
 
 // Required after optional
@@ -33,46 +33,46 @@ type ReqAfterOptionalSpread = [...Optionals, ...A]; // ERROR
 // Unions
 type Union = [1, 2] | [6];
 type FromUnion = [0, ...Union, 9]; // OK
-([0, 1, 2, 9]: FromUnion); // OK
-([0, 6, 9]: FromUnion); // OK
-([0, 9]: FromUnion); // ERROR
+[0, 1, 2, 9] as FromUnion; // OK
+[0, 6, 9] as FromUnion; // OK
+[0, 9] as FromUnion; // ERROR
 
 // Invalid spread
 type InvalidSpread1 = [1, ...2]; // ERROR
 {
   declare const x: InvalidSpread1;
-  (x: empty); // OK
+  x as empty; // OK
 }
 type Alias = {};
 type InvalidSpread2 = [1, ...Alias]; // ERROR
 {
   declare const x: InvalidSpread2;
-  (x: empty); // OK
+  x as empty; // OK
 }
 type ArraySpread = [...Array<mixed>]; // ERROR
 {
   declare const x: ArraySpread;
-  (x: empty); // OK
+  x as empty; // OK
 }
 
 // Spreading `any`
 type AnySpread = [1, 2, ...any]; // OK
 {
   declare const x: AnySpread;
-  (x: empty); // OK
+  x as empty; // OK
 }
 
 // Spreading `empty`
 type EmptySpread = [1, 2, ...empty]; // OK
 {
   declare const x: AnySpread;
-  (x: empty); // OK
+  x as empty; // OK
 }
 
 // Generics
 type WithTail<T> = [1, ...T];
-([1, 2, 3]: WithTail<[2, 3]>); // OK
-([1, 2, 66]: WithTail<[2, 3]>); // ERROR
+[1, 2, 3] as WithTail<[2, 3]>; // OK
+[1, 2, 66] as WithTail<[2, 3]>; // ERROR
 
 declare function f<T>(xs: [1, ...T]): void;
 f([1, 2, 3]); // ERROR: annotation required - reversal not yet implemented
