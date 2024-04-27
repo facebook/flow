a3362df0e80f84733116a51d3771e2a3261de69b
Panos Vekris <pvekris@meta.com>
2024-04-27 01:07:19 -0700

    [flow] add tests to expose bug with mixed in ground subtyping
    Summary: Changelog: [internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D56658078
    
    fbshipit-source-id: e3a7e8ae125372aee7361fe22b3feaffb48822cf

---
+++ tests/mixed_flavors/test.js
@@ -0,0 +1,23 @@
+function test_NonMaybe() {
+  42 as number | null as $NonMaybeType<mixed>; // error null ~> MixedT (NonMaybe)
+
+  42 as number | void as $NonMaybeType<mixed>; // error undefined ~> MixedT (NonMaybe)
+
+  42 as ?number as $NonMaybeType<mixed>; // TODO error null | undefined ~> MixedT (NonMaybe)
+}
+
+function test_function(fn: mixed) {
+  if (typeof fn === "function") {
+      const one = 1 as typeof fn; // TODO error number ~> MixedT (Mixed_function)
+      one.apply;
+  }
+}
+
+function test_truthy(x: mixed) {
+  if (x) {
+      const false_ = false as typeof x; // TODO error false ~> MixedT (Mixed_truthy)
+      if (!false_) { // then branch taken
+          false_ as empty;
+      }
+  }
+}
