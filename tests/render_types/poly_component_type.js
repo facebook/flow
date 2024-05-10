b9770f936218c525dea174e75ee1071232a42c94
Jordan Brown <jmbrown@meta.com>
2024-05-10 15:38:39 -0700

    [flow] Add test demonstrating bug in polymorphic component types
    Summary:
    There is an issue with polymorphic component types + render types. putting up this test and plan to fix it in a stacked diff
    
    Changelog: [internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D57226884
    
    fbshipit-source-id: 133ca46b869439c2fe52682289c838e555eccc3e

---
+++ tests/render_types/poly_component_type.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+declare const Poly: component<T: React.Node>(children: T) renders T;
+
+declare component Button();
+declare component NotAButton();
+
+const el = <Poly><Button /></Poly>;
+el as renders NotAButton; // SHOULD ERROR
