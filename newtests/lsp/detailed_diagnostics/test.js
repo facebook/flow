32981427e85553e31106dd5520cfae4d519e0ac8
Sam Zhou <samzhou19815@meta.com>
2024-06-03 17:17:53 -0700

    [flow][tests] Add tests to demonstrate that client override of vscode detailed errors is not successfully applied
    Summary:
    The bug is that the client override has not been applied universally. We seem to only apply it for live errors. This diff adds newtests to expose that.
    
    Changelog: [internal]
    
    Reviewed By: panagosg7
    
    Differential Revision: D58069302
    
    fbshipit-source-id: 204f07ccc5261bd331a7c2eed63c04d04f21b941

---
+++ newtests/lsp/detailed_diagnostics/test.js
@@ -0,0 +1,183 @@
+/*
+ * @flow
+ * @format
+ */
+
+import type {SuiteType} from '../../Tester';
+const {join} = require('path');
+const {suite, test} = require('../../Tester');
+
+module.exports = (suite(
+  ({
+    lspStartAndConnect,
+    lspRequestAndWaitUntilResponse,
+    lspNotification,
+    addFile,
+    addFiles,
+    modifyFile,
+    lspIgnoreStatusAndCancellation,
+  }) => [
+    test(
+      'textDocument/publishDiagnostics flowconfig enabled, no client config',
+      [
+        lspStartAndConnect(),
+        addFile('file_with_simple_error.js')
+          .waitUntilLSPMessage(
+            9000,
+            'textDocument/publishDiagnostics',
+            '{Cannot assign}',
+          )
+          .verifyLSPMessageSnapshot(
+            join(
+              __dirname,
+              '__snapshots__',
+              'flowconfig-enabled-no-client-config-errors.json',
+            ),
+            ['window/showStatus', '$/cancelRequest'],
+          ),
+      ],
+    ).flowConfig('_flowconfig_enabled'),
+
+    test('textDocument/publishDiagnostics flowconfig enabled, client enabled', [
+      lspStartAndConnect(),
+      lspNotification('workspace/didChangeConfiguration', {
+        settings: {detailedErrorRendering: true},
+      }).verifyAllLSPMessagesInStep(
+        [],
+        [
+          'textDocument/publishDiagnostics',
+          'window/showStatus',
+          '$/cancelRequest',
+        ],
+      ),
+      addFile('file_with_simple_error.js')
+        .waitUntilLSPMessage(
+          9000,
+          'textDocument/publishDiagnostics',
+          '{Cannot assign}',
+        )
+        .verifyLSPMessageSnapshot(
+          join(
+            __dirname,
+            '__snapshots__',
+            'flowconfig-enabled-client-enabled-errors.json',
+          ),
+          ['window/showStatus', '$/cancelRequest'],
+        ),
+    ]).flowConfig('_flowconfig_enabled'),
+
+    test(
+      'textDocument/publishDiagnostics flowconfig enabled, client disabled',
+      [
+        lspStartAndConnect(),
+        lspNotification('workspace/didChangeConfiguration', {
+          settings: {detailedErrorRendering: false},
+        }).verifyAllLSPMessagesInStep(
+          [],
+          [
+            'textDocument/publishDiagnostics',
+            'window/showStatus',
+            '$/cancelRequest',
+          ],
+        ),
+        addFile('file_with_simple_error.js')
+          .waitUntilLSPMessage(
+            9000,
+            'textDocument/publishDiagnostics',
+            '{Cannot assign}',
+          )
+          .verifyLSPMessageSnapshot(
+            join(
+              __dirname,
+              '__snapshots__',
+              'flowconfig-enabled-client-disabled-errors.json',
+            ),
+            ['window/showStatus', '$/cancelRequest'],
+          ),
+      ],
+    ).flowConfig('_flowconfig_enabled'),
+
+    test(
+      'textDocument/publishDiagnostics flowconfig disabled, no client config',
+      [
+        lspStartAndConnect(),
+        addFile('file_with_simple_error.js')
+          .waitUntilLSPMessage(
+            9000,
+            'textDocument/publishDiagnostics',
+            '{Cannot assign}',
+          )
+          .verifyLSPMessageSnapshot(
+            join(
+              __dirname,
+              '__snapshots__',
+              'flowconfig-disabled-no-client-config-errors.json',
+            ),
+            ['window/showStatus', '$/cancelRequest'],
+          ),
+      ],
+    ).flowConfig('_flowconfig_disabled'),
+
+    test(
+      'textDocument/publishDiagnostics flowconfig disabled, client enabled',
+      [
+        lspStartAndConnect(),
+        lspNotification('workspace/didChangeConfiguration', {
+          settings: {detailedErrorRendering: true},
+        }).verifyAllLSPMessagesInStep(
+          [],
+          [
+            'textDocument/publishDiagnostics',
+            'window/showStatus',
+            '$/cancelRequest',
+          ],
+        ),
+        addFile('file_with_simple_error.js')
+          .waitUntilLSPMessage(
+            9000,
+            'textDocument/publishDiagnostics',
+            '{Cannot assign}',
+          )
+          .verifyLSPMessageSnapshot(
+            join(
+              __dirname,
+              '__snapshots__',
+              'flowconfig-disabled-client-enabled-errors.json',
+            ),
+            ['window/showStatus', '$/cancelRequest'],
+          ),
+      ],
+    ).flowConfig('_flowconfig_disabled'),
+
+    test(
+      'textDocument/publishDiagnostics flowconfig disabled, client disabled',
+      [
+        lspStartAndConnect(),
+        lspNotification('workspace/didChangeConfiguration', {
+          settings: {detailedErrorRendering: false},
+        }).verifyAllLSPMessagesInStep(
+          [],
+          [
+            'textDocument/publishDiagnostics',
+            'window/showStatus',
+            '$/cancelRequest',
+          ],
+        ),
+        addFile('file_with_simple_error.js')
+          .waitUntilLSPMessage(
+            9000,
+            'textDocument/publishDiagnostics',
+            '{Cannot assign}',
+          )
+          .verifyLSPMessageSnapshot(
+            join(
+              __dirname,
+              '__snapshots__',
+              'flowconfig-disabled-client-disabled-errors.json',
+            ),
+            ['window/showStatus', '$/cancelRequest'],
+          ),
+      ],
+    ).flowConfig('_flowconfig_disabled'),
+  ],
+): SuiteType);
