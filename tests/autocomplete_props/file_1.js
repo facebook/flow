52e2e885b8810f44ed84fc5e93f30e3d438b8763
Panos Vekris <pvekris@meta.com>
2024-06-10 10:14:53 -0700

    [flow][autocomplete] add tests for members of union of types
    Summary: Changelog: [internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D58316616
    
    fbshipit-source-id: c874c5763ce093ce6ac4d1c60f8a1866d2448b1b

---
+++ tests/autocomplete_props/file_1.js
@@ -0,0 +1,156 @@
+// @flow
+
+function test1() {
+  type Foo = { x: number };
+  type Bar = { foo: Foo };
+  const _: Bar = { foo: {   } };
+//                        ^
+}
+
+function test2() {
+  type Foo = { x: number };
+  type Bar = { foo?: Foo };
+  const _: Bar = { foo: {   } }; // TODO
+//                        ^
+}
+
+function test3() {
+  type Foo = { x: number };
+  type Bar = { foo: ?Foo };
+  const _: Bar = { foo: {   } }; // TODO
+//                        ^
+}
+
+function test4() {
+  type Foo = { x: number };
+  type Bar = { foo?: ?Foo };
+  const _: Bar = { foo: {   } }; // TODO
+//                        ^
+}
+
+function test5() {
+  type Foo = { x: number };
+  type Bar = ?{ foo: Foo };
+  const _: Bar = { foo: {   } }; // TODO
+//                        ^
+}
+
+function test6() {
+  type Foo = { x: number };
+  type Bar = ?{ foo?: Foo };
+  const _: Bar = { foo: {   } }; // TODO
+//                        ^
+}
+
+function test7() {
+  type Foo = { x: number };
+  type Bar = { foo: Foo | void | null }; // TODO
+  const _: Bar = { foo: {   } };
+//                        ^
+}
+
+function test8() {
+  type Foo1 = { x: number };
+  type Foo2 = { y: number };
+  type Bar = { foo: Foo1 | Foo2 };
+  const _: Bar = { foo: {   } }; // TODO
+//                        ^
+}
+
+function test9() {
+  type Foo1 = { x: number };
+  type Foo2 = { y: number };
+  type Bar = ?{ foo?: Foo1 | Foo2 };
+  const _: Bar = { foo: {   } }; // TODO
+//                        ^
+}
+
+function test10() {
+  type Foo1 = { x: number };
+  type Foo2 = { x: string };
+  type Bar = ?{ foo?: Foo1 | Foo2 };
+  const _: Bar = { foo: {   } }; // TODO
+//                        ^
+}
+
+function test11() {
+  type Foo1 = { x: number };
+  type Foo2 = { x: string };
+  type Bar = ?{ foo?: Foo1 & Foo2 };
+  const _: Bar = { foo: {   } }; // TODO
+//                        ^
+}
+
+function test12() {
+  type Foo = {
+    x: number,
+  };
+
+  type Bar = {
+    foo: Foo,
+    foo1: ?Foo,
+    foo2?: Foo,
+    foo3: Foo | null | void,
+    partial: Partial<{foo: Foo}>,
+  };
+
+  const _: Bar = {
+    foo: {   },
+//         ^
+    foo1: {   }, // TODO
+//          ^
+    foo2: {   }, // TODO
+//          ^
+    foo3: {   }, // TODO
+//          ^
+    partial: { foo: {   } } }; // TODO
+//                    ^
+}
+
+function test13() {
+  type Foo1 = { x: number };
+  type Foo2 = { y: number };
+  type Bar = ?$ReadOnly<{ foo?: $ReadOnly<{ ...Foo1, ...Foo2 }> }>
+  const _: Bar = { foo: {   } }; // TODO
+//                        ^
+}
+
+function test14() {
+  type Foo = { x: number };
+  type Bar1 = { foo: Foo };
+  type Bar2 = { bar: Foo };
+  const _: ?(Bar1 | Bar2) = { foo: {   } }; // TODO
+//                                   ^
+}
+
+function test15() {
+  type Foo1 = { tag: "1"; x: number };
+  type Foo2 = { tag: "2"; x: string };
+  type Bar = ?{ foo?: Foo1 | Foo2 };
+  const _1: Bar = { foo: { tag: "1",   } }; // TODO
+//                                   ^
+  const _2: Bar = { foo: { tag: "2",   } }; // TODO
+//                                   ^
+}
+
+function test16() {
+  type Foo1 = ?{ x: number };
+  type Foo2 = ?{ x: string };
+  type Bar1 = ?{ tag: "1"; foo?: Foo1 };
+  type Bar2 = ?{ tag: "2", foo?: Foo2 };
+  type Bar = Bar1 | Bar2;
+  const _1: Bar = { tag: "1", foo: {   } }; // TODO
+//                                   ^
+  const _2: Bar = { tag: "2", foo: {   } }; // TODO
+//                                   ^
+}
+
+function test17() {
+  type Foo1 = { tag: "1"; x?: number };
+  type Foo2 = { tag: "2"; y?: number };
+  type Bar = ?{ foo: Foo1 | Foo2 };
+  const _1: Bar = { foo: { tag: "1",   } }; // TODO
+//                                   ^
+  const _1: Bar = { foo: { tag: "2",   } }; // TODO
+//                                   ^
+}
