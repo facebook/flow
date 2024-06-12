7f31147692106b8dcc4c35de67c0628b620c29d3
Sam Zhou <samzhou19815@meta.com>
2024-06-12 10:22:26 -0700

    [flow] Introduce the concept of symbol
    Summary:
    In this diff, I will finally introduce the concept of symbol, as a replacement of reason where we actually care about the semantics of what's tracked with symbol.
    
    The design is inspired by [TS' symbol](https://github.com/microsoft/TypeScript/blob/d8086f14b6b97c0df34a0cc2f56d4b5926a0c299/src/services/services.ts#L639-L653). It should have the following properties:
    
    1. Tracks only definition instead of use sites.
    2. Must be named
    3. Must be trustworthy
    
    Not all reasons have a path forward to become a symbol, and this is fine. Notably, while all the types must produce a reason, the same does not need to hold for symbols, because not all types are mapped to a named symbol.
    
    In this diff, I only introduced the structure and nothing else. In the next diff, we will start to apply it to modules and namespaces.
    
    Changelog: [internal]
    
    Differential Revision: D58387128
    
    fbshipit-source-id: 06fe1cf8dcd8d0d8934f5d1797e3a3c205313154

---
+++ src/common/symbol.ml
@@ -0,0 +1,88 @@
+(*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *)
+
+open Utils_js
+
+type kind =
+  | SymbolClass
+  | SymbolComponent
+  | SymbolConstant
+  | SymbolConstructor
+  | SymbolEnum
+  | SymbolEnumMember
+  | SymbolFile
+  | SymbolFunction
+  | SymbolInterface
+  | SymbolMethod
+  | SymbolModule
+  | SymbolNamespace
+  | SymbolProperty
+  | SymbolTypeAlias
+  | SymbolTypeParameter
+  | SymbolVariable
+
+let string_of_kind = function
+  | SymbolClass -> "class"
+  | SymbolComponent -> "component"
+  | SymbolConstant -> "const"
+  | SymbolConstructor -> "constructor"
+  | SymbolEnum -> "enum"
+  | SymbolEnumMember -> "enum member"
+  | SymbolFile -> "file"
+  | SymbolFunction -> "function"
+  | SymbolInterface -> "interface"
+  | SymbolMethod -> "method"
+  | SymbolModule -> "module"
+  | SymbolNamespace -> "namespace"
+  | SymbolProperty -> "property"
+  | SymbolTypeAlias -> "type alias"
+  | SymbolTypeParameter -> "type parameter"
+  | SymbolVariable -> "variable"
+
+type symbol = {
+  kind: kind;
+  name: string;
+  def_loc: ALoc.t;
+}
+
+let mk_symbol ~kind ~name ~def_loc = { kind; name; def_loc }
+
+let mk_class_symbol ~name = mk_symbol ~kind:SymbolClass ~name
+
+let mk_component_symbol ~name = mk_symbol ~kind:SymbolComponent ~name
+
+let mk_constant_symbol ~name = mk_symbol ~kind:SymbolConstant ~name
+
+let mk_enum_symbol ~name = mk_symbol ~kind:SymbolEnum ~name
+
+let mk_module_symbol ~name = mk_symbol ~kind:SymbolModule ~name
+
+let mk_namespace_symbol ~name = mk_symbol ~kind:SymbolNamespace ~name
+
+let mk_type_alias_symbol ~name = mk_symbol ~kind:SymbolTypeAlias ~name
+
+let mk_type_parameter_symbol ~name = mk_symbol ~kind:SymbolTypeParameter ~name
+
+let mk_variable_symbol ~name = mk_symbol ~kind:SymbolVariable ~name
+
+(* TODO: create more constructors for other kinds of symbols as needed *)
+
+let kind_of_symbol { kind; _ } = kind
+
+let name_of_symbol { name; _ } = name
+
+let def_loc_of_symbol { def_loc; _ } = def_loc
+
+let string_of_aloc aloc =
+  match ALoc.source aloc with
+  | None -> ""
+  | Some file -> spf "%s:%s" (File_key.to_string file) (ALoc.to_string_no_source aloc)
+
+let dump_symbol { kind; name; def_loc } =
+  spf "%s: %s %s" (string_of_aloc def_loc) (string_of_kind kind) name
+
+let dump_symbol_opt = Base.Option.value_map ~default:"no symbol" ~f:dump_symbol
---
+++ src/common/symbol.mli
@@ -0,0 +1,62 @@
+(*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *)
+
+(**
+ * Symbol keeps track of properties of a named construct at the definition site.
+ * Unlike reason that has to exist on every single type, symbol does not have to.
+ * When they are attached to a type, it must always have a name and always be reliable.
+ *)
+
+type kind =
+  | SymbolClass
+  | SymbolComponent
+  | SymbolConstant
+  | SymbolConstructor
+  | SymbolEnum
+  | SymbolEnumMember
+  | SymbolFile
+  | SymbolFunction
+  | SymbolInterface
+  | SymbolMethod
+  | SymbolModule
+  | SymbolNamespace
+  | SymbolProperty
+  | SymbolTypeAlias
+  | SymbolTypeParameter
+  | SymbolVariable
+
+val string_of_kind : kind -> string
+
+type symbol
+
+val mk_class_symbol : name:string -> def_loc:ALoc.t -> symbol
+
+val mk_component_symbol : name:string -> def_loc:ALoc.t -> symbol
+
+val mk_constant_symbol : name:string -> def_loc:ALoc.t -> symbol
+
+val mk_enum_symbol : name:string -> def_loc:ALoc.t -> symbol
+
+val mk_module_symbol : name:string -> def_loc:ALoc.t -> symbol
+
+val mk_namespace_symbol : name:string -> def_loc:ALoc.t -> symbol
+
+val mk_type_alias_symbol : name:string -> def_loc:ALoc.t -> symbol
+
+val mk_type_parameter_symbol : name:string -> def_loc:ALoc.t -> symbol
+
+val mk_variable_symbol : name:string -> def_loc:ALoc.t -> symbol
+
+val kind_of_symbol : symbol -> kind
+
+val name_of_symbol : symbol -> string
+
+val def_loc_of_symbol : symbol -> ALoc.t
+
+val dump_symbol : symbol -> string
+
+val dump_symbol_opt : symbol option -> string
