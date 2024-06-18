79125669d0187d75109bc40e8eb5d7e21bb61cd1
Sam Zhou <samzhou19815@fb.com>
2024-06-17 18:52:32 -0700

    [ci] Migrate deploy jobs to github actions (#9164)
    Summary:
    This diff migrates all the deploy jobs to github actions, and make the circleci job mostly noop. The only thing tested in the PR is the new linux arm64 build, while I carefully inspected everything else.
    
    Pull Request resolved: https://github.com/facebook/flow/pull/9164
    
    Test Plan: test during release, yolo!
    
    Reviewed By: alexmckenley
    
    Differential Revision: D58701423
    
    Pulled By: SamChou19815
    
    fbshipit-source-id: de28c5bb3827e8501aa08fe5c09b9634dbf17ae8

--- .circleci/deploy_npm.sh
+++ .circleci/deploy_npm.sh
@@ -8,7 +8,7 @@
 set +x
 
 # only run on tags
-if [[ "$CIRCLE_TAG" = "" ]]; then exit 0; fi
+if [[ "$GITHUB_REF_NAME" = "" ]]; then exit 0; fi
 
 if [ -f ~/.npmrc ]; then mv ~/.npmrc ~/.npmrc.bak; fi
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
