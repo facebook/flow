8757d543ea030c718978acd9c80dd193816e2ee6
Sam Zhou <samzhou19815@meta.com>
2024-06-17 17:11:18 -0700

    [flow][website] Unbreak try flow's master version
    Summary:
    D58690829 incorrectly removes the `GEN_DIR` logic. Since we put past versions on npm, the only possible value is master, so I just hardcoded this.
    
    Changelog: [internal]
    
    Reviewed By: gkz
    
    Differential Revision: D58701274
    
    fbshipit-source-id: 43b23d6b45d638ea9d3162cfdc09bad5666a2e44

--- .circleci/build_website.sh
+++ .circleci/build_website.sh
@@ -4,7 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-mkdir -p "website/static/flow/$GEN_DIR"
-cp "dist/flow.js" "website/static/flow/${GEN_DIR}/flow.js"
-cp -r "lib" "website/static/flow/${GEN_DIR}/flowlib"
+mkdir -p "website/static/flow/master"
+cp "dist/flow.js" "website/static/flow/master/flow.js"
+cp -r "lib" "website/static/flow/master/flowlib"
 cd website && INCLUDE_PAST_RELEASES=1 FLOW_MAX_WORKERS=2 DOCUSAURUS_SSR_CONCURRENCY=2 yarn build
