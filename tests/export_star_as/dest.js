17fb488a607398d844f90017b09dc5cfd21042d6
Sam Zhou <samzhou19815@meta.com>
2024-07-31 13:09:32 -0700

    [flow] Add a test with cyclic `export * from '...'`
    Summary:
    Somehow I didn't find any existing tests on this. Add it to prevent accidentally non-termination risk.
    
    Changelog: [internal]
    
    Reviewed By: gkz
    
    Differential Revision:
    D60478774
    
    ------------------------------------------------------------------------
    (from 03d511bde97d6537135a634026a6189493b2aa20)
    
    fbshipit-source-id: 5d3ebb09a905213bb393b0570a373364448adc90

--- tests/export_star_as/dest.js
+++ tests/export_star_as/dest.js
@@ -5,3 +5,6 @@ var b: string = source.num; // Error: num ~> string
 
 var c: string = source.str;
 var d: number = source.str; // Error: num ~> string
+
+import {foo} from './cycle';
+foo as empty; // already errored in cycle
