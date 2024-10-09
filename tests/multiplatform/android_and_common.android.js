aad37c63b50dde8f5bd6885b17007f3ebee29269
Sam Zhou <samzhou19815@meta.com>
2024-10-09 12:12:12 -0700

    [flow][test] Add a test for module resolution order and add test
    Summary:
    The logic here initially confused me and I thought it's wrong, but it's actually correct, but it's poorly tested.
    
    In this diff, I added a test asserting the resolution order, and also added comments to explain the purpose of each resolution case.
    
    Changelog: [internal]
    
    Reviewed By: panagosg7
    
    Differential Revision:
    D64055058
    
    ------------------------------------------------------------------------
    (from 1447c89a00ee94b55807d773b43ff21cc70dc16e)
    
    fbshipit-source-id: 7813b606fbbd0d5e88fec6e3aea0c20e4a739eb9

---
+++ tests/multiplatform/android_and_common.android.js
@@ -0,0 +1,1 @@
+declare export const foo: number; // expected conformance error so we can test which one gets imported
--- tests/multiplatform/android_only_use.android.js
+++ tests/multiplatform/android_only_use.android.js
@@ -1,1 +1,2 @@
 require('./android_only'); // ok
+require('./android_and_common').foo as string; // error: android_and_common.android is actually imported
