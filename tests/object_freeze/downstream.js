7f2ad736658c63cafd871b9bf2bfa2aff605e4f0
Panos Vekris <pvekris@meta.com>
2024-10-09 22:39:09 -0700

    [flow] modernize tests/object_freeze
    Summary:
    * copies all tests to the "downstream" file to test type_sig on the same patterns
    * adds TODOs in cases where frozen object can be unsoundly coerced to writeable objects, due to `is_literal_object_reason RFrozen` still returning false
    * added more cases
    * renamed to more appropriate names
    
    Changelog: [internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D64152003
    
    fbshipit-source-id: 3f30df977551865e0b3ff8d4720655bb4c7fc265

--- tests/object_freeze/downstream.js
+++ tests/object_freeze/downstream.js
@@ -1,10 +1,40 @@
-const { inexact, NegNumber } = require('./object_freeze');
+// @flow
 
-(inexact: {||}); // error: inexact -> exact
+import {
+  frozenObject,
+  frozenObjectWithSpread,
+  frozenNumber,
+  frozenSpreadAny,
+  frozenInexact,
+  frozenNegativeNumber,
+} from './object_freeze';
 
-inexact.p = 0; // error: can't set prop on frozen object
+// Tests
 
-NegNumber.foo as 1; // error -1 ~> 1
-NegNumber.foo as -1; // okay
-1 as typeof NegNumber.foo; // error 1 ~> -1
--1 as typeof NegNumber.foo; // okay
+frozenObject.bar = '23456'; // error bar is not writable
+frozenObject as {bar: '12345'}; // TODO error bar is readonly
+frozenObject as {+bar: '1234'}; // error '12345' ~> '1234'
+frozenObject as {+bar: '12345', baz: '12345'}; // error baz is missing in frozenObject
+Object.assign(frozenObject, {bar: '12345'}); // error bar is not writable
+
+frozenObjectWithSpread.bar = '23456'; // error bar is not writable
+frozenObjectWithSpread.baz = 3456; // error baz is not writable
+frozenObjectWithSpread.corge; // error corge is missing
+frozenObjectWithSpread.constructor = baz; // error baz not a function
+frozenObjectWithSpread.toString = function () {}; // error toString missing
+
+frozenObjectWithSpread as {bar: '12345', baz: 12345}; // TODO error bar and baz are readonly
+frozenObjectWithSpread as {+bar: '1234', +baz: 12345}; // error '12345' ~> '1234'
+frozenObjectWithSpread as {+bar: '12345', }; // error baz is missing in frozenObjectWithSpread
+Object.assign(frozenObjectWithSpread, {bar: '12345'}); // error bar is not writable
+
+frozenNumber as number; // okay
+
+frozenSpreadAny.foo = 'bar'; // okay, there is no frozen form of AnyT so this is "allowed"
+
+frozenInexact as {}; // Error: inexact -> exact
+
+frozenNegativeNumber.foo as 1; // error -1 ~> 1
+frozenNegativeNumber.foo as -1; // okay
+1 as typeof frozenNegativeNumber.foo; // error 1 ~> -1
+-1 as typeof frozenNegativeNumber.foo; // okay
--- tests/object_freeze/object_freeze.js
+++ tests/object_freeze/object_freeze.js
@@ -1,43 +1,46 @@
-/* @flow */
+// @flow
 
-var foo = Object.freeze({bar: '12345'});
-foo.bar = '23456'; // error
+// local
 
-Object.assign(foo, {bar: '12345'}); // error
+const baz = {baz: 12345};
+const inexact: {...} = {p: 0};
+const any_ = 0 as any;
 
-var baz = {baz: 12345};
-var bliffl = Object.freeze({bar: '12345', ...baz});
-bliffl.bar = '23456'; // error
-bliffl.baz = 3456; // error
-bliffl.corge; // error
-bliffl.constructor = baz; // error
-bliffl.toString = function () {}; // error
+// exports
 
-baz.baz = 0;
+export const frozenObject = Object.freeze({bar: '12345'});
+export const frozenObjectWithSpread = Object.freeze({bar: '12345', ...baz});
+export const frozenNumber = Object.freeze(123); // TODO type_sig support
+export const frozenSpreadAny = Object.freeze({...any_});
+export const frozenInexact = Object.freeze({...inexact});
+export const frozenNegativeNumber = Object.freeze({foo: -1});
 
-var x: number = Object.freeze(123);
+// Tests
 
-var xx: {x: number} = Object.freeze({x: 'error'});
+frozenObject.bar = '23456'; // error bar is not writable
+frozenObject as {bar: '12345'}; // TODO error bar is readonly
+frozenObject as {+bar: '1234'}; // error '12345' ~> '1234'
+frozenObject as {+bar: '12345', baz: '12345'}; // error baz is missing in frozenObject
+Object.assign(frozenObject, {bar: '12345'}); // error bar is not writable
 
-function f(x: Object) {
-  Object.freeze({...x}) as Object; // ok
+frozenObjectWithSpread.bar = '23456'; // error bar is not writable
+frozenObjectWithSpread.baz = 3456; // error baz is not writable
+frozenObjectWithSpread.corge; // error corge is missing
+frozenObjectWithSpread.constructor = baz; // error baz not a function
+frozenObjectWithSpread.toString = function () {}; // error toString missing
 
-  let y = Object.freeze({...x});
-  y.foo = 'bar'; // there is no frozen form of AnyT so this is "allowed"
-}
+frozenObjectWithSpread as {bar: '12345', baz: 12345}; // TODO error bar and baz are readonly
+frozenObjectWithSpread as {+bar: '1234', +baz: 12345}; // error '12345' ~> '1234'
+frozenObjectWithSpread as {+bar: '12345', }; // error baz is missing in frozenObjectWithSpread
+Object.assign(frozenObjectWithSpread, {bar: '12345'}); // error bar is not writable
 
-var inexact: {...} = {p: 0};
-Object.freeze({...inexact}) as {||}; // Error: inexact -> exact
+frozenNumber as number; // okay
 
-const NegNumber = Object.freeze({
-  foo: -1,
-});
-NegNumber.foo as 1; // error -1 ~> 1
-NegNumber.foo as -1; // okay
-1 as typeof NegNumber.foo; // error 1 ~> -1
--1 as typeof NegNumber.foo; // okay
+frozenSpreadAny.foo = 'bar'; // okay, there is no frozen form of AnyT so this is "allowed"
 
-module.exports = {
-  inexact: Object.freeze({...inexact}),
-  NegNumber,
-};
+frozenInexact as {}; // Error: inexact -> exact
+
+frozenNegativeNumber.foo as 1; // error -1 ~> 1
+frozenNegativeNumber.foo as -1; // okay
+1 as typeof frozenNegativeNumber.foo; // error 1 ~> -1
+-1 as typeof frozenNegativeNumber.foo; // okay
