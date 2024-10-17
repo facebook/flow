4f740b8e6a556e99a08eaf389b1dccd52e1915cf
Sam Zhou <samzhou19815@meta.com>
2024-10-17 13:33:06 -0700

    [flow][test] Add test showing bad implicit instantiation inference wrt component type with non-spread props
    Summary:
    The current identity reversal doesn't remove the non-spread props. This test exposes it. Next diff will fix it.
    
    Changelog: [internal]
    
    Reviewed By: panagosg7
    
    Differential Revision: D64549306
    
    fbshipit-source-id: 7323a6c6966a78fdb42e88190fe282421596c609

--- tests/component_type/props_implicit_instantiation.js
+++ tests/component_type/props_implicit_instantiation.js
@@ -11,6 +11,11 @@ C as component(ref: React.RefSetter<Set<string>>, foo: string, bar: number); // 
 C as component(ref: React.RefSetter<Set<string>>, foo: string, bar: string); // error: string ~> number
 C as component(ref: React.RefSetter<Set<number>>, foo: string, bar: number); // error: number ~> string
 
-declare component B(foo: string, bar: number);
-const props = extractor(B); // ok
-props as {+foo: number, +bar: string}; // error: string != number
+declare component B(foo: string, bar: number); // bad misplaced error
+const props1 = extractor(B); // ok
+props1 as {+foo: number, +bar: string}; // error: string != number
+
+declare function removeSomeProps<Props: {...}>(c: component(foo: string, ...Props)): Props;
+const props2 = removeSomeProps(B); // ok
+props2 as {+bar: number}; // bad error: foo should be removed
+props2 as empty; // error
