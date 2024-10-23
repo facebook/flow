ebc9a767de8bf33f7bf040b7d7e44ef9599bd7cb
Sam Zhou <samzhou19815@meta.com>
2024-10-23 09:35:14 -0700

    [flow][libdef] Remove deprecated React.AbstractComponent alias
    Summary: Changelog: [errors] Deprecated `React.AbstractComponent` type is removed. We recommend replacing them with [component types](https://flow.org/en/docs/react/component-types/). Since v0.249.0, we have provided a codemod via the `flow-upgrade` package to aid larger codebases with this conversion: `yarn run flow-codemod eliminateAbstractComponent path/to/src`.
    
    Reviewed By: panagosg7
    
    Differential Revision: D64799392
    
    fbshipit-source-id: 97d50c4c014eb758318d6bd251c384bfd8851ce9

--- newtests/lsp/completion/component_syntax_auto_imports/__fixtures__/FooBarBaz5.js
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-component Foo() { return null }
-// React.AbstractComponent vs. React$AbstractComponent in FooBarBaz4
-const FooBarBaz: React.AbstractComponent<{}> = React.memo(Foo);
-export default FooBarBaz;
