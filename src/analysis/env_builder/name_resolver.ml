7321e2fe8f2e67b6994ec37de525712b86c00c98
Sam Zhou <samzhou19815@meta.com>
2024-11-18 12:18:38 -0800

    [flow] Fix non-undefined typeof refinement with optional chaining
    Summary:
    As said in the current comment, the current behavior is very wrong. We should only apply the refinement on optional chaining in the positive case (and the non-nullish instead of the truthy refinement).
    
    Changelog: [internal] Summarized at the end of the stack
    
    Reviewed By: panagosg7
    
    Differential Revision: D66039803
    
    fbshipit-source-id: fac59bfce84007614c9d0ddd5c8090807b4f832a

--- src/analysis/env_builder/name_resolver.ml
+++ src/analysis/env_builder/name_resolver.ml
@@ -5043,24 +5043,21 @@ module Make (Context : C) (FlowAPIUtils : F with type cx = Context.t) :
               refinement;
             if sense then this#negate_new_refinements ()
           ) else (
+            (* It's easier to reason about the following code if we only consider
+             * `typeof foo?.bar === 'string'` for now.
+             * First, we will have typeof refinement on `foo.bar`.
+             * In addition, we also have a separate implicit refinement on the optional chaining.
+             * Since we know that the prop foo.bar must not be nullish and thus `foo` must not be maybe.
+             *
+             * Now for the sense=false case, we simply negative everything at the end.
+             *)
             ignore
             @@ this#optional_chain
-               (* TODO(samzhou19815): This is very problematic.
-                * We add refinements to obj regardless of whether it's === or !==,
-                * and regardless of whether the type contains falsy values *)
                  ~can_refine_obj_to_non_maybe:true
-                 ~can_refine_obj_prop:OptionalChainingRefinement.CanApplyPropTruthyRefi
+                 ~can_refine_obj_prop:OptionalChainingRefinement.CanApplyPropNonNullishRefi
                  arg;
-            let refinement =
-              if sense then
-                ref
-              else
-                NotR ref
-            in
-            this#add_single_refinement
-              refinement_key
-              ~refining_locs:(L.LSet.singleton loc)
-              refinement
+            this#add_single_refinement refinement_key ~refining_locs:(L.LSet.singleton loc) ref;
+            if not sense then this#negate_new_refinements ()
           )
         | _ -> ignore @@ this#expression arg
 
