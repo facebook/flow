ef39f42e3560b2879821231d756ad8ea76f4884f
George Zahariev <gkz@meta.com>
2024-11-27 10:23:20 -0800

    [flow][match] Additional regression tests when parsing `match` in an expression position
    Summary:
    Additional regression tests when parsing `match` in an expression position. All of this parses correctly currently, unlike statements which we fix in the next diff.
    
    Changelog: [internal]
    
    Reviewed By: panagosg7
    
    Differential Revision: D66530671
    
    fbshipit-source-id: 60dd92a8239945005338d1f17adbe0fb37849c4b

--- src/parser/test/flow/match/as-identifier.js
+++ src/parser/test/flow/match/as-identifier.js
@@ -4,6 +4,8 @@ type match = number;
 }
 function match(match: match) {}
 
+// Expression position
+
 const a = match(1);
 
 const b = match(1).f();
@@ -12,6 +14,22 @@ const c = match(1)
 {
   // block statement
 }
+
+const d = match(1) + 2;
+
+const e = match(1)[2];
+
+const f = match(1) ? 2 : 3;
+
+const g = match(1) < 2;
+
+const h = match(1) && 2;
+
+const i = match();
+
+const j = match(...b);
+
+// Statement position
 
 match(1);
 
