00ca27ac1299703b1de796a03dc728b4c8bf40da
Panos Vekris <pvekris@meta.com>
2024-12-05 09:21:42 -0800

    [flow] add matching prop test
    Summary:
    adds test that exposes currently problematic behavior
    
    Changelog: [internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D66802063
    
    fbshipit-source-id: 3180d449525ce1f90c67caf58111eba7411b0033

--- tests/match_failure/disjoint_union.js
+++ tests/match_failure/disjoint_union.js
@@ -38,3 +38,135 @@ function area3(shape: ReadOnlyShape): number {
   }
   throw "unreachable"; // TODO: this shouldn't be needed
 }
+
+function singleton() {
+  const A: 'A' = 'A';
+  const B: 'B' = 'B';
+  const C: 'C' = 'C';
+  const D: 'D' = 'D';
+  const OBJ = Object.freeze({A, B, C, D});
+
+  type AorB = {type: 'A'} | {type: 'B'};
+  type AorC = {type: 'A'} | {type: 'C'};
+
+  function test1(x: AorB) {
+    switch (x.type) {
+      case A: break;
+      case B: break;
+      case C: break; // TODO error C is not included in 'A' | 'B'
+      case D: break; // TODO error D is not included in 'A' | 'B'
+    }
+  }
+
+  function test1a(x: AorB) {
+    switch (x.type) {
+      case OBJ.A: break;
+      case OBJ.B: break;
+      case OBJ.C: break; // TODO error C is not included in 'A' | 'B'
+      case OBJ.D: break; // TODO error D is not included in 'A' | 'B'
+    }
+  }
+
+  function test2(x: AorB) {
+    if (x.type === A) {};
+    if (x.type === B) {};
+    if (x.type === C) {}; // TODO error C is not included in 'A' | 'B'
+    if (x.type === D) {}; // TODO error D is not included in 'A' | 'B'
+  }
+
+  function test3(x: AorB & AorC) {
+    switch (x.type) {
+      case A: break;
+      case B: break; // TODO error B is not included in 'A'
+      case C: break; // TODO error C is not included in 'A'
+      case D: break; // TODO error D is not included in 'A'
+    }
+  }
+
+  function test4(x: AorB & AorC) {
+    if (x.type === A) {};
+    if (x.type === B) {}; // TODO error B is not included in 'A'
+    if (x.type === C) {}; // TODO error C is not included in 'A'
+    if (x.type === D) {}; // TODO error D is not included in 'A'
+  }
+
+  type AandP = {type: 'A', ...} & {prop: 1, ...};
+  type BandP = {type: 'B', ...} & {prop: 1, ...};
+
+  function test5(x: AandP | BandP) {
+    switch (x.type) {
+      case A: break;
+      case B: break;
+      case C: break; // TODO error C is not included in 'A'|'B'
+      case D: break; // TODO error D is not included in 'A'|'B'
+    }
+  }
+
+  function test6(x: AandP | BandP) {
+    if (x.type === A) {};
+    if (x.type === B) {};
+    if (x.type === C) {}; // TODO error C is not included in 'A'|'B'
+    if (x.type === D) {}; // TODO error D is not included in 'A'|'B'
+  }
+}
+
+function literal_types() {
+  const A = 'A';
+  const B = 'B';
+  const C = 'C';
+  const D = 'D';
+
+  type AorB = {type: 'A'} | {type: 'B'};
+  type AorC = {type: 'A'} | {type: 'C'};
+
+  function test1(x: AorB) {
+    switch (x.type) {
+      case A: break;
+      case B: break;
+      case C: break; // error C is not included in 'A' | 'B'
+      case D: break; // error D is not included in 'A' | 'B'
+    }
+  }
+
+  function test2(x: AorB) {
+    if (x.type === A) {};
+    if (x.type === B) {};
+    if (x.type === C) {}; // error C is not included in 'A' | 'B'
+    if (x.type === D) {}; // error D is not included in 'A' | 'B'
+  }
+
+  function test3(x: AorB & AorC) {
+    switch (x.type) {
+      case A: break; // TODO okay
+      case B: break; // error B is not included in 'A'
+      case C: break; // error C is not included in 'A'
+      case D: break; // error D is not included in 'A'
+    }
+  }
+
+  function test4(x: AorB & AorC) {
+    if (x.type === A) {}; // TODO okay
+    if (x.type === B) {}; // error B is not included in 'A'
+    if (x.type === C) {}; // error C is not included in 'A'
+    if (x.type === D) {}; // error D is not included in 'A'
+  }
+
+  type AandP = {type: 'A', ...} & {prop: 1, ...};
+  type BandP = {type: 'B', ...} & {prop: 1, ...};
+
+  function test5(x: AandP | BandP) {
+    switch (x.type) {
+      case A: break;
+      case B: break;
+      case C: break; // error C is not included in 'A'|'B'
+      case D: break; // error D is not included in 'A'|'B'
+    }
+  }
+
+  function test6(x: AandP | BandP) {
+    if (x.type === A) {};
+    if (x.type === B) {};
+    if (x.type === C) {}; // error C is not included in 'A'|'B'
+    if (x.type === D) {}; // error D is not included in 'A'|'B'
+  }
+}
