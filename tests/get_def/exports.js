38f4635a6177185fcd492cd6cf1756546d38c7a6
Sam Zhou <samzhou19815@meta.com>
2025-01-10 11:49:26 -0800

    [flow] Add get-def tests for `export * as Foo from '...'`
    Summary:
    It's not tested at all right now. This diff adds the test.
    
    Changelog: [internal]
    
    Reviewed By: panagosg7
    
    Differential Revision: D68002544
    
    fbshipit-source-id: d7a51f296863c52824cba344eedf5e5e1672bcd3

--- tests/get_def/exports.js
+++ tests/get_def/exports.js
@@ -14,12 +14,16 @@ export {foo as foo5} from "./helpers/exports_named.js";
 //                    ^
 export {foo as foo6} from "./helpers/exports_named.js";
 //                            ^
+export * as Foo7 from "./helpers/exports_named.js";
+//           ^
 export {foo as lib_foo1} from 'test_lib';
 //      ^
 export {foo as lib_foo2} from 'test_lib';
 //             ^
 export {foo as lib_foo3} from 'test_lib';
 //                              ^
+export * as LibFoo4 from 'test_lib';
+//               ^
 
 const bar = 0
 export {bar}
