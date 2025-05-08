c4baf4c10d3a08fa2b082af04f3bfd6e55d6d754
Sam Zhou <samzhou19815@meta.com>
2025-05-07 17:17:11 -0700

    [flow][test] Remove `module.system.haste.name_reducers=...` in tests: 4/n
    Summary:
    This diff migrates the final test that uses custom name reducers. We test the same thing by moving the file to another directory that will make it have the same name.
    
    After that, we can kill the flag.
    
    Changelog: [internal]
    
    Reviewed By: panagosg7
    
    Differential Revision: D74294609
    
    fbshipit-source-id: 130754889c00f88e2a38ea0de721f26fa273304d

--- tests/incremental_haste_duplicate/test.sh
+++ tests/incremental_haste_duplicate/test.sh
@@ -7,14 +7,15 @@
 printf "\nInitial status:\n"
 assert_ok "$FLOW" status --no-auto-start --strip-root .
 
-printf "\nCopy A.js to B.js:\n"
-cp A.js B.js
-assert_ok "$FLOW" force-recheck --no-auto-start A.js B.js
+printf "\nCopy A.js to nested/A.js:\n"
+mkdir nested
+cp A.js nested/A.js
+assert_ok "$FLOW" force-recheck --no-auto-start A.js nested/A.js
 assert_errors "$FLOW" status --no-auto-start --strip-root .
 
-printf "\nMove B.js to A.js:\n"
-mv B.js A.js
-assert_ok "$FLOW" force-recheck --no-auto-start A.js B.js
+printf "\nMove nested/A.js to A.js:\n"
+mv nested/A.js A.js
+assert_ok "$FLOW" force-recheck --no-auto-start A.js nested/A.js
 assert_ok "$FLOW" status --no-auto-start --strip-root .
 
 printf "\nDone!\n"
