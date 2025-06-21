51bdf8dd4951b3cbfea203c77a7fadbb2048ae09
George Zahariev <gkz@meta.com>
2025-06-20 21:41:37 -0700

    [flow][match] Add quickfix for fixing invalid match case syntax
    Summary:
    Add quickfix for fixing invalid match case syntax.
    
    If a `match` has more than one issue, a single error is emitted at the `match_keyword_loc` listing all the issues - so here we make sure to capture the logic for both that and single issue errors.
    
    Changelog: [internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D77057268
    
    fbshipit-source-id: b48c8b40c9565d85a8bbacbb873deb772a4dac0c

--- src/services/code_action/autofix_match_syntax.ml
+++ src/services/code_action/autofix_match_syntax.ml
@@ -11,6 +11,26 @@ type kind =
   | InvalidMatchStatementBody
   | InvalidBindingKind
   | InvalidWildcardSyntax
+  | InvalidCaseSyntax
+
+let is_invalid_case_syntax invalid_syntax =
+  let {
+    Flow_ast.Match.Case.InvalidSyntax.invalid_prefix_case;
+    invalid_infix_colon;
+    invalid_suffix_semicolon;
+  } =
+    invalid_syntax
+  in
+  Base.Option.is_some invalid_prefix_case
+  || Base.Option.is_some invalid_infix_colon
+  || Base.Option.is_some invalid_suffix_semicolon
+
+let empty_invalid_case_syntax =
+  {
+    Flow_ast.Match.Case.InvalidSyntax.invalid_prefix_case = None;
+    invalid_infix_colon = None;
+    invalid_suffix_semicolon = None;
+  }
 
 class mapper target_loc ~kind =
   object (this)
@@ -75,6 +95,44 @@ class mapper target_loc ~kind =
         | _ -> pattern
       in
       super#match_pattern pattern
+
+    method! match_ loc ~on_case_body x =
+      let open Flow_ast.Match in
+      let { match_keyword_loc; _ } = x in
+      let x =
+        if kind = InvalidCaseSyntax && this#is_target match_keyword_loc then
+          let { arg; cases; match_keyword_loc; comments } = x in
+          let cases =
+            Base.List.map cases ~f:(fun case ->
+                let (loc, { Case.pattern; body; guard; comments; invalid_syntax }) = case in
+                if is_invalid_case_syntax invalid_syntax then
+                  let invalid_syntax = empty_invalid_case_syntax in
+                  (loc, { Case.pattern; body; guard; comments; invalid_syntax })
+                else
+                  case
+            )
+          in
+          { arg; cases; match_keyword_loc; comments }
+        else
+          x
+      in
+      super#match_ loc ~on_case_body x
+
+    method! match_case ~on_case_body case =
+      let open Flow_ast.Match.Case in
+      let (loc, { pattern; body; guard; comments; invalid_syntax }) = case in
+      let case =
+        if
+          kind = InvalidCaseSyntax
+          && this#target_contained_by loc
+          && is_invalid_case_syntax invalid_syntax
+        then
+          let invalid_syntax = empty_invalid_case_syntax in
+          (loc, { pattern; body; guard; comments; invalid_syntax })
+        else
+          case
+      in
+      super#match_case ~on_case_body case
   end
 
 let convert_object_shorthand_to_const ast loc =
@@ -95,4 +153,8 @@ let fix_invalid_binding_kind ast loc =
 
 let fix_invalid_wildcard_syntax ast loc =
   let mapper = new mapper loc ~kind:InvalidWildcardSyntax in
+  mapper#program ast
+
+let fix_invalid_case_syntax ast loc =
+  let mapper = new mapper loc ~kind:InvalidCaseSyntax in
   mapper#program ast
