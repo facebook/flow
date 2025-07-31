c9d3c4b8f2a4c6ba17d97e90830354413a708243
Marco Wang <marcoww@meta.com>
2025-07-30 18:47:49 -0700

    [flow][invalid-compare] update snapshot tests for local_inference_annotations
    Reviewed By: gkz
    
    Differential Revision: D79219254
    
    fbshipit-source-id: 1067b3af42916bee9130fc5982dc7dbc83064998

--- tests/local_inference_annotations/switch.js
+++ tests/local_inference_annotations/switch.js
@@ -2,12 +2,12 @@
 
 // If the entire expression has an annot available then we should be able to push
 // the annotation of the return type down to the return expression, even
-// through switch statements 
+// through switch statements
 let a = ((x => {
   switch ((x) => true){ // Guards are not covered by the annot
-    case true:
+    case (x: number) => true:
       return y => 3;
-    case false: {
+    case (x: number) => false: {
       const z = (x) => 3; // Non-return statements are not covered by the annot
       return y => 3;
     }
