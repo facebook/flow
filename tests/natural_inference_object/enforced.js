5b29129e4316337cb5d2f4c0c76dc2226da435ed
Sam Zhou <samzhou19815@meta.com>
2025-08-26 06:08:38 -0700

    [flow][tests] Turn on `experimental.invariant_subtyping_error_message_improvement` in `natural_inference_object`
    Summary:
    It helps to easily show that we don't show the more helpful variants in all the cases we hope to show.
    
    Changelog: [internal]
    
    Reviewed By: panagosg7
    
    Differential Revision: D80968526
    
    fbshipit-source-id: 9f7b2109278c7e5599ea7342793bb6d706a532ca

--- tests/natural_inference_object/enforced.js
+++ tests/natural_inference_object/enforced.js
@@ -1,5 +1,8 @@
 const foo = {};
-foo as {bar?: string}; // error
+foo as {bar?: string}; // error. TODO: should show error with suggestions
 
 const bar = {baz: 1};
 bar as {baz: string | number}; // error
+
+const bazArray = [{baz: 1}];
+bazArray as Array<{baz: string | number}>; // error. TODO: should show error with suggestions
