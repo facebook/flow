d13668694640b77503013aec2896af230173f408
Marco Wang <marcoww@meta.com>
2025-09-08 19:28:50 -0700

    [flow][TS Syntax] Add support for `ReadOnlyWeakMap` and `ReadOnlyWeakSet`
    Summary:
    The existing support of `$ReadOnlyWeakMap` and `$ReadOnlyWeakSet` are added through lib definitions instead of flow binary. This diff adds the `ReadOnlyWeakMap` and `ReadOnlyWeakSet` support in core.js, add tests in read_only_weak_map test and read_only_weak_set and updates all other snapshot tests.
    
    This should not be a breaking change. I checked other exp files and it's just line number changes
    
    Changelog: [internal]
    
    Reviewed By: gkz
    
    Differential Revision: D81970081
    
    fbshipit-source-id: 7c8832cd0058bcc39eaebe00d92b696b0628490d

--- tests/read_only_weak_map/test.js
+++ tests/read_only_weak_map/test.js
@@ -29,3 +29,36 @@ class BazV<K: {}, -V> {
         return new WeakMap();
     }
 }
+
+
+{
+    class Bar<K: {}, V> {
+        good (x: number) : ReadOnlyWeakMap<K, V> {
+            return new WeakMap(); // Fine, Invariant in invariant position
+        }
+    }
+
+    class FooK<+K: {}, V> {
+        bad (x: number) : ReadOnlyWeakMap<K, V> { // Error: K in invariant position
+            return new WeakMap();
+        }
+    }
+
+    class FooV<K: {}, +V> {
+        bad (x: number) : ReadOnlyWeakMap<K, V> { // Fine, V in covarint position
+            return new WeakMap();
+        }
+    }
+
+    class BazK<-K: {}, V> {
+        bad (x: number) : ReadOnlyWeakMap<K, V> {// Error: K in covariant position
+            return new WeakMap();
+        }
+    }
+
+    class BazV<K: {}, -V> {
+        bad (x: number) : ReadOnlyWeakMap<K, V> {// Error: V in covariant position
+            return new WeakMap();
+        }
+    }
+}
--- tests/read_only_weak_set/test.js
+++ tests/read_only_weak_set/test.js
@@ -17,3 +17,23 @@ class Baz<-T: {}> {
         return new WeakSet();
     }
 }
+
+{
+    class Bar<T: {}> {
+        good (x: number) : ReadOnlyWeakSet<T> { // Fine, invariant in invariant position
+            return new WeakSet();
+        }
+    }
+
+    class Foo<+T: {}> {
+        bad (x: number) : ReadOnlyWeakSet<T> { // Error: T in invariant position
+            return new WeakSet();
+        }
+    }
+
+    class Baz<-T: {}> {
+        bad (x: number) : ReadOnlyWeakSet<T> {// Error: T in invariant position
+            return new WeakSet();
+        }
+    }
+}
