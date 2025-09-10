36301ad1a394a29d465aabf2a7fcd8e36cc35c78
Marco Wang <marcoww@meta.com>
2025-09-09 21:00:39 -0700

    [core.js] Update ReadOnlyWeakSet to ReadonlyWeakSet
    Summary:
    Following TS pattern to make it `Readonly` instead of `ReadOnly`.
    
    Maybe we can even drop support for ReadonlyWeakSet and ReadonlyWeakMap because typescript doesn't support them. There's only one use case of ReadOnlyWeakMap in https://www.internalfb.com/code/search?q=ReadonlyWeakMap and zero use case of ReadonlyWeakSet
    
    Changelog: [internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D82073298
    
    fbshipit-source-id: 3f5e5b375907f978965b7fda9a0279612a88827e

--- tests/read_only_weak_map/test.js
+++ tests/read_only_weak_map/test.js
@@ -33,31 +33,31 @@ class BazV<K: {}, -V> {
 
 {
     class Bar<K: {}, V> {
-        good (x: number) : ReadOnlyWeakMap<K, V> {
+        good (x: number) : ReadonlyWeakMap<K, V> {
             return new WeakMap(); // Fine, Invariant in invariant position
         }
     }
 
     class FooK<+K: {}, V> {
-        bad (x: number) : ReadOnlyWeakMap<K, V> { // Error: K in invariant position
+        bad (x: number) : ReadonlyWeakMap<K, V> { // Error: K in invariant position
             return new WeakMap();
         }
     }
 
     class FooV<K: {}, +V> {
-        bad (x: number) : ReadOnlyWeakMap<K, V> { // Fine, V in covarint position
+        bad (x: number) : ReadonlyWeakMap<K, V> { // Fine, V in covarint position
             return new WeakMap();
         }
     }
 
     class BazK<-K: {}, V> {
-        bad (x: number) : ReadOnlyWeakMap<K, V> {// Error: K in covariant position
+        bad (x: number) : ReadonlyWeakMap<K, V> {// Error: K in covariant position
             return new WeakMap();
         }
     }
 
     class BazV<K: {}, -V> {
-        bad (x: number) : ReadOnlyWeakMap<K, V> {// Error: V in covariant position
+        bad (x: number) : ReadonlyWeakMap<K, V> {// Error: V in covariant position
             return new WeakMap();
         }
     }
