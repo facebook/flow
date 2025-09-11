8287377288d527085f7805f95efdfa373dcd8c03
Panos Vekris <pvekris@meta.com>
2025-09-11 11:50:52 -0700

    [flow] Add tests with typeof x in function return type
    Summary: Changelog: [internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D82193100
    
    fbshipit-source-id: 6d90f6a86a4e918b976d6d7e0f38a5b4dd49cca1

--- tests/typeof/function_typeof_import.js
+++ tests/typeof/function_typeof_import.js
@@ -1,6 +1,6 @@
 // @flow
 
-import {f1, f2, f3, f4, poly1, poly2, poly3} from './function_typeof.js';
+import {f1, f2, f3, f4, poly1, poly2, poly3, r1, r2, r3, r4, r5, r6, r7, r8, r9} from './function_typeof.js';
 
 f1 as (x: mixed) => void; // ok because f1 is (x: any) => void
 f2 as (x: number, y: number) => void; // ok
@@ -33,3 +33,35 @@ poly2({f: 1}, 'a'); // error string ~> number
 
 poly3({f: 1}, 3); // TODO okay
 poly3({f: 1}, 'a'); // error (TODO string ~> number)
+
+r1 as (x: number) => number; // ok
+r1 as (x: number) => string; // TODO error
+
+r2 as (x: {f: number}) => number; // ok
+r2 as (x: {f: number}) => string; // TODO error
+
+r3 as (...x: Array<number>) => Array<number>; // ok
+
+r4 as <X>(x: X) => X; // ok
+r4 as <X>(x: X) => empty // TODO error
+
+r5 as <X>(x: X) => {f: X}; // ok
+r5 as <X>(x: X) => {f: empty}; // TODO error
+
+r6 as <X: {f: number}>(x: X) => number; // ok
+r6 as <X: {f: number}>(x: X) => string; // TODO error
+
+r7 as <V, X: {f: V}>(x: X) => V; // ok
+r7 as <V, X: {f: V}>(x: X) => empty; // TODO error
+
+r8 as <X>(...x: Array<X>) => Array<X>; // ok
+r8 as <X>(...x: Array<X>) => Array<empty>; // TODO error
+
+{
+    const x9_1 = r9(true); // inferred as 2
+    x9_1 as 1; // TODO error 2 ~> 1
+    x9_1 as 2; // TODO okay
+    const x9_2 = r9(false); // inferred as 2
+    x9_2 as 1; // TODO error 2 ~> 1
+    x9_2 as 2; // TODO okay
+}
