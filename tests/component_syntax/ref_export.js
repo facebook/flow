6411f4c18ba8ba541ffa3a8e2c5dad4fca6e9bbb
Sam Zhou <samzhou19815@meta.com>
2025-09-16 19:52:05 -0700

    [flow][tests] Prepare some tests to work under both modes of ref-as-prop ny making the ref prop optional
    Summary: Changelog: [internal]
    
    Reviewed By: jbrown215
    
    Differential Revision: D82350299
    
    fbshipit-source-id: f5791b4aa2fe91d79622111012ea71f392e0bbb2

--- tests/component_syntax/ref_export.js
+++ tests/component_syntax/ref_export.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
 
-export component MyExportedInput(other: string, ref: React$RefSetter<?CommonInstance>) {
+export component MyExportedInput(other: string, ref?: React.RefSetter<?CommonInstance>) {
     return <input id={other} ref={ref} />
 }
