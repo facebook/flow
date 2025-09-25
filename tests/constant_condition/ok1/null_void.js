11fd3e261d936d962c4e2d6d0a19596b312f2f0e
Sam Zhou <samzhou19815@meta.com>
2025-09-24 21:33:35 -0700

    [flow][tests] Remove ok directory in constant_condition test suite
    Summary:
    This directory is not included in any of the constant condition checks. As I am going to remove per-directory support, let's remove this first.
    
    Changelog: [internal]
    
    Reviewed By: panagosg7
    
    Differential Revision: D83211747
    
    fbshipit-source-id: b61a4d0f7b7907010d9cca682500b8408de566c9

--- tests/constant_condition/ok1/null_void.js
+++
@@ -1,9 +0,0 @@
-{
-    declare var x: ?{};
-    if (!x) {
-        if (x) {} // ok, x is refined to `null | void`, but directory is turned off for null and void
-    }
-
-    let y: Array<number>;
-    if (y) {} // error, y is undefined, but directory is turned off for null and void
-}
