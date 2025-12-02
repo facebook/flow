f5e39182d9a0fed1cde343286320cbf581a7c732
Panos Vekris <pvekris@meta.com>
2025-12-02 09:20:43 -0800

    [flow] add `flow full-check` command
    Summary:
    This diff adds an alias to `flow check` called `flow full-check`.
    
    The goal is to eventually make `flow check` be an alias of `flow status`. This diff takes the first step so we can migrate CI to use the new command instead.
    
    Changelog: [internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D87963629
    
    fbshipit-source-id: 3e2b34404ca00828e7dc081877aa1f44f1d7627e

--- src/commands/checkCommands.ml
+++ src/commands/checkCommands.ml
@@ -83,6 +83,74 @@ let format_errors
     else
       print_errors None
 
+let full_check_main
+    base_flags
+    error_flags
+    options_flags
+    json
+    pretty
+    json_version
+    offset_style
+    shm_flags
+    ignore_version
+    path_opt
+    () =
+  let flowconfig_name = base_flags.Base_flags.flowconfig_name in
+  let root = CommandUtils.guess_root flowconfig_name path_opt in
+  let (flowconfig, flowconfig_hash) =
+    let flowconfig_path = Server_files_js.config_file flowconfig_name root in
+    read_config_and_hash_or_exit ~enforce_warnings:(not ignore_version) flowconfig_path
+  in
+  let options =
+    let lazy_mode = Some FlowConfig.Non_lazy in
+    (* Saved state doesn't make sense for `flow check`, so disable it. *)
+    let saved_state_options_flags =
+      {
+        (* None would mean we would just use the value in the .flowconfig, if available.
+         * Instead, let's override that and turn off saved state entirely. *)
+        CommandUtils.Saved_state_flags.saved_state_fetcher = Some Options.Dummy_fetcher;
+        saved_state_force_recheck = false;
+        saved_state_no_fallback = false;
+        saved_state_skip_version_check = false;
+        saved_state_verify = false;
+      }
+    in
+
+    make_options
+      ~flowconfig_name
+      ~flowconfig_hash
+      ~flowconfig
+      ~lazy_mode
+      ~root
+      ~options_flags
+      ~saved_state_options_flags
+  in
+  let init_id = Random_id.short_string () in
+  let offset_kind = CommandUtils.offset_kind_of_offset_style offset_style in
+  (* initialize loggers before doing too much, especially anything that might exit *)
+  LoggingUtils.init_loggers ~options ~min_level:Hh_logger.Level.Error ();
+
+  if not ignore_version then assert_version flowconfig;
+
+  let shared_mem_config = shm_config shm_flags flowconfig in
+  let format_errors =
+    let client_include_warnings = error_flags.Flow_errors_utils.Cli_output.include_warnings in
+    let printer =
+      if json || Base.Option.is_some json_version || pretty then
+        Json { pretty; version = json_version }
+      else
+        Cli error_flags
+    in
+    format_errors ~printer ~client_include_warnings ~offset_kind options
+  in
+  let (errors, warnings) = Server.check_once options ~init_id ~shared_mem_config ~format_errors in
+  Exit.exit
+    (get_check_or_status_exit_code
+       errors
+       warnings
+       error_flags.Flow_errors_utils.Cli_output.max_warnings
+    )
+
 module CheckCommand = struct
   let spec =
     {
@@ -108,75 +176,35 @@ module CheckCommand = struct
           exe_name;
     }
 
-  let main
-      base_flags
-      error_flags
-      options_flags
-      json
-      pretty
-      json_version
-      offset_style
-      shm_flags
-      ignore_version
-      path_opt
-      () =
-    let flowconfig_name = base_flags.Base_flags.flowconfig_name in
-    let root = CommandUtils.guess_root flowconfig_name path_opt in
-    let (flowconfig, flowconfig_hash) =
-      let flowconfig_path = Server_files_js.config_file flowconfig_name root in
-      read_config_and_hash_or_exit ~enforce_warnings:(not ignore_version) flowconfig_path
-    in
-    let options =
-      let lazy_mode = Some FlowConfig.Non_lazy in
-      (* Saved state doesn't make sense for `flow check`, so disable it. *)
-      let saved_state_options_flags =
-        {
-          (* None would mean we would just use the value in the .flowconfig, if available.
-           * Instead, let's override that and turn off saved state entirely. *)
-          CommandUtils.Saved_state_flags.saved_state_fetcher = Some Options.Dummy_fetcher;
-          saved_state_force_recheck = false;
-          saved_state_no_fallback = false;
-          saved_state_skip_version_check = false;
-          saved_state_verify = false;
-        }
-      in
+  let command = CommandSpec.command spec full_check_main
+end
 
-      make_options
-        ~flowconfig_name
-        ~flowconfig_hash
-        ~flowconfig
-        ~lazy_mode
-        ~root
-        ~options_flags
-        ~saved_state_options_flags
-    in
-    let init_id = Random_id.short_string () in
-    let offset_kind = CommandUtils.offset_kind_of_offset_style offset_style in
-    (* initialize loggers before doing too much, especially anything that might exit *)
-    LoggingUtils.init_loggers ~options ~min_level:Hh_logger.Level.Error ();
+module FullCheckCommand = struct
+  let spec =
+    {
+      CommandSpec.name = "full-check";
+      doc = "Does a full Flow check in the foreground and prints the results";
+      args =
+        CommandSpec.ArgSpec.(
+          empty
+          |> base_flags
+          |> error_flags
+          |> options_and_json_flags
+          |> json_version_flag
+          |> offset_style_flag
+          |> shm_flags
+          |> ignore_version_flag
+          |> from_flag
+          |> no_cgroup_flag
+          |> anon "root" (optional string)
+        );
+      usage =
+        Printf.sprintf
+          "Usage: %s full-check [OPTION]... [ROOT]\n\nDoes a full Flow check in the foreground and prints the results.\n\nThis command runs the type checker directly in the foreground without connecting to a Flow server.\n\nFlow will search upward for a .flowconfig file, beginning at ROOT.\nROOT is assumed to be the current directory if unspecified.\n"
+          exe_name;
+    }
 
-    if not ignore_version then assert_version flowconfig;
-
-    let shared_mem_config = shm_config shm_flags flowconfig in
-    let format_errors =
-      let client_include_warnings = error_flags.Flow_errors_utils.Cli_output.include_warnings in
-      let printer =
-        if json || Base.Option.is_some json_version || pretty then
-          Json { pretty; version = json_version }
-        else
-          Cli error_flags
-      in
-      format_errors ~printer ~client_include_warnings ~offset_kind options
-    in
-    let (errors, warnings) = Server.check_once options ~init_id ~shared_mem_config ~format_errors in
-    Exit.exit
-      (get_check_or_status_exit_code
-         errors
-         warnings
-         error_flags.Flow_errors_utils.Cli_output.max_warnings
-      )
-
-  let command = CommandSpec.command spec main
+  let command = CommandSpec.command spec full_check_main
 end
 
 module FocusCheckCommand = struct
