807ba09329993ae169e0020539903672c4ec1d6b
Panos Vekris <pvekris@meta.com>
2026-01-20 22:02:28 -0800

    [flow][edenfs] Phase 1.3: create OSS stubs for EdenFS watcher
    Summary: Changelog: [internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D90143082
    
    fbshipit-source-id: c621308ca45aef29bb1b9e722d2c16d2438c9faf

---
+++ src/hack_forked/stubs/edenfs_watcher/edenfs_watcher.ml
@@ -0,0 +1,25 @@
+(*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *)
+
+type clock = string [@@deriving show, eq]
+
+type instance_handle_ffi
+
+type instance = { handle: instance_handle_ffi }
+
+type edenfs_watcher_error = Edenfs_watcher_types.edenfs_watcher_error [@@deriving show]
+
+type changes = Edenfs_watcher_types.changes [@@deriving show]
+
+let init (_settings : Edenfs_watcher_types.settings) = failwith "not implemented"
+
+let get_changes_async (_instance : instance) :
+    (changes list * clock * Hh_json.json option, edenfs_watcher_error) result =
+  failwith "not implemented"
+
+let get_notification_fd (_instance : instance) : (Unix.file_descr, edenfs_watcher_error) result =
+  failwith "not implemented"
---
+++ src/hack_forked/stubs/edenfs_watcher/edenfs_watcher_types.ml
@@ -0,0 +1,93 @@
+(*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *)
+
+type translation_telemetry = {
+  commit_transition_count: int;
+  commit_transition_duration: int;
+  directory_rename_count: int;
+  directory_rename_duration: int;
+  raw_changes_count: int;
+  translated_files_count: int;
+  duration: int;
+}
+
+let yojson_of_translation_telemetry _ = failwith "not implemented"
+
+type async_telemetry = {
+  worker_restart_count: int;
+  notification_count: int;
+  aggregated_translation_telemetry: translation_telemetry;
+}
+
+let yojson_of_async_telemetry _ = failwith "not implemented"
+
+type instance_get_changes_sync_telemetry = {
+  duration: int;
+  eden_get_changes_since_duration: int;
+  async_telemetry: async_telemetry;
+  worker_reset_duration: int;
+  translation_telemetry: translation_telemetry option;
+}
+
+let yojson_of_instance_get_changes_sync_telemetry _ = failwith "not implemented"
+
+type instance_get_changes_async_telemetry = {
+  duration: int;
+  async_telemetry: async_telemetry;
+}
+
+let yojson_of_instance_get_changes_async_telemetry _ = failwith "not implemented"
+
+type instance_get_all_files_telemetry = {
+  duration: int;
+  eden_glob_files_duration: int;
+  post_processing_duration: int;
+}
+
+let yojson_of_instance_get_all_files_telemetry _ = failwith "not implemented"
+
+type standalone_get_changes_since_telemetry = {
+  duration: int;
+  setup_duration: int;
+  eden_get_changes_since_duration: int;
+  translation_telemetry: translation_telemetry option;
+}
+
+let yojson_of_standalone_get_changes_since_telemetry _ = failwith "not implemented"
+
+type watch_spec = {
+  extensions: string list;
+  file_names: string list;
+}
+
+type settings = {
+  root: File_path.t;
+  watch_spec: watch_spec;
+  debug_logging: bool;
+  timeout_secs: int;
+  throttle_time_ms: int;
+  report_telemetry: bool;
+  state_tracking: bool;
+  sync_queries_obey_deferral: bool;
+}
+
+type changes =
+  | FileChanges of string list
+  | CommitTransition of {
+      from_commit: string;
+      to_commit: string;
+      file_changes: string list;
+    }
+  | StateEnter of string
+  | StateLeave of string
+[@@deriving show]
+
+type edenfs_watcher_error =
+  | EdenfsWatcherError of string
+  | LostChanges of string
+  | NonEdenMount
+[@@deriving show]
