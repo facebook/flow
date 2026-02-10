85222f7ddb524185d0250474b4aa73f6e9072c2b
Dagur Páll Ammendrup <dagurp@gmail.com>
2026-02-10 09:58:52 +0000

    Add fetchPriority to Image, Src and Link elements in html.js

--- packages/flow-dev-tools/flow-typed/environments/html.js
+++ packages/flow-dev-tools/flow-typed/environments/html.js
@@ -809,6 +809,7 @@ declare class HTMLImageElement extends HTMLElement {
   isMap: boolean;
   naturalHeight: number; // readonly
   naturalWidth: number; // readonly
+  fetchPriority: "high" | "low" | "auto";
   sizes: string;
   src: string;
   srcset: string;
@@ -1281,6 +1282,7 @@ declare class HTMLLinkElement extends HTMLElement {
   href: string;
   hreflang: string;
   media: string;
+  fetchPriority: "high" | "low" | "auto";
   rel: string;
   sizes: DOMTokenList;
   type: string;
@@ -1293,6 +1295,7 @@ declare class HTMLScriptElement extends HTMLElement {
   charset: string;
   crossOrigin?: string;
   defer: boolean;
+  fetchPriority: "high" | "low" | "auto";
   // flowlint unsafe-getters-setters:off
   get src(): string;
   set src(value: string | TrustedScriptURL): void;
