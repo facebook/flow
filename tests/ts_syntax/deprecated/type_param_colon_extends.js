31e8b2e26d9aa106390831ef97985ea0ae1ceb89
Marco Wang <marcoww@meta.com>
2026-02-20 21:57:35 -0800

    [flow] Add a test to describe a bug in `:` style extends
    Summary:
    A bug is discovered in when auditting erroring files (P2197478516). This diff adds the test and the next diff fixes it.
    
    Changelog:[internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D93928214
    
    fbshipit-source-id: 8a29a7c9361cad254fd16a43d8c76e88718c44bf

--- tests/ts_syntax/deprecated/type_param_colon_extends.js
+++ tests/ts_syntax/deprecated/type_param_colon_extends.js
@@ -2,3 +2,8 @@ type A<T: string> = [T]; // ERROR
 type B<+T: string> = [T]; // ERROR
 type C<T: string = "hi"> = [T]; // ERROR
 type D<+T: string = "hi"> = [T]; // ERROR
+
+export function intersectArrays<T>(arr1: Array<T>, arr2: Array<T>): Array<T> {
+  const set1 = new Set(arr1);
+  return arr2.filter(item => set1.has(item));
+}
