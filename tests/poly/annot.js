e6aab012820eb93e1514c326aecb03b2def6e80a
Marco Wang <marcoww@meta.com>
2026-04-10 14:09:58 -0700

    [flow][tests] Transform legacy utility types in poly
    Summary:
    Codemod legacy Flow utility types to modern equivalents in flow test files.
    
    Changelog:[internal]
    
    ___
    
    overriding_review_checks_triggers_an_audit_and_retroactive_review
    Oncall Short Name: flow
    
    Differential Revision: D100368678
    
    fbshipit-source-id: cf4f39e4180aa95e67a53e6ee84c7deb3467ef47

--- tests/poly/annot.js
+++ tests/poly/annot.js
@@ -1,7 +1,8 @@
-class A<X> { }
-new A<mixed>;
-class B extends A { } // OK, same as above
+class A<X> {}
+new A<unknown>();
+class B extends A {} // OK, same as above
 
-function foo(b: boolean): A<any> { // ok but unsafe, caller may assume any type arg
-  return b ? (new A: A<number>): (new A: A<string>);
+function foo(b: boolean): A<any> {
+  // ok but unsafe, caller may assume any type arg
+  return b ? (new A(): A<number>) : (new A(): A<string>);
 }
