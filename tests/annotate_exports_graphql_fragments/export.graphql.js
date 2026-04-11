e806f69f9221327e9266b6d14d109b859d1204d7
Marco Wang <marcoww@meta.com>
2026-04-10 23:12:36 -0700

    [flow][tests] Transform legacy utility types in annotate_exports_graphql_fragments
    Summary:
    Codemod legacy Flow utility types to modern equivalents. Snapshots re-recorded.
    
    Changelog:[internal]
    
    Reviewed By: gkz
    
    Differential Revision: D100408429
    
    fbshipit-source-id: fb0859056e8cf3ba4345bf1cff72b7fdaeefa11f

--- tests/annotate_exports_graphql_fragments/export.graphql.js
+++ tests/annotate_exports_graphql_fragments/export.graphql.js
@@ -1,25 +1,25 @@
 // @flow
 
 export type Foo = {|
-    +f?: {|
-        +g: {|
-            +h: ?$ReadOnlyArray<{|
-                +i: {||}
-            |}>
-        |};
-    |}
+  +f?: {|
+    +g: {|
+      +h: ?ReadonlyArray<{|
+        +i: {||},
+      |}>,
+    |},
+  |},
 |};
 
 declare export function foo(): Foo;
 
 export type Bar = {|
-    +f: {|
-        +g: {|
-            +h: $ReadOnlyArray<{|
-                +i: {||}
-            |}>
-        |};
-    |}
+  +f: {|
+    +g: {|
+      +h: ReadonlyArray<{|
+        +i: {||},
+      |}>,
+    |},
+  |},
 |};
 
 declare export function bar(): Bar;
