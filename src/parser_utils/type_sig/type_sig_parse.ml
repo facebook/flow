d98d355ed7a46c5fda874664c3b8cf9a431e7f0a
Marco Wang <marcoww@meta.com>
2026-04-11 00:51:07 -0700

    [flow] Remove dead Values ts_utility_syntax guard in type_sig_parse
    Summary:
    `ts_utility_syntax` defaults to true and nobody sets it to false, so the `else Annot (Any loc)` branch for Values can never execute.
    
    Changelog: [internal]
    
    ___
    
    overriding_review_checks_triggers_an_audit_and_retroactive_review
    Oncall Short Name: flow
    
    Differential Revision: D100422727
    
    fbshipit-source-id: b6d72c10b02cf43fc9841a3379b8c88386e3ed76

--- src/parser_utils/type_sig/type_sig_parse.ml
+++ src/parser_utils/type_sig/type_sig_parse.ml
@@ -3174,14 +3174,11 @@ and maybe_special_unqualified_generic opts scope tbls xs loc targs ref_loc =
     | _ -> Err (loc, CheckError)
   end
   | "Values" -> begin
-    if opts.enable_ts_syntax || opts.enable_ts_utility_syntax then
-      match targs with
-      | Some (_, { arguments = [t]; _ }) ->
-        let t = annot opts scope tbls xs t in
-        Annot (Values (loc, t))
-      | _ -> Err (loc, CheckError)
-    else
-      Annot (Any loc)
+    match targs with
+    | Some (_, { arguments = [t]; _ }) ->
+      let t = annot opts scope tbls xs t in
+      Annot (Values (loc, t))
+    | _ -> Err (loc, CheckError)
   end
   | "$Exact" -> begin
     match targs with
