94b718e88ca6a1cfe0216c376c5ae8050801fadb
Marco Wang <marcoww@meta.com>
2026-04-17 10:58:54 -0700

    [flow] Change `+/-` to `in/out` in type_alias_param_variance tests
    Summary:
    Changelog:[internal]
    
    4 errors each — identical. +T → out T, -T → in T, same variance enforcement.
    
    Snapshot tests are expected
    
    ___
    
    overriding_review_checks_triggers_an_audit_and_retroactive_review
    Oncall Short Name: flow
    
    Differential Revision: D101258720
    
    fbshipit-source-id: dc30ebb0e5a99666987b031faa294820c66efe97

--- tests/type_alias_param_variance/test_keyword.js
+++ tests/type_alias_param_variance/test_keyword.js
@@ -1,13 +1,13 @@
 class Foo<T> {}
-class Bar<+T> {}
-class Baz<-T> {}
+class Bar<out T> {}
+class Baz<in T> {}
 
-type A<-T> = Foo<T>; // Error, Foo expects invariant type
-type B<+T> = Foo<T>; // Error, Foo expects invariant type
+type A<in T> = Foo<T>; // Error, Foo expects invariant type
+type B<out T> = Foo<T>; // Error, Foo expects invariant type
 type C<T> = Foo<T>; // Ok!
-type D<-T> = Bar<T>; // Error, Bar expects covariant type
+type D<in T> = Bar<T>; // Error, Bar expects covariant type
 type E<T> = Bar<T>; // Ok! (invariant can flow to anything)
-type F<+T> = Bar<T>; // Ok!
-type G<-T> = Baz<T>; // Ok!
+type F<out T> = Bar<T>; // Ok!
+type G<in T> = Baz<T>; // Ok!
 type H<T> = Baz<T>; // Ok! (invariant can flow to anything)
-type I<+T> = Baz<T>; // Error, Baz expects contravariant type
+type I<out T> = Baz<T>; // Error, Baz expects contravariant type
