7af38da6120cb0bb4aa601560ffaaf2d3b0b15eb
Marco Wang <marcoww@meta.com>
2026-04-16 21:56:30 -0700

    [flow] Add more tests for in / out
    Summary:
    Changelog:[internal]
    
    Add more tests for in and out variance
    
    ___
    
    overriding_review_checks_triggers_an_audit_and_retroactive_review
    Oncall Short Name: flow
    
    Differential Revision: D101252498
    
    fbshipit-source-id: efedf2dc4a7e4afb184071e94b819c92d5be35e5

--- tests/ts_syntax/in_out_variance.js
+++ tests/ts_syntax/in_out_variance.js
@@ -5,3 +5,18 @@ declare function f<in out A>(x: A): A; // ERROR
 declare function f<in>(x: in): void; // OK
 declare function f<out>(x: out): void; // OK
 const out = 1; // OK
+
+type Contravariant<in A> = (A) => void; // OK
+type Covariant<out A> = {readonly prop: A}; // OK
+interface ContravariantInterface<in A> { method(A): void } // OK
+interface CovariantInterface<out A> { readonly prop: A } // OK
+type ContravariantWriteonly<in A> = {-prop: A}; // OK // TODO: replace - with writeonly once implemented in the parser
+
+type BadOut<out A> = (A) => void; // ERROR: out in contravariant position
+type BadIn<in A> = {readonly prop: A}; // ERROR: in in covariant position
+interface BadOutInterface<out A> { method(A): void } // ERROR: out in contravariant position
+interface BadInInterface<in A> { readonly prop: A } // ERROR: in in covariant position
+type BadInWriteonly<out A> = {-prop: A}; // ERROR: out in contravariant position // TODO: replace - with writeonly once implemented
+
+type InOut<in out A> = A; // ERROR: in out is always unsupported
+interface InOutInterface<in out A> { prop: A } // ERROR: in out is always unsupported
