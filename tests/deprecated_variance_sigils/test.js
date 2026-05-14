cd566b8c67c1f5995ae30873c7250b993a7daf6b
Marco Wang <marcoww@meta.com>
2026-05-14 15:29:37 -0700

    [flow] Add a few more tests for deprecated variance errors
    Summary:
    Currently there's only 4 errors, which is very flimsy. Adding more errors to cover different cases. This also helps building the new eslint tool so AI knows what to error on
    
    Changelog:[internal]
    
    ___
    
    overriding_review_checks_triggers_an_audit_and_retroactive_review
    Oncall Short Name: flow
    
    Differential Revision: D105229115
    
    fbshipit-source-id: cbbbe8ac4aefee6571b97ca03998e9886b28c3bd

--- tests/deprecated_variance_sigils/test.js
+++ tests/deprecated_variance_sigils/test.js
@@ -8,6 +8,25 @@ type ContraObj = { -p: number }; // error: - deprecated
 class A<+T> {} // error: + deprecated
 class B<-T> {} // error: - deprecated
 
+type CovIdx = { +[k: string]: number }; // error: + deprecated
+type ContraIdx = { -[k: string]: number }; // error: - deprecated
+
+class CField {
+  +covField: number; // error: + deprecated
+  -contraField: number; // error: - deprecated
+}
+
+type Alias<+T> = T; // error: + deprecated
+type AliasNeg<-T> = (T) => void; // error: - deprecated
+
+interface ICov<+T> {} // error: + deprecated
+interface IContra<-T> {} // error: - deprecated
+
+interface IProp {
+  +covProp: number; // error: + deprecated
+  -contraProp: number; // error: - deprecated
+}
+
 // Keywords still pass when allow_variance_keywords=true.
 type W = { writeonly p: number }; // ok
 class CIn<in T> {} // ok
