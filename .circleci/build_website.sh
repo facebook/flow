f0bb780e426451f9f9a749aa72f855916a3548a5
Sam Zhou <samzhou19815@meta.com>
2026-06-19 14:09:49 -0700

    [flow][website] Disable logging to speed up build script
    Reviewed By: gkz
    
    Differential Revision: D109170736
    
    fbshipit-source-id: 0590c3885441020447cf0c19afbce0926624f67f

--- .circleci/build_website.sh
+++ .circleci/build_website.sh
@@ -10,4 +10,4 @@ mkdir -p "website/static/flow/master"
 cp "dist/flow.js" "website/static/flow/master/flow.js"
 rm -rf "website/static/flow/master/flowlib"
 cp -r "lib" "website/static/flow/master/flowlib"
-cd website && INCLUDE_PAST_RELEASES=1 FLOW_MAX_WORKERS=2 DOCUSAURUS_SSR_CONCURRENCY=2 yarn build
+cd website && INCLUDE_PAST_RELEASES=1 IN_FLOW_TEST=1 FLOW_MAX_WORKERS=2 DOCUSAURUS_SSR_CONCURRENCY=2 yarn build
