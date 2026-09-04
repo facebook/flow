00a90c7cb43004dd9c43c69412e99e4f4d1c1767
Panos Vekris <pvekris@meta.com>
2026-09-03 20:28:00 -0700

    [flow] Fix staleTransaction hover expectation for Rust-style links
    Summary:
    D118669437 changed LSP hover definition links to a single Rust-style
    navigation row ('Go to [A](...) | [B](...)') and updated
    newtests/lsp/queries, but missed newtests/lsp/staleTransaction, which
    still expected one entry per symbol. Update it to the new format.
    
    Changelog: [internal] Fix staleTransaction hover expectation for Rust-style links.
    
    ___
    
    Differential Revision: D118741125
    
    fbshipit-source-id: 59f0f56d5eabface86d9d65f83cf206fc28d01d4

--- newtests/lsp/staleTransaction/test.js
+++ newtests/lsp/staleTransaction/test.js
@@ -58,8 +58,8 @@ export default function App(): typeof Widget {
                   value:
                     '(alias) component Widget(...Props)\nimport Widget from "./a"',
                 },
-                '`Props` defined at [`b.js:3:12`](<PLACEHOLDER_PROJECT_URL>/b.js#L3,13)',
-                '`Widget` defined at [`a.js:4:15`](<PLACEHOLDER_PROJECT_URL>/a.js#L4,16)',
+                'Go to [Props](<PLACEHOLDER_PROJECT_URL>/b.js#L3,13) | ' +
+                  '[Widget](<PLACEHOLDER_PROJECT_URL>/a.js#L4,16)',
               ],
               range: {
                 end: {
