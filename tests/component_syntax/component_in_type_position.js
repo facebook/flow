116d2629e31b987af639f57ce1f9b353831bd2d2
Sam Zhou <samzhou19815@meta.com>
2026-09-03 18:08:13 -0700

    [flow][tests] Eliminate unnecessary `ExactReactElement_DEPRECATED` in tests 1/n
    Summary:
    The purpose of these weird ones is to test the representation. We can achieve the same by extracting the component props out, which is already covered by those `React.PropsOf` tests, so let's just delete.
    
    Changelog: [internal]
    
    ___
    
    Differential Revision: D118708283
    
    fbshipit-source-id: fdc1d271c0e361f8900a51e9fa1e66ecbcf84796

--- tests/component_syntax/component_in_type_position.js
+++ tests/component_syntax/component_in_type_position.js
@@ -8,8 +8,6 @@ const fooRenderShorthand: renders Foo = <Foo />; // OK
 const fooRenderVerbose: renders React$RendersExactly<typeof Foo> = <Foo />; // OK
 
 export component Poly<T>(prop: T) { return null }
-const polyElem: Poly<number> extends React$RendersExactly<infer C> ? ExactReactElement_DEPRECATED<C> : empty = <Poly prop={3} />; // OK
-const polyElemBad: Poly<number> extends React$RendersExactly<infer C> ? ExactReactElement_DEPRECATED<C> : empty = <Poly prop="STRING" />; // ERROR
 type PolyNoTargs = Poly; // ERROR
 
 export type AliasedFoo = Foo;
--- tests/component_syntax/import_component_in_type_position.js
+++ tests/component_syntax/import_component_in_type_position.js
@@ -8,17 +8,10 @@ import type {
 } from './component_in_type_position';
 import {Foo, Poly, MemoFoo} from './component_in_type_position';
 
-const fooElem: FooType extends React$RendersExactly<infer C> ? ExactReactElement_DEPRECATED<C> : empty = <Foo />; // OK
-const polyElem: PolyType<number> extends React$RendersExactly<infer C> ? ExactReactElement_DEPRECATED<C> : empty = <Poly prop={3} />; // OK
-const polyElemError: PolyType<number> extends React$RendersExactly<infer C> ? ExactReactElement_DEPRECATED<C> : empty = <Poly prop="STRING" />; // ERROR
-
 component Bar() {
   return null;
 }
 (<Bar />) as Foo; // ERROR
-
-const aliasedFoo: AliasedFoo extends React$RendersExactly<infer C> ? ExactReactElement_DEPRECATED<C> : empty = <Foo />;
-aliasedFoo as number; // ERROR
 
 const rendersMemoFoo1: renders MemoFooType =  <MemoFoo />;
 const rendersMFoo2: renders MemoFoo =  <MemoFoo />;
